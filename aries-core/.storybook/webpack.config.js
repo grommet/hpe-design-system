546e1fbfae5d30cb1755b2be5b159b49cb129cd6
shimisun <shimrit.yacobi@gmail.com>
2019-11-12 17:16:38 -0700

    add storysource addon to display stories source code

---
+++ aries-core/.storybook/webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = function({ config }) {
+  config.module.rules.push({
+    test: /stories\/.*\.js?$/,
+    //   test: /\.stories\.js?$/,
+    loaders: [require.resolve('@storybook/source-loader')],
+    enforce: 'pre',
+  });
+
+  return config;
+};
