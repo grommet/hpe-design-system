4fda29fb22052cce882bf9ce579a0de349f8aff0
Matt Glissmann <mdglissmann@gmail.com>
2019-11-14 09:44:01 -0700

    babel build config ignore test files

--- aries-core/babel.config.js
+++ aries-core/babel.config.js
@@ -1,16 +1,18 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
 
-  const presets = [
-    "@babel/preset-env",
-    "@babel/preset-react",
-  ];
-  const plugins = [
-    "@babel/plugin-proposal-class-properties",
+  const presets = ['@babel/preset-env', '@babel/preset-react'];
+  const plugins = ['@babel/plugin-proposal-class-properties'];
+
+  const ignore = [
+    'src/**/*.test.js', // test.js files
+    'src/**/__tests__', // __test__ directories
+    'src/**/stories', // stories directories
   ];
 
   return {
     presets,
     plugins,
+    ignore,
   };
-}
+};
