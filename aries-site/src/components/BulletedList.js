f163ede4d51b9fab66f374774c7e511940847080
Taylor Seamans <taylor.seamans@yahoo.com>
2019-11-19 15:18:11 -0800

    Add Principles page

---
+++ aries-site/src/components/BulletedList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Text } from 'grommet';
+
+export const BulletedList = ({ items }) => {
+  return (
+    <>
+      {items.map(item => (
+        <Text margin={{ bottom: 'xsmall' }}>— {item}</Text>
+      ))}
+    </>
+  );
+};
+
+BulletedList.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.string),
+};
