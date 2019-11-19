d013c8f97d56516247d9af911a9ca8ba79fdb9d1
Matt Glissmann <mdglissmann@gmail.com>
2019-11-18 18:05:05 -0700

    adding general layout scaffolding

--- aries-site/src/layouts/index.js
+++ aries-site/src/layouts/index.js
@@ -1,27 +1,5 @@
-import React from 'react';
-import { Grommet, grommet, Box } from 'grommet';
-import { AnchorGroup, Nav } from 'aries-core';
-import Head from 'next/head';
+import Layout from './Layout';
+import MainContent from './MainContent';
+import SideBar from './SideBar';
 
-export const Layout = ({ children }) => (
-  <Grommet theme={grommet} full>
-    <Box>
-      <Head>
-        <title>Aries | HPE Design System</title>
-        <link rel="icon" href="/static/favicon.ico" />
-      </Head>
-      <Nav title="Aries">
-        <AnchorGroup
-          items={[
-            { label: 'Start' },
-            { label: 'Elements' },
-            { label: 'Components' },
-            { label: 'Layouts' },
-            { label: 'Templates' },
-          ]}
-        />
-      </Nav>
-      {children}
-    </Box>
-  </Grommet>
-);
+export { Layout, MainContent, SideBar };
