049cca3def40c53e3c40838d14a0b994118bceaa
Taylor Seamans <taylor.seamans@yahoo.com>
2019-11-20 09:59:12 -0800

    Create placeholder pages

---
+++ aries-site/src/pages/inclusiveness.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Text } from 'grommet';
+
+import { ContentSection, Layout, MainContent, SideBar } from '../layouts';
+import { MainHeading } from '../components';
+
+const Index = () => (
+  <>
+    <Layout>
+      <SideBar>
+        <Text>Secondary Nav</Text>
+      </SideBar>
+      <MainContent>
+        <ContentSection>
+          <MainHeading>Inclusiveness</MainHeading>
+        </ContentSection>
+      </MainContent>
+    </Layout>
+  </>
+);
+
+export default Index;
