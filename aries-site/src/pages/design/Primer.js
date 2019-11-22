ad0e874224c416fc61a2ca9a866efbb4506fb522
shimisun <shimrit.yacobi@gmail.com>
2019-11-22 13:30:51 -0700

    refactoring and clean-ups for design pages

---
+++ aries-site/src/pages/design/Primer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { ContentSection, PageLayout } from '../../layouts';
+import { MainDescription, MainHeading } from '../../components';
+
+const title = 'Primer';
+
+const Primer = () => (
+  <PageLayout title={title}>
+    <ContentSection>
+      <MainHeading>{title}</MainHeading>
+      <MainDescription>
+        Each element is tailored for the web and has resources for both the
+        designer, developer, and casual users of Aries. The elements alone
+        provide a base set of guidelines that will help you succeed when
+        desiging experiences for HPE.
+      </MainDescription>
+    </ContentSection>
+  </PageLayout>
+);
+
+export default Primer;
--- aries-site/src/pages/design/index.js
+++ aries-site/src/pages/design/index.js
@@ -1,22 +1,7 @@
 import React from 'react';
 
-import { ContentSection, PageLayout, MainContent } from '../../layouts';
-import { MainDescription, MainHeading } from '../../components';
+import Primer from './Primer';
 
-const Index = () => (
-  <PageLayout title="Design">
-    <MainContent>
-      <ContentSection>
-        <MainHeading>Primer</MainHeading>
-        <MainDescription>
-          Each element is tailored for the web and has resources for both the
-          designer, developer, and casual users of Aries. The elements alone
-          provide a base set of guidelines that will help you succeed when
-          desiging experiences for HPE.
-        </MainDescription>
-      </ContentSection>
-    </MainContent>
-  </PageLayout>
-);
+const Index = () => <Primer />;
 
 export default Index;
