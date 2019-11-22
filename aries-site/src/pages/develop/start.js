8c6ec7ecc2b3f08f24fa7abd074334749de0b0a1
shimisun <shimrit.yacobi@gmail.com>
2019-11-22 13:22:20 -0700

    refactoring and clean-ups for develop pages

--- aries-site/src/pages/develop/start.js
+++ aries-site/src/pages/develop/start.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-import { ContentSection, PageLayout, MainContent } from '../../layouts';
+import { ContentSection, PageLayout } from '../../layouts';
 import { MainHeading } from '../../components';
 
-const Index = () => (
-  <PageLayout title="Start">
-    <MainContent>
-      <ContentSection>
-        <MainHeading>Start</MainHeading>
-      </ContentSection>
-    </MainContent>
+const title = 'start';
+
+const Start = () => (
+  <PageLayout title={title}>
+    <ContentSection>
+      <MainHeading>{title}</MainHeading>
+    </ContentSection>
   </PageLayout>
 );
 
-export default Index;
+export default Start;
