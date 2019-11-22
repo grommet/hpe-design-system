94a9710601c90df1e0594a9629a0f1d95356a698
shimisun <shimrit.yacobi@gmail.com>
2019-11-22 12:06:27 -0700

    extracting reusable strings

--- aries-site/src/pages/resources/themer.js
+++ aries-site/src/pages/resources/themer.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { ContentSection, PageLayout, MainContent } from '../../layouts';
 import { MainHeading } from '../../components';
 
+const title = 'Themer';
+
 const Index = () => (
-  <PageLayout title="Themer">
+  <PageLayout title={title}>
     <MainContent>
       <ContentSection>
-        <MainHeading>Themer</MainHeading>
+        <MainHeading>{title}</MainHeading>
       </ContentSection>
     </MainContent>
   </PageLayout>
