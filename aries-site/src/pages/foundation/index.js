1475f43e6300a1c317d7d4ccacf84b592557bc19
shimisun <shimrit.yacobi@gmail.com>
2019-11-22 13:21:20 -0700

    refactoring and clean-ups for foundation pages

--- aries-site/src/pages/foundation/index.js
+++ aries-site/src/pages/foundation/index.js
@@ -1,23 +1,21 @@
 import React from 'react';
 
-import { ContentSection, PageLayout, MainContent } from '../../layouts';
+import { ContentSection, PageLayout } from '../../layouts';
 import { MainDescription, MainHeading } from '../../components';
 
 const title = 'Primer';
 
 const Index = () => (
   <PageLayout title={title}>
-    <MainContent>
-      <ContentSection>
-        <MainHeading>{title}</MainHeading>
-        <MainDescription>
-          HPE Aries is an open-source library and the official design system of
-          HPE for all digital products and experiences. Aries consists of
-          working code, best practices, design resources, human interface
-          guidelines, and a virbant community of contributors.
-        </MainDescription>
-      </ContentSection>
-    </MainContent>
+    <ContentSection>
+      <MainHeading>{title}</MainHeading>
+      <MainDescription>
+        HPE Aries is an open-source library and the official design system of
+        HPE for all digital products and experiences. Aries consists of working
+        code, best practices, design resources, human interface guidelines, and
+        a virbant community of contributors.
+      </MainDescription>
+    </ContentSection>
   </PageLayout>
 );
 
