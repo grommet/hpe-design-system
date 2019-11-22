1138f3da5c4dad13930385d5428e58cb2801e84a
shimisun <shimrit.yacobi@gmail.com>
2019-11-22 09:51:18 -0700

    adding SideBar list items

--- aries-site/src/layouts/main/PageLayout.js
+++ aries-site/src/layouts/main/PageLayout.js
@@ -3,11 +3,10 @@ import PropTypes from 'prop-types';
 
 import { Layout } from './Layout';
 import { MainContent } from '../content';
-import { SideBar } from '../navigation';
+
 
 export const PageLayout = ({ children, title }) => (
   <Layout title={title}>
-    <SideBar items={['About', 'Principles']} />
     <MainContent>{children}</MainContent>
   </Layout>
 );
