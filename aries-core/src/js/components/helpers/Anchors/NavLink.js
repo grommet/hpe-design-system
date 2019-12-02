a9b031549a9b006f6447da98116707fc477e3084
Matt Glissmann <mdglissmann@gmail.com>
2019-12-02 13:44:37 -0700

    style NavLink in component

--- aries-core/src/js/components/helpers/Anchors/NavLink.js
+++ aries-core/src/js/components/helpers/Anchors/NavLink.js
@@ -1,31 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Anchor, ThemeContext } from 'grommet';
-import { css } from 'styled-components';
+import { Anchor, Text } from 'grommet';
 
-export const NavLink = ({ ...rest }) => {
+export const NavLink = ({ active, children, label, size, ...rest }) => {
+  const activeTextColor = active ? 'text' : null;
+  const defaultTextColor = 'text-weak';
+  const [textColor, setTextColor] = useState(defaultTextColor);
+
   return (
-    <ThemeContext.Extend
-      value={{
-        anchor: {
-          extend: css`
-            opacity: 70%;
-            ${props => props.active && 'opacity: 100;'}
-          `,
-          hover: {
-            extend: {
-              opacity: '100%',
-            },
-          },
-          fontWeight: 400,
-        },
-      }}
-    >
-      <Anchor {...rest} />
-    </ThemeContext.Extend>
+    <Anchor
+      color={activeTextColor || textColor}
+      label={<Text weight="normal">{label || children}</Text>}
+      onBlur={() => setTextColor(defaultTextColor)}
+      onFocus={() => setTextColor(activeTextColor)}
+      onMouseOut={() => setTextColor(defaultTextColor)}
+      onMouseOver={() => setTextColor(activeTextColor)}
+      size={size}
+      {...rest}
+    />
   );
 };
 
 NavLink.propTypes = {
   active: PropTypes.bool,
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+  label: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+  size: PropTypes.string,
+};
+
+NavLink.defaultProps = {
+  size: 'medium',
 };
