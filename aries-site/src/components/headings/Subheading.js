9b12519c5feaf8eca43e3e5627ab7b384c700f94
Taylor Seamans <taylor.seamans@yahoo.com>
2019-12-06 09:50:13 -0800

    Update colors page

--- aries-site/src/components/headings/Subheading.js
+++ aries-site/src/components/headings/Subheading.js
@@ -2,15 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Heading } from 'grommet';
 
-<<<<<<< HEAD
 export const Subheading = ({ children, headingSize, level }) => {
   return (
     <Heading level={level} margin={{ vertical: 'none' }} size={headingSize}>
-=======
-export const Subheading = ({ children, ...rest }) => {
-  return (
-    <Heading level={2} margin={{ vertical: 'none' }} {...rest}>
->>>>>>> master
       {children}
     </Heading>
   );
