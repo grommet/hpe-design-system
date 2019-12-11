1b9ab2b9c31c352ea7f484d50a75ac942f9ebb0b
Taylor Seamans <taylor.seamans@yahoo.com>
2019-12-11 14:31:44 -0800

    Allow subsection to include h1

--- aries-site/src/pages/foundation/color.js
+++ aries-site/src/pages/foundation/color.js
@@ -7,12 +7,7 @@ import {
   PageLayout,
   Subsection,
 } from '../../layouts';
-import {
-  MainDescription,
-  LastUpdated,
-  MainHeading,
-  BodyText,
-} from '../../components';
+import { LastUpdated, BodyText } from '../../components';
 import { greyscaleColors, primaryColors, supportingColors } from '../../data';
 
 const title = 'Color';
@@ -20,32 +15,33 @@ const title = 'Color';
 const Color = () => (
   <PageLayout title={title}>
     <ContentSection>
-      <MainHeading>{title}</MainHeading>
-      <MainDescription>
-        Our color palette brings out the depth and dimension of our identity.
-        From sophisticated, neutral hues reflective of technology to vibrant,
-        saturated colors that evoke energy and inspiration.
-      </MainDescription>
-      <Box gap="medium" pad={{ vertical: 'medium' }}>
-        <Button
-          label={
-            <Text
-              color={{ dark: 'text-strong', light: 'text-strong' }}
-              weight={500}
-            >
-              Download Color Swatches Pack
-            </Text>
-          }
-          primary
-          onClick={() => {
-            // Download color swatch pack (once content is available)
-          }}
-        />
-        <LastUpdated date={new Date('2019-07-17T03:24:00')} />
-      </Box>
+      <Subsection name={title}>
+        <BodyText>
+          Our color palette brings out the depth and dimension of our identity.
+          From sophisticated, neutral hues reflective of technology to vibrant,
+          saturated colors that evoke energy and inspiration.
+        </BodyText>
+        <Box gap="medium" pad={{ vertical: 'medium' }} align="start">
+          <Button
+            label={
+              <Text
+                color={{ dark: 'text-strong', light: 'text-strong' }}
+                weight={500}
+              >
+                Download Color Swatches Pack
+              </Text>
+            }
+            primary
+            onClick={() => {
+              // Download color swatch pack (once content is available)
+            }}
+          />
+          <LastUpdated date={new Date('2019-07-17T03:24:00')} />
+        </Box>
+      </Subsection>
     </ContentSection>
     <ContentSection>
-      <Subsection name="Color Palettes">
+      <Subsection name="Color Palettes" level={2}>
         <BodyText>
           Color is a key way we express our brand. Using color on interface
           elements, how typography is presented, displaying data visualizations,
