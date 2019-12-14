604eaad11ab5a265b169b0d1aa75bd1a1e5a293c
shimisun <shimrit.yacobi@gmail.com>
2019-12-14 09:44:18 -0700

    Setting Ground to Landing Page

---
+++ aries-site/src/layouts/main/NavLayout.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+import { Box, Main, ResponsiveContext } from 'grommet';
+
+import { SideBar, SideBarItemList } from '../navigation';
+
+const filterChildren = (children, type) => {
+  const filteredChildren = React.Children.map(children, child => {
+    return child.props[type] ? child : null;
+  });
+
+  if (filteredChildren.length > 1) {
+    console.warn(
+      `Expected a single ${type}, received ${filteredChildren.length}.`,
+      `Only first ${type} element will be rendered.`,
+    );
+  }
+
+  return filteredChildren;
+};
+
+export const NavLayout = ({ mainContentChilds }) => {
+  // Only expect a single child of the following types
+  const mainContent = filterChildren(mainContentChilds, 'MainContent');
+
+  // TODO selectedNav should be retrived from aries-core
+  // as the selected element of the NavBar
+  const selectedNav = 'start';
+  return (
+    <ResponsiveContext.Consumer>
+      {size => (
+        <>
+          <Box
+            direction="row"
+            // pad uses Nav pad from aries-core
+            pad={{
+              horizontal: size !== 'small' ? 'xlarge' : 'large',
+              vertical: 'large',
+            }}
+          >
+            {size !== 'small' && (
+              <Box fill="vertical">
+                <SideBar
+                  items={SideBarItemList[selectedNav]}
+                  prefix={selectedNav}
+                />
+              </Box>
+            )}
+            <Main flex>{mainContent[0]}</Main>
+          </Box>
+        </>
+      )}
+    </ResponsiveContext.Consumer>
+  );
+};
+
+NavLayout.propTypes = {
+  mainContentChilds: PropTypes.oneOfType([PropTypes.element, PropTypes.array]),
+};
