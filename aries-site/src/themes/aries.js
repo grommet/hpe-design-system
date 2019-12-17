15bf934860f9de4b801c32980026418ae608a7b5
shimisun <shimrit.yacobi@gmail.com>
2019-12-17 15:31:16 -0700

    Update HPE button theme

--- aries-site/src/themes/aries.js
+++ aries-site/src/themes/aries.js
@@ -1,5 +1,6 @@
 import { hpe } from 'grommet-theme-hpe';
 import { deepMerge } from 'grommet/utils';
+import { css } from 'styled-components';
 
 const baseSpacing = 24;
 
@@ -156,6 +157,9 @@ export const aries = deepMerge(hpe, {
     border: {
       radius: '6px',
     },
+    extend: css`
+      ${props => !props.plain && 'font-weight: bold;'}
+    `,
   },
   heading: {
     level: {
