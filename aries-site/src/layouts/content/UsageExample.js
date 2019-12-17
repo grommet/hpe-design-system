2c54fcea677858c3d78d2bfab87fe8488409ebce
Matt Glissmann <mdglissmann@gmail.com>
2019-12-17 12:07:53 -0700

    adjusting text and paragraph sizes in alignment with latest design

--- aries-site/src/layouts/content/UsageExample.js
+++ aries-site/src/layouts/content/UsageExample.js
@@ -7,7 +7,9 @@ import { colors } from '../../themes/aries';
 export const UsageExample = ({ children, label, themeMode, pad, ...rest }) => {
   return (
     <Box gap="xsmall" margin={{ vertical: 'xsmall' }}>
-      <Text weight={400}>{label}</Text>
+      <Text weight={400} size="small">
+        {label}
+      </Text>
       {/* Because UsageExamples need to present content in both light and dark
       contexts, regardless of the site's themeMode, forcing box background to 
       serve examples with the desired themeMode */}
