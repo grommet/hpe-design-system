bb3f6bd93ada578991a51c6c7eaee9c64efdb69b
Taylor Seamans <taylor.seamans@yahoo.com>
2019-12-18 10:53:08 -0800

    Add sidebar nav items to nav pages

--- aries-site/src/pages/design/index.js
+++ aries-site/src/pages/design/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { PageLayout } from '../../layouts';
+import { PageLayout, SideBar, SideBarItemList } from '../../layouts';
 import { MainHeading } from '../../components';
 
 const title = 'Design';
+const prefix = title.toLowerCase();
+
 const Design = () => {
   return (
     <PageLayout title={title} isLanding>
       <MainHeading>{title}</MainHeading>
+      <SideBar items={SideBarItemList[prefix]} prefix={prefix} />
     </PageLayout>
   );
 };
