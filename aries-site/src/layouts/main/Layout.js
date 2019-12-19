0a64e2885b6da6d7267e44a9261522c3873d5163
shimisun <shimrit.yacobi@gmail.com>
2019-12-19 16:16:35 -0700

    adding a landing page

--- aries-site/src/layouts/main/Layout.js
+++ aries-site/src/layouts/main/Layout.js
@@ -1,27 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Grommet, Main, ResponsiveContext } from 'grommet';
+import { Box, Grommet, Main, ResponsiveContext } from 'grommet';
 
 import { aries } from '../../themes/aries';
 import { Header, Head, SidebarLayout } from '..';
+
+const calcPad = size => {
+  const val = size !== 'small' ? 'xlarge' : 'large';
+  return val;
+};
 
 export const Layout = ({ children, title, isLanding }) => {
   return (
     <Grommet theme={aries} full>
       <ResponsiveContext.Consumer>
         {size => (
-          <>
+          <Box
+            // pad={{ horizontal: calcPad(size) }}
+            width={{ max: 'xxlarge' }}
+            margin="auto"
+          >
             <Head title={title} />
             <Header showLinks={!isLanding} />
             {!isLanding ? (
               <SidebarLayout mainContentChildren={children} />
             ) : (
               // aligns with responsive padding for aries-core Nav
-              <Main pad={size !== 'small' ? 'xlarge' : 'large'}>
+              <Main align="center" pad={calcPad(size)}>
                 {children}
               </Main>
             )}
-          </>
+          </Box>
         )}
       </ResponsiveContext.Consumer>
     </Grommet>
