32f5423d40c1984d53cdc080365d6b3d4d8e145e
shimisun <shimrit.yacobi@gmail.com>
2019-12-19 17:26:28 -0700

    Adding responsive to tiles

--- aries-site/src/pages/index.js
+++ aries-site/src/pages/index.js
@@ -1,21 +1,25 @@
 import React, { useEffect } from 'react';
 import { initialize, pageview } from 'react-ga';
 
-import { Box, Image } from 'grommet';
+import { Box, Image, ResponsiveContext } from 'grommet';
 import { Tile, Tiles } from 'aries-core';
 
 import { Config } from '../../config';
 import { PageLayout } from '../layouts';
 import { data, TileContent, IntroTile } from '../components/home';
 
-const HomeTiles = ({ ...rest }) => (
-  <Tiles
-    gap="large"
-    columns={{ count: 'fit', size: 'medium' }}
-    rows="medium"
-    {...rest}
-  />
-);
+const HomeTiles = ({ ...rest }) => {
+  const size = React.useContext(ResponsiveContext);
+
+  return (
+    <Tiles
+      gap={size !== 'small' ? 'large' : 'medium'}
+      columns={{ count: 'fit', size: size !== 'small' ? 'medium' : 'fill' }}
+      rows="medium"
+      {...rest}
+    />
+  );
+};
 
 const Index = () => {
   useEffect(() => {
