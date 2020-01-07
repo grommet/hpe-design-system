dbef66c3c7194ca9b5d5e1bab825b44c93072093
Matt Glissmann <mdglissmann@gmail.com>
2020-01-07 10:20:05 -0700

    fix border width

--- aries-site/src/components/icons/foundation/IconBrand.js
+++ aries-site/src/components/icons/foundation/IconBrand.js
@@ -5,9 +5,7 @@ import { Blank } from 'grommet-icons';
 export const IconBrand = props => (
   <Blank {...props}>
     <g stroke="#000000" strokeWidth="1" fill="none">
-      <g strokeWidth="2">
-        <rect x="1" y="1" width="22" height="22" rx="3" />
-      </g>
+      <rect x="1" y="1" width="22" height="22" rx="3" />
       <rect x="6.5" y="10.5" width="11" height="3" />
     </g>
   </Blank>
