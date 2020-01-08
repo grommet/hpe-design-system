83bd8677b680823689930046e8815b6513c78a9f
Taylor Seamans <taylor.seamans@yahoo.com>
2020-01-08 08:31:35 -0800

    Use .find()

--- aries-site/src/pages/guidelines/index.js
+++ aries-site/src/pages/guidelines/index.js
@@ -4,7 +4,7 @@ import { DescriptiveHeader } from '../../components/headings';
 import { structure } from '../../data';
 
 const title = 'Guidelines';
-const [topic] = structure.filter(page => page.name === title);
+const topic = structure.find(page => page.name === title);
 
 const Guidelines = () => {
   const descriptiveHeader = (
