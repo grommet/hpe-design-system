c6bb7854757f86c0da2f8614b17b35075cb41dc8
Matt Glissmann <mdglissmann@gmail.com>
2020-01-09 11:33:44 -0700

    merge master

--- aries-site/src/utils/search.js
+++ aries-site/src/utils/search.js
@@ -7,30 +7,42 @@ export const formatName = name => {
     .toLowerCase();
 };
 
-const getTopicName = itemName => {
-  let topic;
-  let item;
-  let pageArray;
-  structure.forEach(p => {
-    if (p.pages) {
-      pageArray = p.pages;
-    }
-    [item] = pageArray.filter(s => s === itemName);
-    if (item) {
-      topic = p.name;
-    }
-  });
-  return topic;
+export const getParentPage = currentPage =>
+  structure.find(page =>
+    page.pages ? page.pages.includes(currentPage) : false,
+  );
+
+export const getNextContent = current => {
+  // Returns next sibling, parent's next sibling, or self if page has no parent
+  const parent = getParentPage(current);
+
+  if (typeof parent === 'undefined') {
+    return current;
+  }
+
+  const { pages } = parent;
+  const currentIndex = pages.indexOf(current);
+
+  return currentIndex + 1 < pages.length
+    ? pages[currentIndex + 1]
+    : getNextContent(parent.name);
 };
 
 export const nameToPath = name => {
   const [page] = structure.filter(p => p.name === name);
+
+  if (typeof page === 'undefined') {
+    return undefined;
+  }
+
   if (page.pages) {
     return `/${formatName(page.name)}`;
   }
+
   // Item clicked is a sub-topic of a main topic, so we need to find
   // what topic it falls under
-  const topicName = getTopicName(name);
+  const topicName = getParentPage(name).name;
+
   if (topicName) {
     return `/${formatName(topicName)}/${formatName(name)}`;
   }
