9d78855998654f626719aa1b0f718502e53a7091
Taylor Seamans <taylor.seamans@yahoo.com>
2020-01-09 12:29:22 -0800

    Create useDarkMode from use-dark-mode

---
+++ aries-site/src/utils/hooks/useDarkMode.js
@@ -0,0 +1,55 @@
+import { useEffect, useCallback, useMemo } from 'react';
+import { initialize, useEventListener } from '..';
+
+export const useDarkMode = (
+  initialValue = false,
+  {
+    element,
+    classNameDark,
+    classNameLight,
+    onChange,
+    storageKey = 'darkMode',
+    storageProvider,
+    global,
+  } = {},
+) => {
+  const {
+    usePersistedDarkModeState,
+    getDefaultOnChange,
+    getInitialValue,
+    mediaQueryEventTarget,
+  } = useMemo(() => initialize(storageKey, storageProvider, global), [
+    storageKey,
+    storageProvider,
+    global,
+  ]);
+
+  const [state, setState] = usePersistedDarkModeState(
+    getInitialValue(initialValue),
+  );
+
+  const stateChangeCallback = useMemo(
+    () =>
+      onChange || getDefaultOnChange(element, classNameDark, classNameLight),
+    [onChange, element, classNameDark, classNameLight, getDefaultOnChange],
+  );
+
+  // Call the onChange handler
+  useEffect(() => {
+    stateChangeCallback(state);
+  }, [stateChangeCallback, state]);
+
+  // Listen for media changes and set state.
+  useEventListener(
+    'change',
+    ({ matches }) => setState(matches),
+    mediaQueryEventTarget,
+  );
+
+  return {
+    value: state,
+    enable: useCallback(() => setState(true), [setState]),
+    disable: useCallback(() => setState(false), [setState]),
+    toggle: useCallback(() => setState(current => !current), [setState]),
+  };
+};
---
+++ aries-site/src/utils/index.js
@@ -0,0 +1,6 @@
+export * from './createGlobalState';
+export * from './createPersistedState';
+export * from './createStorage';
+export * from './hooks';
+export * from './initialize';
+export * from './search';
