2336937494e78ae0fde7a8150a8625048d3495d9
Taylor Seamans <taylor.seamans@yahoo.com>
2020-01-09 11:27:59 -0800

    merge master

--- aries-site/src/layouts/main/Footer.js
+++ aries-site/src/layouts/main/Footer.js
@@ -2,38 +2,40 @@ import React, { useContext } from 'react';
 import { FooterLink } from 'aries-core';
 import { Box, Footer as GrommetFooter, ResponsiveContext, Text } from 'grommet';
 import { Hpe } from 'grommet-icons';
+import { ThemeModeToggle } from '../../components';
 
 export const Footer = () => {
   const size = useContext(ResponsiveContext);
   return (
-    <GrommetFooter
-      direction={size !== 'small' ? 'row' : 'column'}
+    <Box
+      gap="medium"
       pad={{
         vertical: size !== 'small' ? 'small' : 'large',
         // Match horizontal padding of aries-core Nav
         horizontal: size !== 'small' ? 'xlarge' : 'large',
       }}
-      align={size !== 'small' ? 'center' : undefined}
     >
-      <Box
-        direction={size !== 'small' ? 'row' : 'column'}
+      <Box>
+        <ThemeModeToggle />
+      </Box>
+      <GrommetFooter
+        direction="row-responsive"
         align={size !== 'small' ? 'center' : undefined}
-        gap={size !== 'small' ? 'medium' : undefined}
       >
         <Hpe size="large" color="brand" />
         <Text size="small">&copy; 2020 Hewlett Packard Enterprise</Text>
-      </Box>
-      <Box direction="row" gap="medium">
-        <FooterLink label="Terms" href="#" />
-        <FooterLink label="Privacy" href="#" />
-        <FooterLink label="Security" href="#" />
-        <FooterLink
-          label="Feedback"
-          href="https://github.com/hpe-design/aries/issues"
-          target="_blank"
-          rel="noreferrer noopener"
-        />
-      </Box>
-    </GrommetFooter>
+        <Box direction="row" gap="medium">
+          <FooterLink label="Terms" href="#" />
+          <FooterLink label="Privacy" href="#" />
+          <FooterLink label="Security" href="#" />
+          <FooterLink
+            label="Feedback"
+            href="https://github.com/hpe-design/aries/issues"
+            target="_blank"
+            rel="noreferrer noopener"
+          />
+        </Box>
+      </GrommetFooter>
+    </Box>
   );
 };
