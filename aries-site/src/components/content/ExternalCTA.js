0420738358b38d38c5526069a8e1f45e3d0413d6
Taylor Seamans <taylor.seamans@yahoo.com>
2020-01-13 12:20:15 -0800

    Update alignment on ExternalCTA

--- aries-site/src/components/content/ExternalCTA.js
+++ aries-site/src/components/content/ExternalCTA.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { AnchorCallToAction } from 'aries-core';
+import { Box, AnchorCallToAction } from 'aries-core';
 
 const labels = {
   storybook: 'Storybook',
@@ -11,12 +11,18 @@ export const ExternalCTA = ({ label, type, ...rest }) => {
   const defaultLabel = labels[type];
 
   return (
-    <AnchorCallToAction
-      label={label || defaultLabel}
-      target="_blank"
-      rel="noopener noreferrer"
-      {...rest}
-    />
+    // Align start ensures that the focusable/clickable area of anchor
+    // is restricted to the label + icon so it doesn't span the width
+    // of Subsection. Placing "align=start" on Subsection caused some
+    // examples on other pages not to maintain their full width.
+    <Box align="start">
+      <AnchorCallToAction
+        label={label || defaultLabel}
+        target="_blank"
+        rel="noopener noreferrer"
+        {...rest}
+      />
+    </Box>
   );
 };
 
