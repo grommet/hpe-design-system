2f97a5bfe3c104282b08808a77a6dfd0793961a3
Taylor Seamans <taylor.seamans@yahoo.com>
2020-01-15 15:58:43 -0800

    Create examples for Layout page

---
+++ aries-site/src/examples/components/layouts/LayerExample.js
@@ -0,0 +1,55 @@
+import React, { useContext, useState } from 'react';
+import { Button, Box, Heading, Layer, ResponsiveContext, Text } from 'grommet';
+import { Close } from 'grommet-icons';
+
+export const LayerExample = () => {
+  const [open, setOpen] = useState(false);
+  const size = useContext(ResponsiveContext);
+  const onOpen = () => setOpen(true);
+  const onClose = () => setOpen(undefined);
+  return (
+    <>
+      <Box align="start">
+        <Button
+          color="blue"
+          label="Show me the Layer"
+          onClick={onOpen}
+          primary
+        />
+      </Box>
+      {open && (
+        <Layer
+          position="right"
+          full={size !== 'small' ? 'vertical' : true}
+          modal
+          onClickOutside={onClose}
+          onEsc={onClose}
+        >
+          <Box
+            fill="vertical"
+            overflow="auto"
+            width={size !== 'small' ? 'medium' : undefined}
+            pad="medium"
+          >
+            <Box flex={false} direction="row" justify="between">
+              <Heading level={2} margin="none">
+                This is a Layer.
+              </Heading>
+              <Button icon={<Close />} onClick={onClose} />
+            </Box>
+            <Box flex="grow" overflow="auto" pad={{ vertical: 'medium' }}>
+              <Text>
+                This layer is set to appear on the right and fill the screen
+                vertically. You can add content in here such as a Form or a
+                Chart.
+              </Text>
+            </Box>
+            <Box flex={false} align="start">
+              <Button label="Close Layer" primary onClick={onClose} />
+            </Box>
+          </Box>
+        </Layer>
+      )}
+    </>
+  );
+};
