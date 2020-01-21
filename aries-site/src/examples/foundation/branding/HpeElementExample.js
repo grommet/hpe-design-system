f01c992319450af02ee7a819ad412ad52cadf70f
Matt Glissmann <mdglissmann@gmail.com>
2020-01-21 13:00:34 -0700

    remove placeholder content

---
+++ aries-site/src/examples/foundation/branding/HpeElementExample.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Box, Text } from 'grommet';
+import { Hpe } from 'grommet-icons';
+
+export const HpeElementExample = () => {
+  const textSize = 'small';
+
+  return (
+    <Box direction="row" align="center" gap="medium">
+      <Hpe color="brand" size="66px" />
+      <Box direction="row" gap="xsmall">
+        <Text size={textSize} weight="bold">
+          HPE
+        </Text>
+        <Text size={textSize}>Service Name</Text>
+      </Box>
+    </Box>
+  );
+};
