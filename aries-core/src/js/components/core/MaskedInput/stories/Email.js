7de00b9cf52b2fc35db951dda51c1bcf8038ac3f
Taylor Seamans <taylor.seamans@yahoo.com>
2020-01-22 14:42:38 -0800

    MaskedInput updates

---
+++ aries-core/src/js/components/core/MaskedInput/stories/Email.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Box, Grommet, MaskedInput } from 'grommet';
+import { aries } from '../../../../../../../aries-site/src/themes/aries';
+
+export default {
+  title: 'MaskedInput',
+};
+
+export const Email = () => {
+  const [value, setValue] = React.useState('');
+  return (
+    <Grommet theme={aries} full>
+      <Box fill align="center" justify="start" pad="large">
+        <Box width="medium">
+          <MaskedInput
+            mask={[
+              {
+                regexp: /^[\w\-_.]+$/,
+                placeholder: 'example',
+              },
+              { fixed: '@' },
+              {
+                regexp: /^[\w]+$/,
+                placeholder: 'my',
+              },
+              { fixed: '.' },
+              {
+                regexp: /^[\w]+$/,
+                placeholder: 'com',
+              },
+            ]}
+            value={value}
+            onChange={event => setValue(event.target.value)}
+          />
+        </Box>
+      </Box>
+    </Grommet>
+  );
+};
