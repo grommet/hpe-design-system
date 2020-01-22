428d2ceca4146b0a123b6c1a0ecc674830dd1b66
Taylor Seamans <taylor.seamans@yahoo.com>
2020-01-22 14:44:01 -0800

    Select updates

---
+++ aries-core/src/js/components/core/Select/stories/Simple.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { Box, Grommet, Select } from 'grommet';
+import { aries } from '../../../../../../../aries-site/src/themes/aries';
+
+export default {
+  title: 'Select',
+};
+
+export const Simple = () => {
+  const options = ['one', 'two'];
+  const [value, setValue] = useState('');
+
+  return (
+    <Grommet theme={aries} full>
+      <Box align="center" justify="start" pad="large" fill>
+        <Select
+          id="select"
+          name="select"
+          placeholder="Select"
+          value={value}
+          options={options}
+          onChange={({ option }) => setValue(option)}
+        />
+      </Box>
+    </Grommet>
+  );
+};
