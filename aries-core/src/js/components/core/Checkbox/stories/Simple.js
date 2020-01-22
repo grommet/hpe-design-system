c190fff1dade40ff83eb342a5a0db6ecd00ca4a2
Taylor Seamans <taylor.seamans@yahoo.com>
2020-01-22 14:42:12 -0800

    Checkbox updates

---
+++ aries-core/src/js/components/core/Checkbox/stories/Simple.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { CheckBox, Box, Grommet } from 'grommet';
+import { aries } from '../../../../../../../aries-site/src/themes/aries';
+
+export default {
+  title: 'Checkbox',
+};
+
+export const Simple = () => {
+  const [checked, setChecked] = useState(false);
+  const onChange = event => setChecked(event.target.checked);
+  return (
+    <Grommet theme={aries} full>
+      <Box align="center" pad="large">
+        <CheckBox checked={checked} onChange={onChange} label="Choice" />
+      </Box>
+    </Grommet>
+  );
+};
