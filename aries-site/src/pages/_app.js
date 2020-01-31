3f63fbec2a36ce7f0e2698787795eed7586e3934
Eric Soderberg <eric.soderberg@hpe.com>
2020-01-31 14:04:28 -0800

    Improve UsageExample tooltips

--- aries-site/src/pages/_app.js
+++ aries-site/src/pages/_app.js
@@ -1,6 +1,7 @@
 import '../../../node_modules/prismjs/themes/prism-tomorrow.css';
 
 // This default export is required in a new `pages/_app.js` file.
+// eslint-disable-next-line react/prop-types
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
