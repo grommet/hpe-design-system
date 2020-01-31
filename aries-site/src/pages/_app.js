7799cc3b6221b0d85535f73a2bbd6d4bee34e0d4
Eric Soderberg <eric.soderberg@hpe.com>
2020-01-31 14:00:15 -0800

    Show code example inline

---
+++ aries-site/src/pages/_app.js
@@ -0,0 +1,7 @@
+import '../../../node_modules/prismjs/themes/prism-tomorrow.css';
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
