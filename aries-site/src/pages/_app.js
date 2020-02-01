f1a94fb170c844065ece40af5edbc4e19bd6ebd8
Eric Soderberg <eric.soderberg@hpe.com>
2020-01-31 17:14:26 -0800

    Change Example syntax highlighting based on theme darkness

--- aries-site/src/pages/_app.js
+++
@@ -1,9 +0,0 @@
-// import '../../../node_modules/prismjs/themes/prism.css';
-import '../../../node_modules/prismjs/themes/prism-tomorrow.css';
-
-// This default export is required in a new `pages/_app.js` file.
-// eslint-disable-next-line react/prop-types
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
