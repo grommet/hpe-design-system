ceaef9ae8c3fa2e3d85c0b2b7fb0b10f4a99be76
Taylor Seamans <taylor.seamans@yahoo.com>
2020-02-05 16:52:38 -0800

    Create repeatKeyPress function

--- aries-site/src/tests/functions.js
+++ aries-site/src/tests/functions.js
@@ -5,3 +5,9 @@ export const baseUrl = 'http://localhost:3000';
 export const getLocation = ClientFunction(() => document.location.href);
 
 export const formatForTyping = ClientFunction(text => text.split('').join(' '));
+
+export const repeatKeyPress = ClientFunction((key, number) => {
+  // Array.join puts the argument between the array elements,
+  // so we need to add 1
+  return Array(number + 1).join(`${key} `);
+});
