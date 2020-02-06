4d47fee969a4d6fbfac70f87edc9697e2786aa0e
shimisun <shimrit.yacobi@gmail.com>
2020-02-06 15:01:13 -0700

    Incoporate design review feedback on the site

--- aries-site/src/components/home/TileContent.js
+++ aries-site/src/components/home/TileContent.js
@@ -1,25 +1,16 @@
 import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 
-import { Box, Text, Heading, ResponsiveContext } from 'grommet';
+import { Box, Heading, Paragraph } from 'grommet';
 
 export const TileContent = forwardRef(({ icon, title, subTitle }, ref) => (
-  <ResponsiveContext.Consumer>
-    {size => (
-      <Box
-        pad="small"
-        gap={size === 'small' ? 'large' : 'medium'}
-        align="start"
-        ref={ref}
-      >
-        {icon}
-        <Heading level={2} margin="none" responsive={false}>
-          {title}
-        </Heading>
-        <Text size="small">{subTitle}</Text>
-      </Box>
-    )}
-  </ResponsiveContext.Consumer>
+  <Box pad="small" align="start" ref={ref}>
+    {icon}
+    <Heading level={2} margin={{ bottom: 'small' }} responsive={false}>
+      {title}
+    </Heading>
+    <Paragraph>{subTitle}</Paragraph>
+  </Box>
 ));
 
 TileContent.propTypes = {
