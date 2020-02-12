0d0b149b40db078538d1de8e1e76b9c38e3bb49c
Taylor Seamans <taylor.seamans@yahoo.com>
2020-02-12 10:41:19 -0800

    Start tests in light mode

--- aries-site/src/tests/accessibility.js
+++ aries-site/src/tests/accessibility.js
@@ -1,9 +1,22 @@
 /* eslint-disable max-len */
 /* eslint-disable no-undef */
+import { Selector } from 'testcafe';
 import { axeCheck, createReport } from 'axe-testcafe';
+import { ReactSelector, waitForReact } from 'testcafe-react-selectors';
 import { baseUrl } from './functions';
 
-fixture('TestCafe tests with Axe').page(baseUrl);
+fixture('TestCafe tests with Axe')
+  .page(baseUrl)
+  // start tests in light mode
+  .beforeEach(async t => {
+    await waitForReact();
+    const themeMode = Selector('body').getAttribute('class');
+    const themeToggleButton = ReactSelector('ThemeModeToggle');
+    const lightMode = (await themeMode) === 'light-mode';
+    if (!lightMode) {
+      await t.click(themeToggleButton);
+    }
+  });
 
 test('Landing Page', async t => {
   // Only need to run in one browser
@@ -15,9 +28,11 @@ test('Landing Page', async t => {
         // To allow other accessibility features to be checked, these elements are temporarily
         // being excluded from the testing. After resolving, this should be removed.
         [
+          // Paragraph on Foundation tile
           '.hFYaZW > .jkPKaQ.StyledBox-sc-13pk1d4-0 > .kphxBL.StyledBox-sc-13pk1d4-0 > .GvaHH',
         ],
         [
+          // Paragraph on Design tile
           '.hrOJdT > .jkPKaQ.StyledBox-sc-13pk1d4-0 > .kphxBL.StyledBox-sc-13pk1d4-0 > .GvaHH',
         ],
       ],
@@ -48,8 +63,11 @@ test.before(async t => {
         // https://github.com/hpe-design/design-system/issues/251
         // To allow other accessibility features to be checked, these elements are temporarily
         // being excluded from the testing. After resolving, this should be removed.
+        // Text on See in Figma button
         ['.fIVxyY'],
+        // NextContent Text
         ['footer > .oKpSL.StyledText-sc-1sadyjn-0'],
+        // NextContent Link
         ['.cMjHfA > .gvcthr'],
       ],
     };
@@ -69,9 +87,13 @@ test.before(async t => {
         // https://github.com/hpe-design/design-system/issues/251
         // To allow other accessibility features to be checked, these elements are temporarily
         // being excluded from the testing. After resolving, this should be removed.
+        // HPE in header
         ['.fkwEyF'],
+        // Design System in header
         ['.dOIJAP.StyledBox-sc-13pk1d4-0 > .oKpSL.StyledText-sc-1sadyjn-0'],
+        // Placeholder text in search
         ['input'],
+        // Paragraph in header
         ['p'],
       ],
     };
