a0374dea849949942a54dc942aa1f143e158fd4c
Brittany Archibeque <brittanyarchibeque@brittanys-mbp-2.americas.hpqcorp.net>
2020-02-18 10:01:03 -0700

    add deep linking tests

--- aries-site/src/tests/navigation/homepage.js
+++ aries-site/src/tests/navigation/homepage.js
@@ -27,3 +27,18 @@ test('should navigate to correct path when a tile is choosen via keyboard', asyn
     .expect(getLocation())
     .contains(expectedPath);
 });
+
+/*Added range to this test since now there is some issues with different browsers as to where
+the page actually lands when routed to a deep link
+*/
+
+fixture('Deep linking').page(baseUrl);
+
+test('should navigate to correct hash of page when a deep link is directly routed to', async t => {
+  const url = `${baseUrl}/foundation/color#background-colors`
+  const pageSection = Selector('#background-colors');
+  await t
+    .navigateTo(url)
+    .expect(pageSection.getBoundingClientRectProperty("top"))
+    .within(-150, 150);
+});
