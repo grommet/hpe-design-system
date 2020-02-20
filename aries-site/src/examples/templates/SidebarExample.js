35bcbd6e2ede571ff6f9d0cd586bc92c9a25aafe
Taylor Seamans <taylor.seamans@yahoo.com>
2020-02-20 14:17:46 -0800

    Placeholders for responsive context

--- aries-site/src/examples/templates/SidebarExample.js
+++ aries-site/src/examples/templates/SidebarExample.js
@@ -11,37 +11,41 @@ import {
   StatusUnknown,
 } from 'grommet-icons';
 
-export const SidebarExample = ({ direction, mobile, ...rest }) => (
-  <Box
-    direction={direction}
-    background="blue"
-    align="center"
-    pad={{ horizontal: 'small', vertical: 'medium' }}
-    {...rest}
-  >
+export const SidebarExample = ({ direction, mobile, ...rest }) => {
+  // placeholder for responsive context
+  const size = mobile && 'small';
+  return (
     <Box
-      height="xxsmall"
-      width="xxsmall"
-      round="full"
-      margin={mobile ? { right: 'small' } : { bottom: 'medium' }}
-      // eslint-disable-next-line max-len
-      background="url(//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80)"
-    />
+      direction={direction}
+      background="blue"
+      align="center"
+      pad={{ horizontal: 'small', vertical: 'medium' }}
+      {...rest}
+    >
+      <Box
+        height="xxsmall"
+        width="xxsmall"
+        round="full"
+        margin={size !== 'small' ? { right: 'small' } : { bottom: 'medium' }}
+        // eslint-disable-next-line max-len
+        background="url(//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80)"
+      />
 
-    <Box flex="grow" direction={direction}>
-      <Button icon={<Clock color="text-xweak" />} />
-      <Button icon={<Apps color="text-xweak" />} />
-      <Button icon={<Terminal color="text-xweak" />} />
-      <Button icon={<ShieldSecurity color="text-xweak" />} />
-      <Button icon={<Configure color="text-xweak" />} />
-    </Box>
+      <Box flex="grow" direction={direction}>
+        <Button icon={<Clock color="text-xweak" />} />
+        <Button icon={<Apps color="text-xweak" />} />
+        <Button icon={<Terminal color="text-xweak" />} />
+        <Button icon={<ShieldSecurity color="text-xweak" />} />
+        <Button icon={<Configure color="text-xweak" />} />
+      </Box>
 
-    <Box direction={direction}>
-      <Button icon={<Chat color="text-xweak" />} />
-      <Button icon={<StatusUnknown color="text-xweak" />} />
+      <Box direction={direction}>
+        <Button icon={<Chat color="text-xweak" />} />
+        <Button icon={<StatusUnknown color="text-xweak" />} />
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
 
 SidebarExample.propTypes = {
   direction: PropTypes.string,
