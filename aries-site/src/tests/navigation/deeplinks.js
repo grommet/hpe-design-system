0620d6e9f0c5b1da6143327721b88b7f82ed05ff
Brittany Archibeque <brittanyarchibeque@brittanys-mbp-2.americas.hpqcorp.net>
2020-02-20 13:14:42 -0700

    fix the max-len

--- aries-site/src/tests/navigation/deeplinks.js
+++ aries-site/src/tests/navigation/deeplinks.js
@@ -1,10 +1,12 @@
-/* eslint-disable max-len */
 /* eslint-disable no-undef */
 import {  waitForReact } from 'testcafe-react-selectors';
 import { Selector } from 'testcafe';
 import { baseUrl } from '../utils';
 
-// A deeplink is a subsection of a page. These tests check if the website is taken to the right scroll position of the page to match the hash of the intended subsection.
+/* A deeplink is a subsection of a page. These tests check if the
+website is taken to the right scroll position of the
+page to match the hash of the intended subsection.
+*/
 
 fixture('Deep Linking')
   .page(baseUrl)
@@ -13,11 +15,13 @@ fixture('Deep Linking')
   });
 
 
-/* This test will fail until issue #320 is fixed. The page scroll is at the incorrect section of the 
+/* This test will fail until issue #320 is fixed.
+The page scroll is at the incorrect section of the 
 page therefore it is failing. Going to leave commented out for now. 
 */
 
-// test('should navigate to correct hash after user clicks a suggestion that leads to a page subsection', async t => {
+// test('should navigate to correct hash after user clicks a
+// suggestion that leads to a page subsection', async t => {
 //   const page = 'Background Colors';
 //   const backgroundColors = Selector('#background-colors')
 //   const suggestion = getSuggestion(page);
@@ -29,11 +33,13 @@ page therefore it is failing. Going to leave commented out for now.
 //     .within(-150, 150);
 // });
 
-/* Added range to this test since now there is some issues with different browsers as to where
+/* Added range to this test since now there is some issues
+with different browsers as to where
 the page actually lands when routed to a deep link
 */
 
-test('should navigate to correct hash of page when a deep link is directly routed to', async t => {
+test('should navigate to correct hash of page when'
++ 'a deep link is directly routed to', async t => {
   const url = `${baseUrl}/foundation/color#background-colors`;
   const pageSection = Selector('#background-colors');
   await t
