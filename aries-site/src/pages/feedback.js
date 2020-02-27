c22a0a20fdf8c22f492087f4503416a868c9c98c
Taylor Seamans <taylor.seamans@yahoo.com>
2020-02-27 10:35:09 -0800

    Fix spacing

--- aries-site/src/pages/feedback.js
+++ aries-site/src/pages/feedback.js
@@ -1,21 +1,14 @@
 import React from 'react';
 
-import { Box, Grid, ResponsiveContext } from 'grommet';
-import { Layout, ContentSection, Subsection } from '../layouts';
-import {
-  Meta,
-  Subscribe,
-  JoinConversation,
-  Contribute,
-  SubsectionText,
-} from '../components';
+import { Box } from 'grommet';
+import { Layout, Subsection } from '../layouts';
+import { Meta, FeedbackOptions, SubsectionText } from '../components';
 import { getPageDetails } from '../utils';
 
 const title = 'Feedback';
 const page = getPageDetails(title);
 
 const Feedback = () => {
-  const size = React.useContext(ResponsiveContext);
   return (
     <Layout title={title} isLanding>
       <Meta
@@ -23,26 +16,14 @@ const Feedback = () => {
         description={page.seoDescription}
         canonicalUrl="https://design-system.hpe.design/feedback"
       />
-      <ContentSection lastSection>
+      <Box gap="medium">
         <Subsection name="Have feedback to share?" level={2}>
           <SubsectionText>
             Get in touch to help make the HPE Design System better.
           </SubsectionText>
         </Subsection>
-        <Box flex={false}>
-          <Grid
-            fill="horizontal"
-            columns={
-              size !== 'small' ? { count: 'fit', size: 'small' } : 'auto'
-            }
-            gap="large"
-          >
-            <Subscribe />
-            <JoinConversation />
-            <Contribute />
-          </Grid>
-        </Box>
-      </ContentSection>
+        <FeedbackOptions />
+      </Box>
     </Layout>
   );
 };
