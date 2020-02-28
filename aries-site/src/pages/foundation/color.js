cdf96f615fe6d068cd631448bdea95aff9f5900d
Brittany Archibeque <brittanyarchibeque@brittanys-mbp-2.americas.hpqcorp.net>
2020-02-28 09:58:43 -0700

    add content changes

--- aries-site/src/pages/foundation/color.js
+++ aries-site/src/pages/foundation/color.js
@@ -57,15 +57,16 @@ const Color = () => (
     <ContentSection>
       <Subsection level={1} name={title} topic={topic}>
         <SubsectionText>
-          Our color palette brings out the depth and dimension of our identity.
-          From sophisticated, neutral hues reflective of technology to vibrant,
-          saturated colors that evoke energy and inspiration.
+          You will notice that the HPE Design System Color guidance
+          is different from Brand Central. Prioritize this HPE Design
+          System color palette over Brand Central when identifying 
+          colors for your digital experience.
         </SubsectionText>
         <SubsectionText size="medium">
-          The library of colors is approved for use by both HPE and Aruba
-          products and services. All colors have been tested for accessibility,
-          readability, and usability with guidance on how to apply them to your
-          experience.
+          The HPE Design System team is working with HPE Brand to ensure
+          that the Brand Central color Palletes (like 'seconday colors')
+          are updated to meet ADA complicance levels in a large variety 
+          of user contexts.
         </SubsectionText>
         <ButtonRow>
           <Button
