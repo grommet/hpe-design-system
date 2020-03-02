2b8c04f258049d910dff8989f5efdb5cee3703d7
Brittany Archibeque <brittanyarchibeque@brittanys-mbp-2.americas.hpqcorp.net>
2020-03-02 15:51:37 -0700

    change main footer to new footer nav

--- aries-site/src/layouts/main/Footer.js
+++ aries-site/src/layouts/main/Footer.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import Link from 'next/link';
-import { NavLink } from 'aries-core';
+import { FooterLink } from 'aries-core';
 import { Box, Footer as GrommetFooter, ResponsiveContext, Text } from 'grommet';
 import { nameToPath } from '../../utils';
 
@@ -18,7 +18,6 @@ export const Footer = () => {
       label: 'Security',
       href: 'https://www.hpe.com/us/en/legal/privacy.html#datacollection',
     },
-    { label: 'Feedback', href: 'https://github.com/hpe-design/aries/issues' },
   ];
   return (
     <GrommetFooter
@@ -36,28 +35,19 @@ export const Footer = () => {
         </Text>
       </Box>
       <Box alignSelf="center" direction="row" gap="medium">
-        <NavLink
-          label="Terms"
-          href="https://www.hpe.com/us/en/about/legal/terms-of-use.html"
-          target="_blank"
-          rel="noreferrer noopener"
-        />
-        <NavLink
-          label="Privacy"
-          href="https://www.hpe.com/us/en/legal/privacy.html"
-          target="_blank"
-          rel="noreferrer noopener"
-        />
-        <NavLink
-          label="Security"
-          href="https://www.hpe.com/us/en/legal/privacy.html#datacollection"
-          target="_blank"
-          rel="noreferrer noopener"
-        />
+        {footerLinks.map((link, index) => (
+          <FooterLink
+            key={index}
+            href={link.href}
+            label={link.label}
+            target="_blank"
+            rel="noreferrer noopener"
+          />
+        ))}
         {/* Need to pass href because of:
         https://github.com/zeit/next.js/#forcing-the-link-to-expose-href-to-its-child */}
         <Link href={nameToPath('Feedback')} passHref>
-          <NavLink label="Feedback" />
+          <FooterLink label="Feedback" />
         </Link>
       </Box>
     </GrommetFooter>
