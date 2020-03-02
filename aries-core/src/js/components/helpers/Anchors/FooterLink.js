043df50601d35ed4797736720f94b7f5669419f6
Brittany Archibeque <brittanyarchibeque@brittanys-mbp-2.americas.hpqcorp.net>
2020-03-02 15:48:41 -0700

    added the footerLink to aries core back to use for footer

---
+++ aries-core/src/js/components/helpers/Anchors/FooterLink.js
@@ -0,0 +1,40 @@
+import React, { forwardRef, useState } from 'react';
+import PropTypes from 'prop-types';
+import { Anchor, Text } from 'grommet';
+
+export const FooterLink = forwardRef(
+  ({ active, children, label, size, ...rest }, ref) => {
+    const activeTextColor = 'text-xweak';
+    const defaultTextColor = 'text';
+    const [textColor, setTextColor] = useState(
+      active ? activeTextColor : defaultTextColor,
+    );
+
+    return (
+      <Anchor
+        color={textColor}
+        label={
+          <Text weight="normal" size={size} color={textColor}>
+            {label || children}
+          </Text>
+        }
+        onBlur={() => !active && setTextColor(defaultTextColor)}
+        onFocus={() => !active && setTextColor(activeTextColor)}
+        onMouseOut={() => !active && setTextColor(defaultTextColor)}
+        onMouseOver={() => !active && setTextColor(activeTextColor)}
+        ref={ref}
+        {...rest}
+      />
+    );
+  },
+);
+FooterLink.propTypes = {
+  active: PropTypes.bool,
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+  label: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+  size: PropTypes.string,
+};
+
+FooterLink.defaultProps = {
+  size: 'medium',
+};
