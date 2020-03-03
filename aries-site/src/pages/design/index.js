2deaa60c02bdf342897a5d08a4729a6a478990f8
Eric Soderberg <eric.soderberg@hpe.com>
2020-03-03 08:32:35 -0800

    Added first pass at Develop page

--- aries-site/src/pages/design/index.js
+++ aries-site/src/pages/design/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { Layout, NavPage } from '../../layouts';
-import { ComingSoon, DescriptiveHeader, Meta } from '../../components';
+import { ContentSection, Layout, NavPage, Subsection } from '../../layouts';
+import { DescriptiveHeader, Meta, SubsectionText } from '../../components';
 import { getPageDetails } from '../../utils';
 
 const title = 'Design';
@@ -27,7 +27,58 @@ const Design = () => {
       {page.pages.length ? (
         <NavPage items={page.pages} topic={page.name.toLowerCase()} />
       ) : (
-        <ComingSoon />
+        <>
+          <ContentSection>
+            <Subsection name="Preferred">
+              <SubsectionText>
+                The HPE Design System is primarily focused on user interfaces
+                developed with ReactJS and Grommet. There are more resources,
+                examples, and community help for this environment.
+              </SubsectionText>
+              <SubsectionText size="medium">
+                The HPE Design System contains an open-source library of
+                elements consisting of working code, best practices, design
+                resources, human-centered guidelines, and a vibrant community of
+                contributors.
+              </SubsectionText>
+            </Subsection>
+          </ContentSection>
+          <ContentSection>
+            <Subsection name="Alternatives">
+              <SubsectionText>
+                The HPE Design System’s ethos is rooted and thrives in an
+                environment of generosity and community. It seeks to share and
+                invite conversations that are inclusive, as well as to boldly
+                express the cultural DNA of Hewlett Packard Enterprise.
+                Embracing the courage and commitment to learn, share and serve
+                with uncompromising integrity, the HPE Design System will
+                advance the way people live and work.
+              </SubsectionText>
+            </Subsection>
+            <Subsection name="Generous">
+              <SubsectionText>
+                Living generously requires understanding the needs of others.
+                The HPE Design System is crafted upon user research and
+                listening to customers first. We can then bring to bear the
+                experiences that generously provide for the needs of people and
+                their work. It requires a humility and position that is ready to
+                serve others and continues to relentless pursue excellence in
+                its craft for the sake of others.
+              </SubsectionText>
+            </Subsection>
+            <Subsection name="Community">
+              <SubsectionText>
+                Partnering in community allows the HPE Design System to be built
+                upon the energy and empowerment of collaboration. The community
+                provides accountability, conversation and relationships that are
+                the crucible for generating innovation. Community is the fuel
+                for the fires of creativity and allows the Design System to
+                expand and thrive. Best of all, it’s a great place to find
+                support and celebrate success!
+              </SubsectionText>
+            </Subsection>
+          </ContentSection>
+        </>
       )}
     </Layout>
   );
--- aries-site/src/pages/develop/index.js
+++ aries-site/src/pages/develop/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Anchor } from 'grommet';
 
-import { Layout, NavPage } from '../../layouts';
-import { ComingSoon, DescriptiveHeader, Meta } from '../../components';
+import { ContentSection, Layout, NavPage, Subsection } from '../../layouts';
+import { DescriptiveHeader, Meta, SubsectionText } from '../../components';
 import { getPageDetails } from '../../utils';
 
 const title = 'Develop';
@@ -27,7 +28,65 @@ const Develop = () => {
       {page.pages.length ? (
         <NavPage items={page.pages} topic={page.name.toLowerCase()} />
       ) : (
-        <ComingSoon />
+        <>
+          <ContentSection>
+            <Subsection name="Preferred">
+              <SubsectionText>
+                The HPE Design System is primarily focused on user interfaces
+                developed with ReactJS and Grommet. There are more resources,
+                examples, and community help for this environment.
+              </SubsectionText>
+              <SubsectionText size="medium">
+                If you are new to ReactJS, the{' '}
+                <Anchor
+                  label="React Tutorial"
+                  href="https://reactjs.org/tutorial/tutorial.html"
+                />{' '}
+                is a good place to start.
+              </SubsectionText>
+              <SubsectionText size="medium">
+                If you are already familiar with ReactJS but are unfamiliar with
+                Grommet, check out the{' '}
+                <Anchor
+                  label="Getting Started with Grommet"
+                  href="https://v2.grommet.io/starter"
+                />{' '}
+                guide.
+              </SubsectionText>
+              <SubsectionText size="medium">
+                If you are already familiar with ReactJS and Grommet, make sure
+                you have the latest <code>grommet-theme-hpe</code> theme
+                via <code>npm</code> or <code>yarn</code>.
+              </SubsectionText>
+              <SubsectionText size="medium">
+                Questions and feedback should be routed through the
+                #hpe-design-system channel on{' '}
+                <Anchor label="Slack" href="https://grommet.slack.com" />.
+              </SubsectionText>
+            </Subsection>
+          </ContentSection>
+          <ContentSection>
+            <Subsection name="Alternatives">
+              <SubsectionText>
+                For non-ReactJS environments, such as those with a large code
+                base that are not ready to absorb changing the UI framework, we
+                recommend using the examples and patterns shown in the HPE
+                Design System as a reference to update their style to get close.
+                This could be done by inspecting the styling in the browser
+                developer tools.
+              </SubsectionText>
+              <SubsectionText>
+                If you have questions or would like some guidance in migrating
+                to ReactJS, please reach out to the{' '}
+                <Anchor
+                  label="HPE Design System team"
+                  href="mailto:designsystem-req@hpe.com"
+                />
+                .
+              </SubsectionText>
+            </Subsection>
+          </ContentSection>
+        </>
       )}
     </Layout>
   );
