700ce0925504b61d05897f03de21b2b8a4c1eb01
Taylor Seamans <taylor.seamans@yahoo.com>
2020-03-10 10:04:19 -0700

    Add comment for tab function

--- aries-site/src/tests/utils.js
+++ aries-site/src/tests/utils.js
@@ -30,8 +30,11 @@ export const getTabCount = ClientFunction(expectedPath => {
   let hrefIndex;
   for (let i = 0; i < tabbableElements.length; i += 1) {
     if (tabbableElements[i].getAttribute('href') === expectedPath) {
-      hrefIndex = i + 1;
+      hrefIndex = i;
     }
   }
-  return hrefIndex;
+  // Need to add one for correct tab count because array indexes start at 0.
+  // If item we are aiming to tab to is at index = 3, that means we have to tab
+  // 4 times to get there.
+  return hrefIndex + 1;
 });
