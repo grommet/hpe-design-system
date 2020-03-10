2692bf6e0b0ecacd5adee2c90f19e85db2533a4c
Brittany Archibeque <brittanyarchibeque@brittanys-mbp-2.americas.hpqcorp.net>
2020-03-10 11:46:31 -0600

    add pay example and export from main index

--- aries-site/src/examples/templates/forms/index.js
+++ aries-site/src/examples/templates/forms/index.js
@@ -1,6 +1,7 @@
 export * from './ChangePasswordExample';
 export * from './FormContainer';
 export * from './formHelpers';
+export * from './PayExample';
 export * from './SettingsExample';
 export * from './SignInExample';
 export * from './SignUpExample';
