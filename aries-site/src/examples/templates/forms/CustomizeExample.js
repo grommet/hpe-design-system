48f1d49b008ed7f44c8b23d2a817e3d901291572
Taylor Seamans <taylor.seamans@yahoo.com>
2020-03-12 10:53:33 -0700

    Merge master

---
+++ aries-site/src/examples/templates/forms/CustomizeExample.js
@@ -0,0 +1,134 @@
+import React from 'react';
+import {
+  Anchor,
+  Box,
+  Button,
+  Form,
+  FormField,
+  Header,
+  Heading,
+  RadioButtonGroup,
+  Text,
+} from 'grommet';
+import { FormContainer } from '.';
+
+export const CustomizeExample = () => {
+  const [formValues, setFormValues] = React.useState({});
+
+  // eslint-disable-next-line no-unused-vars
+  const onSubmit = ({ value, touched }) => {
+    // Your submission logic here
+  };
+
+  return (
+    <FormContainer width="medium">
+      <Box gap="medium">
+        <Header
+          direction="column"
+          align="start"
+          gap="xxsmall"
+          pad={{ horizontal: 'xxsmall' }}
+        >
+          <Heading level={3} margin="none">
+            Customize
+          </Heading>
+          <Text>your HPE Edgeline Server </Text>
+        </Header>
+        <Box
+          // Padding used to prevent focus from being cutoff
+          pad={{ horizontal: 'xxsmall' }}
+        >
+          <Form
+            validate="blur"
+            value={formValues}
+            onChange={setFormValues}
+            onSubmit={({ value, touched }) => onSubmit({ value, touched })}
+          >
+            <FormField
+              label={
+                <Heading level={4} margin="none">
+                  Memory
+                </Heading>
+              }
+              help={
+                <Anchor href="#" size="xsmall">
+                  How much memory does my server need?
+                </Anchor>
+              }
+              htmlFor="memory"
+              name="memory"
+            >
+              <RadioButtonGroup
+                name="memory"
+                options={[
+                  '8GB (1x8GB) DDR4-2400 ECC memory',
+                  '32GB (2x16GB) DDR4-2400 ECC memory',
+                  '64GB (2x32GB) DDR4-2400 ECC memory',
+                ]}
+              >
+                {(option, { checked, hover }) => {
+                  let border;
+                  if (checked) border = { color: 'brand', side: 'all' };
+                  else if (hover) border = { color: 'active', side: 'all' };
+                  else border = 'text-weak';
+                  return (
+                    <Box
+                      border={border}
+                      pad={{ horizontal: 'small', vertical: 'xsmall' }}
+                      round="xsmall"
+                    >
+                      {option}
+                    </Box>
+                  );
+                }}
+              </RadioButtonGroup>
+            </FormField>
+            <FormField
+              label={
+                <Heading level={4} margin="none">
+                  CPU
+                </Heading>
+              }
+              help={
+                <Anchor href="#" size="xsmall">
+                  What is the right processor for my workload?
+                </Anchor>
+              }
+              htmlFor="cpu"
+              name="cpu"
+            >
+              <RadioButtonGroup
+                name="cpu"
+                options={[
+                  'AMD Opteron™ 1.6GHz (Turbo Boost 2.8GHz), 4 cores, 6 ' +
+                    'graphic cores, 1MB',
+                  'AMD Opteron™ 2.1GHz (Turbo Boost 3.4GHz), 4 cores, 8 ' +
+                    'graphic cores, 2MB',
+                ]}
+              >
+                {(option, { checked, hover }) => {
+                  let border;
+                  if (checked) border = { color: 'brand', side: 'all' };
+                  else if (hover) border = { color: 'active', side: 'all' };
+                  else border = 'text-weak';
+                  return (
+                    <Box
+                      border={border}
+                      pad={{ horizontal: 'small', vertical: 'xsmall' }}
+                      round="xsmall"
+                    >
+                      {option}
+                    </Box>
+                  );
+                }}
+              </RadioButtonGroup>
+            </FormField>
+            <Box align="start" margin={{ top: 'medium', bottom: 'small' }}>
+              <Button label="Continue" primary type="submit" />
+            </Box>
+          </Form>
+        </Box>
+      </Box>
+    </FormContainer>
+  );
+};
