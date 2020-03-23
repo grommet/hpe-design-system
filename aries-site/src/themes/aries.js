ccc0c0e5018a97f7c81b087dd350a46f809d8e61
Taylor Seamans <taylor.seamans@yahoo.com>
2020-03-23 10:45:01 -0700

    Update primary button color

--- aries-site/src/themes/aries.js
+++ aries-site/src/themes/aries.js
@@ -3,6 +3,12 @@ import { deepMerge, normalizeColor } from 'grommet/utils';
 
 export const aries = deepMerge(hpe, {
   defaultMode: 'dark',
+  button: {
+    color: props =>
+      props.primary
+        ? '#FFFFFF'
+        : props.theme.global.colors.text[props.theme.dark ? 'dark' : 'light'],
+  },
   checkBox: {
     color: 'selected-text',
     gap: 'small',
