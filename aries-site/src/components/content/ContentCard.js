f3ff682194e63ae7eb0640af17c96b8f13fe0520
Taylor Seamans <taylor.seamans@yahoo.com>
2020-03-27 14:36:44 -0700

    Add Foundation previews

--- aries-site/src/components/content/ContentCard.js
+++ aries-site/src/components/content/ContentCard.js
@@ -3,10 +3,12 @@ import PropTypes from 'prop-types';
 import { Box, Text } from 'grommet';
 import { Identifier, Tile } from 'aries-core';
 import { aries } from '../../themes/aries';
+import { useDarkMode } from '../../utils';
 
 export const ContentCard = forwardRef(({ topic, ...rest }, ref) => {
   const { description, name, parent, preview } = topic;
   const [isFocused, setIsFocused] = React.useState(false);
+  const themeMode = useDarkMode().value ? 'dark' : 'light';
   return (
     <Tile
       align="start"
@@ -29,7 +31,7 @@ export const ContentCard = forwardRef(({ topic, ...rest }, ref) => {
           overflow="hidden"
           style={{ position: 'relative' }}
         >
-          {preview && preview(aries)}
+          {preview && preview(aries, themeMode)}
         </Box>
         <Box gap="small">
           <Identifier title={name} align="start" gap="xsmall" size="xxlarge">
