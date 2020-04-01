d78461efbe6021f112dd841e06f3f58084267bdf
Taylor Seamans <taylor.seamans@yahoo.com>
2020-04-01 16:46:25 -0700

    Move ThemeMode to layouts

--- aries-site/src/components/content/index.js
+++ aries-site/src/components/content/index.js
@@ -3,5 +3,4 @@ export * from './CollapsibleSection';
 export * from './ComingSoon';
 export * from './SubsectionText';
 export * from './SubmitFeedback';
-export * from './ThemeMode';
 export * from './ThemeModeToggle';
