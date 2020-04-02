449a194e405beae3997b68587c57793b17656120
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2020-04-02 09:33:33 -0600

    change light mode toggle to have hover

--- aries-site/src/components/content/ThemeModeToggle.js
+++ aries-site/src/components/content/ThemeModeToggle.js
@@ -1,21 +1,42 @@
-import React from 'react';
-import { Box, Button } from 'grommet';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Box, Button, Text } from 'grommet';
 import { IconMoon, IconSun } from '../icons';
 import { useDarkMode } from '../../utils';
 
-export const ThemeModeToggle = () => {
+export const ThemeModeToggle = ({ active }) => {
+  const [hover, setHover] = useState(false);
   const darkMode = useDarkMode();
   const label = darkMode.value ? 'Light Mode' : 'Dark Mode';
   const icon = darkMode.value ? <IconSun /> : <IconMoon />;
   return (
     <Box width={{ max: 'small' }}>
       <Button
-        color='background-back'
-        label={label}
-        icon={icon}
+        onMouseOver={() => setHover(true)}
+        onFocus={() => setHover(true)}
+        onMouseOut={() => setHover(false)}
+        onBlur={() => setHover(false)}
+        color="background-back"
         primary
         onClick={() => darkMode.toggle()}
-      />
+      >
+        <Box
+          pad={{ horizontal: 'small', vertical: 'xsmall' }}
+          round="xxsmall"
+          background={active || hover ? 'active-background' : undefined}
+          direction="row"
+          gap="xsmall"
+        >
+          {icon}
+          <Text weight="bold" margin="none">
+            {label}
+          </Text>
+        </Box>
+      </Button>
     </Box>
   );
+};
+
+ThemeModeToggle.propTypes = {
+  active: PropTypes.bool,
 };
