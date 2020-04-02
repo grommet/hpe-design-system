650bbd70410ca70b047f4c78dbbcfa2abeba72b0
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2020-04-02 12:04:34 -0600

    add align

--- aries-site/src/components/button/NavButton.js
+++ aries-site/src/components/button/NavButton.js
@@ -11,6 +11,7 @@ export const NavButton = forwardRef(({ active, item, ...rest }, ref) => {
         onMouseOut={() => setHover(false)}
         onBlur={() => setHover(false)}
         ref={ref}
+        alignSelf='start'
         {...rest}
       >
         <Box
