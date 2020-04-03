7d819db299a46e3b2bf27d431abd1ec1fc67e28d
Matt Glissmann <mdglissmann@gmail.com>
2020-04-03 14:43:22 -0600

    button example horizontal layouts

--- aries-site/src/examples/components/button/ButtonStatesExample.js
+++ aries-site/src/examples/components/button/ButtonStatesExample.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Box, Button } from 'grommet';
+import { Box, Button, ResponsiveContext } from 'grommet';
 
 export const ButtonStatesExample = () => {
+  const size = React.useContext(ResponsiveContext);
+
   return (
-    <Box direction="row" gap="large">
-      <Box align="center" gap="medium">
+    <Box
+      direction={size !== 'small' ? 'column' : 'row'}
+      gap={size !== 'small' ? 'small' : 'medium'}
+    >
+      <Box direction={size !== 'small' ? 'row' : 'column'} gap="medium">
         <Button label="Default" onClick={() => {}} />
         <Button label="Active" active onClick={() => {}} />
         <Button label="Disabled" disabled onClick={() => {}} />
       </Box>
-      <Box align="center" gap="medium">
+      <Box direction={size !== 'small' ? 'row' : 'column'} gap="medium">
         <Button label="Primary" primary onClick={() => {}} />
         <Button label="Primary Active" primary active onClick={() => {}} />
         <Button label="Primary Disabled" primary disabled onClick={() => {}} />
