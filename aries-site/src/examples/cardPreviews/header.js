1f263ee1e8281bc56d0d01b7ff6a41f3cfe38670
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2020-04-07 14:57:05 -0600

    add header preview

---
+++ aries-site/src/examples/cardPreviews/header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Box, Header, Button, Text } from 'grommet';
+import { Hpe, Menu } from 'grommet-icons';
+
+export const HeaderPreview = () => {
+  return (
+    <Header background="background-front" fill="horizontal">
+      <Box direction="row">
+        <Button icon={<Hpe color="brand" />} />
+        <Box gap='xsmall' direction="row">
+          <Text weight="bold" alignSelf="center">
+            {' '}
+            HPE{' '}
+          </Text>
+          <Text alignSelf="center"> Text</Text>
+        </Box>
+      </Box>
+      <Button icon={<Menu />} />
+    </Header>
+  );
+};
