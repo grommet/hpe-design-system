0d97a6d3fc3e7c440f8849810bfad036d79f9ed8
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2020-04-07 15:06:35 -0600

    add radio button and range input

---
+++ aries-site/src/examples/cardPreviews/radiobuttonGroup.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { RadioButtonGroup } from 'grommet';
+
+export const RadioButtonGroupPreview = () => {
+  return (
+      <RadioButtonGroup
+        name="radio"
+        options={[
+          { label: 'Choice 1', value: 'c1' },
+          { label: 'Choice 2', value: 'c2' },
+          { label: 'Choice 3', value: 'c3' },
+        ]}
+      />
+  );
+};
