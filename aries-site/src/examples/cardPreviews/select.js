c37917294a86267ff500443fc9369c8d6b87e7bf
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2020-04-07 15:08:27 -0600

    add select preview

---
+++ aries-site/src/examples/cardPreviews/select.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Select } from 'grommet';
+
+export const SelectPreview = () => {
+  return (
+      <Select options={['First']} placeholder="Choices" />
+  );
+};
