fc49658529385113d50aa2c95b355a4c7b9dde6b
Taylor Seamans <taylor.seamans@yahoo.com>
2020-04-09 15:50:47 -0700

    Fix nested anchor error

--- aries-site/src/examples/cardPreviews/anchor.js
+++ aries-site/src/examples/cardPreviews/anchor.js
@@ -5,7 +5,9 @@ export const AnchorPreview = () => {
   return (
     <Box direction="row" gap="xsmall">
       <Text>The ship's</Text>
-      <Anchor href="#" label="Anchor. " />
+      {/* Placing as span to avoid nested Anchor errors in DOM. Since this is
+       purely a visual preview, functionality isn't important here */}
+      <Anchor as="span" label="Anchor." />
     </Box>
   );
 };
