6b358e911e01ec1bfb67af7d06c435352678d5fb
britt6612 <42451602+britt6612@users.noreply.github.com>
2020-04-10 11:38:11 -0600

    Update aries-site/src/examples/cardPreviews/anchor.js
    Co-Authored-By: Matthew Glissmann <mdglissmann@gmail.com>

--- aries-site/src/examples/cardPreviews/anchor.js
+++ aries-site/src/examples/cardPreviews/anchor.js
@@ -7,7 +7,7 @@ export const AnchorPreview = () => {
       <Text>The ship's</Text>
       {/* Placing as span to avoid nested Anchor errors in DOM. Since this is
        purely a visual preview, functionality isn't important here */}
-      <Anchor as="span" label="Anchor." />
+      <Anchor as="span" label="Anchor" />.
     </Box>
   );
 };
