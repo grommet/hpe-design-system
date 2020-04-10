c6d5933d54ef314c4c76f6de3bfe8789941dbcf3
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2020-04-10 11:50:53 -0600

    take out accordion that is not being used

--- aries-site/src/examples/cardPreviews/footer.js
+++ aries-site/src/examples/cardPreviews/footer.js
@@ -3,14 +3,18 @@ import { Box, Footer, Text } from 'grommet';
 
 export const FooterPreview = () => {
   return (
-    <Footer background="background-front" border={{ color: 'background-contrast', side: 'top' }} fill="horizontal">
+    <Footer
+      background="background-front"
+      border={{ color: 'background-contrast', side: 'top' }}
+      fill="horizontal"
+    >
       <Box direction="row" gap="xsmall">
         <Text size="small">&copy; 2020 HPE</Text>
       </Box>
       <Box direction="row" gap="xsmall">
-        <Text size='small'>Terms</Text>
-        <Text size='small'>Privacy</Text>
-        <Text size='small'>Security</Text>
+        <Text size="small">Terms</Text>
+        <Text size="small">Privacy</Text>
+        <Text size="small">Security</Text>
       </Box>
     </Footer>
   );
