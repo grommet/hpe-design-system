93a4b55a11eb1b74d83998aedd255a6e163a3de0
Matthew Glissmann <mdglissmann@gmail.com>
2020-04-17 11:56:16 -0600

    Update aries-site/src/examples/foundation/branding/HpeElementExample.js
    Co-Authored-By: taysea <taylor.seamans@yahoo.com>

--- aries-site/src/examples/foundation/branding/HpeElementExample.js
+++ aries-site/src/examples/foundation/branding/HpeElementExample.js
@@ -7,7 +7,7 @@ export const HpeElementExample = () => {
 
   return (
     <Box direction="row" align="center" gap="medium">
-      <Hpe color="brand" size="large" />
+      <Hpe color="brand" />
       <Box direction="row" gap="xsmall">
         <Text size={textSize} weight="bold">
           HPE
