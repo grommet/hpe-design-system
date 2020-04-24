35483e432fd2521570581252e1c8d4c386a5b2f4
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2020-04-23 20:27:24 -0600

    first pass on  accordion  docs

--- aries-site/src/examples/components/controls/AccordionExample.js
+++ aries-site/src/examples/components/controls/AccordionExample.js
@@ -1,27 +1,34 @@
 import React from 'react';
-import { Accordion, AccordionPanel, Box } from 'grommet';
+import { Accordion, AccordionPanel, Box, Heading } from 'grommet';
+
+
+const PanelHeader = (title) => (
+  <Box pad={{ horizontal: 'small', vertical: 'xsmall' }}>
+      <Heading margin="none" level={4}>{title}</Heading>
+  </Box>
+);
 
 export const AccordionExample = () => {
-  const pad = { vertical: 'medium' };
+  const pad = "small";
 
   return (
       <Accordion>
-        <AccordionPanel label="Our Company">
+        <AccordionPanel label={PanelHeader('Our Company')}>
           <Box pad={pad}>We are HPE.</Box>
         </AccordionPanel>
-        <AccordionPanel label="Our History">
+        <AccordionPanel label={PanelHeader('Our History')}>
           <Box pad={pad}>
             At Hewlett Packard Enterprise, we advance the way you live and work
             by engineering experiences that unlock your full potential.
           </Box>
         </AccordionPanel>
-        <AccordionPanel label="Our Purpose">
+        <AccordionPanel label={PanelHeader('Our Purpose')}>
           <Box pad={pad}>
             We advance the way you live and work by engineering experiences that
             unlock your full potential.
           </Box>
         </AccordionPanel>
-        <AccordionPanel label="What's New">
+        <AccordionPanel label={PanelHeader("What's New")}>
           <Box pad={pad}>We make Bold Moves.</Box>
         </AccordionPanel>
       </Accordion>
