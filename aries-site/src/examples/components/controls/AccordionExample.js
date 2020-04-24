45d73c1b5a11b23173af61caabdf72572b14e33f
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2020-04-24 11:09:07 -0600

    put up 2 examples for vicky

--- aries-site/src/examples/components/controls/AccordionExample.js
+++ aries-site/src/examples/components/controls/AccordionExample.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { Accordion, AccordionPanel, Box, Heading } from 'grommet';
 
+const PanelHeader = title => (
+  <Box pad={{ horizontal: 'small', vertical: 'small' }}>
+    <Heading margin="none" level={4}>
+      {title}
+    </Heading>
+  </Box>
+);
 
-const PanelHeader = (title) => (
-  <Box pad={{ horizontal: 'small', vertical: 'xsmall' }}>
-      <Heading margin="none" level={4}>{title}</Heading>
+const PanelHeader1 = title => (
+  <Box pad={{ horizontal: 'small', vertical: 'medium' }}>
+    <Heading margin="none" level={4}>
+      {title}
+    </Heading>
   </Box>
 );
 
 export const AccordionExample = () => {
-  const pad = "small";
+  const pad = 'small';
 
   return (
+    <Box gap="xlarge">
       <Accordion>
         <AccordionPanel label={PanelHeader('Our Company')}>
           <Box pad={pad}>We are HPE.</Box>
@@ -32,5 +42,26 @@ export const AccordionExample = () => {
           <Box pad={pad}>We make Bold Moves.</Box>
         </AccordionPanel>
       </Accordion>
+      <Accordion>
+        <AccordionPanel label={PanelHeader1('Our Company')}>
+          <Box pad={pad}>We are HPE.</Box>
+        </AccordionPanel>
+        <AccordionPanel label={PanelHeader1('Our History')}>
+          <Box pad={pad}>
+            At Hewlett Packard Enterprise, we advance the way you live and work
+            by engineering experiences that unlock your full potential.
+          </Box>
+        </AccordionPanel>
+        <AccordionPanel label={PanelHeader1('Our Purpose')}>
+          <Box pad={pad}>
+            We advance the way you live and work by engineering experiences that
+            unlock your full potential.
+          </Box>
+        </AccordionPanel>
+        <AccordionPanel label={PanelHeader1("What's New")}>
+          <Box pad={pad}>We make Bold Moves.</Box>
+        </AccordionPanel>
+      </Accordion>
+    </Box>
   );
 };
