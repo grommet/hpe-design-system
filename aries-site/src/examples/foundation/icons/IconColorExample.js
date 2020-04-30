6abdd3957467a0399f45993e85f7ad31bbc7e832
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2020-04-30 16:34:09 -0600

    add icon page content

---
+++ aries-site/src/examples/foundation/icons/IconColorExample.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Box } from 'grommet';
+import { Hpe } from 'grommet-icons';
+
+export const IconColorExample = () => {
+  return (
+    <Box direction="row-responsive" gap="medium">
+      <Hpe color="brand" size="large" />
+    </Box>
+  );
+};
