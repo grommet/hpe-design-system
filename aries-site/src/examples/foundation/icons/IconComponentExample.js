45d8bb6afef8e9029ccec39c1e2aba1bcbdbf886
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2020-04-30 17:42:51 -0600

    feedback from icon first pass

---
+++ aries-site/src/examples/foundation/icons/IconComponentExample.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Menu } from 'grommet';
+import { Add } from 'grommet-icons';
+
+export const IconComponentExample = () => {
+  return <Menu icon={<Add />} label="Add User" />;
+};
