840b83de67dad98e604d85f46be40ff9bba89f72
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2020-05-01 14:46:37 -0600

    feedback addressed

--- aries-site/src/pages/components/accordion.js
+++ aries-site/src/pages/components/accordion.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Text } from 'grommet';
+
 import { CardGrid, Meta, SubsectionText, BulletedList } from '../../components';
 import { ContentSection, Layout, Subsection, Example } from '../../layouts';
 import { AccordionExample } from '../../examples';
@@ -23,7 +25,7 @@ const Accordion = () => (
         <Example
           docs="https://v2.grommet.io/accordion#props"
           designer="https://designer.grommet.io/accordion?id=HPE-design-system-hpedesignsystem-hpe-com"
-          code="https://raw.githubusercontent.com/hpe-design/design-system/master/aries-site/src/examples/components/controls/AccordionExample.js"
+          code="https://raw.githubusercontent.com/hpe-design/design-system/master/aries-site/src/examples/components/accordion/AccordionExample.js"
           figma="https://www.figma.com/file/UvTBUQdhk07wV79nijsrtr/HPE-Accordion-Component?node-id=71%3A25"
         >
           <AccordionExample />
@@ -50,15 +52,18 @@ const Accordion = () => (
         <SubsectionText>
           Accordion has two states:
           <BulletedList level={3} items={['Collapsed', 'Expanded']} />
-          The chevron icon is used to identify the expand or collapse action
-          while the entire header can be clicked to expand or collapse content.
-          The default behavior is for all of the panels to be closed, with the
-          user only having visual to the heading.
+          <Text weight="bold">The chevron icon</Text> is used to identify the
+          expand or collapse action while the entire header can be clicked to
+          expand or collapse content.
         </SubsectionText>
         <SubsectionText>
-          The accordion labels should be kept short and to the point of the
-          information following in each panel. Lengthy panel content will want
-          to be avoided.
+          <Text weight="bold">The default behavior</Text> is for all of the
+          panels to be closed, with the user only having visual to the heading.
+        </SubsectionText>
+        <SubsectionText>
+          <Text weight="bold">The accordion labels</Text> should be kept short
+          and to the point of the information following in each panel. Lengthy
+          panel content will want to be avoided.
         </SubsectionText>
       </Subsection>
       <Subsection name="Accessibility" level={3} gap="small">
