72d256d0456194ca161d0e04aed09a580ab4feb7
Taylor Seamans <taylor.seamans@yahoo.com>
2020-05-05 09:38:14 -0700

    Adjust labels and extract mask logic

--- aries-site/src/examples/components/maskedinput/MaskedIPAddressExample.js
+++ aries-site/src/examples/components/maskedinput/MaskedIPAddressExample.js
@@ -3,6 +3,32 @@ import { Form, FormField, MaskedInput } from 'grommet';
 
 const IPv4ElementExp = /^[0-1][0-9][0-9]$|^2[0-4][0-9]$|^25[0-5]$|^[0-9][0-9]$|^[0-9]$/;
 
+const maskIp = [
+  {
+    length: [1, 3],
+    regexp: IPv4ElementExp,
+    placeholder: 'xxx',
+  },
+  { fixed: '.' },
+  {
+    length: [1, 3],
+    regexp: IPv4ElementExp,
+    placeholder: 'xxx',
+  },
+  { fixed: '.' },
+  {
+    length: [1, 3],
+    regexp: IPv4ElementExp,
+    placeholder: 'xxx',
+  },
+  { fixed: '.' },
+  {
+    length: [1, 3],
+    regexp: IPv4ElementExp,
+    placeholder: 'xxx',
+  },
+];
+
 export const MaskedIPAddressExample = () => (
   <Form>
     <FormField
@@ -13,31 +39,7 @@ export const MaskedIPAddressExample = () => (
       <MaskedInput
         name="maskedIpAddress"
         id="masked-ip-address"
-        mask={[
-          {
-            length: [1, 3],
-            regexp: IPv4ElementExp,
-            placeholder: 'xxx',
-          },
-          { fixed: '.' },
-          {
-            length: [1, 3],
-            regexp: IPv4ElementExp,
-            placeholder: 'xxx',
-          },
-          { fixed: '.' },
-          {
-            length: [1, 3],
-            regexp: IPv4ElementExp,
-            placeholder: 'xxx',
-          },
-          { fixed: '.' },
-          {
-            length: [1, 3],
-            regexp: IPv4ElementExp,
-            placeholder: 'xxx',
-          },
-        ]}
+        mask={maskIp}
       />
     </FormField>
   </Form>
