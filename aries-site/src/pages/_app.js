00906ff7200158e1f4ae27c5116a9142843ce654
Taylor Seamans <taylor.seamans@yahoo.com>
2020-05-19 09:58:09 -0700

    Add comment

--- aries-site/src/pages/_app.js
+++ aries-site/src/pages/_app.js
@@ -3,6 +3,9 @@ import { ThemeMode } from '../layouts';
 
 function App({ Component, pageProps }) {
   return (
+    // need to wrap Component here so that
+    // scroll position is correct when link
+    // or search suggestion directs to a hash
     <ThemeMode>
       <Component {...pageProps} />
     </ThemeMode>
