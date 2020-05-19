8b062698e166d292f783a92fa5724b196bf8fe56
taysea <taylor.seamans@yahoo.com>
2020-05-19 10:22:27 -0700

    Update aries-site/src/pages/_app.js
    Co-authored-by: Matthew Glissmann <mdglissmann@gmail.com>

--- aries-site/src/pages/_app.js
+++ aries-site/src/pages/_app.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { ThemeMode } from '../layouts';
-
+/* _app.js allows for customizing Next.js's default <App> component
+ * Details: https://nextjs.org/docs/advanced-features/custom-app
+ * 
+ * The `Component` prop is the active `page`, so whenever you 
+ * navigate between routes, `Component` will change to the new `page`. 
+ */
 function App({ Component, pageProps }) {
   return (
     // need to wrap Component here so that
