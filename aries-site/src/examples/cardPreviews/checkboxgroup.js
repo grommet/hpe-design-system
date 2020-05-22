9352a88270a020bb2a32d3f274714711ff48752e
Taylor Seamans <taylor.seamans@yahoo.com>
2020-05-22 11:32:24 -0700

    Wrap in Form

--- aries-site/src/examples/cardPreviews/checkboxgroup.js
+++ aries-site/src/examples/cardPreviews/checkboxgroup.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { CheckBoxGroup, FormField } from 'grommet';
+import { CheckBoxGroup, Form, FormField } from 'grommet';
 
 export const CheckBoxGroupPreview = () => {
   return (
-    <FormField
-      name="checkboxgroup-preview"
-      fill="horizontal"
-      htmlFor="preview-checkboxgroup"
-      label="Label"
-    >
-      <CheckBoxGroup
-        options={['Option 1', 'Option 2', 'Option 3']}
-        value={['Option 2']}
+    <Form>
+      <FormField
         name="checkboxgroup-preview"
-        id="simple-checkboxgroup"
-      />
-    </FormField>
+        fill="horizontal"
+        htmlFor="preview-checkboxgroup"
+        label="Label"
+      >
+        <CheckBoxGroup
+          options={['Option 1', 'Option 2', 'Option 3']}
+          value={['Option 2']}
+          name="checkboxgroup-preview"
+          id="simple-checkboxgroup"
+        />
+      </FormField>
+    </Form>
   );
 };
