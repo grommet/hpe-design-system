8d0c3123b72b7901f0d9421b182f8b31b2795721
Taylor Seamans <taylor.seamans@yahoo.com>
2020-05-22 14:38:26 -0700

    Add exampel

---
+++ aries-site/src/examples/components/checkboxgroup/CheckBoxGroupObjectExample.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { CheckBoxGroup, Form, FormField } from 'grommet';
+
+const objectOptions = [];
+for (let i = 1; i <= 3; i += 1) {
+  objectOptions.push({
+    label: `Option ${i}`,
+    val: i,
+  });
+}
+
+export const CheckBoxGroupObjectExample = () => {
+  return (
+    <Form>
+      <FormField
+        name="checkboxgroup-objectoptions"
+        fill
+        htmlFor="objectoptions-checkboxgroup"
+        label="Label"
+      >
+        <CheckBoxGroup
+          options={objectOptions}
+          name="checkboxgroup-objectoptions"
+          id="objectoptions-checkboxgroup"
+        />
+      </FormField>
+    </Form>
+  );
+};
