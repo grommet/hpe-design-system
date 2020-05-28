bd872cbed23f0516a891d15b0a829531f98cf1b6
Matt Glissmann <mdglissmann@gmail.com>
2020-05-28 17:15:54 -0600

    testing woff2 file formats

--- aries-site/src/themes/aries.js
+++ aries-site/src/themes/aries.js
@@ -3,6 +3,46 @@ import { deepMerge, normalizeColor } from 'grommet/utils';
 
 export const aries = deepMerge(hpe, {
   defaultMode: 'dark',
+  global: {
+    font: {
+      family: "'Metric', Arial, sans-serif",
+      face: `
+        @font-face {
+          font-family: "Metric";
+          src: url("https://hpe-design-system.s3.amazonaws.com/fonts/HPEXS-Metric-Fonts/MetricHPEXS-Regular.woff2") format('woff2'), 
+               url("https://hpe-design-system.s3.amazonaws.com/fonts/HPEXS-Metric-Fonts/MetricHPEXS-Regular.woff") format('woff');
+        }
+        @font-face {
+          font-family: "Metric";
+          src: url("https://hpe-design-system.s3.amazonaws.com/fonts/HPEXS-Metric-Fonts/MetricHPEXS-Bold.woff2") format('woff2'),
+               url("https://hpe-design-system.s3.amazonaws.com/fonts/HPEXS-Metric-Fonts/MetricHPEXS-Bold.woff") format('woff');
+          font-weight: 700;
+        }
+        @font-face {
+          font-family: "Metric";
+          src: url("https://hpe-design-system.s3.amazonaws.com/fonts/HPEXS-Metric-Fonts/MetricHPEXSSemibold-Regular.woff2") format('woff2'),
+               url("https://hpe-design-system.s3.amazonaws.com/fonts/HPEXS-Metric-Fonts/MetricHPEXSSemibold-Regular.woff") format('woff');
+          font-weight: 600;
+        }
+        @font-face {
+          font-family: "Metric";
+          src: url("https://hpe-design-system.s3.amazonaws.com/fonts/HPEXS-Metric-Fonts/MetricHPEXSMedium-Regular.woff2") format('woff2'),
+               url("https://hpe-design-system.s3.amazonaws.com/fonts/HPEXS-Metric-Fonts/MetricHPEXSMedium-Regular.woff") format('woff');
+          font-weight: 500;
+        }
+        @font-face {
+          font-family: "Metric";
+          src: url("https://hpe-design-system.s3.amazonaws.com/fonts/HPEXS-Metric-Fonts/MetricHPEXSLight-Regular.woff2") format('woff2'),
+               url("https://hpe-design-system.s3.amazonaws.com/fonts/HPEXS-Metric-Fonts/MetricHPEXSLight-Regular.woff") format('woff');
+          font-weight: 100;
+        }`,
+    },
+  },
+  formField: {
+    label: {
+      weight: 500,
+    },
+  },
   rangeInput: {
     thumb: {
       color: {
