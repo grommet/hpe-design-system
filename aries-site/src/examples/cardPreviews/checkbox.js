b6c0c0b800ff7643824b099f8fa9911b1353fe0b
Taylor Seamans <taylor.seamans@yahoo.com>
2020-06-02 15:34:32 -0700

    Update previews so that preview components cannot receive tabaccess

--- aries-site/src/examples/cardPreviews/checkbox.js
+++ aries-site/src/examples/cardPreviews/checkbox.js
@@ -3,8 +3,8 @@ import { CheckBox, FormField } from 'grommet';
 
 export const CheckBoxPreview = () => {
   return (
-    <FormField fill='horizontal'>
-      <CheckBox  aria-label="preview" checked label="Value" />
+    <FormField fill="horizontal" tabIndex={-1}>
+      <CheckBox aria-label="preview" checked label="Value" tabIndex={-1} />
     </FormField>
   );
 };
--- aries-site/src/examples/cardPreviews/textarea.js
+++ aries-site/src/examples/cardPreviews/textarea.js
@@ -2,5 +2,7 @@ import React from 'react';
 import { TextArea } from 'grommet';
 
 export const TextAreaPreview = () => {
-  return <TextArea aria-label="preview" placeholder="Placeholder" />;
+  return (
+    <TextArea aria-label="preview" placeholder="Placeholder" tabIndex={-1} />
+  );
 };
