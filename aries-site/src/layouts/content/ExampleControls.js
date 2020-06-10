f5d8b31970ca5d656ba62828dd245bbbfafb7691
Matt Glissmann <mdglissmann@gmail.com>
2020-06-10 09:57:24 -0600

    tweaking responsive behavior

--- aries-site/src/layouts/content/ExampleControls.js
+++ aries-site/src/layouts/content/ExampleControls.js
@@ -1,15 +1,16 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-import { Box, Button, Grid, ResponsiveContext } from 'grommet';
+import { Box, Button, ResponsiveContext } from 'grommet';
 import { Document, Expand, Grommet, Figma } from 'grommet-icons';
 
 export const ExampleControls = ({ designer, docs, figma, setShowLayer }) => {
   const size = useContext(ResponsiveContext);
-  const buttonSize = size !== 'small' ? undefined : 'small';
+  const isSmall = size === 'small';
+  const buttonSize = isSmall ? 'small' : undefined;
 
   return (
     <Box
-      direction="row"
+      direction={isSmall ? 'column-reverse' : 'row'}
       align="start"
       background="background-front"
       border={{
@@ -17,50 +18,51 @@ export const ExampleControls = ({ designer, docs, figma, setShowLayer }) => {
         color: 'background-back',
         size: 'xsmall',
       }}
+      gap="medium"
       justify="between"
       pad={{ horizontal: 'medium', vertical: 'small' }}
       round={{ corner: 'bottom', size: 'small' }}
     >
-      <Grid
-        columns={{ count: 'fill', size: ['small', 'auto'] }}
-        fill
-        gap="xsmall"
-        align="start"
-        justify="start"
-      >
+      <Box direction="row" wrap>
         {designer && (
-          <Button
-            href={designer}
-            icon={<Grommet color="plain" />}
-            label="Open in Grommet Designer"
-            target="_blank"
-            size={buttonSize}
-          />
+          <Box flex={false}>
+            <Button
+              href={designer}
+              icon={<Grommet color="plain" />}
+              label="Open in Grommet Designer"
+              target="_blank"
+              size={buttonSize}
+            />
+          </Box>
         )}
         {figma && (
-          <Button
-            href={figma}
-            icon={<Figma color="plain" />}
-            label="Open in Figma"
-            target="_blank"
-            size={buttonSize}
-          />
+          <Box flex={false}>
+            <Button
+              href={figma}
+              icon={<Figma color="plain" />}
+              label="Open in Figma"
+              target="_blank"
+              size={buttonSize}
+            />
+          </Box>
         )}
         {docs && (
-          <Button
-            href={docs}
-            icon={<Document />}
-            label="Open docs"
-            target="_blank"
-            size={buttonSize}
-          />
+          <Box flex={false}>
+            <Button
+              href={docs}
+              icon={<Document />}
+              label="Open docs"
+              target="_blank"
+              size={buttonSize}
+            />
+          </Box>
         )}
-      </Grid>
-      <Box flex={false}>
+      </Box>
+      <Box flex={false} alignSelf={isSmall ? 'end' : 'start'}>
         <Button
           icon={<Expand />}
           a11yTitle="See Fullscreen"
-          label={size !== 'small' ? 'See Fullscreen' : undefined}
+          label="See Fullscreen"
           onClick={() => setShowLayer(true)}
           size={buttonSize}
         />
