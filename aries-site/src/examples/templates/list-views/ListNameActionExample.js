56b41dfcf2810eba2b79cfef161d4a1ccf6574a5
Taylor Seamans <taylor.seamans@yahoo.com>
2020-07-07 12:49:57 -0700

    Update List examples width

--- aries-site/src/examples/templates/list-views/ListNameActionExample.js
+++ aries-site/src/examples/templates/list-views/ListNameActionExample.js
@@ -1,27 +1,29 @@
 import React from 'react';
-import { List, Menu, Text } from 'grommet';
+import { Box, List, Menu, Text } from 'grommet';
 import { More } from 'grommet-icons';
 
 const data = ['Server 1', 'Server 2', 'Server 3', 'Server 4', 'Server 5'];
 
 export const ListNameActionExample = () => {
   return (
-    <List
-      data={data}
-      action={() => (
-        <Menu
-          icon={<More />}
-          hoverIndicator
-          items={[{ label: 'Deactivate' }, { label: 'Suspend' }]}
-        />
-      )}
-      onClickItem={() => {}}
-    >
-      {(datum, index) => (
-        <Text weight="bold" key={index}>
-          {datum}
-        </Text>
-      )}
-    </List>
+    <Box width={{ max: 'xxlarge' }} margin="auto" fill>
+      <List
+        data={data}
+        action={() => (
+          <Menu
+            icon={<More />}
+            hoverIndicator
+            items={[{ label: 'Deactivate' }, { label: 'Suspend' }]}
+          />
+        )}
+        onClickItem={() => {}}
+      >
+        {(datum, index) => (
+          <Text weight="bold" key={index}>
+            {datum}
+          </Text>
+        )}
+      </List>
+    </Box>
   );
 };
