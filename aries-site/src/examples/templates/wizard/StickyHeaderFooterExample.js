825ba9f3366778e299cd0a7f921ce6eb46905480
Taylor Seamans <taylor.seamans@yahoo.com>
2020-08-05 14:34:42 -0700

    Add clarifying comment

--- aries-site/src/examples/templates/wizard/StickyHeaderFooterExample.js
+++ aries-site/src/examples/templates/wizard/StickyHeaderFooterExample.js
@@ -141,6 +141,9 @@ export const StickyHeaderFooterExample = () => {
   const [open, setOpen] = useState(false);
   const size = useContext(ResponsiveContext);
 
+  // ref allows us to access the wizard container and ensure scroll position
+  // is at the top as user advances between steps. useEffect is triggered
+  // when the active step changes.
   const wizardRef = React.useRef();
 
   React.useEffect(() => {
