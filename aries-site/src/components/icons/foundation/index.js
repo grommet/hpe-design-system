eb36b37ab1bdc36cfe70b19a4103dd9cccf733fc
taysea <taylor.seamans@yahoo.com>
2020-08-07 15:44:37 -0700

    Remove unused icons (#1041)

--- aries-site/src/components/icons/foundation/index.js
+++
@@ -1,6 +0,0 @@
-export * from './IconIcons';
-export * from './IconLayout';
-export * from './IconToken';
-export * from './IconType';
-export * from './IconBrand';
-export * from './IconColor';
--- aries-site/src/components/icons/guidelines/index.js
+++
@@ -1,3 +0,0 @@
-export * from './IconPhilosophy';
-export * from './IconHumanCentered';
-export * from './IconPrinciples';
