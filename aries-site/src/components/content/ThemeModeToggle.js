6351488a5be463189d1910d2685949de0aa7ce2b
shimisun <shimrit.yacobi@gmail.com>
2020-08-07 10:31:18 -0600

    removing alias

--- aries-site/src/components/content/ThemeModeToggle.js
+++ aries-site/src/components/content/ThemeModeToggle.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'grommet';
-import { Moon as IconMoon, Sun as IconSun } from 'grommet-icons';
+import { Moon, Sun } from 'grommet-icons';
 import { useDarkMode } from '../../utils';
 
 export const ThemeModeToggle = ({ active }) => {
   const darkMode = useDarkMode();
   const label = darkMode.value ? 'Light Mode' : 'Dark Mode';
-  const icon = darkMode.value ? <IconSun /> : <IconMoon />;
+  const icon = darkMode.value ? <Sun /> : <Moon />;
   return (
     <Button
       active={active}
