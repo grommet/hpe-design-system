1c71e55c0c8ac8b10f9898b5b5051b604854e213
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2020-08-17 20:25:44 -0600

    fix focus on textinput

--- aries-site/src/examples/cardPreviews/textinput.js
+++ aries-site/src/examples/cardPreviews/textinput.js
@@ -7,15 +7,15 @@ export const TextInputPreview = () => {
       <FormField
         htmlFor="focus-id"
         name="focus"
-        style={{
-          boxShadow: '0 0 2px 2px #00E8CF',
-          borderRadius: '4px',
-        }}
       >
         <TextInput
           aria-label="preview"
           id="focus-id"
           name="focus"
+          style={{
+            boxShadow: '0 0 2px 2px #00E8CF',
+            borderRadius: '4px',
+          }}
           placeholder="Enter a username"
           tabIndex={-1}
         />
