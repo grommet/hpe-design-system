f0af6d76b1aec1347eab2d06e3b4d2b5f4c9c096
Taylor Seamans <taylor.seamans@yahoo.com>
2020-10-08 09:20:14 -0700

    Simplify logic of Card Grid

--- aries-site/src/components/cards/CardGrid.js
+++ aries-site/src/components/cards/CardGrid.js
@@ -1,11 +1,11 @@
-import React, { useContext } from 'react';
+import React, { Fragment, useContext } from 'react';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 import { Grid, ResponsiveContext } from 'grommet';
 
 import { ContentCard } from '.';
 import { nameToPath } from '../../utils';
-import { internalLink } from '../../components';
+import { internalLink } from '..';
 
 export const CardGrid = ({ cards, minimal, ...rest }) => {
   const size = useContext(ResponsiveContext);
@@ -21,27 +21,25 @@ export const CardGrid = ({ cards, minimal, ...rest }) => {
       {cards &&
         cards.map(topic => {
           const href = nameToPath(topic.name);
-          const contentCardProps = {
-            /* forwardedAs maintains styling provided by Higher Order 
-        Components. Styled-Components 4.3.0 docs: https://styled-components.com/docs/api#forwardedas-prop
-        https://github.com/styled-components/styled-components/issues/1981#issuecomment-548860710
-        */
-            forwardedAs: 'a',
-            style: { textDecoration: 'none' },
-            topic: topic,
-            minimal: minimal,
+          const isInternalLink = internalLink.test(href);
+          const Wrapper = isInternalLink ? Link : Fragment;
+          const wrapperProps = isInternalLink && {
+            href,
+            passHref: true,
           };
 
-          return internalLink.test(href) ? (
-            <Link key={topic.name} href={href} passHref>
-              <ContentCard {...contentCardProps} />
-            </Link>
-          ) : (
-            <ContentCard
-              key={topic.name}
-              onClick={() => window.open(href)} // open external link in new tab
-              {...contentCardProps}
-            />
+          return (
+            <Wrapper key={topic.name} {...wrapperProps}>
+              <ContentCard
+                forwardedAs="a"
+                style={{ textDecoration: 'none' }}
+                topic={topic}
+                minimal={minimal}
+                target={!isInternalLink ? '_blank' : undefined}
+                // if internal link, href comes from Link wrapper
+                href={!isInternalLink ? href : undefined}
+              />
+            </Wrapper>
           );
         })}
     </Grid>
