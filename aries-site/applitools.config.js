683cf2e5ae80273fc3addea0fa82a67c8e3a8645
Taylor Seamans <taylor.seamans@yahoo.com>
2020-10-23 15:20:10 -0700

    Add batchName

--- aries-site/applitools.config.js
+++ aries-site/applitools.config.js
@@ -2,5 +2,6 @@ module.exports = {
   appName: 'HPE Design System Website',
   apiKey: process.env.APPLITOOLS_API_KEY,
   concurrency: 20,
+  batchName: 'TestCafe-GitActions',
   batchId: process.env.APPLITOOLS_BATCH_ID,
 };
