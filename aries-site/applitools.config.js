c58ff40b11d1ac9a3f23d2758775e6c03c7870c6
Taylor Seamans <taylor.seamans@yahoo.com>
2020-11-02 11:04:40 -0800

    Remove batchName and Id from config

--- aries-site/applitools.config.js
+++ aries-site/applitools.config.js
@@ -2,6 +2,6 @@ module.exports = {
   appName: 'HPE Design System Website',
   apiKey: process.env.APPLITOOLS_API_KEY,
   concurrency: 20,
-  batchName: 'TestCafe-GitActions',
-  batchId: process.env.APPLITOOLS_BATCH_ID,
+  // batchName: 'TestCafe-GitActions',
+  // batchId: process.env.APPLITOOLS_BATCH_ID,
 };
