b3923eed122170f43208f5c16aaf1920b2ef0ee2
taysea <taylor.seamans@yahoo.com>
2020-11-12 10:04:15 -0800

    Update aries-site/src/examples/cardPreviews/page-layouts/HeaderFooterPreview.js
    Co-authored-by: Matthew Glissmann <mdglissmann@gmail.com>

--- aries-site/src/examples/cardPreviews/page-layouts/HeaderFooterPreview.js
+++ aries-site/src/examples/cardPreviews/page-layouts/HeaderFooterPreview.js
@@ -16,7 +16,7 @@ export const HeaderFooterPreview = () => {
       />
       <Box
         height={defaultProps.theme.global.edgeSize.medium}
-        // remove in prod, for demo purposes only
+        // remove dashed border in prod, for demo purposes only
         border={{ color: 'border', style: 'dashed' }}
       />
     </Box>
