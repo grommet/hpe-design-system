d564e69731dc5d8837573d485eef3d3c514ba8cb
Taylor Seamans <taylor.seamans@yahoo.com>
2020-11-20 12:11:33 -0800

    Update Examples and guidance

--- aries-site/src/examples/extend/global-header/AppIdentity.js
+++ aries-site/src/examples/extend/global-header/AppIdentity.js
@@ -1,32 +1,40 @@
 import React, { useContext } from 'react';
-import { Button, Box, Text } from 'grommet';
+import { Button, Box, ResponsiveContext, Text } from 'grommet';
 import { Hpe } from 'grommet-icons';
-import { UserContext } from '.';
+import { UserContext, SidebarLayer } from '.';
 
 export const AppIdentity = () => {
   const { user } = useContext(UserContext);
+  const size = useContext(ResponsiveContext);
   return (
-    <Button>
-      <Box
-        direction="row"
-        align="start"
-        gap="small"
-        // pad maintains accessible hit target
-        // non-responsive maintains same dimensions for mobile
-        pad={{ vertical: 'small' }}
-        responsive={false}
-      >
-        <Hpe color="brand" />
-        <Box direction="row" gap="xsmall" wrap>
-          {user ? (
-            <Text color="text-strong" weight="bold">
-              HPE Console
-            </Text>
-          ) : (
-            <Text color="text-strong">Hewlett Packard Enterprise</Text>
-          )}
+    <Box
+      align="center"
+      direction="row"
+      gap={size !== 'small' ? 'medium' : 'small'}
+    >
+      {user && <SidebarLayer />}
+      <Button>
+        <Box
+          direction="row"
+          align="start"
+          gap="small"
+          // pad maintains accessible hit target
+          // non-responsive maintains same dimensions for mobile
+          pad={{ vertical: 'small' }}
+          responsive={false}
+        >
+          <Hpe color="brand" />
+          <Box direction="row" gap="xsmall" wrap>
+            {user ? (
+              <Text color="text-strong" weight="bold">
+                HPE Console
+              </Text>
+            ) : (
+              <Text color="text-strong">Hewlett Packard Enterprise</Text>
+            )}
+          </Box>
         </Box>
-      </Box>
-    </Button>
+      </Button>
+    </Box>
   );
 };
--- aries-site/src/examples/extend/global-header/AppIdentityExample.js
+++ aries-site/src/examples/extend/global-header/AppIdentityExample.js
@@ -1,66 +1,36 @@
-import React, { forwardRef } from 'react';
-import PropTypes from 'prop-types';
-import { Box, Button, Text } from 'grommet';
-import { Aruba, Hpe } from 'grommet-icons';
+import React, { useState } from 'react';
+import { Box, CheckBox } from 'grommet';
+import { AppIdentity, UserContext } from '.';
 
-const brands = {
-  hpe: {
-    name: 'HPE',
-    logo: <Hpe color="brand" />,
-  },
-  aruba: {
-    name: 'Aruba',
-    logo: <Aruba color="plain" />,
-  },
+const defaultUser = {
+  firstName: 'John',
+  lastName: 'Doe',
+  email: 'john.doe@hpe.com',
+  image: '//s.gravatar.com/avatar/99020cae7ff399a4fbea19c0634f77c3?s=80',
 };
 
-const AppIdentity = forwardRef(
-  ({ brand, logoOnly, href, simple, title, weight, ...rest }, ref) => {
-    const textSize = 'medium';
+export const AppIdentityExample = () => {
+  const [user, setUser] = useState(defaultUser);
+  const [checked, setChecked] = useState(true);
 
-    return (
-      <Button href={href} ref={ref} {...rest}>
-        <Box
-          direction="row"
-          align="start"
-          gap="medium"
-          // pad maintains accessible hit target
-          // non-responsive maintains same dimensions for mobile
-          pad={{ vertical: 'small' }}
-          responsive={false}
-        >
-          {brand && brands[brand].logo}
-          {!logoOnly && (
-            <Box direction="row" gap="xsmall" wrap>
-              {!simple && (
-                <Text weight="bold" size={textSize} color="text-strong">
-                  {brands[brand].name}
-                </Text>
-              )}
-              <Text size={textSize} color="text-strong" weight={weight}>
-                {title}
-              </Text>
-            </Box>
-          )}
-        </Box>
-      </Button>
-    );
-  },
-);
+  const onToggle = () => {
+    if (user) setUser();
+    else setUser(defaultUser);
 
-AppIdentity.propTypes = {
-  brand: PropTypes.string.isRequired,
-  logoOnly: PropTypes.bool,
-  href: PropTypes.string,
-  simple: PropTypes.bool,
-  title: PropTypes.string,
-  weight: PropTypes.string,
+    setChecked(!checked);
+  };
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      <Box gap="small" align="start">
+        <AppIdentity />
+        <CheckBox
+          label="Signed In"
+          toggle
+          checked={checked}
+          onClick={onToggle}
+        />
+      </Box>
+    </UserContext.Provider>
+  );
 };
-
-export const AppIdentityExample = () => (
-  <Box gap="small">
-    <AppIdentity brand="hpe" title="Console" weight="bold" />
-    <AppIdentity brand="hpe" title="Hewlett Packard Enterprise" simple />
-    <AppIdentity brand="aruba" title="Network Manager" />
-  </Box>
-);
