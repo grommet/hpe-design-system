558b3d4372bff5f00ba4de41e402a00332fdde9c
Matt Glissmann <mdglissmann@gmail.com>
2021-01-19 12:55:06 -0700

    markdown component styling for whats new

--- aries-site/src/components/content/MarkdownComponents.js
+++ aries-site/src/components/content/MarkdownComponents.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { Anchor, Heading } from 'grommet';
+
+import { colors } from '../../themes/aries';
 import { SubsectionHeader } from '../../layouts';
 import { SubsectionText } from '.';
 
@@ -42,6 +44,16 @@ export const components = {
   h1: props => <Heading margin={{ vertical: 'small' }} level={1} {...props} />,
   h2: props => <SubsectionHeader level={2} {...props} />,
   h3: props => <SubsectionHeader level={3} {...props} />,
+  hr: () => (
+    <hr
+      style={{
+        backgroundColor: colors.border.light, // TODO: get access to theme mode
+        borderStyle: 'unset',
+        height: '1px',
+        width: '100%',
+      }}
+    />
+  ),
 };
 
 components.a.propTypes = {
---
+++ aries-site/src/themes/whatsNew.js
@@ -0,0 +1,8 @@
+export const whatsNew = {
+  heading: {
+    extend: `
+      margin-top: 0px;
+    `,
+    weight: 400,
+  },
+};
