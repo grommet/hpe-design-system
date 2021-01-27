5648ede75b7c08a8bd8f49440700a8c654c9f035
Taylor Seamans <taylor.seamans@yahoo.com>
2021-01-27 08:46:30 -0800

    Create snapshot changes for heading color change

--- aries-site/src/themes/aries.js
+++ aries-site/src/themes/aries.js
@@ -6,6 +6,12 @@ export const aries = deepMerge(hpe, {
   // To be stripped out once theme changes are made in grommet-theme-hpe
   // keeping file for use as playground for future theme adjusments that need
   // to be quickly tested
+  heading: {
+    extend: ({ theme }) =>
+      `color: ${
+        theme.global.colors['text-strong'][theme.dark ? 'dark' : 'light']
+      };`,
+  },
 });
 
 export const { colors } = aries.global;
