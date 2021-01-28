7e6bc2701f1a5a426fa98b588c0e58afb8a90e2f
Taylor Seamans <taylor.seamans@yahoo.com>
2021-01-28 10:21:20 -0800

    Show both variations for comparison

--- aries-site/src/examples/components/tabs/TabsExample.js
+++ aries-site/src/examples/components/tabs/TabsExample.js
@@ -1,28 +1,73 @@
 import React from 'react';
-import { Box, Tab, Tabs } from 'grommet';
+import { Box, Tab, Tabs, ThemeContext } from 'grommet';
 
 export const TabsExample = () => {
   const [index, setIndex] = React.useState();
   const onActive = nextIndex => setIndex(nextIndex);
 
   return (
-    <Box>
-      <Tabs activeIndex={index} onActive={onActive} justify="start">
-        <Tab title="General">
-          <Box margin="small" gap="small">
-            User Information
-          </Box>
-        </Tab>
-        <Tab title="Account">
-          <Box margin="small">Account Information</Box>
-        </Tab>
-        <Tab title="Billing">
-          <Box margin="small">Billing Information</Box>
-        </Tab>
-        <Tab title="Notifications">
-          <Box margin="small">Notifications will show here.</Box>
-        </Tab>
-      </Tabs>
+    <Box gap="medium">
+      <Box>
+        <Tabs activeIndex={index} onActive={onActive} justify="start">
+          <Tab title="General">
+            <Box margin="small" gap="small">
+              User Information
+            </Box>
+          </Tab>
+          <Tab title="Account">
+            <Box margin="small">Account Information</Box>
+          </Tab>
+          <Tab title="Billing">
+            <Box margin="small">Billing Information</Box>
+          </Tab>
+          <Tab title="Notifications">
+            <Box margin="small">Notifications will show here.</Box>
+          </Tab>
+        </Tabs>
+      </Box>
+      <ThemeContext.Extend
+        value={{
+          tab: {
+            active: {
+              color: 'text-strong',
+            },
+            border: {
+              side: 'bottom',
+              size: 'medium',
+              color: 'transparent',
+              active: {
+                color: 'border-strong',
+                width: 'large',
+              },
+              disabled: {
+                color: 'transparent',
+              },
+              hover: {
+                color: 'transparent',
+              },
+            },
+          },
+        }}
+      >
+        <Box>
+          <Tabs activeIndex={index} onActive={onActive} justify="start">
+            <Tab title="General">
+              <Box margin="small" gap="small">
+                User Information
+              </Box>
+            </Tab>
+            <Tab title="Account">
+              <Box margin="small">Account Information</Box>
+            </Tab>
+            <Tab title="Billing">
+              <Box margin="small">Billing Information</Box>
+            </Tab>
+            <Tab title="Notifications">
+              <Box margin="small">Notifications will show here.</Box>
+            </Tab>
+          </Tabs>
+        </Box>
+      </ThemeContext.Extend>
     </Box>
   );
 };
