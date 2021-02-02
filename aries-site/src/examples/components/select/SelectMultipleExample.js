a2021ad7a0d6ba5c416b50713dbc7ef10ae44577
taysea <taylor.seamans@yahoo.com>
2021-02-02 08:05:25 -0800

    Update multiselect example (#1400)

--- aries-site/src/examples/components/select/SelectMultipleExample.js
+++ aries-site/src/examples/components/select/SelectMultipleExample.js
@@ -19,6 +19,7 @@ export const SelectMultipleExample = () => {
           placeholder="Select multiple items"
           options={options}
           value={selected}
+          closeOnChange={false}
           onChange={({ value: nextValue }) => setSelected(nextValue)}
           multiple
         />
