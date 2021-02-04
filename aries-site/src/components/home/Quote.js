7effce4b3ee2f1193e49d8ee11dc2ee10b4cf26a
Mike Kingdom <michael.kingdom@hpe.com>
2021-02-04 23:52:21 +0000

    Added quote background lines and improved hero layout

--- aries-site/src/components/home/Quote.js
+++ aries-site/src/components/home/Quote.js
@@ -1,66 +1,72 @@
 import React from 'react';
 import { Avatar, Box, Image, Paragraph, Stack, Text } from 'grommet';
+import { useDarkMode } from '../../utils';
 
-export const Quote = () => (
-  <Box direction="row" gap="large" justify="center" pad="large">
-    <Box
-      align="center"
-      justify="center"
-      gap="medium"
-      background={{
-        image: 'url(/static/images/quote-lines.svg)',
-        size: 'auto',
-        position: 'top left',
-      }}
-      pad={{ left: 'xlarge' }}
-      flex={false}
-    >
-      <Avatar src="/static/images/t_harms_bio_image.png" size="149px" />
-      <Box align="center">
-        <Text size="xlarge" weight="bold" color="text-strong">
-          Tim Harms
-        </Text>
-        <Text size="xlarge">HPE Head of Brand</Text>
-      </Box>
-    </Box>
-    <Stack>
+export const Quote = () => {
+  const darkMode = useDarkMode();
+
+  const quoteLines = `/static/images/quote-lines${
+    darkMode.value ? '-dark' : ''
+  }.svg`;
+
+  return (
+    <Box direction="row" gap="large" justify="center" pad="large">
       <Box
-        border={{ size: '3px', color: 'text-weak' }}
-        pad="medium"
-        margin="small"
-        width={{ max: '678px' }}
+        align="center"
+        justify="center"
+        gap="medium"
+        background={{
+          image: `url(${quoteLines})`,
+          size: 'auto',
+          position: 'top left',
+        }}
+        pad={{ left: 'xlarge' }}
+        flex={false}
       >
-        <Paragraph size="xxlarge" textAlign="center">
-          The HPE Design System empowers developers, designers and others to
-          create consistent, accessible and flexible interfaces
-        </Paragraph>
+        <Avatar src="/static/images/t_harms_bio_image.png" size="149px" />
+        <Box align="center">
+          <Text size="xlarge" weight="bold" color="text-strong">
+            Tim Harms
+          </Text>
+          <Text size="xlarge">HPE Head of Brand</Text>
+        </Box>
       </Box>
-      <Box fill align="end" justify="end">
-        <Image
-          src="/static/images/quote-lines.svg"
-          margin={{ right: '-138px' }}
-        />
-      </Box>
-      <Box fill justify="between">
-        <Text
-          size="124px"
-          weight="bold"
-          color="brand"
-          style={{ marginLeft: '-15px' }}
+      <Stack>
+        <Box
+          border={{ size: '3px', color: 'text-weak' }}
+          pad="medium"
+          margin="small"
+          width={{ max: '678px' }}
         >
-          &ldquo;
-        </Text>
-        <Text
-          size="124px"
-          color="brand"
-          weight="bold"
-          alignSelf="end"
-          textAlign="end"
-          style={{ marginRight: '-15px' }}
-        >
-          &rdquo;
-        </Text>
-      </Box>
-    </Stack>
-  </Box>
-);
+          <Paragraph size="xxlarge" textAlign="center">
+            The HPE Design System empowers developers, designers and others to
+            create consistent, accessible and flexible interfaces
+          </Paragraph>
+        </Box>
+        <Box fill align="end" justify="end">
+          <Image src={quoteLines} margin={{ right: '-138px' }} />
+        </Box>
+        <Box fill justify="between">
+          <Text
+            size="124px"
+            weight="bold"
+            color="brand"
+            style={{ marginLeft: '-15px' }}
+          >
+            &ldquo;
+          </Text>
+          <Text
+            size="124px"
+            color="brand"
+            weight="bold"
+            alignSelf="end"
+            textAlign="end"
+            style={{ marginRight: '-15px' }}
+          >
+            &rdquo;
+          </Text>
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
