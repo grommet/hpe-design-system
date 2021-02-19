19df63312b72299e63779731e2f575cb13174595
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-02-19 08:57:01 -0700

    remove fragment

--- aries-site/src/examples/components/tooltip/ToolTipDefinitionExample.js
+++ aries-site/src/examples/components/tooltip/ToolTipDefinitionExample.js
@@ -3,23 +3,22 @@ import { Box, Tip, Text } from 'grommet';
 
 export const ToolTipDefinitionExample = () => {
   return (
-    <>
-      <Box align="center" justify="center" fill>
-        <Text size="large">
-          The HPE Design System team is{' '}
-          <Tip
-            content={
-              <Box align="center">
-                <Text>feeling dedication and loyalty to a cause</Text>
-              </Box>
-            }
-          >
-            committed
-          </Tip>{' '}
-          to conducting thorough research so you don't have to think about it.
-          Just find what you need, design and deliver quickly!
-        </Text>
-      </Box>
-    </>
+    <Box gap="small" align="center" justify="center" fill>
+      <Text size="large">
+        The HPE Design System team is{' '}
+        <Tip
+          dropProps={{ align: { bottom: 'top' } }}
+          content={
+            <Box align="center">
+              <Text>feeling dedication and loyalty to a cause</Text>
+            </Box>
+          }
+        >
+          committed
+        </Tip>{' '}
+        to conducting thorough research so you don't have to think about it.
+        Just find what you need, design and deliver quickly!
+      </Text>
+    </Box>
   );
 };
--- aries-site/src/examples/components/tooltip/ToolTipExitExample.js
+++ aries-site/src/examples/components/tooltip/ToolTipExitExample.js
@@ -4,25 +4,23 @@ import { FormClose } from 'grommet-icons';
 
 export const ToolTipExitExample = () => {
   return (
-    <>
-      <Box align="center" justify="center" fill>
-        <Box gap="xsmall" round="xsmall">
-          <Box justify="between" direction="row">
-            <Heading level={4} size="small" margin="none">
-              Add Service
-            </Heading>
-            <Tip dropProps={{ align: { left: 'right' } }} content="Close">
-              <Button icon={<FormClose />} />
-            </Tip>
-          </Box>
-          <Box width="small">
-            <Text>
-              Visit the HPE catalog to start addiing all of your favoriite
-              service.{' '}
-            </Text>
-          </Box>
+    <Box align="center" justify="center" fill>
+      <Box gap="xsmall" round="xsmall">
+        <Box justify="between" direction="row">
+          <Heading level={4} size="small" margin="none">
+            Add Service
+          </Heading>
+          <Tip dropProps={{ align: { left: 'right' } }} content="Close">
+            <Button icon={<FormClose />} />
+          </Tip>
+        </Box>
+        <Box width="small">
+          <Text>
+            Visit the HPE catalog to start addiing all of your favoriite
+            service.{' '}
+          </Text>
         </Box>
       </Box>
-    </>
+    </Box>
   );
 };
