5798fc82be93c4f9e8a137612682090bb523566e
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-02-19 08:57:01 -0700

    remove fragment

--- aries-site/src/examples/components/tooltip/ToolTipIconExample.js
+++ aries-site/src/examples/components/tooltip/ToolTipIconExample.js
@@ -4,12 +4,10 @@ import { Projects } from 'grommet-icons';
 
 export const ToolTipIconExample = () => {
   return (
-    <>
-      <Box align="center" justify="center" fill>
-        <Tip content="Projects">
-          <Button icon={<Projects />} />
-        </Tip>
-      </Box>
-    </>
+    <Box align="center" justify="center" fill>
+      <Tip content="Projects">
+        <Button icon={<Projects />} />
+      </Tip>
+    </Box>
   );
 };
--- aries-site/src/examples/components/tooltip/ToolTipSimpleExample.js
+++ aries-site/src/examples/components/tooltip/ToolTipSimpleExample.js
@@ -3,21 +3,16 @@ import { Box, Button, Tip, Text } from 'grommet';
 
 export const ToolTipSimpleExample = () => {
   return (
-    <>
-      <Box align="center" justify="center" fill>
-        <Tip
-          content={
-            <Box
-              width={{ max: 'small' }}
-              round="xsmall"
-            >
-              <Text>This is a ToolTip</Text>
-            </Box>
-          }
-        >
-          <Button label="Button" />
-        </Tip>
-      </Box>
-    </>
+    <Box align="center" justify="center" fill>
+      <Tip
+        content={
+          <Box width={{ max: 'small' }} round="xsmall">
+            <Text>This is a ToolTip</Text>
+          </Box>
+        }
+      >
+        <Button label="Button" />
+      </Tip>
+    </Box>
   );
 };
