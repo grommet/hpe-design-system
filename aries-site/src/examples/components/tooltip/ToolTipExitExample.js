12b4815fe1ffe2fff554b87247ef6c3ee6f82b0e
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-02-19 14:06:13 -0700

    fix tooltip grammar

--- aries-site/src/examples/components/tooltip/ToolTipExitExample.js
+++ aries-site/src/examples/components/tooltip/ToolTipExitExample.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import { Box, Button, Tip, Text, Heading } from 'grommet';
+import { Box, Button, Tip, Text } from 'grommet';
 import { FormClose } from 'grommet-icons';
 
 export const ToolTipExitExample = () => {
   return (
-      <Box align="center" justify="center" fill>
-        <Box gap="xsmall" round="xsmall">
-          <Box justify="between" direction="row">
-            <Heading level={4} size="small" margin="none">
-              Add Service
-            </Heading>
-            <Tip dropProps={{ align: { left: 'right' } }} content="Close">
-              <Button icon={<FormClose />} />
-            </Tip>
-          </Box>
-          <Box width="small">
-            <Text>
-              Visit the HPE catalog to start addiing all of your favoriite
-              service.
-            </Text>
-          </Box>
+    <Box align="center" justify="center" fill>
+      <Box gap="xsmall" round="xsmall">
+        <Box justify="between" direction="row">
+          <Text bold level={4} size="large">
+            Add Service
+          </Text>
+          <Tip dropProps={{ align: { left: 'right' } }} content="Close">
+            <Button icon={<FormClose />} />
+          </Tip>
+        </Box>
+        <Box width="small">
+          <Text>
+            Visit the HPE catalog to start addiing all of your favoriite
+            service.
+          </Text>
         </Box>
       </Box>
+    </Box>
   );
 };
