15fa30e776b96d26ce0ec94d89761e82926d1961
Taylor Seamans <taylor.seamans@yahoo.com>
2021-02-19 12:13:08 -0800

    Remove local theme definitions, use published theme

--- aries-site/src/themes/aries.js
+++ aries-site/src/themes/aries.js
@@ -6,37 +6,6 @@ export const aries = deepMerge(hpe, {
   // To be stripped out once theme changes are made in grommet-theme-hpe
   // keeping file for use as playground for future theme adjusments that need
   // to be quickly tested
-  button: {
-    size: {
-      small: {
-        border: {
-          radius: '4px',
-        },
-        pad: {
-          vertical: '4px',
-          horizontal: '8px',
-        },
-      },
-      medium: {
-        border: {
-          radius: '4px',
-        },
-        pad: {
-          vertical: '6px',
-          horizontal: '12px',
-        },
-      },
-      large: {
-        border: {
-          radius: '6px',
-        },
-        pad: {
-          vertical: '6px',
-          horizontal: '16px',
-        },
-      },
-    },
-  },
 });
 
 export const { colors } = aries.global;
