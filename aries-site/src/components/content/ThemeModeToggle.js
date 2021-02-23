e0521383d3691ad93ba5877a1fe98b402124c382
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-02-23 09:08:47 -0700

    update card

--- aries-site/src/components/content/ThemeModeToggle.js
+++ aries-site/src/components/content/ThemeModeToggle.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Box, Button, Text, Paragraph } from 'grommet';
+import { Box, Text, Paragraph } from 'grommet';
 import { Moon, Sun } from 'grommet-icons';
 import { useDarkMode } from '../../utils';
 import { ContentPreviewCard } from '../cards';
@@ -10,32 +10,29 @@ export const ThemeModeToggle = ({ active }) => {
   const label = darkMode.value ? 'Light Mode' : 'Dark Mode';
   const icon = darkMode.value ? <Sun size="large" /> : <Moon size="large" />;
   return (
-    <Button
+    <ContentPreviewCard
       id="toggle-button"
       active={active}
       onClick={() => darkMode.toggle()}
+      forwardedAs="a"
+      style={{ textDecoration: 'none' }}
+      pad="medium"
     >
-      <ContentPreviewCard
-        forwardedAs="a"
-        style={{ textDecoration: 'none' }}
-        pad="medium"
+      <Box width="100%" round="xsmall">
+        {icon}
+      </Box>
+      <Text
+        weight="bold"
+        size="xlarge"
+        color="text-strong"
+        margin={{ top: 'small' }}
       >
-        <Box width="100%" round="xsmall">
-          {icon}
-        </Box>
-        <Text
-          weight="bold"
-          size="xlarge"
-          color="text-strong"
-          margin={{ top: 'small' }}
-        >
-          {label}
-        </Text>
-        <Paragraph size="small">
-          View the design system in both light and dark mode!
-        </Paragraph>
-      </ContentPreviewCard>
-    </Button>
+        {label}
+      </Text>
+      <Paragraph size="small">
+        View the design system in both light and dark mode!
+      </Paragraph>
+    </ContentPreviewCard>
   );
 };
 
