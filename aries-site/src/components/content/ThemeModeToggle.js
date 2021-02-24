3ab3fc1043f1bb07ded1d34b1157500c2251635b
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-02-24 11:28:42 -0700

    have toggle have its own section on page

--- aries-site/src/components/content/ThemeModeToggle.js
+++ aries-site/src/components/content/ThemeModeToggle.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'grommet';
+import { Box, Button, Heading, Text } from 'grommet';
 import { Moon, Sun } from 'grommet-icons';
 import { useDarkMode } from '../../utils';
 
 export const ThemeModeToggle = ({ active }) => {
   const darkMode = useDarkMode();
   const label = darkMode.value ? 'Light Mode' : 'Dark Mode';
-  const icon = darkMode.value ? <Sun /> : <Moon />;
+  const icon = darkMode.value ? <Sun size="large" /> : <Moon size="large" />;
   return (
-    <Button
-      active={active}
-      onClick={() => darkMode.toggle()}
-      icon={icon}
-      label={label}
-    />
+    <Box gap="medium">
+      {icon}
+      <Box>
+        <Heading level={2} margin="none" size="xsmall">
+          {label}
+        </Heading>
+        <Text>Have fun switching up themes!</Text>
+      </Box>
+      <Button
+        label={label}
+        active={active}
+        onClick={() => darkMode.toggle()}
+        secondary
+        alignSelf="start"
+      />
+    </Box>
   );
 };
 
