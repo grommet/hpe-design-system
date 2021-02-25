7560b990b4c2f2d06e604534a797133cac2d782b
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-02-25 11:59:26 -0700

    clean up boxes

--- aries-site/src/examples/components/tooltip/ToolTipExitExample.js
+++ aries-site/src/examples/components/tooltip/ToolTipExitExample.js
@@ -4,22 +4,19 @@ import { FormClose } from 'grommet-icons';
 
 export const ToolTipExitExample = () => {
   return (
-    <Box align="start" justify="center" fill>
-      <Box gap="small" direction="row">
-        <Box width="small">
-          <Text color="text-strong" size="large" weight="bold">
-            Add Service
-          </Text>
-          <Text>
-            Visit the HPE catalog to start addiing all of your favoriite
-            services.
-          </Text>
-        </Box>
-        <Box>
-          <Tip content="Close">
-            <Button a11yTitle="close" icon={<FormClose />} />
-          </Tip>
-        </Box>
+    <Box gap="small" direction="row">
+      <Box width="small">
+        <Text color="text-strong" size="large" weight="bold">
+          Add Service
+        </Text>
+        <Text>
+          Visit the HPE catalog to start addiing all of your favoriite services.
+        </Text>
+      </Box>
+      <Box>
+        <Tip content="Close">
+          <Button a11yTitle="close" icon={<FormClose />} />
+        </Tip>
       </Box>
     </Box>
   );
