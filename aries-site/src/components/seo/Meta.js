487810d8d42ac0b7a68b1c904843c51153bdcd4b
Jessica Filben <jcfilben@rams.colostate.edu>
2021-03-10 14:04:37 -0700

    added content secrity policy

--- aries-site/src/components/seo/Meta.js
+++ aries-site/src/components/seo/Meta.js
@@ -89,6 +89,15 @@ export const Meta = ({ title, description, canonicalUrl, socialImageUrl }) => {
       <meta key="segment" name="segment" content={segment} />
       <meta key="lifecycle" name="lifecycle" content={lifecycle} />
       <meta key="page_content" name="page_content" content={pageContent} />
+      <meta httpEquiv="Content-Security-Policy" content="default-src 'self' 'unsafe-eval'; 
+        style-src 'self' *.hpe.com/hfws-static/slim/css/ 'unsafe-inline';
+        connect-src 'self' *.githubusercontent.com/grommet/hpe-design-system/ *.google-analytics.com;
+        media-src 'self' https://d3hq6blov2iije.cloudfront.net/media/HPE+Design+System-v3.mp4;
+        img-src 'self' *.google-analytics.com;
+        script-src-elem 'self' *.hpe.com https://www.google-analytics.com/analytics.js;
+        font-src *.hpe.com hpefonts.s3.amazonaws.com *.cloudfront.net/fonts/;
+        object-src none;"></meta>
+      {/* <meta http-equiv="Content-Security-Policy" content="default-src 'self' https://h50007.www5.hpe.com/hfws-static/js/framework/jquery/v-3-5-1/jquery.js https://h50007.www5.hpe.com/hfws/us/en/hpe/slim/root?contentType=js&color_scheme=dark 'unsafe-inline'; "></meta> */}
     </Head>
   );
 };
