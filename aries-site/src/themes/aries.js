5b24f4ec252e77249de88f0830d5491c01dac5a1
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-03-15 18:21:49 -0600

    add  correct   size to theme

--- aries-site/src/themes/aries.js
+++ aries-site/src/themes/aries.js
@@ -3,6 +3,12 @@ import { deepMerge } from 'grommet/utils';
 
 export const aries = deepMerge(hpe, {
   defaultMode: 'dark',
+  spinner: {
+    size: {
+      small: '36px',
+    },
+  },
+
   // To be stripped out once theme changes are made in grommet-theme-hpe
   // keeping file for use as playground for future theme adjusments that need
   // to be quickly tested
