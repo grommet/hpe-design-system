0680f9ecf138ee3e5eda27292dc1f0e31a026dd7
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-03-22 11:22:08 -0600

    clean up theme

--- aries-site/src/themes/aries.js
+++ aries-site/src/themes/aries.js
@@ -3,27 +3,9 @@ import { deepMerge } from 'grommet/utils';
 
 export const aries = deepMerge(hpe, {
   defaultMode: 'dark',
-  spinner: {
-    container: {
-      border: [
-        { side: 'all', color: 'background-contrast', size: 'medium' },
-        { side: 'right', color: 'green', size: 'medium' },
-        { side: 'top', color: 'green', size: 'medium' },
-        { side: 'left', color: 'green', size: 'medium' },
-      ],
-    },
-  },
-
   // To be stripped out once theme changes are made in grommet-theme-hpe
   // keeping file for use as playground for future theme adjusments that need
   // to be quickly tested
-  pagination: {
-    button: {
-      disabled: {
-        color: 'text-weak',
-      },
-    },
-  },
 });
 
 export const { colors } = aries.global;
