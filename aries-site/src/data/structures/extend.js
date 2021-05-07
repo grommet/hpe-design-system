55a61661d169a8530d341ab3fc690b85976ecc10
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-05-06 20:57:55 -0600

    take out unused pages

--- aries-site/src/data/structures/extend.js
+++ aries-site/src/data/structures/extend.js
@@ -1,36 +1,6 @@
 /* eslint-disable max-len */
 export const extend = [
   {
-    name: 'API Chomp',
-    available: false,
-    description:
-      'Got API, but need to easily pull data out of it for use in your app or designs. Chomp! Chomp!',
-    preview: {
-      image: {
-        src: '/extendImages/extend-preview-chomp.svg',
-        alt: 'HPE API Chomp Preview',
-      },
-    },
-    seoDescription:
-      'Got API, but need to easily pull data out of it for use in your app or designs. Chomp! Chomp!',
-    sections: [],
-  },
-  {
-    name: 'Designer',
-    available: true,
-    description:
-      'Build HPE compliant designs in code that are ready to be consumed on the web.',
-    preview: {
-      image: {
-        src: '/extendImages/extend-preview-designer.svg',
-        alt: 'HPE Designer Preview',
-      },
-    },
-    seoDescription:
-      'Build HPE compliant designs in code that are ready to be consumed on the web.',
-    sections: [],
-  },
-  {
     name: 'Designer Guidance',
     available: true,
     description:
@@ -53,111 +23,7 @@ export const extend = [
       'Before you start desiging',
       'HPE Design System Figma Library',
     ],
-    relatedContent: ['Components', 'Templates', 'Designer'],
-  },
-  {
-    name: 'Global Sidebar',
-    available: false,
-    description:
-      'For use when building applications and services that live in the HPE eccosystem.',
-    preview: {
-      image: {
-        src: {
-          light: '/extendImages/extend-preview-sidebar.svg',
-          dark: '/extendImages/extend-preview-sidebar-invert.svg',
-        },
-        alt: 'HPE Global Sidebar Preview',
-      },
-    },
-    seoDescription:
-      'For use when building applications and services that live in the HPE eccosystem.',
-    sections: [],
-  },
-  {
-    name: 'HPE Audience',
-    available: false,
-    description:
-      'Keep it simple! Present and share slides with this lightweight service.',
-    preview: {
-      image: {
-        src: {
-          light: '/extendImages/extend-preview-slides-audience.svg',
-          dark: '/extendImages/extend-preview-slides-audience-invert.svg',
-        },
-        alt: 'HPE Audience Preview',
-      },
-    },
-    seoDescription:
-      'Keep it simple! Present and share slides with this lightweight service.',
-    sections: [],
-  },
-  {
-    name: 'HPE Docs',
-    available: false,
-    description:
-      'The only CMS you will ever need! Publish content quickly and easily.',
-    preview: {
-      image: {
-        src: {
-          light: '/extendImages/extend-preview-hpe-docs.svg',
-          dark: '/extendImages/extend-preview-hpe-docs-invert.svg',
-        },
-        alt: 'HPE docs Preview',
-      },
-    },
-    seoDescription:
-      'The only CMS you will ever need! Publish content quickly and easily.',
-    sections: [],
-  },
-  {
-    name: 'HPE Images',
-    available: false,
-    description:
-      'Need a place to host images in the cloud for use by your HPE services and apps?',
-    preview: {
-      image: {
-        src: '/extendImages/extend-preview-hpe-images.png',
-        alt: 'HPE Images Preview',
-      },
-    },
-    seoDescription:
-      'Need a place to host images in the cloud for use by your HPE services and apps?',
-    sections: [],
-  },
-  {
-    name: 'React Router',
-    available: false,
-    description:
-      'A collection of navigational components that compose declaratively with your application.',
-    preview: {
-      image: {
-        src: {
-          dark: '/extendImages/extend-preview-router-invert.svg',
-          light: '/extendImages/extend-preview-router.svg',
-        },
-        alt: 'HPE React Router Preview',
-      },
-    },
-    seoDescription:
-      'A collection of navigational components that compose declaratively with your application.',
-  },
-  {
-    name: 'Table Topper',
-    available: false,
-    description:
-      'Build tables for your app quickly with all the filtering, sorting, and controls you could need!',
-    preview: {
-      image: {
-        src: {
-          light: '/extendImages/extend-preview-table.svg',
-          dark: '/extendImages/extend-preview-table-invert.svg',
-        },
-        alt: 'HPE Table Topper Preview',
-      },
-    },
-    seoDescription:
-      'Build tables for you app quickly with all the filtering, sorting, and controls you could need!',
-    sections: [],
+    relatedContent: ['Components', 'Templates'],
   },
   {
     name: 'Roadmap',
