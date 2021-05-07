5fcf94ece537d772e1131eef4bae8159e61dedfc
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-05-07 16:54:32 -0600

    move designer guidence

--- aries-site/src/data/structures/extend.js
+++ aries-site/src/data/structures/extend.js
@@ -1,31 +1,6 @@
 /* eslint-disable max-len */
 export const extend = [
   {
-    name: 'Designer Guidance',
-    available: true,
-    description:
-      'Starter files, patterns, interactions, and workflows on how to succeed using the design resources from HPE Design System and the HPE Brand.',
-    preview: {
-      image: {
-        src: {
-          dark: '/extendImages/extend-preview-designer-guidance-invert.svg',
-          light: '/extendImages/extend-preview-designer-guidance.svg',
-        },
-        alt: 'HPE Designer Guidance Preview',
-      },
-    },
-    seoDescription:
-      'View patterns, interactions, and other best practices for how to succeed using resources included with the HPE Design System.',
-    sections: [
-      'Getting started',
-      'Setting up your Figma account',
-      'Joining the HPE Design System Figma team',
-      'Before you start desiging',
-      'HPE Design System Figma Library',
-    ],
-    relatedContent: ['Components', 'Templates'],
-  },
-  {
     name: 'Roadmap',
     available: true,
     description:
