c228ff1d62ed17376c82fff351139e03267a0fc6
Taylor Seamans <taylor.seamans@yahoo.com>
2021-05-25 12:29:43 -0700

    Remove local theming

--- aries-site/src/themes/aries.js
+++ aries-site/src/themes/aries.js
@@ -3,19 +3,6 @@ import { deepMerge } from 'grommet/utils';
 
 export const aries = deepMerge(hpe, {
   defaultMode: 'dark',
-  button: {
-    hover: {
-      toolbar: {
-        background: 'transparent',
-      },
-    },
-    toolbar: {
-      border: {
-        color: 'border',
-        width: '1px',
-      },
-    },
-  },
   // To be stripped out once theme changes are made in grommet-theme-hpe
   // keeping file for use as playground for future theme adjusments that need
   // to be quickly tested
