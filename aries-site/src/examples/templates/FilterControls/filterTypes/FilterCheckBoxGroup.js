9e240eff50d594e6e2bb433c09b9cb5adb05fb82
Taylor Seamans <taylor.seamans@yahoo.com>
2021-06-02 16:36:18 -0700

    Initial table filter work

--- aries-site/src/examples/templates/FilterControls/filterTypes/FilterCheckBoxGroup.js
+++ aries-site/src/examples/templates/FilterControls/filterTypes/FilterCheckBoxGroup.js
@@ -1,13 +1,36 @@
 import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import { Box, CheckBoxGroup, FormField } from 'grommet';
+import { Box, Button, CheckBoxGroup, FormField } from 'grommet';
 
 import { useFilters } from '..';
 
+// Button that appears like an anchor
+// necessary for accessibility behavior because onClick is used instead of href
+// without href, anchor will not be accessible by keyboard
+const StyledButton = styled(Button)`
+  text-decoration: underline;
+  font-weight: 500;
+  padding: 0;
+
+  &:hover {
+    background-color: transparent;
+  }
+`;
+
 export const FilterCheckBoxGroup = ({ attr }) => {
   const { data, filters, setFilters, getFilterOptions } = useFilters();
-  const { property, label, inputProps, sort = true, convertToString } = attr;
+  const {
+    property,
+    label,
+    inputProps,
+    max = 10,
+    sort = true,
+    convertToString,
+    render,
+  } = attr;
   const [value, setValue] = useState(filters[property]);
+  const [showMore, setShowMore] = useState(false);
   let options = getFilterOptions(data, property);
 
   if (sort)
@@ -17,13 +40,22 @@ export const FilterCheckBoxGroup = ({ attr }) => {
       return 0;
     });
   if (convertToString) options = options.map(option => option.toString());
-
+  if (render) {
+    console.log(
+      options,
+      options.map(option => render(option)),
+    );
+    options = options.map(option => render(option));
+  }
   useEffect(() => {
     setValue(filters[property] || []);
   }, [filters, property, setValue]);
 
   return (
-    <Box flex={false}>
+    <Box
+      flex={false}
+      margin={options.length > max ? { bottom: 'small' } : undefined}
+    >
       <FormField htmlFor={property} name={property} label={label}>
         <CheckBoxGroup
           id={property}
@@ -35,10 +67,18 @@ export const FilterCheckBoxGroup = ({ attr }) => {
             nextFilters[property] = nextValue;
             setFilters(nextFilters);
           }}
-          options={options}
+          options={showMore ? options : options.slice(0, max)}
           {...inputProps}
         />
       </FormField>
+      {options.length > max && (
+        <StyledButton
+          alignSelf="start"
+          label={`Show ${showMore ? 'less' : 'more'}`}
+          size="xsmall"
+          onClick={() => setShowMore(!showMore)}
+        />
+      )}
     </Box>
   );
 };
@@ -47,7 +87,9 @@ FilterCheckBoxGroup.propTypes = {
   attr: PropTypes.shape({
     property: PropTypes.string,
     label: PropTypes.string,
+    max: PropTypes.number,
     inputProps: PropTypes.object,
+    render: PropTypes.func,
     sort: PropTypes.bool,
     convertToString: PropTypes.bool,
   }),
