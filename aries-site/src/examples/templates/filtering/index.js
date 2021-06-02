f9b5b8ab9d68944bd842eeb127ec40c9a04749ef
Taylor Seamans <taylor.seamans@yahoo.com>
2021-06-02 16:36:18 -0700

    Initial table filter work

--- aries-site/src/examples/templates/filtering/index.js
+++ aries-site/src/examples/templates/filtering/index.js
@@ -1,5 +1,6 @@
 export * from './FilteringCards/index';
 export * from './FilteringLists';
+export * from './FilteringTable/index';
 export * from './FilteringWithRangeSelector';
 export * from './FilteringWithDropButton';
 export * from './FilteringWithSelect';
