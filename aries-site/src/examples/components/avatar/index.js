a984d75cd20d1d38ade27b6845c71e48cc387c21
Taylor Seamans <taylor.seamans@yahoo.com>
2021-06-28 10:28:56 -0700

    Add jump to links

--- aries-site/src/examples/components/avatar/index.js
+++ aries-site/src/examples/components/avatar/index.js
@@ -1,9 +1,1 @@
-import React from 'react';
-
-import { Avatar } from 'grommet';
-
-export const AvatarExample = () => {
-  const src = '//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80';
-
-  return <Avatar src={src} />;
-};
+export * from './AvatarExample';
