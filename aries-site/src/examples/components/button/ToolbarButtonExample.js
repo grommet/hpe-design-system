09b3992809b154b13441458c478d4a646b88b6d2
Taylor Seamans <taylor.seamans@yahoo.com>
2021-07-15 14:48:16 -0700

    Use actions prop

--- aries-site/src/examples/components/button/ToolbarButtonExample.js
+++ aries-site/src/examples/components/button/ToolbarButtonExample.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { Box, Menu } from 'grommet';
+import { Menu } from 'grommet';
 import {
   FilterControls,
   FiltersProvider,
 } from '../../templates/FilterControls';
 
 export const ToolbarButtonExample = () => (
-  <Box align="start" direction="row" justify="between" wrap>
-    <FiltersProvider>
-      <FilterControls
-        data={[]}
-        filters={[]}
-        searchFilter={{ placeholder: 'Search users...' }}
-      />
-    </FiltersProvider>
-    <Menu label="Actions" kind="toolbar" />
-  </Box>
+  <FiltersProvider>
+    <FilterControls
+      actions={<Menu label="Actions" kind="toolbar" />}
+      data={[]}
+      filters={[]}
+      searchFilter={{ placeholder: 'Search users...' }}
+    />
+  </FiltersProvider>
 );
