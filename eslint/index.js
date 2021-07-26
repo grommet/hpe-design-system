3e820986f1d651beb80eceed3217a85b03901e77
Taylor Seamans <taylor.seamans@yahoo.com>
2021-07-26 16:10:16 -0700

    Add new files

---
+++ eslint/index.js
@@ -0,0 +1,24 @@
+module.exports = {
+  rules: {
+    'alt-text': {
+      create(context) {
+        return {
+          JSXElement(node) {
+            if (node?.openingElement?.name?.name === 'Image') {
+              let alt = false;
+              node.openingElement.attributes.forEach(attribute => {
+                if (attribute?.name?.name === 'alt') alt = true;
+              });
+              if (!alt)
+                context.report(
+                  node,
+                  `Image requires alt text that is descriptive about what the 
+                  image contains.`,
+                );
+            }
+          },
+        };
+      },
+    },
+  },
+};
