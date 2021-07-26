b04d7bffc7421e128ce30a4586f9807992f98ec3
Matt Glissmann <mdglissmann@gmail.com>
2021-07-26 13:53:59 -0600

    update form example headings

--- aries-site/src/examples/templates/forms/ChangePasswordExample.js
+++ aries-site/src/examples/templates/forms/ChangePasswordExample.js
@@ -5,8 +5,8 @@ import {
   Form,
   FormField,
   Header,
+  Heading,
   ResponsiveContext,
-  Text,
   TextInput,
 } from 'grommet';
 
@@ -50,9 +50,14 @@ export const ChangePasswordExample = () => {
         gap="xxsmall"
         pad={{ horizontal: 'xxsmall' }}
       >
-        <Text size="xxlarge" weight="bold">
+        {/* Use semantically correct heading level and adjust size as 
+        needed. In this instance, this example is presented within an 
+        HTML section element and this is the first heading within the 
+        section, therefor h2 is the semantically correct heading. For 
+        additional detail, see https://design-system.hpe.design/foundation/typography#semantic-usage-of-heading-levels). */}
+        <Heading level={2} margin="none">
           Change Password
-        </Text>
+        </Heading>
       </Header>
       <Box
         // Padding used to prevent focus from being cutoff
--- aries-site/src/examples/templates/forms/SignInExample.js
+++ aries-site/src/examples/templates/forms/SignInExample.js
@@ -8,6 +8,7 @@ import {
   Form,
   FormField,
   Header,
+  Heading,
   Layer,
   ResponsiveContext,
   Text,
@@ -136,9 +137,14 @@ export const SignInExample = () => {
         gap="xxsmall"
         pad={{ horizontal: 'xxsmall' }}
       >
-        <Text size="xxlarge" weight="bold">
+        {/* Use semantically correct heading level and adjust size as 
+        needed. In this instance, this example is presented within an 
+        HTML section element and this is the first heading within the 
+        section, therefor h2 is the semantically correct heading. For 
+        additional detail, see https://design-system.hpe.design/foundation/typography#semantic-usage-of-heading-levels). */}
+        <Heading level={2} margin="none">
           Sign In
-        </Text>
+        </Heading>
         <Text>to Hewlett Packard Enterprise</Text>
       </Header>
       <Box
