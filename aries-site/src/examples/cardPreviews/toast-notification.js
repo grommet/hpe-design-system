b7c5b82b5fbaea0d3f5bfa067c9f239a70d73b8b
amandadupell <anddupell@gmail.com>
2021-08-12 13:27:02 -0400

    moving some content around

--- aries-site/src/examples/cardPreviews/toast-notification.js
+++ aries-site/src/examples/cardPreviews/toast-notification.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { StatusGoodSmall } from 'grommet-icons/icons/StatusGoodSmall';
 import { FormClose } from 'grommet-icons/icons/FormClose';
 
-import { Notification } from '../../examples';
-import { Box, Button, Text, Diagram, Stack } from 'grommet';
+import { Box, Button, Text } from 'grommet';
 
 export const ToastPreview = () => (
     <Box align="center" justify="center" fill background="background-back">
@@ -42,65 +41,3 @@ export const ToastPreview = () => (
       </Box>
     </Box>
   );
-
-const color = 'black';
-const anchor = 'vertical';
-const thickness = 'xxxsmall';
-const type = 'rectilinear';
-
-const connections = [
-    {
-        anchor,
-        type,
-        color,
-        thickness,
-        fromTarget: '1',
-        toTarget: 'status-indicator',
-    },
-    {
-        anchor,
-        type,
-        color,
-        thickness,
-        fromTarget: '2',
-        toTarget: 'content',
-    },
-    {
-        anchor,
-        type,
-        color,
-        thickness,
-        fromTarget: '3',
-        toTarget: 'close-button',
-    },
-];
-
-export const ToastDiagram = () => (
-    <Stack>
-      <Box gap="medium" pad={{bottom: 'medium'}}>
-        <Box direction="row" gap="xlarge">
-          <Box 
-            id="1" 
-            border 
-            round="full" 
-            pad={{ horizontal: 'small', vertical: 'xsmall' }}
-          >1</Box>
-          <Box 
-            id="2" 
-            border 
-            round="full" 
-            pad={{ horizontal: 'small', vertical: 'xsmall' }}
-          >2</Box>
-          <Box 
-            id="3" 
-            border 
-            round="full" 
-            pad={{ horizontal: 'small', vertical: 'xsmall' }}
-          >3</Box>
-        </Box>
-        <Notification title="Status Title" message="This is a status message" onClose />
-      </Box>
-      <Diagram connections={connections} />
-    </Stack>
-);
-
