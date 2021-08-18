7259cfc3bab4dabd47d65d5216e091700860117a
britt6612 <brittany.archibeque@hpe.com>
2021-08-18 12:00:33 -0600

    take out from aries

--- aries-site/src/themes/aries.js
+++ aries-site/src/themes/aries.js
@@ -3,40 +3,6 @@ import { deepMerge } from 'grommet/utils';
 
 export const aries = deepMerge(hpe, {
   defaultMode: 'dark',
-  dataTable: {
-    pinned: {
-      header: {
-        extend: ({ theme }) => `
-        backdrop-filter: blur(8px);
-        @-moz-document url-prefix() {
-        background: ${
-          theme.dark ? 'rgba(64, 75, 92, 0.95)' : 'rgba(255, 255, 255, 0.95)'
-        };
-        }
-          `,
-      },
-      body: {
-        extend: ({ theme }) => `
-        backdrop-filter: blur(8px);
-        @-moz-document url-prefix() {
-        background: ${
-          theme.dark ? 'rgba(64, 75, 92, 0.95)' : 'rgba(255, 255, 255, 0.95)'
-        };
-        }
-          `,
-      },
-      footer: {
-        extend: ({ theme }) => `
-        backdrop-filter: blur(8px);
-        @-moz-document url-prefix() {
-        background: ${
-          theme.dark ? 'rgba(64, 75, 92, 0.95)' : 'rgba(255, 255, 255, 0.95)'
-        };
-        }
-          `,
-      },
-    },
-  },
   // To be stripped out once theme changes are made in grommet-theme-hpe
   // keeping file for use as playground for future theme adjusments that need
   // to be quickly tested
