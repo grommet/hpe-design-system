3cdeef9f9c1fc31bd54c09342805cea279108348
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-09-08 17:41:26 -0600

    add theme changes to see snapshot changes

--- aries-site/src/themes/aries.js
+++ aries-site/src/themes/aries.js
@@ -3,6 +3,14 @@ import { deepMerge } from 'grommet/utils';
 
 export const aries = deepMerge(hpe, {
   defaultMode: 'dark',
+  global: {
+    input: {
+      padding: {
+        horizontal: '11px',
+        vertical: '5px',
+      },
+    },
+  },
   // To be stripped out once theme changes are made in grommet-theme-hpe
   // keeping file for use as playground for future theme adjusments that need
   // to be quickly tested
