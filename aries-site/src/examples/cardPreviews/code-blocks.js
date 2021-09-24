6bb319b925ad8daba9352c39ee41750db8083e7b
Jessica Filben <jessica.filben@gmail.com>
2021-09-24 09:12:47 -0600

    Add links and update yarn.lock and fix exports

--- aries-site/src/examples/cardPreviews/code-blocks.js
+++ aries-site/src/examples/cardPreviews/code-blocks.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import { Box } from 'grommet';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import DarkCodeTheme from '../templates/code-blocks/DarkCodeTheme';
-import LightCodeTheme from '../templates/code-blocks/LightCodeTheme';
+import { DarkCodeTheme } from '../templates/code-blocks/DarkCodeTheme';
+import { LightCodeTheme } from '../templates/code-blocks/LightCodeTheme';
 import { useDarkMode } from '../../utils';
 
 export const CodeBlocksPreview = () => {
