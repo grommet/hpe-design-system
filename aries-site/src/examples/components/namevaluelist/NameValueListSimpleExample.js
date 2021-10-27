44e4a37690653cf7ecfe73101356bb79395ed8b3
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-10-27 12:59:36 -0600

    fix simple example

--- aries-site/src/examples/components/namevaluelist/NameValueListSimpleExample.js
+++ aries-site/src/examples/components/namevaluelist/NameValueListSimpleExample.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Box, NameValueList, NameValuePair } from 'grommet';
+import { Anchor, Box, NameValueList, NameValuePair } from 'grommet';
 
 const data = {
   'Created On': '13/02/2021 at 14:23:32',
-  'Default Storage Class': 'Block Storage Cost'
+  'Created by': (
+    <Anchor label="jane.doe@email.com" href="mailto:jane.doe@email.com" />
+  ),
+  'Default Storage Class': 'Block Storage Cost',
   Description: `Full-service AI insights, security and unified infrastructure 
   management for campus, branch, remote, and data center networks`,
   'IP Address': '172.98.09182.98.00.1.2567',
