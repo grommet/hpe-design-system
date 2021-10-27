369e8d528c2fe15ac58e501ebbc6e0e793537272
amandadupell <anddupell@gmail.com>
2021-10-27 12:09:50 -0400

    addressed prettier spacing and best practice comments

--- aries-site/src/examples/foundation/accessibility/GuideCards.js
+++ aries-site/src/examples/foundation/accessibility/GuideCards.js
@@ -1,35 +1,34 @@
 import React, { useContext } from 'react';
-import { Button, Grid, ResponsiveContext } from 'grommet';
+import { Box, Button, Grid, ResponsiveContext } from 'grommet';
 import { LinkCard } from '../../../components';
+
+const guides = [
+  {
+    title: 'Complex Images in Software',
+    link: 'https://hpe.sharepoint.com/:b:/s/F5/CTO/Office/Accessibility/EYWU1LUDz8xLhVu-omWEYpwB9ZcpjszrA0hMC7vjSx5jiA?e=EctwhC',
+  },
+  {
+    title: 'Links and HyperLinks',
+    link: 'https://hpe.sharepoint.com/:b:/s/F5/CTO/Office/Accessibility/EXN7DvqsNvBEmfKD1ScESIMBBqFCSjtygi79PSQwJ_jWFQ?e=YnAjhe',
+  },
+  {
+    title: 'Evaluation Tools and Toolbars',
+    link: 'https://hpe.sharepoint.com/:b:/s/F5/CTO/Office/Accessibility/EbNhmRbHK6BCsiyLbRRNaE0BQZQlM3D2lVM60cL3nPR67g?e=dgfrFs',
+  },
+  {
+    title: 'Documentation to Inform and Enable ',
+    link: 'https://hpe.sharepoint.com/:b:/s/F5/CTO/Office/Accessibility/EbKbkBd8q2NLkmc2KKz5j_4BnuVCs6voFzhH2KwKuqFFIQ?e=ZcMBfn',
+  },
+];
 
 export const GuideCards = () => {
     const size = useContext(ResponsiveContext);
-
-    const guides = [
-      {
-        title: 'Complex Images in Software',
-        link: 'https://hpe.sharepoint.com/:b:/s/F5/CTO/Office/Accessibility/EYWU1LUDz8xLhVu-omWEYpwB9ZcpjszrA0hMC7vjSx5jiA?e=EctwhC',
-      },
-      {
-        title: 'Links and HyperLinks',
-        link: 'https://hpe.sharepoint.com/:b:/s/F5/CTO/Office/Accessibility/EXN7DvqsNvBEmfKD1ScESIMBBqFCSjtygi79PSQwJ_jWFQ?e=YnAjhe',
-      },
-      {
-        title: 'Evaluation Tools and Toolbars',
-        link: 'https://hpe.sharepoint.com/:b:/s/F5/CTO/Office/Accessibility/EbNhmRbHK6BCsiyLbRRNaE0BQZQlM3D2lVM60cL3nPR67g?e=dgfrFs',
-      },
-      {
-        title: 'Documentation to Inform and Enable ',
-        link: 'https://hpe.sharepoint.com/:b:/s/F5/CTO/Office/Accessibility/EbKbkBd8q2NLkmc2KKz5j_4BnuVCs6voFzhH2KwKuqFFIQ?e=ZcMBfn',
-      },
-    ];
     return (
-      <>
+      <Box gap="large">
         <Grid
           columns={size !== 'small' ? ['medium', 'auto'] : '100%'}
           rows="auto"
           gap="medium"
-          pad={{ bottom: 'large' }}
         >
             {
                 guides.map((item) => 
@@ -50,6 +49,6 @@ export const GuideCards = () => {
           rel="noreferrer noopener"
           secondary
         />
-      </>
+      </Box>
     );
   };
