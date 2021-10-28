2dc50958bfe1218eb81f29a84d2dc0936e3dbb50
Taylor Seamans <taylor.seamans@yahoo.com>
2021-10-28 15:02:26 -0700

    Use forwardedAs

--- aries-site/src/components/cards/LinkCard.js
+++ aries-site/src/components/cards/LinkCard.js
@@ -12,11 +12,7 @@ export const LinkCard = ({ title, link }) => {
 
   return (
     <Wrapper {...wrapperProps}>
-      <Card
-        as="a"
-        target="_blank"
-        style={{ textDecoration: 'none' }}
-      >
+      <Card forwardedAs="a" target="_blank" style={{ textDecoration: 'none' }}>
         <CardBody direction="row" gap="small">
           <Box
             pad="small"
