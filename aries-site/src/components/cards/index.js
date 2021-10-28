f5cd425775742375ea710d0c515ed76b8f83bbcf
Amanda Dupell <38971752+amandadupell@users.noreply.github.com>
2021-10-28 18:58:28 -0400

    Feat/accessibility-guides-1971: added new guides section (#1984)
    * added new guides section
    
    * small change
    
    * small change again
    
    * yarn.lock
    
    * addressed comments from matt
    
    * small spacing fixes
    
    * added PAO link and removed guides from bullet list
    
    * addressed prettier spacing and best practice comments
    
    * small format change
    
    * removed set width on cards and added more responsive grid
    
    * Use forwardedAs
    
    Co-authored-by: Matt Glissmann <mdglissmann@gmail.com>
    Co-authored-by: Taylor Seamans <taylor.seamans@yahoo.com>

--- aries-site/src/components/cards/index.js
+++ aries-site/src/components/cards/index.js
@@ -2,4 +2,5 @@ export * from './CardGrid';
 export * from './ContentCard';
 export * from './ContentPreviewCard';
 export * from './ExampleImagePreview';
+export * from './LinkCard';
 export * from './PreviewCard';
