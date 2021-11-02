e0c669446db32042aefa919990d807fb74d95886
Taylor Seamans <taylor.seamans@yahoo.com>
2021-11-02 14:39:18 -0700

    Logic cleanup

--- aries-site/src/examples/components/namevaluelist/Examples/NameValueListReadMoreExample.js
+++ aries-site/src/examples/components/namevaluelist/Examples/NameValueListReadMoreExample.js
@@ -6,33 +6,21 @@ export const NameValueListReadMoreExample = () => {
   const MAX_LENGTH = 192;
   const [showAll, setShowAll] = useState(false);
 
-  const showMore = () => setShowAll(true);
-  const showLess = () => setShowAll(false);
   return (
     <Box pad="small">
       <NameValueList>
         {Object.entries(contentTruncationData).map(([name, value]) => (
           <NameValuePair key={name} name={name}>
-            {value.length <= MAX_LENGTH && <>{value}</>}
-            {value.length > MAX_LENGTH && showAll && (
+            {value.length <= MAX_LENGTH ? (
+              value
+            ) : (
               <>
-                {value}
+                {!showAll ? `${value.substring(0, MAX_LENGTH)}...` : value}
                 <Button
                   alignSelf="start"
                   size="small"
-                  label="Read less"
-                  onClick={showLess}
-                />
-              </>
-            )}
-            {value.length > MAX_LENGTH && !showAll && (
-              <>
-                {`${value.substring(0, MAX_LENGTH)}...`}
-                <Button
-                  size="small"
-                  label="Read more"
-                  onClick={showMore}
-                  alignSelf="start"
+                  label={`See ${!showAll ? 'more' : 'less'}`}
+                  onClick={() => setShowAll(!showAll)}
                 />
               </>
             )}
