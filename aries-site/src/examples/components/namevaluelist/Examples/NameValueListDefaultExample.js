440aab12f4f1ac1b79327f93cf3f2a7c842687d4
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-11-02 16:13:09 -0600

    format date

--- aries-site/src/examples/components/namevaluelist/Examples/NameValueListDefaultExample.js
+++ aries-site/src/examples/components/namevaluelist/Examples/NameValueListDefaultExample.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { Box, NameValueList, NameValuePair, Text } from 'grommet';
+import { Box, NameValueList, NameValuePair } from 'grommet';
 import { defaultData } from '../data';
 
 export const NameValueListDefaultExample = () => (
   <Box pad="small">
     <NameValueList nameProps={{ width: 'xsmall' }}>
-      {Object.entries(defaultData).map(([name, value]) => (
-        <NameValuePair key={name} name={name}>
-          <Text color="text-strong">{value}</Text>
-        </NameValuePair>
-      ))}
+      {Object.entries(defaultData).map(([name, value]) => {
+        let date;
+        if (name === 'Created on') {
+          const event = new Date(value);
+          date = event.toLocaleDateString();
+        }
+        return (
+          <NameValuePair key={name} name={name}>
+            {name === 'Created on' ? <> {date}</> : <>{value}</>}
+          </NameValuePair>
+        );
+      })}
     </NameValueList>
   </Box>
 );
