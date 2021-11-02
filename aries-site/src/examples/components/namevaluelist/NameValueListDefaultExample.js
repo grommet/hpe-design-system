efb12d15273ff7d7adf4b5fcb2f5e171d77235ad
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-11-01 22:49:34 -0600

    feedback on designs

--- aries-site/src/examples/components/namevaluelist/NameValueListDefaultExample.js
+++ aries-site/src/examples/components/namevaluelist/NameValueListDefaultExample.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import { Box, NameValueList, NameValuePair, Text } from 'grommet';
-import { StatusCriticalSmall, StatusGoodSmall } from 'grommet-icons';
 import { defaultData } from './data';
 
 export const NameValueListDefaultExample = () => (
   <Box pad="small">
     <NameValueList nameProps={{ width: 'xsmall' }}>
-      {Object.entries(defaultData).map(([name, value]) => {
-        let icon;
-        if (value === 'Ok')
-          icon = <StatusGoodSmall color="green" size="small" />;
-        else if (value === 'Critical')
-          icon = <StatusCriticalSmall color="red" size="small" />;
-        return (
-          <NameValuePair key={name} name={name}>
-            <Box align="center" direction="row" gap="xsmall">
-              {icon}
-              <Text color="text-strong">{value}</Text>
-            </Box>
-          </NameValuePair>
-        );
-      })}
+      {Object.entries(defaultData).map(([name, value]) => (
+        <NameValuePair key={name} name={name}>
+          <Text color="text-strong">{value}</Text>
+        </NameValuePair>
+      ))}
     </NameValueList>
   </Box>
 );
