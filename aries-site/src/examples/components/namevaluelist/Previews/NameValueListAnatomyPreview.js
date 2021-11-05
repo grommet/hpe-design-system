48b8efd59bb51eea62dcce336f9a1776ab15679f
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-11-05 14:20:14 -0600

    address fix in previews

--- aries-site/src/examples/components/namevaluelist/Previews/NameValueListAnatomyPreview.js
+++ aries-site/src/examples/components/namevaluelist/Previews/NameValueListAnatomyPreview.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import { Anchor, Box, NameValueList, NameValuePair } from 'grommet';
+import React, { useContext } from 'react';
+import {
+  Anchor,
+  Box,
+  NameValueList,
+  NameValuePair,
+  ResponsiveContext,
+} from 'grommet';
 import { anatomyData } from '../data';
 
-export const NameValueListAnatomyPreview = () => (
-  <Box pad="small">
-    <NameValueList>
-      {Object.entries(anatomyData).map(([name, value]) => (
-        <NameValuePair key={name} name={name}>
-          {name === 'Created by' ? (
-            <Anchor label={value} href={`mailto:${value}`} />
-          ) : (
-            <>{value}</>
-          )}
-        </NameValuePair>
-      ))}
-    </NameValueList>
-  </Box>
-);
+export const NameValueListAnatomyPreview = () => {
+  const size = useContext(ResponsiveContext);
+  return (
+    <Box pad="small">
+      <NameValueList
+        valueProps={size === 'medium' ? { width: 'small' } : undefined}
+      >
+        {Object.entries(anatomyData).map(([name, value]) => (
+          <NameValuePair key={name} name={name}>
+            {name === 'Created by' ? (
+              <Anchor label={value} href={`mailto:${value}`} />
+            ) : (
+              <>{value}</>
+            )}
+          </NameValuePair>
+        ))}
+      </NameValueList>
+    </Box>
+  );
+};
