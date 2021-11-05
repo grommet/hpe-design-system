198d721aa3eabeb4cf603d8c69af8b40256d7d93
Matt Glissmann <mdglissmann@gmail.com>
2021-11-05 14:21:43 -0600

    convert background colors to MDX

--- aries-site/src/examples/foundation/color/ColorPalettes.js
+++ aries-site/src/examples/foundation/color/ColorPalettes.js
@@ -10,6 +10,13 @@ const {
   primaryColors,
 } = colorExamples.palettes;
 
+const {
+  contrastDark,
+  contrastLight,
+  darkBackgrounds,
+  lightBackgrounds,
+} = colorExamples.backgrounds;
+
 const generateColorExamples = (colors, textColor) => (
   <Box fill>
     {colors.map(color => (
@@ -39,3 +46,27 @@ export const DarkPalette = () =>
   darkColors && (
     <UsageExample pad="none">{generateColorExamples(darkColors)}</UsageExample>
   );
+
+export const BackgroundLight = () => (
+  <UsageExample themeMode="light" label="Light Background" pad="none">
+    {lightBackgrounds && generateColorExamples(lightBackgrounds)}
+  </UsageExample>
+);
+
+export const BackgroundDark = () => (
+  <UsageExample themeMode="dark" label="Dark Background" pad="none">
+    {darkBackgrounds && generateColorExamples(darkBackgrounds)}
+  </UsageExample>
+);
+
+export const ContrastLight = () => (
+  <UsageExample themeMode="light" label="Light Background" pad="none">
+    {contrastLight && generateColorExamples(contrastLight)}
+  </UsageExample>
+);
+
+export const ContrastDark = () => (
+  <UsageExample themeMode="dark" label="Dark Background" pad="none">
+    {contrastDark && generateColorExamples(contrastDark)}
+  </UsageExample>
+);
