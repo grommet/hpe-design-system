bbd9f0bac99450b12860dd89b732ae842a0dbb31
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-11-08 08:43:29 -0700

    first pass fix diagram

--- aries-site/src/examples/components/namevaluelist/NameValueListAnatomy.js
+++ aries-site/src/examples/components/namevaluelist/NameValueListAnatomy.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Box, Text, Diagram, Stack } from 'grommet';
+import { Box, Grid, Text, Diagram, Stack } from 'grommet';
 
 const color = 'text-strong';
 const anchor = 'vertical';
@@ -47,14 +47,14 @@ const AnatomyBox = ({ background, id, visualId }) => (
     <Box
       id={visualId}
       height="xxsmall"
-      width="xxsmall"
+      width="xsmall"
       background={background}
       round="xxsmall"
     />
     <Box
       id={id}
       height="xxsmall"
-      width="small"
+      width="medium"
       background={background}
       round="xxsmall"
     />
@@ -64,9 +64,12 @@ const AnatomyBox = ({ background, id, visualId }) => (
 const CircleBox = ({ id, target }) => (
   <Box
     id={id}
+    alignSelf="center"
     align="center"
-    pad={{ horizontal: 'small', vertical: 'xxsmall' }}
+    justify="center"
     round
+    height="xxsmall"
+    width="xxsmall"
     background="background-front"
   >
     <Text weight="bold">{target}</Text>
@@ -74,34 +77,20 @@ const CircleBox = ({ id, target }) => (
 );
 
 export const NameValueListAnatomy = () => (
-  <Stack>
-    <Box gap="medium" pad={{ bottom: 'medium' }}>
-      <Box
-        margin={{ left: 'xlarge' }}
-        justify="between"
-        gap="large"
-        direction="row"
-        width="medium"
-      >
+  <Stack alignSelf="start" margin="small">
+    <Box alignSelf="start" gap="medium">
+      <Grid gap="medium" justify="center" columns={{ count: 2, size: 'auto' }}>
         <CircleBox id={1} target="1" />
         <CircleBox id={2} target="2" />
-      </Box>
-      <Box gap="medium" direction="row">
         <AnatomyBox
           id="name"
           background="status-warning"
           visualId="nameVisual"
         />
         <AnatomyBox id="value" background="purple!" visualId="valueVisual" />
-      </Box>
-      <Box
-        justify="between"
-        width="medium"
-        direction="row"
-      >
         <CircleBox id={3} target="3" />
         <CircleBox id={4} target="3" />
-      </Box>
+      </Grid>
     </Box>
     <Diagram connections={connections} />
   </Stack>
@@ -117,3 +106,25 @@ CircleBox.propTypes = {
   id: PropTypes.number,
   target: PropTypes.string,
 };
+
+/* <Stack alignSelf="start" margin="small">
+<Box alignSelf="start" gap="medium">
+  <Grid gap="medium" justify="center" columns={{ count: 2, size: 'auto' }}>
+    <CircleBox id={1} target="1" />
+    <CircleBox id={2} target="2" />
+  </Grid>
+  <Grid gap="medium" justify="center" columns={{ count: 2, size: 'auto' }}>
+    <AnatomyBox
+      id="name"
+      background="status-warning"
+      visualId="nameVisual"
+    />
+    <AnatomyBox id="value" background="purple!" visualId="valueVisual" />
+  </Grid>
+  <Grid justify="start" columns={{ count: 2, size: 'auto' }}>
+    <CircleBox id={3} target="3" />
+    <CircleBox id={4} target="3" />
+  </Grid>
+</Box>
+<Diagram connections={connections} />
+</Stack> */
