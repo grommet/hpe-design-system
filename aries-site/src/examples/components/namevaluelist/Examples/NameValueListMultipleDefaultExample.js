d5d8467a6f47cb087d61baff53cd91eec1599a61
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-11-10 15:14:31 -0700

    revert back to way it was

--- aries-site/src/examples/components/namevaluelist/Examples/NameValueListMultipleDefaultExample.js
+++ aries-site/src/examples/components/namevaluelist/Examples/NameValueListMultipleDefaultExample.js
@@ -13,45 +13,43 @@ export const NameValueListMultipleDefaultExample = () => {
       </Heading>
       <Tabs activeIndex={index} onActive={onActive} justify="start">
         <Tab title="Overview">
-          <Box gap="large">
-            <Heading level={2} size="small">
-              Details
-            </Heading>
-            <NameValueList>
-              {Object.entries(serverData).map(([name, value]) => (
-                <NameValuePair key={name} name={name}>
-                  {name === 'Health' ? (
-                    <Box align="center" gap="xsmall" direction="row">
-                      <StatusGoodSmall size="small" color="status-ok" />
-                      {value}
-                    </Box>
-                  ) : (
-                    <> {value}</>
-                  )}
-                </NameValuePair>
-              ))}
-            </NameValueList>
-            <Heading level={2} size="small">
-              Kubernetes
-            </Heading>
-            <NameValueList>
-              {Object.entries(kubernetesData).map(([name, value]) => (
-                <NameValuePair key={name} name={name}>
-                  {value}
-                </NameValuePair>
-              ))}
-            </NameValueList>
-            <Heading level={2} size="small">
-              Tags
-            </Heading>
-            <NameValueList>
-              {Object.entries(tagsData).map(([name, value]) => (
-                <NameValuePair key={name} name={name}>
-                  {value}
-                </NameValuePair>
-              ))}
-            </NameValueList>
-          </Box>
+          <Heading level={2} size="small">
+            Details
+          </Heading>
+          <NameValueList>
+            {Object.entries(serverData).map(([name, value]) => (
+              <NameValuePair key={name} name={name}>
+                {name === 'Health' ? (
+                  <Box align="center" gap="xsmall" direction="row">
+                    <StatusGoodSmall size="small" color="status-ok" />
+                    {value}
+                  </Box>
+                ) : (
+                  <> {value}</>
+                )}
+              </NameValuePair>
+            ))}
+          </NameValueList>
+          <Heading level={2} size="small">
+            Kubernetes
+          </Heading>
+          <NameValueList>
+            {Object.entries(kubernetesData).map(([name, value]) => (
+              <NameValuePair key={name} name={name}>
+                {value}
+              </NameValuePair>
+            ))}
+          </NameValueList>
+          <Heading level={2} size="small">
+            Tags
+          </Heading>
+          <NameValueList>
+            {Object.entries(tagsData).map(([name, value]) => (
+              <NameValuePair key={name} name={name}>
+                {value}
+              </NameValuePair>
+            ))}
+          </NameValueList>
         </Tab>
         <Tab title="Policies">
           <Box margin="small">Policies Information</Box>
