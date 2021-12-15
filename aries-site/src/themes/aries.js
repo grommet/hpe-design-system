0c643f90800c8100e4149fee0fa41a735ac6782b
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2021-12-15 14:11:05 -0700

    add button hover theme changes for review

--- aries-site/src/themes/aries.js
+++ aries-site/src/themes/aries.js
@@ -2,6 +2,35 @@ import { hpe } from 'grommet-theme-hpe';
 import { deepMerge } from 'grommet/utils';
 
 export const aries = deepMerge(hpe, {
+  button: {
+    hover: {
+      primary: {
+        // Designs call for using text-strong for the text color.
+        // Keeping the label color the same on hover to maintain readablility.
+        color: hpe.global.colors['text-strong'].light,
+        background: {
+          dark: 'validation-ok',
+          // Designs call for using a lower opacity of exisiting green!
+          // instead of creating a new color name for this single usecase.
+          light: {
+            color: 'green!',
+            opacity: 0.75,
+          },
+        },
+        extend: ({ colorValue, primary, theme }) => `
+      ${primary &&
+        colorValue &&
+        `:hover {
+        background-color: ${
+          theme.dark
+            ? theme.global.colors[colorValue].dark
+            : theme.global.colors[colorValue].light
+        };
+      }`}
+    `,
+      },
+    },
+  },
   defaultMode: 'dark',
   // To be stripped out once theme changes are made in grommet-theme-hpe
   // keeping file for use as playground for future theme adjusments that need
