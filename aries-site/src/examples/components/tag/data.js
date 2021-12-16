dd2f1059e297bcf4f4c20f398b6daabcc380d9be
Taylor Seamans <taylor.seamans@yahoo.com>
2021-12-16 11:27:09 -0800

    Separate data and add code links

---
+++ aries-site/src/examples/components/tag/data.js
@@ -0,0 +1,95 @@
+export const details = {
+  name: {
+    displayName: 'Name',
+    value: 'store-C-3PO',
+  },
+  model: {
+    displayName: 'Model',
+    value: 'Aruba 530 32930F',
+  },
+  type: {
+    displayName: 'Type',
+    value: 'Gateway',
+  },
+  partNumber: {
+    displayName: 'Part Number',
+    value: 'JL258A',
+  },
+  serialNumber: {
+    displayName: 'Serial Number',
+    value: '381503203862',
+  },
+  macAddress: {
+    displayName: 'MAC Address',
+    value: '38:17:C7:B8:90210',
+  },
+  ipAddress: {
+    displayName: 'IP Address',
+    value: '172.16.255.321.8',
+  },
+  location: {
+    displayName: 'Location',
+    value: 'Atlanta, Georgia',
+  },
+  firmwareVersion: {
+    displayName: 'Firmware Version',
+    value: 'v.2.0',
+  },
+};
+
+export const assignment = {
+  application: {
+    displayName: 'Application',
+    value: 'ClearPass Device Insight',
+  },
+  applicationIstance: {
+    displayName: 'Application Instance',
+    value: 'US-West-1',
+  },
+};
+
+export const subscription = {
+  subscriptionKey: {
+    displayName: 'Subscription Key',
+    value: 'ENXYQZJTPTUSETBY',
+  },
+  expirationDate: {
+    displayName: 'Expiration Date',
+    value: 'July 5, 2022',
+  },
+  subscriptionTier: {
+    displayName: 'Subscription Tier',
+    value: 'Aruba Airwave 25 Device License LTE E-LTU',
+  },
+  warrantyStart: {
+    displayName: 'Warranty Start',
+    value: 'July 5, 2021',
+  },
+  warrantyEnd: {
+    displayName: 'Warranty End',
+    value: 'July 4, 2021',
+  },
+};
+
+export const tags = {
+  department: {
+    displayName: 'Department',
+    value: 'Finance',
+  },
+  timezone: {
+    displayName: 'Timezone',
+    value: 'MT',
+  },
+  location: {
+    displayName: 'Location',
+    value: 'FTC_DC',
+  },
+  environment: {
+    displayName: 'Environment',
+    value: 'Test',
+  },
+  application: {
+    displayName: 'Application',
+    value: 'MyApp',
+  },
+};
--- aries-site/src/examples/components/tag/index.js
+++ aries-site/src/examples/components/tag/index.js
@@ -1,3 +1,4 @@
+export * from './data';
 export * from './TagBasicAnatomy';
 export * from './TagAnatomy';
 export * from './TagResource';
