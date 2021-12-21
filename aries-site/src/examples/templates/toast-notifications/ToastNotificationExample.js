ede5878855a053276a39e3f8af1700033ea1bca5
Taylor Seamans <taylor.seamans@yahoo.com>
2021-12-21 09:40:40 -0800

    "Updates based on discussion with Edgar"

--- aries-site/src/examples/templates/toast-notifications/ToastNotificationExample.js
+++ aries-site/src/examples/templates/toast-notifications/ToastNotificationExample.js
@@ -2,35 +2,30 @@ import React, { useState } from 'react';
 import { Box, Button, Notification, Paragraph } from 'grommet';
 
 export const ToastNotificationExample = () => {
+  const [visible, setVisible] = useState();
 
-    const [visible, setVisible] = useState();
+  const onOpen = () => setVisible(true);
+  const onClose = () => setVisible(undefined);
 
-    const onOpen = () => setVisible(true);
-    const onClose = () => setVisible(undefined);
-
-    return (
-        <>
-            <Box align="center" gap="medium">
-                <Paragraph textAlign="center">
-                    This notification will disappear after 8 seconds
-                    if not dismissed via the close button.
-                </Paragraph>
-                <Button 
-                    label="Show the Toast Notification" 
-                    onClick={onOpen} 
-                    primary 
-                />
-            </Box>
-            <Box align="center" gap="small">
-                {visible && <Notification 
-                    toast
-                    status="normal"
-                    title="Toast success!"
-                    message="The operation was successful"
-                    onClose={onClose}
-                />}
-            </Box>
-        </>
-    );
+  return (
+    <>
+      <Box align="center" gap="medium">
+        <Paragraph textAlign="center">
+          This notification will disappear after 8 seconds if not dismissed via
+          the close button.
+        </Paragraph>
+        <Button label="Show the Toast Notification" onClick={onOpen} primary />
+      </Box>
+      <Box align="center" gap="small">
+        {visible && (
+          <Notification
+            toast
+            status="normal"
+            title='Starting deployment of application "XYZ"'
+            onClose={onClose}
+          />
+        )}
+      </Box>
+    </>
+  );
 };
-
