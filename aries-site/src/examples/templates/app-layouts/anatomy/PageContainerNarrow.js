accad844e004f868d16546d1e94dd695e807bcf8
Matthew Glissmann <mdglissmann@gmail.com>
2021-12-23 11:01:44 -0700

    App Layouts - Add container width diagrams (#2176)
    * Initial App Layouts page scaffolding and content
    
    * incorporate copy suggestions
    
    * add app container and page container diagrams
    
    * fix link
    
    * remove extraneous caption
    
    * add wide container diagram
    
    * add narrow container diagram
    
    * add full container diagram
    
    * fix contrast WAVE errors

---
+++ aries-site/src/examples/templates/app-layouts/anatomy/PageContainerNarrow.js
@@ -0,0 +1,95 @@
+import { useContext } from 'react';
+import { Box, Diagram, Stack, Text, ThemeContext } from 'grommet';
+import { LinkPrevious, LinkNext } from 'grommet-icons';
+
+import { ContentArea } from './components';
+
+const connections = [
+  {
+    fromTarget: 'narrow-left-edge',
+    toTarget: 'narrow-container',
+    anchor: 'horizontal',
+    color: 'text-strong',
+  },
+  {
+    fromTarget: 'narrow-right-edge',
+    toTarget: 'narrow-container',
+    anchor: 'horizontal',
+    color: 'text-strong',
+  },
+];
+
+const PAGE_CONTAINER_WIDTH = '768px';
+const PAGE_CONTAINER_SCALE = '50%';
+
+export const PageContainerNarrow = () => {
+  const theme = useContext(ThemeContext);
+  const diagramHeight = theme.global.size.medium;
+  const diagramWidth = `${(diagramHeight.replace('px', '') * 4) / 3}px`;
+  const annotationMargin = `${(100 - PAGE_CONTAINER_SCALE.replace('%', '')) /
+    2}%`;
+
+  const widthAnnnotation = (
+    <Box
+      direction="row"
+      align="center"
+      justify="between"
+      fill="vertical"
+      margin={{ horizontal: annotationMargin }}
+      pad={{ top: 'xlarge' }}
+    >
+      <LinkPrevious id="narrow-left-edge" color="text-strong" />
+      <Box
+        id="narrow-container"
+        pad={{ horizontal: 'xsmall' }}
+        background="orange"
+      >
+        <Text color="text-strong" weight="bold">
+          {PAGE_CONTAINER_WIDTH}
+        </Text>
+      </Box>
+      <LinkNext id="narrow-right-edge" color="text-strong" />
+    </Box>
+  );
+
+  return (
+    <Stack>
+      <ContentArea
+        title="App Container"
+        border
+        flex={false}
+        gap="small"
+        round="xsmall"
+        width={diagramWidth}
+      >
+        <ContentArea title="Global Header" background="green" flex={false} />
+        <Stack>
+          <Box>
+            <ContentArea
+              title="Page Container"
+              alignSelf="center"
+              border
+              flex={false}
+              gap="small"
+              width={PAGE_CONTAINER_SCALE}
+            >
+              <ContentArea
+                title="Page Header"
+                background="purple!"
+                flex={false}
+              />
+              <ContentArea
+                title="Page Content"
+                background="orange"
+                height="small"
+              />
+            </ContentArea>
+          </Box>
+          {widthAnnnotation}
+        </Stack>
+        <ContentArea title="Global Footer" background="green" flex={false} />
+      </ContentArea>
+      <Diagram connections={connections} />
+    </Stack>
+  );
+};
