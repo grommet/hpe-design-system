11db024beac683f91a904cc9e4b6852766e4f4e3
Taylor Seamans <taylor.seamans@yahoo.com>
2022-01-10 10:20:00 -0800

    Remove sidebar examples and references to sidebar

--- aries-site/src/examples/cardPreviews/page-layouts/index.js
+++ aries-site/src/examples/cardPreviews/page-layouts/index.js
@@ -1,5 +1,3 @@
-export * from './SidebarHeaderFooterPreview';
 export * from './HeaderFooterPreview';
 export * from './HeaderOnlyPreview';
-export * from './SidebarHeaderPreview';
 export * from './StickyHeaderPreview';
