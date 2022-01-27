16a8d0478219c00a150cff9f91b86feaf01cd7cf
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2022-01-27 11:12:15 -0700

    give new caption

--- aries-site/src/layouts/content/Example/Example.js
+++ aries-site/src/layouts/content/Example/Example.js
@@ -279,7 +279,7 @@ Example.propTypes = {
     type: PropTypes.oneOf(['do', 'dont']).isRequired,
     message: PropTypes.string,
   }),
-  caption: PropTypes.string,
+  caption: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
   children: PropTypes.element,
   code: PropTypes.oneOfType([
     PropTypes.string,
