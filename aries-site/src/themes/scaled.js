0d6ebdbe789e44e0dc5d50be09730dc918c67ff6
Matt Glissmann <mdglissmann@gmail.com>
2022-01-28 09:58:41 -0700

    clean up naming

--- aries-site/src/themes/scaled.js
+++ aries-site/src/themes/scaled.js
@@ -3,23 +3,26 @@ import { deepMerge } from 'grommet/utils';
 import { aries } from './aries';
 
 export const scaled = (theme = aries, scale = 0.5) => {
-  const nextTheme = deepMerge(base, theme);
-  const output = { ...nextTheme };
+  const source = deepMerge(base, theme);
+  const scaledTheme = { ...source };
   const scaleAttributes = [
-    output.global.breakpoints,
-    output.global.edgeSize,
-    output.global.size,
+    source.global.breakpoints,
+    source.global.edgeSize,
+    source.global.size,
   ];
 
   scaleAttributes.forEach(attr => {
     Object.keys(attr).forEach(key => {
       if (attr[key].value) {
+        /* eslint-disable no-param-reassign */
+        // eslint-disable-next-line operator-assignment
         attr[key].value = attr[key].value * scale;
       } else if (typeof attr[key] === 'string' && attr[key].includes('px')) {
         attr[key] = `${Math.ceil(parseInt(attr[key], 10) * scale)}px`;
+        /* eslint-enable no-param-reassign */
       }
     });
   });
 
-  return output;
+  return scaledTheme;
 };
