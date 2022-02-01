c73511e90b5b4e88080ec6a1462476d46e9097b1
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2022-02-01 12:01:31 -0700

    add button alignment

---
+++ aries-site/src/examples/components/button/ButtonLeftAlignExample.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Box, Button, Header, Text } from 'grommet';
+
+export const ButtonLeftAlignExample = () => (
+  <Box width="large" gap="medium" pad="small">
+    <Header
+      direction="column"
+      align="start"
+      gap="xsmall"
+      pad={{ horizontal: 'xxsmall' }}
+    >
+      <Text weight="bold" size="large">Left-aligned buttons</Text>
+    </Header>
+    <Box direction="row" gap="medium">
+      <Button primary label="Primary" onClick={() => {}} />
+      <Button secondary label="Secondary" onClick={() => {}} />
+      <Button label="Default" onClick={() => {}} />
+    </Box>
+  </Box>
+);
