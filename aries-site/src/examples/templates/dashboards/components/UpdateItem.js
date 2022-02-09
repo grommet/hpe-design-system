e1019e1ce3f854a1ed660fdef7b7c164991a9b39
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2022-02-09 09:05:01 -0700

    add truncation guidance

--- aries-site/src/examples/templates/dashboards/components/UpdateItem.js
+++ aries-site/src/examples/templates/dashboards/components/UpdateItem.js
@@ -1,33 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Anchor, Box, Button, Text } from 'grommet';
-import { FormDown, FormUp } from 'grommet-icons';
+import { Anchor, Box, Paragraph, Text } from 'grommet';
 
 export const UpdateItem = ({ title, description }) => {
-  // HPE Design System guidance for string length prior to truncation.
-  const MAX_STRING_LENGTH = 192;
+  // HPE Design System guidance for string length
+  // when presenting content within a Card.
+  // https://design-system.hpe.design/components/card#general
+  const MAX_STRING_LENGTH = 100;
 
-  const [showAllDescription, setShowAllDescription] = useState(false);
   return (
     <Box>
       <Text weight="bold">{`${title} is available.`}</Text>
       <Box gap="xsmall">
         {description.length <= MAX_STRING_LENGTH ? (
-          <Text>{description}</Text>
+          <Paragraph size="small" margin="none">
+            {description}
+          </Paragraph>
         ) : (
           <>
-            {!showAllDescription ? (
-              <Paragraph>{`${description.substring(
-                0,
-                MAX_STRING_LENGTH,
-              )} ...`}</Text>
-            ) : (
-              <Text>{description}</Text>
-            )}
-            <TruncateButton
-              showAll={showAllDescription}
-              setShowAll={setShowAllDescription}
-            />
+            <Paragraph size="small" margin="none">{`${description.substring(
+              0,
+              MAX_STRING_LENGTH,
+            )}...`}</Paragraph>
           </>
         )}
         <Anchor
@@ -51,20 +45,4 @@ export const UpdateItem = ({ title, description }) => {
 UpdateItem.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
-};
-
-const TruncateButton = ({ showAll, setShowAll }) => (
-  <Button
-    alignSelf="start"
-    size="small"
-    label={`Show ${!showAll ? 'all' : 'less'}`}
-    onClick={() => setShowAll(!showAll)}
-    icon={!showAll ? <FormDown /> : <FormUp />}
-    reverse
-  />
-);
-
-TruncateButton.propTypes = {
-  showAll: PropTypes.bool,
-  setShowAll: PropTypes.func,
 };
