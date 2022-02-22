16e2a8b985c18a92755d3e5acca71db244a83cc9
Taylor Seamans <taylor.seamans@yahoo.com>
2022-02-22 15:32:08 -0800

    Add placeholder for four column dashboard

---
+++ aries-site/src/examples/templates/dashboards/FourColumnDashboard.js
@@ -0,0 +1,62 @@
+import { useContext } from 'react';
+import { Grid, Main, ResponsiveContext } from 'grommet';
+import {
+  AppContainer,
+  PageContainer,
+  PageContainerContext,
+} from '../page-layouts/components';
+import { ContentArea } from '../page-layouts/anatomy/components';
+import { PageHeader } from './components';
+
+export const FourColumnDashboard = () => (
+  <AppContainer background="background-back">
+    <ContentArea title="Global Header" />
+    <PageContainer>
+      <PageHeader title="Dashboard" />
+      <PageContent />
+    </PageContainer>
+    <ContentArea title="Global Footer" />
+  </AppContainer>
+);
+
+const parentGrid = {
+  columns: {
+    xsmall: ['100%'],
+    small: ['100%'],
+    medium: [
+      ['small', 'large'],
+      ['small', 'large'],
+    ],
+    large: [
+      ['small', 'medium'],
+      ['small', 'medium'],
+      ['small', 'medium'],
+    ],
+    xlarge: { count: 4, size: ['small', 'auto'] },
+  },
+  gap: {
+    xsmall: 'large',
+    small: 'large',
+    medium: 'medium',
+    large: 'large',
+    xlarge: 'large',
+  },
+};
+
+const PageContent = () => {
+  const size = useContext(ResponsiveContext);
+  const { ...pageContainer } = useContext(PageContainerContext);
+
+  return (
+    <Main {...pageContainer}>
+      <Grid columns={parentGrid.columns[size]} gap={parentGrid.gap[size]}>
+        <ContentArea title={1} height="medium" />
+        <ContentArea title={2} height="medium" />
+        <ContentArea title={3} height="medium" />
+        <ContentArea title={4} height="medium" />
+        <ContentArea title={5} height="medium" />
+        <ContentArea title={6} height="medium" />
+      </Grid>
+    </Main>
+  );
+};
