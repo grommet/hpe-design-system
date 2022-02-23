9d8a9d9d629bef0b304e73bc245b80a44bbde8c2
Taylor Seamans <taylor.seamans@yahoo.com>
2022-02-23 12:59:49 -0800

    Add placeholder for four column dashboard (#2380)
    * Add placeholder for four column dashboard
    
    * Use minmax convention
    
    * Adjust gap
    
    * Update yarn.lock

--- aries-site/src/examples/templates/dashboards/index.js
+++ aries-site/src/examples/templates/dashboards/index.js
@@ -1,5 +1,6 @@
 export * from './components';
 export * from './content';
 export * from './DashboardExample';
+export * from './FourColumnDashboard';
 export * from './ThreeColumnDashboard';
 export * from './TwoColumnDashboard';
