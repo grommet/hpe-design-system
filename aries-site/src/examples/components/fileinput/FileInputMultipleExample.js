6c807dde63405b7cbc2458436f5c140811abf90b
Taylor Seamans <taylor.seamans@yahoo.com>
2022-02-25 13:05:06 -0800

    Align FileInput button label to Figma

--- aries-site/src/examples/components/fileinput/FileInputMultipleExample.js
+++ aries-site/src/examples/components/fileinput/FileInputMultipleExample.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, FileInput } from 'grommet';
 
-export const FileInputMultipleExample = () => (
-  <Box width="medium">
-    <FileInput
-      messages={{
-        dropPrompt: 'Drag and drop',
-        browse: 'Select Files',
-      }}
-      onChange={event => {
-        const fileList = event.target.files;
-        for (let i = 0; i < fileList.length; i += 1) {
-          const file = fileList[i];
-          console.log(file.name);
-        }
-      }}
-      multiple
-    />
-  </Box>
-);
+export const FileInputMultipleExample = () => {
+  const [numFiles, setNumFiles] = useState(0);
+
+  return (
+    <Box width="medium">
+      <FileInput
+        messages={{
+          dropPrompt: 'Drag and drop',
+          browse: numFiles > 0 ? 'Select More Files' : 'Select Files',
+        }}
+        onChange={(event, { files }) => {
+          const fileList = files;
+          setNumFiles(fileList.length);
+          for (let i = 0; i < fileList.length; i += 1) {
+            const file = fileList[i];
+            console.log(file.name);
+          }
+        }}
+        multiple
+      />
+    </Box>
+  );
+};
