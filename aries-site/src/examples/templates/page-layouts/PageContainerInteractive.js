d2b2f400f3b0c399da721e8906b5a6dcf9998e10
Matt Glissmann <mdglissmann@gmail.com>
2022-02-28 15:22:41 -0700

    Clean up page container widths demonstration and show code example

--- aries-site/src/examples/templates/page-layouts/PageContainerInteractive.js
+++ aries-site/src/examples/templates/page-layouts/PageContainerInteractive.js
@@ -21,25 +21,24 @@ const demoStyle = {
 
 export const PageContainerInteractive = () => (
   <AppContainer
-    /* `as`, `title`, and `demoStyle` for Design System site demonstration.
-         Remove from your implementation */
+    // `as`, `title`, and `demoStyle` for Design System site demonstration.
+    //  Remove from your implementation.
     as={ContentArea}
     title="AppContainer"
     {...demoStyle}
   >
     {/* ContentArea is specific for the Design System site and is used
-     * as a visual aid to help present layout concepts. Remove from your
-     * implementation.
-     */}
+     as a visual aid to help present layout concepts. Remove from your
+     implementation. */}
     <ContentArea title="Global Header" background="status-unknown" />
+    {/* PageContainer is a context provider which specifies the page's 
+      max-width, margins, and horizontal alignment for it's children. 
+      Possible values for `kind` are wide, narrow, and full. This implementation
+      will be replaced by a Page component in Grommet which provides this 
+      behavior without the need for implementing your own context nor values
+      which will be specified by the HPE theme. 
+      Follow Grommet Page PR: https://github.com/grommet/grommet/pull/5960 */}
     <PageContainer kind="wide" fill>
-      {/* Children of PageContainer should maintain horizontal whitespace on
-       * each side of the child so that the content has room to breathe and
-       * is not tightly condensed against the browser window's edge.
-       *
-       * Spacing is executed as the padding on each child to allow background
-       * colors and scroll regions to behave properly.
-       */}
       <PageChildren />
     </PageContainer>
     <ContentArea title="Global Footer" background="status-unknown" />
@@ -51,34 +50,26 @@ const PageChildren = () => {
 
   return (
     <ContentArea title="PageContainer" {...pageContainer} {...demoStyle}>
-      <PageHeader />
-      <PageContent />
-    </ContentArea>
-  );
-};
-
-const PageHeader = () => {
-  const { ...pageContainer } = useContext(PageContainerContext);
-
-  return (
-    <>
-      <Header {...pageContainer}>
-        {/* Typically, the Header will contain elements such as the page's
-         * h1, any "ascending a.k.a. back to parent" navigation, and/or
-         * page-level menu, action buttons, etc.
-         */}
-        <ContentArea title="Page Header" background="purple!" fill />
+      <Header
+        // `as`, `title`, `background`, and `border` for Design System site
+        //  demonstration. Remove from your implementation.
+        as={ContentArea}
+        title="Page Header"
+        background="purple!"
+        border
+      >
+        {/* Typically, the Header will contain elements such as the page's h1,
+         contextual navigation, and page-level menu, action buttons, etc. */}
       </Header>
-    </>
-  );
-};
-
-const PageContent = () => {
-  const { ...pageContainer } = useContext(PageContainerContext);
-
-  return (
-    <Main {...pageContainer}>
-      <ContentArea title="Page Content" background="orange" border fill />
-    </Main>
+      <Main
+        as={ContentArea}
+        title="Page Content"
+        background="orange"
+        border
+        fill={false}
+      >
+        {/* All of the page's content */}
+      </Main>
+    </ContentArea>
   );
 };
