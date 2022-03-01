6242c9537339cd6eb9dab37ee0c87edb78e34691
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2022-03-01 10:22:01 -0700

    address feedback

--- aries-site/src/examples/components/menu/index.js
+++ aries-site/src/examples/components/menu/index.js
@@ -2,4 +2,3 @@ export * from './MenuDisabledExample';
 export * from './MenuHeaderExample';
 export * from './MenuIconExample';
 export * from './MenuExample';
-export * from './MenuToolBarExample';
