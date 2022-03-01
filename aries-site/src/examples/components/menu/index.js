6f2e7e62bf13ba8e21ddbbab72779077cf7c5655
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2022-03-01 09:58:50 -0700

    add menu example to align with figma

--- aries-site/src/examples/components/menu/index.js
+++ aries-site/src/examples/components/menu/index.js
@@ -2,3 +2,4 @@ export * from './MenuDisabledExample';
 export * from './MenuHeaderExample';
 export * from './MenuIconExample';
 export * from './MenuExample';
+export * from './MenuToolBarExample';
