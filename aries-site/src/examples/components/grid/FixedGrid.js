ae9c302658b72a19970473d15e02eaa27b945d35
Matt Glissmann <mdglissmann@gmail.com>
2022-03-02 17:17:21 -0700

    add fixed grid example

--- aries-site/src/examples/components/grid/FixedGrid.js
+++ aries-site/src/examples/components/grid/FixedGrid.js
@@ -1,13 +1,18 @@
-import { Box, Grid } from 'grommet';
+import { Grid } from 'grommet';
+import { ContentArea } from '../../templates/page-layouts/anatomy/components';
 
-const columns = ['xsmall', 'xxsmall', 'xxsmall'];
+// Three columns with fixed sizes
+const columns = ['xsmall', 'small', 'xxsmall'];
+// Two rows with fixed sizes
 const rows = ['xsmall', 'small'];
 
 export const FixedGrid = () => (
-  <Grid columns={columns} rows={rows}>
-    <Box>mamma</Box>
-    <Box>tiger</Box>
-    <Box>kitty</Box>
-    <Box>kat</Box>
+  <Grid columns={columns} rows={rows} gap="xsmall">
+    {/* By default, Grid children fill grid cells left to right. */}
+    <ContentArea title="1" background="red" border />
+    <ContentArea title="2" background="purple" border />
+    <ContentArea title="3" background="green" border />
+    <ContentArea title="4" background="orange" border />
+    <ContentArea title="5" background="blue" border />
   </Grid>
 );
