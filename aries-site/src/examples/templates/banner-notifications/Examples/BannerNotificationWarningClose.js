515801e8d17bd5a33d5b0720513fa7501f377ef1
Taylor Seamans <taylor.seamans@yahoo.com>
2022-03-09 15:21:20 -0800

    Propose how examples should be broken up

--- aries-site/src/examples/templates/banner-notifications/Examples/BannerNotificationWarningClose.js
+++ aries-site/src/examples/templates/banner-notifications/Examples/BannerNotificationWarningClose.js
@@ -12,5 +12,6 @@ export const BannerNotificationWarningClose = () => (
     status="warning"
     onClose={() => {}}
     message="Your subscription will be ending in 7 days (03/03/2022)."
+    global
   />
 );
