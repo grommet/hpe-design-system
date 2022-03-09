73c62f7cf63308101f6a00c9f8c41081db1758e8
Taylor Seamans <taylor.seamans@yahoo.com>
2022-03-09 15:21:20 -0800

    Propose how examples should be broken up

--- aries-site/src/examples/templates/banner-notifications/Examples/BannerNotificationCritical.js
+++ aries-site/src/examples/templates/banner-notifications/Examples/BannerNotificationCritical.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Box, Notification } from 'grommet';
+import { Notification } from 'grommet';
 
 export const BannerNotificationCritical = () => (
-  <Box width='large'>
-    <Notification
-      status="critical"
-      global
-      message="This service is currently down for maintenance. Check back soon."
-    />
-  </Box>
+  <Notification
+    status="critical"
+    global
+    message="This service is currently down for maintenance. Check back soon."
+  />
 );
--- aries-site/src/examples/templates/banner-notifications/Examples/BannerNotificationInfo.js
+++ aries-site/src/examples/templates/banner-notifications/Examples/BannerNotificationInfo.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { Box, Notification } from 'grommet';
+import { Notification } from 'grommet';
 
 export const BannerNotificationInfo = () => (
-  <Box fill="horizontal">
-    <Notification
-    status='info'
-      onClose={() => {}}
-      actions={[
-        {
-          href: '#',
-          label: 'View more',
-        },
-      ]}
-      message="Updates to this service will be available soon
+  <Notification
+    status="info"
+    onClose={() => {}}
+    actions={[
+      {
+        href: '#',
+        label: 'View more',
+      },
+    ]}
+    message="Updates to this service will be available soon
      including feature a, b, and c."
-    />
-  </Box>
+    global
+  />
 );
