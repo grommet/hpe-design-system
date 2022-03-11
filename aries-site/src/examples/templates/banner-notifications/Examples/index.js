168a4caea4cec22850ca44e540ec38fc0519fe72
britt6612 <42451602+britt6612@users.noreply.github.com>
2022-03-11 12:25:36 -0700

    Delete index.js

--- aries-site/src/examples/templates/banner-notifications/Examples/index.js
+++
@@ -1,5 +0,0 @@
-export * from './BannerNotificationCritical';
-export * from './BannerNotificationCriticalClose';
-export * from './BannerNotificationInfo';
-export * from './BannerNotificationWarning';
-export * from './BannerNotificationWarningClose';
