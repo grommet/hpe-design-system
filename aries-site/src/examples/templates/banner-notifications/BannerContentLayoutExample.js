98a243806c91056e0cdf829c84e2484c488bc7ba
brittany archibeque <brittany.archibeque@hpe.com>
2022-03-10 18:44:15 -0700

    add content blocks

--- aries-site/src/examples/templates/banner-notifications/BannerContentLayoutExample.js
+++ aries-site/src/examples/templates/banner-notifications/BannerContentLayoutExample.js
@@ -3,15 +3,18 @@ import {
   Avatar,
   Box,
   Button,
+  Grid,
   Header,
   Heading,
+  Main,
   Page,
   PageContent,
   Notification,
   Text,
+  ResponsiveContext,
 } from 'grommet';
 import { HelpOption, HomeRounded } from 'grommet-icons';
-import { AppContainer, PageContainerContext } from '../page-layouts/components';
+import { AppContainer } from '../page-layouts/components';
 import { ContentArea } from '../page-layouts/anatomy/components';
 
 export const BannerContentLayoutExample = () => (
@@ -51,21 +54,90 @@ export const BannerContentLayoutExample = () => (
         global
       />
     </Box>
-    <Page flex>
-      <PageHeader />
+    <Page pad="medium" flex>
+      <Header>
+        <Heading level={1} margin="none" size="small">
+          Page Header
+        </Heading>
+      </Header>
+      <Content />
       <PageContent />
     </Page>
   </AppContainer>
 );
 
-const PageHeader = () => {
-  const { ...pageContainer } = useContext(PageContainerContext);
+const parentGrid = {
+  columns: {
+    xsmall: '100%',
+    small: '100%',
+    medium: ['auto', 'auto'],
+    large: ['auto', 'medium'],
+    xlarge: ['auto', 'medium'],
+  },
+  gap: {
+    xsmall: 'large',
+    small: 'large',
+    medium: 'medium',
+    large: 'large',
+    xlarge: 'large',
+  },
+};
+
+const firstChildGrid = {
+  columns: {
+    xsmall: '100%',
+    small: '100%',
+    medium: '100%',
+    large: ['auto', ['small', 'medium']],
+    xlarge: ['auto', ['small', 'medium']],
+  },
+  gap: 'medium',
+};
+
+const secondChildGrid = {
+  columns: {
+    xsmall: '100%',
+    small: '100%',
+    medium: '100%',
+    large: { count: 'fit', size: ['medium', 'auto'] },
+    xlarge: { count: 'fit', size: ['medium', 'auto'] },
+  },
+  gap: 'medium',
+};
+
+const Content = () => {
+  const size = useContext(ResponsiveContext);
+
   return (
-    <Header {...pageContainer}>
-      <Heading level={1} margin="none" size="small">
-        {/* for dev purposes, will be replaced with dashboard content */}
-        Page Header
-      </Heading>
-    </Header>
+    <Main>
+      <Grid gap={parentGrid.gap[size]} columns={parentGrid.columns[size]}>
+        {/* Content Block 1 is top priority content. At narrow breakpoints, 
+          place as first content element. Otherwise, place in second column. */}
+        {['xsmall', 'small'].includes(size) && <ContentBlock title="1" />}
+        <Grid gap={firstChildGrid.gap}>
+          <Grid columns={firstChildGrid.columns[size]} gap={firstChildGrid.gap}>
+            <ContentBlock title="2" />
+            <ContentBlock title="3" />
+          </Grid>
+          <Grid
+            columns={secondChildGrid.columns[size]}
+            gap={secondChildGrid.gap}
+          >
+            <Box gap={secondChildGrid.gap}>
+              <ContentBlock title="4" />
+              <ContentBlock title="5" />
+            </Box>
+            <ContentBlock title="6" fill="vertical" />
+          </Grid>
+        </Grid>
+        {!['xsmall', 'small'].includes(size) && size !== 'xsmall' && (
+          <ContentBlock title="1" fill="vertical" />
+        )}
+      </Grid>
+    </Main>
   );
 };
+
+const ContentBlock = ({ ...rest }) => (
+  <ContentArea elevation="medium" height="small" round="small" {...rest} />
+);
