89fe01ebf43368970e17845223e7f0ee5209a699
Matt Glissmann <mdglissmann@gmail.com>
2022-03-11 07:51:01 -0700

    add fluid grid examples

---
+++ aries-site/src/examples/components/grid/FluidGrid2.js
@@ -0,0 +1,31 @@
+import { Grid } from 'grommet';
+import { ContentArea } from '../../templates/page-layouts/anatomy/components';
+import { contentAreaProps as props } from './utils';
+const contentAreaProps = {
+  ...props,
+  height: undefined,
+};
+// used for creating some variation in the ContentArea heights
+const heightMap = {
+  0: 'xsmall',
+  1: 'xxsmall',
+  2: '75%',
+  3: 'xsmall',
+};
+
+const columns = 'small';
+const rows = 'xsmall';
+const gap = 'small';
+
+export const FluidGrid2 = () => (
+  <Grid columns={columns} rows={rows} gap={gap} align="start">
+    {new Array(11).fill({}).map((item, index) => (
+      <ContentArea
+        key={index}
+        title={index + 1}
+        {...contentAreaProps}
+        height={heightMap[index % 4]}
+      />
+    ))}
+  </Grid>
+);
