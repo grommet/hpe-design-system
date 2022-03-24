0c6280c3fc681801ff61481e18558438cd895016
amandadupell <anddupell@gmail.com>
2022-03-24 15:27:38 -0400

    added poster image, aria-describedby and trancript file

--- aries-site/src/components/accessibility/Video.js
+++ aries-site/src/components/accessibility/Video.js
@@ -6,7 +6,19 @@ export const AccessVideo = () => {
 
   return (
     <Box width="large" pad={{ bottom: 'large' }}>
-      <video controls>
+      <video 
+        aria-label="Accessibility at HPE Video" 
+        aria-describedby="video-alt" 
+        poster="/static/images/BILL-ACCESS-COVER.png" 
+        transcript="transcript.html"
+        controls
+      >
+        <div id="video-alt">
+          The video shows Bill, a white man with blondish brown hair and a 
+          mustache, in his office wearing a long-sleeved lavender-blue 
+          dress shirt with the HPE rectangular-shaped, multi-color logo
+          against a black background as the background.
+        </div>
         <source
           src="https://d3hq6blov2iije.cloudfront.net/media/BILL-ACCESS-VID.mp4"
           type="video/mp4"
