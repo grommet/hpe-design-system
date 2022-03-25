4c633cfd033558385289c2279dbb701da58f688c
amandadupell <anddupell@gmail.com>
2022-03-24 22:39:10 -0400

    added new transcript file link

--- aries-site/src/components/accessibility/Video.js
+++ aries-site/src/components/accessibility/Video.js
@@ -1,35 +1,45 @@
 import React from 'react';
 
-import { Box } from 'grommet';
+import { Anchor, Box } from 'grommet';
 
 export const AccessVideo = () => {
 
   return (
-    <Box width="large" pad={{ bottom: 'large' }}>
-      <video 
-        aria-label="Accessibility at HPE Video" 
-        aria-describedby="video-alt" 
-        poster="/static/images/BILL-ACCESS-COVER.png" 
-        transcript="transcript.html"
-        controls
-      >
-        <div id="video-alt">
-          The video shows Bill, a white man with blondish brown hair and a 
-          mustache, in his office wearing a long-sleeved lavender-blue 
-          dress shirt with the HPE rectangular-shaped, multi-color logo
-          against a black background as the background.
-        </div>
-        <source
-          src="https://d3hq6blov2iije.cloudfront.net/media/BILL-ACCESS-VID.mp4"
-          type="video/mp4"
-        />
-        <track
-          default
-          kind="captions"
-          srcLang="en"
-          src="/static/Bill.Access.Captions.vtt"
-        />
-      </video>
-    </Box>
+    <>
+      <Box gap="small" width="large" pad={{ bottom: 'large' }}>
+        <video 
+          aria-label="Accessibility at HPE Video" 
+          aria-describedby="video-alt" 
+          poster="/static/images/BILL-ACCESS-COVER.png" 
+          transcript="/static/transcript.html"
+          controls
+        >
+          <source
+            src="https://d3hq6blov2iije.cloudfront.net/media/BILL-ACCESS-VID.mp4"
+            type="video/mp4"
+          />
+          <track
+            default
+            kind="captions"
+            srcLang="en"
+            src="/static/Bill.Access.Captions.vtt"
+          />
+
+          <div id="video-alt">
+            The video shows Bill, a white man with blondish brown hair and a 
+            mustache, in his office wearing a long-sleeved lavender-blue 
+            dress shirt with the HPE rectangular-shaped, multi-color logo
+            against a black background as the background.
+          </div>
+        </video>
+        <Anchor 
+          a11yTitle="Accessibility at HPE with Bill Tipton"
+          href="/static/transcript.html" 
+          target="_blank"
+        >
+          Transcript file
+        </Anchor>
+      </Box>
+    </>
   );
 };
