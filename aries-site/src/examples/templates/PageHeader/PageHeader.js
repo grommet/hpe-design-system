9d52bbbdd1a8c6906643894b838f3646ad66e4dc
Matt Glissmann <mdglissmann@gmail.com>
2022-04-07 14:33:14 -0600

    remove PageHeader enhancements

--- aries-site/src/examples/templates/PageHeader/PageHeader.js
+++ aries-site/src/examples/templates/PageHeader/PageHeader.js
@@ -1,65 +1,24 @@
-import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Box, Header, Heading, Paragraph } from 'grommet';
-import { FormPrevious } from 'grommet-icons';
 
-import { UpButton } from './UpButton';
-import { BreadCrumbs } from './BreadCrumbs';
-
-export const PageHeader = ({
-  title,
-  subtitle,
-  actions,
-  contextualNav,
-  ...rest
-}) => {
-  const [upButton, setUpButton] = useState(null);
-  const [breadCrumbs, setBreadCrumbs] = useState(null);
-
-  useEffect(() => {
-    if (contextualNav) {
-      setUpButton(contextualNav.upButton);
-      setBreadCrumbs(contextualNav.breadCrumbs);
-    }
-  }, [contextualNav]);
-
-  return (
-    <Header {...rest}>
-      <Box>
-        <Box gap="xsmall">
-          <>
-            {breadCrumbs && <BreadCrumbs links={breadCrumbs} />}
-            {upButton && (
-              <UpButton
-                alignSelf="start"
-                icon={<FormPrevious />}
-                {...upButton}
-                label={upButton.label}
-                href={upButton.href}
-              />
-            )}
-          </>
-          <Heading level={1} margin="none" size="small">
-            {title}
-          </Heading>
-        </Box>
-        {typeof subtitle === 'string' ? (
-          <Paragraph margin="none">{subtitle}</Paragraph>
-        ) : (
-          <>{subtitle}</>
-        )}
-      </Box>
-      {actions}
-    </Header>
-  );
-};
+export const PageHeader = ({ title, subtitle, actions, ...rest }) => (
+  <Header {...rest}>
+    <Box>
+      <Heading level={1} margin="none" size="small">
+        {title}
+      </Heading>
+      {typeof subtitle === 'string' ? (
+        <Paragraph margin="none">{subtitle}</Paragraph>
+      ) : (
+        <>{subtitle}</>
+      )}
+    </Box>
+    {actions}
+  </Header>
+);
 
 PageHeader.propTypes = {
   actions: PropTypes.oneOfType([PropTypes.node]),
-  contextualNav: PropTypes.shape({
-    upButton: PropTypes.object,
-    breadCrumbs: PropTypes.arrayOf(PropTypes.object),
-  }),
   subtitle: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
   title: PropTypes.string,
 };
--- aries-site/src/examples/templates/PageHeader/index.js
+++ aries-site/src/examples/templates/PageHeader/index.js
@@ -1,2 +1,1 @@
 export * from './PageHeader';
-export * from './UpButton';
