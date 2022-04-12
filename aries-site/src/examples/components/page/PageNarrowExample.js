219e0972ffe26032ec051716b93c1fcf836c195f
Matthew Glissmann <mdglissmann@gmail.com>
2022-04-12 09:54:52 -0600

    Page Component Page - Alternate to #2471 (#2508)
    * add page component page
    
    * change color
    
    * change controls
    
    * update yarn
    
    * Update aries-site/src/pages/components/page.mdx
    
    Co-authored-by: Jessica Filben <54560994+jcfilben@users.noreply.github.com>
    
    * Update aries-site/src/data/structures/components.js
    
    Co-authored-by: Jessica Filben <54560994+jcfilben@users.noreply.github.com>
    
    * change kind to wide per feedback
    
    * reword max width per feedback
    
    * add sections
    
    * change example
    
    * add preview images
    
    * Update aries-site/src/data/structures/components.js
    
    Co-authored-by: Matthew Glissmann <mdglissmann@gmail.com>
    
    * Update aries-site/src/data/structures/components.js
    
    Co-authored-by: Matthew Glissmann <mdglissmann@gmail.com>
    
    * Update aries-site/src/pages/components/page.mdx
    
    Co-authored-by: Matthew Glissmann <mdglissmann@gmail.com>
    
    * address Taylor feedback
    
    * address Taylor feedback
    
    * add link
    
    * create PageHeader component
    
    * yarn.lock
    
    * progess on content
    
    * Update page.mdx
    
    * PageHeader enhancements
    
    * advance single page content example
    
    * complete SinglePageContent example
    
    * Page kind copy
    
    * add wide variant example
    
    * add responsive to wide example
    
    * intro example
    
    * Update aries-site/src/pages/components/page.mdx
    
    Co-authored-by: Taylor Seamans <taylor.seamans@yahoo.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Taylor Seamans <taylor.seamans@yahoo.com>
    
    * add copy to page background example
    
    * a lot of stuff
    
    * ensure correct font contrast
    
    * finish pageBackground example
    
    * add code and github references
    
    * address wave accessibility errors
    
    * add comments and remove default prop setting
    
    * remove PageHeader enhancements
    
    * add missing preposition
    
    * incorporate final feedback
    
    * fix capitalization
    
    Co-authored-by: brittany archibeque <brittany.archibeque@hpe.com>
    Co-authored-by: britt6612 <42451602+britt6612@users.noreply.github.com>
    Co-authored-by: Jessica Filben <54560994+jcfilben@users.noreply.github.com>
    Co-authored-by: Taylor Seamans <taylor.seamans@yahoo.com>

---
+++ aries-site/src/examples/components/page/PageNarrowExample.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import {
+  Box,
+  Heading,
+  Page,
+  PageContent,
+  Form,
+  FormField,
+  Select,
+  TextArea,
+  RadioButtonGroup,
+  Text,
+  TextInput,
+  Button,
+} from 'grommet';
+
+import { PageHeader } from '../../templates';
+// `demoStyle` is specific for the Design System site and is used
+// as a visual aid to help present layout concepts. Remove from
+// your implementation.
+import { demoStyle } from './demoStyle';
+
+export const PageNarrowExample = () => (
+  <Page kind="narrow" flex="grow" pad={{ vertical: 'large' }} {...demoStyle}>
+    <PageContent gap="large" {...demoStyle}>
+      <PageHeader
+        title="Narrow Page"
+        actions={[<Button key="Action A" secondary label="Action A" />]}
+      />
+      <FormSection />
+    </PageContent>
+  </Page>
+);
+
+const FormSection = () => (
+  <Form>
+    <Box width="medium" gap="medium">
+      <>
+        <Heading level={2} size="small" margin="none">
+          A Form Section
+        </Heading>
+        <FormField
+          htmlFor="field1"
+          name="field1"
+          label="Field 1"
+          help="Some descriptive text providing context for the input."
+        >
+          <TextArea
+            id="field1"
+            name="field1"
+            placeholder="Enter a description ..."
+          />
+        </FormField>
+        <FormField htmlFor="field2__input" name="field2" label="Field 2">
+          <Select
+            id="field2"
+            name="field2"
+            placeholder="Select an option"
+            options={new Array(12)
+              .fill()
+              .map((_, index) => `Option ${index + 1}`)}
+          />
+        </FormField>
+        <FormField htmlFor="field3" name="field3" label="Field 3">
+          <RadioButtonGroup
+            id="field3"
+            name="field3"
+            options={new Array(4).fill().map((_, index) => ({
+              label: <Text>Option {index + 1}</Text>,
+              value: index + 1,
+            }))}
+          />
+        </FormField>
+      </>
+      <>
+        <Heading level={2} size="small" margin="none">
+          Another Form Section
+        </Heading>
+        {new Array(5).fill().map((_, index) => (
+          <FormField
+            key={index + 1}
+            htmlFor={`toggle${index + 1}`}
+            name={`toggle${index + 1}`}
+            label={`Label ${index + 1}`}
+          >
+            <TextInput
+              id={`toggle${index + 1}`}
+              name={`toggle${index + 1}`}
+              placeholder="Placeholder..."
+            />
+          </FormField>
+        ))}
+      </>
+      <Button alignSelf="start" primary label="Apply Settings" />
+    </Box>
+  </Form>
+);
