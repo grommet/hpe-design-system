0ad8bf1c2771ab997610355deec30b9904b09c89
brittany archibeque <brittany.archibeque@hpe.com>
2022-04-19 15:08:47 -0600

    fix pad

--- aries-site/src/examples/components/layer/LayerStickyFormExample.js
+++ aries-site/src/examples/components/layer/LayerStickyFormExample.js
@@ -58,107 +58,104 @@ export const LayerStickyFormExample = () => {
           onEsc={onClose}
         >
           <Box
+            gap="small"
             fill="vertical"
             overflow="auto"
             width={!['xsmall', 'small'].includes(size) ? 'medium' : undefined}
+            pad="medium"
           >
             <Header
               background="background"
               sticky="scrollup"
-              pad={{ vertical: 'medium', horizontal: 'small' }}
+              pad={{ bottom: 'medium' }}
             >
               <Heading margin="none" level={2} size="small">
                 Add Monitor
               </Heading>
             </Header>
-            <Box
-              // Padding used to prevent focus from being cutoff
-              pad={{ bottom: 'medium', horizontal: 'small' }}
+            <Form
+              onSubmit={({ value, touched }) => onSubmit({ value, touched })}
+              value={formValues}
+              onChange={onFormChange}
+              method="post"
             >
-              <Form
-                onSubmit={({ value, touched }) => onSubmit({ value, touched })}
-                value={formValues}
-                onChange={onFormChange}
-                method="post"
-              >
-                <Box margin={{ bottom: 'small' }}>
-                  <FormField name="monitorName" label="Monitor Name">
-                    <TextInput name="monitorName" />
-                  </FormField>
-                  <FormField name="desc" label="Bussiness Description">
-                    <TextArea name="monitorName" />
-                  </FormField>
-                  <Text
-                    margin={{ vertical: 'medium' }}
-                    weight="bold"
-                    color="text-strong"
-                  >
-                    Monitor Detail
-                  </Text>
-                  <FormField name="monitorType" label="Monitor Type">
-                    <TextInput name="monitorType" />
-                  </FormField>
-                  <FormField name="rules">
-                    <CheckBox
-                      name="rules"
-                      label="Include Process Rules"
-                      toggle
-                      fill
-                      checked
-                      reverse
-                    />
-                  </FormField>
-                  <FormField name="frameworks" label="Frameworks">
-                    <Select options={options} name="frameworks" />
-                  </FormField>
-                  <FormField name="severity" label="Rule Severity">
-                    <Select options={options} name="severity" />
-                  </FormField>
-                  <FormField name="provider" label="Provider">
-                    <Select options={options} name="provider" />
-                  </FormField>
-                  <FormField name="account" label="Account">
-                    <Select options={options} name="account" />
-                  </FormField>
-                  <FormField name="ruleName" label="Rule Name">
-                    <Select options={options} name="ruleName" />
-                  </FormField>
-                  <Text
-                    margin={{ vertical: 'medium' }}
-                    weight="bold"
-                    color="text-strong"
-                  >
-                    Email Notification
-                  </Text>
-                  <FormField name="notifications">
-                    <CheckBox
-                      name="notifications"
-                      label="Email Notifications"
-                      toggle
-                      fill
-                      checked
-                      reverse
-                    />
-                  </FormField>
-                  <FormField name="email" label="Primary Email">
-                    <TextInput name="email" />
-                  </FormField>
+              <Box margin={{ bottom: 'small' }}>
+                <FormField name="monitorName" label="Monitor Name">
+                  <TextInput name="monitorName" />
+                </FormField>
+                <FormField name="desc" label="Bussiness Description">
+                  <TextArea name="monitorName" />
+                </FormField>
+                <Text
+                  margin={{ vertical: 'medium' }}
+                  weight="bold"
+                  color="text-strong"
+                >
+                  Monitor Detail
+                </Text>
+                <FormField name="monitorType" label="Monitor Type">
+                  <TextInput name="monitorType" />
+                </FormField>
+                <FormField name="rules">
+                  <CheckBox
+                    name="rules"
+                    label="Include Process Rules"
+                    toggle
+                    fill
+                    checked
+                    reverse
+                  />
+                </FormField>
+                <FormField name="frameworks" label="Frameworks">
+                  <Select options={options} name="frameworks" />
+                </FormField>
+                <FormField name="severity" label="Rule Severity">
+                  <Select options={options} name="severity" />
+                </FormField>
+                <FormField name="provider" label="Provider">
+                  <Select options={options} name="provider" />
+                </FormField>
+                <FormField name="account" label="Account">
+                  <Select options={options} name="account" />
+                </FormField>
+                <FormField name="ruleName" label="Rule Name">
+                  <Select options={options} name="ruleName" />
+                </FormField>
+                <Text
+                  margin={{ vertical: 'medium' }}
+                  weight="bold"
+                  color="text-strong"
+                >
+                  Email Notification
+                </Text>
+                <FormField name="notifications">
+                  <CheckBox
+                    name="notifications"
+                    label="Email Notifications"
+                    toggle
+                    fill
+                    checked
+                    reverse
+                  />
+                </FormField>
+                <FormField name="email" label="Primary Email">
+                  <TextInput name="email" />
+                </FormField>
+              </Box>
+              <Box>
+                <Box
+                  direction="row"
+                  gap="small"
+                  align={
+                    !['xsmall', 'small'].includes(size) ? 'start' : undefined
+                  }
+                  margin={{ top: 'small', bottom: 'small' }}
+                >
+                  <Button label="Apply Settings" primary type="submit" />
+                  <Button label="Cancel" onClick={() => setOpen(false)} />
                 </Box>
-                <Box>
-                  <Box
-                    direction="row"
-                    gap="small"
-                    align={
-                      !['xsmall', 'small'].includes(size) ? 'start' : undefined
-                    }
-                    margin={{ top: 'small', bottom: 'small' }}
-                  >
-                    <Button label="Apply Settings" primary type="submit" />
-                    <Button label="Cancel" onClick={() => setOpen(false)} />
-                  </Box>
-                </Box>
-              </Form>
-            </Box>
+              </Box>
+            </Form>
           </Box>
         </Layer>
       )}
