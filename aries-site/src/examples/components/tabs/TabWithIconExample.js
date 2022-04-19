a6d257827a37f66656c055ec3549b4782c832777
Taylor Seamans <taylor.seamans@yahoo.com>
2022-04-19 14:56:09 -0700

    Address feedback

--- aries-site/src/examples/components/tabs/TabWithIconExample.js
+++ aries-site/src/examples/components/tabs/TabWithIconExample.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Tab, Tabs } from 'grommet';
+import { Tab, Tabs } from 'grommet';
 import { Currency, HomeRounded, User } from 'grommet-icons';
 import { TabContent } from './TabContent';
 
@@ -8,18 +8,16 @@ export const TabWithIconExample = () => {
   const onActive = nextIndex => setIndex(nextIndex);
 
   return (
-    <Box>
-      <Tabs activeIndex={index} onActive={onActive} justify="start">
-        <Tab title="General" icon={<HomeRounded />}>
-          <TabContent>General Information</TabContent>
-        </Tab>
-        <Tab title="Account" icon={<User />}>
-          <TabContent>Account Information</TabContent>
-        </Tab>
-        <Tab title="Billing" icon={<Currency />}>
-          <TabContent>Billing Information</TabContent>
-        </Tab>
-      </Tabs>
-    </Box>
+    <Tabs activeIndex={index} onActive={onActive} justify="start">
+      <Tab title="General" icon={<HomeRounded />}>
+        <TabContent>General Information</TabContent>
+      </Tab>
+      <Tab title="Account" icon={<User />}>
+        <TabContent>Account Information</TabContent>
+      </Tab>
+      <Tab title="Billing" icon={<Currency />}>
+        <TabContent>Billing Information</TabContent>
+      </Tab>
+    </Tabs>
   );
 };
