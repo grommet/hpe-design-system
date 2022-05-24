bcad97069aa9a8e8c90d358fae28c8c747a24568
Taylor Seamans <taylor.seamans@yahoo.com>
2022-05-24 15:41:42 -0700

    Adjust Header documentation to point to PageHeader

--- aries-site/src/examples/components/header/index.js
+++ aries-site/src/examples/components/header/index.js
@@ -1,6 +1,5 @@
 export * from './HeaderExample';
 export * from './HeaderActionExample';
 export * from './HeaderAvatarExample';
-export * from './HeaderPageExample';
 export * from './HeaderSearchActionsExample';
 export * from './HeaderNavigationExample';
