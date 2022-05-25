1f2535ef5ee2b25936ee2c6418118b62f02d1140
Taylor Seamans <taylor.seamans@yahoo.com>
2022-05-25 16:21:01 -0700

    Update aries-site/src/examples/components/pageheader/PageHeaderAnatomy.js
    Co-authored-by: Matthew Glissmann <mdglissmann@gmail.com>

--- aries-site/src/examples/components/pageheader/PageHeaderAnatomy.js
+++ aries-site/src/examples/components/pageheader/PageHeaderAnatomy.js
@@ -56,7 +56,7 @@ export const PageHeaderAnatomy = ({ background }) => {
         rows={
           mobileLayout
             ? ['auto', 'auto', 'auto', 'auto']
-            : ['auto', 'auto', 'auto']
+            : ['31px', '58px', '30px']
         }
         areas={
           mobileLayout
