b8eda3166f5ac317f95109913dd77644c6e679df
Jessica Filben <jessica.filben@gmail.com>
2022-06-16 16:37:25 -0600

    changes based on review comments

--- aries-site/src/examples/components/tabs/TabResponsive.js
+++ aries-site/src/examples/components/tabs/TabResponsive.js
@@ -2,33 +2,24 @@ import React from 'react';
 import { Tab, Tabs, Paragraph } from 'grommet';
 
 export const TabResponsiveExample = () => (
-    <Tabs justify="start">
-        <Tab title="General">
-            <Paragraph>General Information</Paragraph>
-        </Tab>
-        <Tab title="Account">
-            <Paragraph>Account Information</Paragraph>
-        </Tab>
-        <Tab title="Billing">
-            <Paragraph>Billing Information</Paragraph>
-        </Tab>
-        <Tab title="Overview">
-            <Paragraph>Overview Information</Paragraph>
-        </Tab>
-        <Tab title="Services">
-            <Paragraph>Service Information</Paragraph>
-        </Tab>
-        <Tab title="Management">
-            <Paragraph>Management Information</Paragraph>
-        </Tab>
-        <Tab title="Roles">
-            <Paragraph>Roles</Paragraph>
-        </Tab>
-        <Tab title="Configuration">
-            <Paragraph>Configuration</Paragraph>
-        </Tab>
-        <Tab title="Survey">
-            <Paragraph>Survey</Paragraph>
-        </Tab>
-    </Tabs>
+  <Tabs justify="start">
+    <Tab title="General">
+      <Paragraph>General Information</Paragraph>
+    </Tab>
+    <Tab title="Account">
+      <Paragraph>Account Information</Paragraph>
+    </Tab>
+    <Tab title="Billing">
+      <Paragraph>Billing Information</Paragraph>
+    </Tab>
+    <Tab title="Overview">
+      <Paragraph>Overview Information</Paragraph>
+    </Tab>
+    <Tab title="Services">
+      <Paragraph>Service Information</Paragraph>
+    </Tab>
+    <Tab title="Management">
+      <Paragraph>Management Information</Paragraph>
+    </Tab>
+  </Tabs>
 );
