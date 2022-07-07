34f686262fa5ae2a49717c1ad5af3cb1c4b7208f
Jeffrey Chen <j.chen14641@gmail.com>
2022-07-07 12:23:24 -0700

    Updated DataTable vertical list example table (#2705)
    * Updated DataTable vertical list example table
    
    * absorb code comments and apply prettier extension
    
    * remove box align in ViewMore const
    
    * update yarn.lock for e2e tests
    
    * Finalize updates to DataTable Vertical Example
    
    * Update yarn.lock
    
    * Update DataTable example to include icons
    
    * Update copy
    
    * Update mockData's serverGroups.json to display correctly
    
    * Update DataTable Vertical example to include more realisitc data, fix links
    
    * Update yarn.lock
    
    * Update pagination links on datatable page
    
    Co-authored-by: Jeffrey Chen <jeffreychen@Jeffreys-MacBook-Pro.local>
    Co-authored-by: Taylor Seamans <taylor.seamans@yahoo.com>

---
+++ aries-site/src/examples/components/datatable/DataTableVerticalListExample.js
@@ -0,0 +1,135 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Box,
+  Button,
+  DataTable,
+  Heading,
+  Text,
+  ResponsiveContext,
+} from 'grommet';
+
+import { StatusGoodSmall, StatusWarningSmall } from 'grommet-icons';
+
+import ServerGroups from '../../../data/mockData/serverGroups.json';
+
+/* This function displays an arr's values in a vertical list within a table 
+   cell, hiding all values from view that exceed the given maximum list 
+   length until the "View More" button is clicked, at which point the 
+   button label changes to "View Less". */
+const ViewMore = (data, max) => {
+  const [viewMore, setViewMore] = useState(false);
+  return (
+    <Box>
+      {viewMore
+        ? data.map(item => <Text>{item}</Text>)
+        : data.slice(0, max).map(item => <Text>{item}</Text>)}
+
+      {data.length > max && (
+        <Button
+          alignSelf="start"
+          label={viewMore ? 'View less' : `View more (${data.length - max})`}
+          onClick={() => setViewMore(!viewMore)}
+        />
+      )}
+    </Box>
+  );
+};
+
+const formatData = dataSet =>
+  dataSet.map(datum => {
+    const adjustedDatum = { ...datum };
+    switch (datum.status) {
+      case 'OK':
+        adjustedDatum.status = {
+          label: 'ok',
+          icon: StatusGoodSmall,
+          color: 'status-ok',
+        };
+        break;
+      case 'Warning':
+        adjustedDatum.status = {
+          label: 'warning',
+          icon: StatusWarningSmall,
+          color: 'status-warning',
+        };
+        break;
+      default:
+        adjustedDatum.status = {
+          label: datum.status,
+        };
+    }
+    return adjustedDatum;
+  });
+
+const data = ServerGroups.groups;
+const max = 3;
+const columns = [
+  {
+    property: 'name',
+    header: 'Name',
+    render: datum => <Text truncate>{datum.name}</Text>,
+  },
+  {
+    property: 'servers',
+    header: 'Servers',
+    render: datum => ViewMore(datum.servers, max),
+    sortable: false,
+  },
+];
+
+// designSystemDemo is used for DS site only, can be removed in production.
+export const DataTableVerticalListExample = ({ designSystemDemo }) => {
+  const size = React.useContext(ResponsiveContext);
+  return (
+    <>
+      <Heading
+        id="server-groups-vertical-align"
+        level={3}
+        margin={{ bottom: 'small', top: 'none' }}
+      >
+        Server Groups
+      </Heading>
+      <Box
+        // Height is restricted to keep inline doc page examples more compact.
+        // In production, DataTable height should follow height guidelines.
+        // https://design-system.hpe.design/components/datatable#setting-the-height-of-a-table
+        height={designSystemDemo ? undefined : 'medium'}
+        overflow="auto"
+        alignSelf="start"
+      >
+        <DataTable
+          aria-describedby="server-groups-vertical-align"
+          data={formatData(data)}
+          columns={[
+            {
+              property: 'status.label',
+              header: 'Status',
+              render: datum => (
+                <Box direction="row" align="center" gap="xsmall">
+                  <datum.status.icon color={datum.status.color} size="small" />
+                  <Text>{datum.status.label}</Text>
+                </Box>
+              ),
+            },
+            {
+              property: 'id',
+              header: 'Id',
+              primary: true,
+              render: datum => datum.id.slice(datum.id.length - 5),
+              pin: ['xsmall', 'small'].includes(size),
+            },
+            ...columns,
+          ]}
+          pin
+          sortable
+          verticalAlign={{ body: 'top' }}
+        />
+      </Box>
+    </>
+  );
+};
+
+DataTableVerticalListExample.propTypes = {
+  designSystemDemo: PropTypes.bool,
+};
