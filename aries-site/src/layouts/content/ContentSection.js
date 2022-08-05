a57a36f8d91a6880b391bdbcd911c26e491f1d86
Taylor Seamans <taylor.seamans@yahoo.com>
2022-08-05 14:49:55 -0700

    Proposal to rely on theme values instead of custom px

--- aries-site/src/layouts/content/ContentSection.js
+++ aries-site/src/layouts/content/ContentSection.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Box } from 'grommet';
 
-export const ContentSection = ({ ...rest }) => (
+export const ContentSection = ({ pad, ...rest }) => (
   <Box
     as="section"
     pad={{
       bottom: 'medium',
       horizontal: 'xxsmall',
       top: 'large',
+      ...pad,
     }}
     {...rest}
   />
 );
+
+ContentSection.propTypes = {
+  pad: PropTypes.object,
+};
