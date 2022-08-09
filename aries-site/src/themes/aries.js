f49f14613e054bc036d451511ab4f72da8fe3076
Taylor Seamans <taylor.seamans@yahoo.com>
2022-08-09 13:20:31 -0700

    Trivial change back [skip ci]

--- aries-site/src/themes/aries.js
+++ aries-site/src/themes/aries.js
@@ -4,7 +4,7 @@ import { deepMerge } from 'grommet/utils';
 export const aries = deepMerge(hpe, {
   defaultMode: 'dark',
   // To be stripped out once theme changes are made in grommet-theme-hpe
-  // keeping file for use as a playground for future theme adjusments that need
+  // keeping file for use as playground for future theme adjusments that need
   // to be quickly tested
   menu: {
     group: {
