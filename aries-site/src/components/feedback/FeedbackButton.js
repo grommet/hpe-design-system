383db2298a0618e7bd3a0e66b9798972dcffea26
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2022-08-24 14:37:52 -0600

    add outline for focus

--- aries-site/src/components/feedback/FeedbackButton.js
+++ aries-site/src/components/feedback/FeedbackButton.js
@@ -7,4 +7,8 @@ export const FeedbackButton = styled(Button)`
   right: 24px;
   z-index: 9999;
   box-shadow: 0px 12px 24px rgba(0, 0, 0, 0.24);
+  &:focus {
+    box-shadow: 0 0 2px 2px #00e8cf;
+    outline: none;
+  }
 `;
