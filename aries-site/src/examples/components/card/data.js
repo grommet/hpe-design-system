a0e6e5096d9432b71b89a1d8a9a41c2b2157c477
Taylor Seamans <taylor.seamans@yahoo.com>
2022-09-13 11:28:35 -0700

    Add new files

---
+++ aries-site/src/examples/components/card/data.js
@@ -0,0 +1,26 @@
+export const navigationalData = [
+  {
+    title: 'Virtual Machine Provisioning',
+    subtitle: '20 minute session',
+    description: `Learn how to create a virtual machine instance in the 
+          HPE GreenLake for Private Cloud service. You’ll also learn about 
+          infrastructure groups and tagging on provisioning.`,
+    tag: 'Virtualization',
+  },
+  {
+    title: 'Create an Application Deployment Pattern',
+    subtitle: '40 minute session',
+    description: `Learn how to provision a NodeJS Chat application blueprint 
+      and instantiate the NodeJS application in the HPE GreenLake for Private 
+      Cloud service.`,
+    tag: 'Virtualization',
+  },
+  {
+    title: 'Deploy an Application',
+    subtitle: '20 minute session',
+    description: `Metering and analytics of HPE GreenLake. The consumption-based
+       IT offering from HPE Pointnext Services that gives you the agility and 
+       economics of the public cloud in your data center.`,
+    tag: 'Virtualization',
+  },
+];
