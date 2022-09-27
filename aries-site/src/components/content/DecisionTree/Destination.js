23df8027dcc5b83165eddadb8df0d7499738a31a
Matt Glissmann <mdglissmann@gmail.com>
2022-09-27 17:29:39 -0600

    flow chart shapes and structures

--- aries-site/src/components/content/DecisionTree/Destination.js
+++ aries-site/src/components/content/DecisionTree/Destination.js
@@ -3,46 +3,60 @@ import PropTypes from 'prop-types';
 import { Box, Button, Collapsible, Paragraph, Text } from 'grommet';
 import { FormDown, FormUp } from 'grommet-icons';
 
-export const Destination = ({ children, detail, useCases, ...rest }) => {
-  const [open, setOpen] = useState(false);
+export const Destination = ({ children, detail, id, useCases, ...rest }) => {
+  const [showMore, setShowMore] = useState(false);
 
   return (
-    <Box border {...rest}>
-      <Button onClick={() => setOpen(!open)}>
-        <Box
-          alignSelf="start"
-          round={!open ? 'small' : { corner: 'top', size: 'small' }}
-          background={{ color: 'blue', opacity: 'weak' }}
-          pad="medium"
-        >
-          <Box justify="between" direction="row" align="center">
-            <Paragraph margin="none">{children}</Paragraph>
-            {open ? <FormUp /> : <FormDown />}
-          </Box>
-        </Box>
-      </Button>
-      <Collapsible open={open}>
-        <Box
-          background={{ color: 'blue', opacity: 'weak' }}
-          pad={{ top: 'small', bottom: 'medium', horizontal: 'medium' }}
-          round={{ corner: 'bottom', size: 'small' }}
-          gap="small"
-        >
-          <Paragraph margin="none">{detail}</Paragraph>
-          {useCases && (
-            <Box>
-              <Text>Example use cases:</Text>
-              <Box as="ul" pad="small" margin="none">
+    <Box {...rest}>
+      <Box id={id} background={{ color: 'yellow!' }} round="small">
+        <Box pad={{ horizontal: 'medium', top: 'medium' }}>
+          <Text color="text-strong" weight={500}>
+            {children}
+          </Text>
+          <Paragraph
+            color="text-strong"
+            margin={{ top: 'small', bottom: 'xsmall' }}
+            maxLines={showMore ? undefined : 3}
+          >
+            {detail}
+          </Paragraph>
+          <Collapsible open={showMore}>
+            <Text color="text-strong" margin="none">
+              Example use cases:
+            </Text>
+            {useCases && (
+              <Box
+                as="ul"
+                gap="xsmall"
+                pad={{ left: 'small', top: 'small' }}
+                margin="none"
+              >
                 {useCases.map((useCase, index) => (
-                  <Paragraph as="li" key={index} margin="none">
+                  <Paragraph
+                    as="li"
+                    key={index}
+                    color="text-strong"
+                    margin="none"
+                  >
                     {useCase}
                   </Paragraph>
                 ))}
               </Box>
-            </Box>
-          )}
+            )}
+          </Collapsible>
         </Box>
-      </Collapsible>
+        <Box pad={{ horizontal: 'small', bottom: 'small' }}>
+          <Button
+            alignSelf="start"
+            label={showMore ? 'Show less' : 'Show all'}
+            icon={showMore ? <FormUp /> : <FormDown />}
+            onClick={() => setShowMore(!showMore)}
+            margin={{ left: 'xsmall' }} // align button label with text above
+            reverse
+            size="small"
+          />
+        </Box>
+      </Box>
     </Box>
   );
 };
