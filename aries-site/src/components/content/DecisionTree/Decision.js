2d353fdb7c0e71c20834f245faeab32147b546b6
Matt Glissmann <mdglissmann@gmail.com>
2022-09-29 14:19:13 -0600

    improve decision tree responsiveness

--- aries-site/src/components/content/DecisionTree/Decision.js
+++ aries-site/src/components/content/DecisionTree/Decision.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import { useContext } from 'react';
 import PropTypes from 'prop-types';
-import { Box, Paragraph } from 'grommet';
+import { Box, Paragraph, ResponsiveContext } from 'grommet';
 
-export const Decision = ({ children, id, ...rest }) => (
-  <Box {...rest}>
-    <Box id={id} background="purple!" flex={false} pad="medium" round="large">
-      <Paragraph margin="none" color="text-strong">
-        {children}
-      </Paragraph>
+export const Decision = ({ children, id, ...rest }) => {
+  const breakpoint = useContext(ResponsiveContext);
+  const textSize = ['xsmall', 'small', 'medium'].includes(breakpoint)
+    ? 'small'
+    : 'medium';
+
+  return (
+    <Box {...rest}>
+      <Box id={id} background="purple!" flex={false} pad="medium" round="large">
+        <Paragraph margin="none" color="text-strong" size={textSize}>
+          {children}
+        </Paragraph>
+      </Box>
+      <Box fill />
     </Box>
-    <Box fill />
-  </Box>
-);
+  );
+};
 
 Decision.propTypes = {
   children: PropTypes.string,
--- aries-site/src/examples/templates/forms/FormPresentationDecisionTree.js
+++ aries-site/src/examples/templates/forms/FormPresentationDecisionTree.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Diagram, Grid, Stack } from 'grommet';
+import React, { useContext } from 'react';
+import { Diagram, Grid, ResponsiveContext, Stack } from 'grommet';
 import { Answer, Decision, Destination } from '../../../components';
 
 const connection = (fromTarget, toTarget, { ...rest }) => ({
@@ -38,21 +38,6 @@ export const FormPresentationDecisionTree = () => {
   );
 };
 
-const columns = [
-  'xxsmall',
-  'xxsmall',
-  'xxsmall',
-  'xxsmall',
-  'xxsmall',
-  'xxsmall',
-  'xxsmall',
-  'xxsmall',
-  'xxsmall',
-  'xxsmall',
-  'xxsmall',
-  'xxsmall',
-];
-const rows = ['auto', 'auto', 'auto', 'auto', 'auto', 'auto', 'auto', 'auto'];
 const areas = [
   [
     'null-0-0',
@@ -168,116 +153,131 @@ const areas = [
   ],
 ];
 
-const DecisionTree = () => (
-  <Grid
-    columns={columns}
-    areas={areas}
-    rows={rows}
-    gap="large"
-    pad={{ vertical: 'medium' }}
-  >
-    <Decision gridArea="return-or-continue" id="return-or-continue">
-      Does the user want to return to their starting context or continue on
-      their current path after completing the form?
-    </Decision>
-    <Answer gridArea="return-to-start" id="return-to-start">
-      Return to start
-    </Answer>
-    <Answer
-      alignSelf="center"
-      gridArea="continue-on-new-path"
-      id="continue-on-new-path"
+const DecisionTree = () => {
+  const breakpoint = useContext(ResponsiveContext);
+  console.log(breakpoint);
+
+  const columns = new Array(12).fill(undefined);
+  const rows = ['auto'];
+  const gap = {
+    xsmall: 'medium',
+    small: 'medium',
+    medium: 'medium',
+    large: 'medium',
+    xlarge: 'large',
+  };
+
+  return (
+    <Grid
+      columns={columns}
+      areas={areas}
+      rows={rows}
+      gap={gap[breakpoint]}
+      pad={{ vertical: 'medium' }}
     >
-      Continue on new path
-    </Answer>
-    <Destination
-      gridArea="layer-path"
-      id="layer-path"
-      detail={`Because the user wants to return to their starting context, 
+      <Decision gridArea="return-or-continue" id="return-or-continue">
+        Does the user want to return to their starting context or continue on
+        their current path after completing the form?
+      </Decision>
+      <Answer gridArea="return-to-start" id="return-to-start">
+        Return to start
+      </Answer>
+      <Answer
+        alignSelf="center"
+        gridArea="continue-on-new-path"
+        id="continue-on-new-path"
+      >
+        Continue on new path
+      </Answer>
+      <Destination
+        gridArea="layer-path"
+        id="layer-path"
+        detail={`Because the user wants to return to their starting context, 
   presenting the form in a layer is useful because the layer is placed 
   atop the current page. When the form is completed, the layer closes 
   and reveals the starting page.`}
-      useCases={[
-        `Managing a group of objects which are part of a collection. 
+        useCases={[
+          `Managing a group of objects which are part of a collection. 
   The user may be able to perform CRUD and/or other actions such as 
   triggering a process on the objects in that collection. After 
   completing the action, they want to return to the collection so that 
   they can review the updated state and or perform additional actions on 
   that collection.`,
-      ]}
-    >
-      Use a layer. Which type?
-    </Destination>
-    <Destination
-      gridArea="dedicated-page"
-      id="dedicated-page"
-      detail="A dedicated page allows for focused attention. Upon completion, 
+        ]}
+      >
+        Use a layer. Which type?
+      </Destination>
+      <Destination
+        gridArea="dedicated-page"
+        id="dedicated-page"
+        detail="A dedicated page allows for focused attention. Upon completion, 
   it can display the resulting data for review, further edits, and may 
   allow for other actions to be taken from or applied to the result. It 
   also provides the benefit that it may be linked to from multiple 
   locations or flows within an application."
-      useCases={['Creating/editing an account, role, user, profile, etc.']}
-    >
-      Use a dedicated page.
-    </Destination>
-    <Decision gridArea="reference-needed" id="reference-needed">
-      Is there a need or benefit for the user to reference information on the
-      current page?
-    </Decision>
-    <Answer gridArea="reference-no" id="reference-no">
-      No
-    </Answer>
-    <Answer alignSelf="center" gridArea="reference-yes" id="reference-yes">
-      Yes
-    </Answer>
-    <Decision gridArea="lengthy-form" id="lengthy-form">
-      Is the form lengthy or benefit from being presented in multiple discrete
-      steps?
-    </Decision>
-    <Destination
-      gridArea="side-drawer"
-      id="side-drawer"
-      detail="Using a side-drawer presents the form fixed to the edge of 
+        useCases={['Creating/editing an account, role, user, profile, etc.']}
+      >
+        Use a dedicated page.
+      </Destination>
+      <Decision gridArea="reference-needed" id="reference-needed">
+        Is there a need or benefit for the user to reference information on the
+        current page?
+      </Decision>
+      <Answer gridArea="reference-no" id="reference-no">
+        No
+      </Answer>
+      <Answer alignSelf="center" gridArea="reference-yes" id="reference-yes">
+        Yes
+      </Answer>
+      <Decision gridArea="lengthy-form" id="lengthy-form">
+        Is the form lengthy or benefit from being presented in multiple discrete
+        steps?
+      </Decision>
+      <Destination
+        gridArea="side-drawer"
+        id="side-drawer"
+        detail="Using a side-drawer presents the form fixed to the edge of 
   the screen, allowing the content on the originating page to be visible 
   to the user as they complete the form."
-      useCases={[
-        `Creating or editing an object from a page displaying a data 
+        useCases={[
+          `Creating or editing an object from a page displaying a data 
   collection, where it would be helpful to reference other objects and 
   their attributes in that collection.`,
-      ]}
-    >
-      Use a side-drawer.
-    </Destination>
-    <Answer gridArea="lengthy-yes" id="lengthy-yes">
-      Yes
-    </Answer>
-    <Answer alignSelf="center" gridArea="lengthy-no" id="lengthy-no">
-      No
-    </Answer>
-    <Destination
-      gridArea="fullscree-layer"
-      id="fullscree-layer"
-      detail={`The form may either be presented as a full-page form or as a 
+        ]}
+      >
+        Use a side-drawer.
+      </Destination>
+      <Answer gridArea="lengthy-yes" id="lengthy-yes">
+        Yes
+      </Answer>
+      <Answer alignSelf="center" gridArea="lengthy-no" id="lengthy-no">
+        No
+      </Answer>
+      <Destination
+        gridArea="fullscree-layer"
+        id="fullscree-layer"
+        detail={`The form may either be presented as a full-page form or as a 
   wizard. The advantage of presenting the layer in fullscreen mode is that 
   it allows the user’s attention to be solely focused on 
   the task at hand.`}
-      useCases={[
-        `Creating or editing an object with a lot of attributes to be set or 
+        useCases={[
+          `Creating or editing an object with a lot of attributes to be set or 
     configured.`,
-        'Progressive forms revealing inputs based on preceding values.',
-        `Performing an action where decisions need to be made and reviewed 
+          'Progressive forms revealing inputs based on preceding values.',
+          `Performing an action where decisions need to be made and reviewed 
     along the way (e.g. applying new firmware to a group of servers)`,
-      ]}
-    >
-      Use a fullscreen layer.
-    </Destination>
-    <Destination
-      gridArea="centered-modal"
-      id="centered-modal"
-      detail={`If the form is brief, the center modal presentation draws 
+        ]}
+      >
+        Use a fullscreen layer.
+      </Destination>
+      <Destination
+        gridArea="centered-modal"
+        id="centered-modal"
+        detail={`If the form is brief, the center modal presentation draws 
   the user’s eye and presents the form task focussed and distraction free.`}
-    >
-      Use a centered modal.
-    </Destination>
-  </Grid>
-);
+      >
+        Use a centered modal.
+      </Destination>
+    </Grid>
+  );
+};
