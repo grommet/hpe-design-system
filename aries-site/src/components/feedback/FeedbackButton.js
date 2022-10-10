67b6d18539699250e52ffc9a47c85745d75d79e3
Taylor Seamans <taylor.seamans@yahoo.com>
2022-10-10 15:10:28 -0700

    Fix label color of feedback button (#2959)
    * Fix label color of feedback button
    
    * update yarn.lock

--- aries-site/src/components/feedback/FeedbackButton.js
+++ aries-site/src/components/feedback/FeedbackButton.js
@@ -1,15 +1,32 @@
-import styled from 'styled-components';
+import { useContext } from 'react';
+import styled, { ThemeContext } from 'styled-components';
+import PropTypes from 'prop-types';
 import { Box, Button } from 'grommet';
 
 const PositionedBox = styled(Box)`
   position: fixed;
   bottom: 0px;
+  border-radius: 6px;
   right: 0px;
   z-index: 10;
 `;
 
-export const FeedbackButton = ({ elevation, margin, ...buttonProps }) => (
-  <PositionedBox elevation={elevation} margin={margin}>
-    <Button {...buttonProps} />
-  </PositionedBox>
-);
+// temp fix until this theme issue is resolved:
+// https://github.com/grommet/grommet-theme-hpe/issues/283
+const StyledButton = styled(Button)`
+  color: ${props => props.theme.global.colors['text-strong'].dark};
+`;
+
+export const FeedbackButton = ({ elevation, margin, ...buttonProps }) => {
+  const theme = useContext(ThemeContext);
+  return (
+    <PositionedBox elevation={elevation} margin={margin}>
+      <StyledButton theme={theme} {...buttonProps} />
+    </PositionedBox>
+  );
+};
+
+FeedbackButton.propTypes = {
+  elevation: PropTypes.string,
+  margin: PropTypes.shape({}),
+};
