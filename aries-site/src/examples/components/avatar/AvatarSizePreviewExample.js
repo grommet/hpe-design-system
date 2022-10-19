daad8d8de86b25148baeebd2a93a7db7eb91bc7a
Brittany Archibeque <brittanyarchibeque@dhcp-10-89-85-95.its.hpecorp.net>
2022-10-19 09:33:13 -0600

    clean up code

--- aries-site/src/examples/components/avatar/AvatarSizePreviewExample.js
+++ aries-site/src/examples/components/avatar/AvatarSizePreviewExample.js
@@ -3,31 +3,48 @@ import { Avatar, Box, Text } from 'grommet';
 
 export const AvatarSizePreviewExample = () => (
   <Box align="center" height="small" direction="row" gap="xlarge">
-    <Box align="center" gap="small" width="small">
-      <Avatar
-        size="small"
-        src="//s.gravatar.com/avatar/99020cae7ff399a4fbea19c0634f77c3?s=80"
-        a11yTitle="Eric Soderberg image"
-      />
-      <Text size="small">Small avatar with profile picture</Text>
-    </Box>
-    <Box align="center" gap="small" width="small">
-      <Avatar
-        a11yTitle="Avatar containing initial letters J and S"
-        background="green"
-      >
-        JS
-      </Avatar>
-      <Text size="small">Medium avatar with initials</Text>
-    </Box>
-    <Box align="center" gap="small" width="small">
-      <Avatar
-        round="medium"
-        a11yTitle="shape logo"
-        size="xlarge"
-        src="/Shape.svg"
-      />
-      <Text size="small">Large avatar with logo</Text>
-    </Box>
+    <AvatarContainer
+      alt="Eric Soderberg image"
+      size="small"
+      src="//s.gravatar.com/avatar/99020cae7ff399a4fbea19c0634f77c3?s=80"
+      title="Small avatar with profile picture"
+    ></AvatarContainer>
+    <AvatarContainer
+      background="green"
+      alt="Avatar containing initial letters J and S"
+      title="Medium avatar with initials"
+    >
+      JS
+    </AvatarContainer>
+    <AvatarContainer
+      round="medium"
+      alt="shape logo"
+      size="xlarge"
+      src="/Shape.svg"
+      title="Large avatar with logo"
+    ></AvatarContainer>
+  </Box>
+);
+
+const AvatarContainer = ({
+  alt,
+  background,
+  children,
+  round,
+  size,
+  src,
+  title,
+}) => (
+  <Box direction="column" align="center" gap="small" width="small">
+    <Avatar
+      round={round}
+      a11yTitle={alt}
+      size={size}
+      src={src}
+      background={background}
+    >
+      {children}
+    </Avatar>
+    <Text size="small">{title}</Text>
   </Box>
 );
