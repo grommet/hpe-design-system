547a22872ad0714aa7f88e1957076ecd27a1cc74
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2022-11-04 15:48:28 -0600

    fix lint

--- aries-site/src/examples/components/avatar/AvatarPageHeaderExample.js
+++ aries-site/src/examples/components/avatar/AvatarPageHeaderExample.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Avatar, Box, Button, PageHeader } from 'grommet';
 
 export const AvatarPageHeaderExample = () => (
-  <Box align='end' gap='medium' direction="row">
-    <Avatar 
-      flex={false} 
-      size='large' 
+  <Box align="end" gap="medium" direction="row">
+    <Avatar
+      flex={false}
+      size="large"
       src="//s.gravatar.com/avatar/99020cae7ff399a4fbea19c0634f77c3?s=80"
     />
     <PageHeader
