e4c94f914bc8bfa0b60f7c3e9c7d444f8e994879
britt6612 <42451602+britt6612@users.noreply.github.com>
2022-11-04 14:40:36 -0600

    Update AvatarPageHeaderExample.js

--- aries-site/src/examples/components/avatar/AvatarPageHeaderExample.js
+++ aries-site/src/examples/components/avatar/AvatarPageHeaderExample.js
@@ -3,7 +3,11 @@ import { Avatar, Box, Button, PageHeader } from 'grommet';
 
 export const AvatarPageHeaderExample = () => (
   <Box align='end' gap='medium' direction="row">
-    <Avatar flex={false} size='large' src="//s.gravatar.com/avatar/99020cae7ff399a4fbea19c0634f77c3?s=80" />
+    <Avatar 
+      flex={false} 
+      size='large' 
+      src="//s.gravatar.com/avatar/99020cae7ff399a4fbea19c0634f77c3?s=80"
+    />
     <PageHeader
       title="Account Details"
       subtitle="View your account’s general information"
