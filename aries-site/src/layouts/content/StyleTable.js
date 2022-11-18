3698736ea10408be23640e6b0980da294ff217bf
Taylor Seamans <taylor.seamans@yahoo.com>
2022-11-18 12:53:31 -0800

    Make diagram responsive

---
+++ aries-site/src/layouts/content/StyleTable.js
@@ -0,0 +1,72 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Card,
+  ResponsiveContext,
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableRow,
+  Text,
+} from 'grommet';
+import { Annotation } from './Annotation';
+
+const columns = [
+  { property: 'label', render: 'Label' },
+  { property: 'property', render: 'Property' },
+  { property: 'value', render: 'Value' },
+];
+
+export const StyleTable = ({ data }) => {
+  const breakpoint = useContext(ResponsiveContext);
+  return !['large', 'xlarge'].includes(breakpoint) ? (
+    <Card
+      alignSelf="start"
+      elevation="none"
+      pad={{ top: 'small', horizontal: 'medium', bottom: 'medium' }}
+      width="medium"
+      round="xsmall"
+    >
+      {/* style tag is necessary in order for table not 
+    to exceed container width */}
+      <Table style={{ tableLayout: 'fixed', width: '100%' }}>
+        <TableHeader>
+          <TableRow>
+            {columns.map(c => (
+              <TableCell
+                border="bottom"
+                key={c.property}
+                scope="col"
+                pad={{ vertical: 'small' }}
+              >
+                <Text color="text-strong" weight="bold">
+                  {c.render}
+                </Text>
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data.map(datum => (
+            <TableRow>
+              {columns.map(c => (
+                <TableCell pad={{ vertical: 'xsmall' }}>
+                  {c.property === 'label' ? (
+                    <Annotation target={datum[c.property]} kind="style" />
+                  ) : (
+                    <Text>{datum[c.property]}</Text>
+                  )}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Card>
+  ) : null;
+};
+
+StyleTable.propTypes = {
+  data: PropTypes.array,
+};
