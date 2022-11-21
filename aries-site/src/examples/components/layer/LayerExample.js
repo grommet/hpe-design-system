78d7c9ba2f0c4889e1221b877775f7f9265bfebc
Taylor Seamans <taylor.seamans@yahoo.com>
2022-11-21 13:38:45 -0800

    Update first layer example

--- aries-site/src/examples/components/layer/LayerExample.js
+++ aries-site/src/examples/components/layer/LayerExample.js
@@ -1,58 +1,41 @@
 import React, { useContext, useState } from 'react';
-import {
-  Button,
-  Box,
-  Header,
-  Heading,
-  Layer,
-  ResponsiveContext,
-  Text,
-} from 'grommet';
-import { FormClose } from 'grommet-icons';
+import { Button, Box, Heading, Layer, ResponsiveContext } from 'grommet';
+import { LayerForm } from './ConfigurationForm';
 
 export const LayerExample = () => {
   const [open, setOpen] = useState(false);
-  const size = useContext(ResponsiveContext);
-  const onOpen = () => setOpen(true);
-  const onClose = () => setOpen(undefined);
+  const breakpoint = useContext(ResponsiveContext);
+
   return (
     <>
       <Box align="start">
-        <Button label="Show me the layer" onClick={onOpen} primary />
+        <Button
+          label="Show me the layer"
+          onClick={() => setOpen(true)}
+          primary
+        />
       </Box>
       {open && (
         <Layer
           position="right"
-          full={!['xsmall', 'small'].includes(size) ? 'vertical' : true}
-          modal
-          onClickOutside={onClose}
-          onEsc={onClose}
+          full={['xsmall', 'small'].includes(breakpoint) ? true : 'vertical'}
+          onEsc={() => setOpen(false)}
         >
-          <Box
-            fill="vertical"
-            overflow="auto"
-            width={!['xsmall', 'small'].includes(size) ? 'medium' : undefined}
-            pad="medium"
-          >
-            <Header>
-              <Heading level={2} margin="none">
-                This is a Layer.
-              </Heading>
+          <Box pad="medium" gap="medium" overflow="auto">
+            <Heading level={2} size="small" margin="none">
+              Add application
+            </Heading>
+            <Box>
+              <LayerForm setOpen={setOpen} />
+            </Box>
+            <Box direction="row" gap="small" flex={false}>
               <Button
-                a11yTitle={`You are on a Close button in a layer containing
-                a text description. To close the layer 
-                and return to the primary content, press Enter.`}
-                autoFocus
-                icon={<FormClose />}
-                onClick={onClose}
+                label="Add application"
+                primary
+                type="submit"
+                form="application-form"
               />
-            </Header>
-            <Box overflow="auto" pad={{ vertical: 'medium' }}>
-              <Text>
-                This layer is set to appear on the right and fill the screen
-                vertically. You can add content in here such as a Form or a
-                Chart.
-              </Text>
+              <Button label="Cancel" onClick={() => setOpen(false)} />
             </Box>
           </Box>
         </Layer>
