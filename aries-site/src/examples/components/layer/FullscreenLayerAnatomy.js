2ea46b3a9c68dfec95094a852a3a1a5566453fed
Taylor Seamans <taylor.seamans@yahoo.com>
2022-12-02 11:09:11 -0800

    Update fullscreen

--- aries-site/src/examples/components/layer/FullscreenLayerAnatomy.js
+++ aries-site/src/examples/components/layer/FullscreenLayerAnatomy.js
@@ -1,66 +1,114 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Box, Button, Diagram, Grid, Page, PageContent, Stack } from 'grommet';
+import { FormClose } from 'grommet-icons/icons/FormClose';
 import { Annotation } from '../../../layouts';
 import { connection } from '../../../utils/utils';
 import { LayerHeader } from './components/LayerHeader';
 
-const connections = [
-  connection('1', 'layer-close-button'),
-  connection('2', 'layer-actions'),
-];
+export const FullscreenLayerAnatomy = ({ informational }) => {
+  const closeId = informational
+    ? 'informational-close-button'
+    : 'actionable-close-buttonn';
 
-export const FullscreenLayerAnatomy = () => (
-  <Stack interactiveChild="first">
-    <Grid
-      align="center"
-      columns={['flex', 'auto']}
-      rows={['24px', '37px', '24px', '24px', 'small', '24px', '36px', '24px']}
-      areas={[
-        ['layer', 'empty-1'],
-        ['layer', 'annotation-1'],
-        ['layer', 'empty-2'],
-        ['layer', 'empty-2'],
-        ['layer', 'empty-2'],
-        ['layer', 'empty-2'],
-        ['layer', 'annotation-2'],
-        ['layer', 'empty-3'],
-      ]}
-      gap={{ column: 'medium' }}
-    >
-      <Annotation
-        alignSelf="center"
-        id="1"
-        target="1"
-        gridArea="annotation-1"
-      />
-      <Annotation
-        alignSelf="center"
-        id="2"
-        target="2"
-        gridArea="annotation-2"
-      />
-      <LayerContent gridArea="layer" />
-    </Grid>
-    <Diagram connections={connections} />
-  </Stack>
-);
+  const connections = [
+    connection(informational ? 'info-1' : 'actionable-1', closeId),
+    connection('2', 'layer-actions'),
+  ];
 
-const LayerContent = ({ ...rest }) => (
-  <Page background="background-front" id="layer-container" {...rest}>
-    <PageContent gap="medium" pad="medium">
-      <LayerHeader informational />
-      <Box
-        border={{ style: 'dashed' }}
-        height="small"
+  let rows = ['36px', '36px', 'auto', '36px', '24px'];
+  if (informational) {
+    rows = rows.slice(0, 6);
+  }
+
+  return (
+    <Stack interactiveChild="first">
+      <Grid
         align="center"
-        justify="center"
+        columns={['flex', 'auto']}
+        rows={rows}
+        areas={[
+          ['layer', 'empty-1'],
+          ['layer', 'annotation-1'],
+          ['layer', 'empty-2'],
+          ['layer', 'annotation-2'],
+          ['layer', 'empty-3'],
+        ]}
+        gap={{ column: 'medium' }}
       >
-        Layer body content goes here.
-      </Box>
-      <Box direction="row" gap="small" justify="end" flex={false}>
-        <Button label="Cancel" />
-        <Button label="Confirm action" primary id="layer-actions" />
-      </Box>
-    </PageContent>
-  </Page>
+        <Annotation
+          alignSelf="center"
+          id={informational ? 'info-1' : 'actionable-1'}
+          target="1"
+          gridArea="annotation-1"
+        />
+        {!informational ? (
+          <Annotation
+            alignSelf="center"
+            id="2"
+            target="2"
+            gridArea="annotation-2"
+          />
+        ) : null}
+        <LayerContent
+          gridArea="layer"
+          closeId={closeId}
+          informational={informational}
+        />
+      </Grid>
+      <Diagram connections={connections} />
+    </Stack>
+  );
+};
+
+FullscreenLayerAnatomy.propTypes = {
+  informational: PropTypes.bool,
+};
+
+const LayerContent = ({ closeId, informational, ...rest }) => (
+  <Box {...rest}>
+    <Box
+      direction="row"
+      flex={false}
+      gap="xsmall"
+      /* This Box represents a header of a browser application window. 
+            The selected background color provides better contrast between the 
+            header and the examples being showcased. */
+      background={{ color: 'border-weak' }}
+      align="center"
+      pad="small"
+    >
+      <Box round pad="xsmall" background="red" />
+      <Box round pad="xsmall" background="yellow" />
+      <Box round pad="xsmall" background="green" />
+    </Box>
+    <Page background="background-front" id="layer-container">
+      <Button alignSelf="end" icon={<FormClose />} id={closeId} />
+      <PageContent
+        gap="medium"
+        pad={{ horizontal: 'medium', bottom: 'medium' }}
+      >
+        <LayerHeader />
+        <Box
+          border={{ style: 'dashed' }}
+          height="small"
+          align="center"
+          justify="center"
+        >
+          Layer body content goes here.
+        </Box>
+        {!informational ? (
+          <Box direction="row" gap="small" flex={false}>
+            <Button label="Confirm action" primary />
+            <Button label="Cancel" id="layer-actions" />
+          </Box>
+        ) : null}
+      </PageContent>
+    </Page>
+  </Box>
 );
+
+LayerContent.propTypes = {
+  closeId: PropTypes.string,
+  informational: PropTypes.bool,
+};
