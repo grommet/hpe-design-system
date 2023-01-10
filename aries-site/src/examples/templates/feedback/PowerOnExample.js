3bf4a328d10194e1161ac5ebec82ca3c137005cc
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2023-01-10 14:07:49 -0700

    change flow of example

--- aries-site/src/examples/templates/feedback/PowerOnExample.js
+++ aries-site/src/examples/templates/feedback/PowerOnExample.js
@@ -1,65 +1,29 @@
-import React, { useCallback, useContext, useEffect, useState } from 'react';
+import React, { useCallback, useContext, useState } from 'react';
 import {
   Anchor,
+  AnnounceContext,
   Box,
   Button,
-  Card,
-  Layer,
   Form,
-  Footer,
   FormField,
-  Heading,
   Spinner,
   StarRating,
-  ResponsiveContext,
   Text,
   TextArea,
 } from 'grommet';
+import { ModalBody, ModalDialog, ModalFooter } from '../ModalDialog';
 import { StatusGoodSmall } from 'grommet-icons';
 
-const FeedbackLayout = ({ children, onClick, onClose }) => {
-  const breakpoint = useContext(ResponsiveContext);
-  return (
-    // feedback layout
-    <Layer>
-      <Box
-        fill="vertical"
-        overflow="auto"
-        width={!['xsmall', 'small'].includes(breakpoint) ? 'medium' : undefined}
-        pad="medium"
-        gap="medium"
-      >
-        <Heading level={4} size="small" margin="none">
-          Feedback on powering devices
-        </Heading>
-        {children}
-        <Footer direction="row" justify="end" gap="small">
-          <Button onClick={onClose} label="Cancel" />
-          <Button
-            onClick={onClick}
-            label="Submit Feedback"
-            primary
-            type="submit"
-          />
-        </Footer>
-      </Box>
-    </Layer>
-  );
-};
-
-export const PowerOnExample = () => {
+export const PowerOnExample = ({ containerRef }) => {
+  // containerRef is for demonstration purposes on this site. Most
+  // implementations should likely remove.
+  const [showModal, setShowModal] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
   const [isSuccessful, setIsSuccessful] = useState(false);
   const [rating, setRating] = useState();
   const [showFeedback, setShowFeedback] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      if (isSuccessful) {
-        setShowFeedback(true);
-      }
-    }, 1000);
-  }, [isSuccessful]);
+  // announce when the layer opens
+  const announce = useContext(AnnounceContext);
 
   // simulating a call to a power on API call. Demo purposes
   const PowerOnLoading = () => {
@@ -103,103 +67,152 @@ export const PowerOnExample = () => {
   }, []);
 
   return (
-    <Card width="large" gap="medium" justify="center" pad="medium">
-      <Box>
-        <Heading level={1} size="small" margin="none">
-          Power On
-        </Heading>
-        <Anchor href="#" label="Learn more about server actions" />
-      </Box>
-      {!isSuccessful ? (
-        <Text size="large">You are powering on 1 device</Text>
-      ) : (
-        <Box align="center" gap="xsmall" direction="row">
-          <StatusGoodSmall color="green" />
-          <Text size="large">Power On initiated for 1 device.</Text>
-        </Box>
-      )}
-      {!isLoading && !isSuccessful && (
-        <Text size="large">FTCUSAMountain_ML350Gen10_testcluster</Text>
-      )}
-      {isLoading && (
-        <Spinner
-          alignSelf="center"
-          size="medium"
-          message={{
-            start: 'Loading more data.',
-            end: 'Data has been loaded.',
+    <Box align="center" justify="center" fill>
+      <Button
+        primary
+        label="Power on device"
+        onClick={() => {
+          setShowModal(true);
+        }}
+      />
+      {showModal ? (
+        <ModalDialog
+          title="Power On"
+          onEsc={() => {
+            announce(
+              `Power on device} modal cancelled and closed.`,
+              'assertive',
+            );
+            setShowModal(false);
           }}
-        />
-      )}
-      {!isSuccessful && (
-        <Box
-          justify="end"
-          gap="medium"
-          margin={{ top: 'medium', bottom: 'small' }}
-          direction="row"
+          subtitle={<Anchor href="#" label="Learn more about server actions" />}
+          target={containerRef && containerRef.current}
         >
-          <Button label="Dismiss" />
-          <Button
-            label="Power on 1 Device"
-            onClick={PowerOnLoading}
-            primary
-            type="submit"
-          />
-        </Box>
-      )}
-      {isSuccessful && showFeedback && (
-        <FeedbackLayout
-          onClick={() => {
-            setIsSuccessful(false);
-            setRating(undefined);
-            setShowFeedback(false);
-          }}
-          onClose={() => {
-            setShowFeedback(false);
-          }}
-        >
-          <Box gap="medium">
-            <Form
-              method="post"
-              validate="submit"
-              kind="survey"
-              onChange={(nextValue, { touched }) => {
-                setRating(touched);
-              }}
-              onSubmit={value => {
-                console.log('submit', value);
-              }}
-            >
-              <FormField
-                // remove border when using StarRating & ThumbRating
-                contentProps={{
-                  border: false,
+          <ModalBody gap="medium">
+            {!isSuccessful ? (
+              <Text size="large">You are powering on 1 device</Text>
+            ) : (
+              <Box align="center" gap="xsmall" direction="row">
+                <StatusGoodSmall color="green" />
+                <Text size="large">Power On initiated for 1 device.</Text>
+              </Box>
+            )}
+            {!isLoading && !isSuccessful && (
+              <Text size="large">FTCUSAMountain_ML350Gen10_testcluster</Text>
+            )}
+            {isLoading && (
+              <Spinner
+                alignSelf="center"
+                size="medium"
+                message={{
+                  start: 'Loading more data.',
+                  end: 'Data has been loaded.',
                 }}
-                label="How easy was it for you to power off your selected devices?"
-                htmlFor="power-rating"
-                name="power-rating-example"
-              >
-                <StarRating id="power-rating" name="power-rating-example" />
-              </FormField>
-              {rating && (
-                <Box gap="medium">
-                  <FormField
-                    name="power-textArea-example"
-                    htmlFor="power-text-area-example"
-                    label="How satisfied were you with this process?"
-                  >
-                    <TextArea
+              />
+            )}
+          </ModalBody>
+          <ModalFooter justify="end">
+            {!isSuccessful && !isLoading ? (
+              <Box justify="end" gap="medium" direction="row">
+                <Button onClick={() => setShowModal(false)} label="Dismiss" />
+                <Button
+                  label="Power on 1 Device"
+                  onClick={PowerOnLoading}
+                  primary
+                  type="submit"
+                />
+              </Box>
+            ) : (
+              <Box justify="end">
+                <Button
+                  label="Close"
+                  disabled={isLoading ? true : false}
+                  onClick={() => {
+                    setShowFeedback(true);
+                    setShowModal(false);
+                  }}
+                  primary
+                />
+              </Box>
+            )}
+          </ModalFooter>
+        </ModalDialog>
+      ) : null}
+      {isSuccessful && showFeedback && (
+        <ModalDialog
+          title="Feedback on powering devices"
+          onEsc={() => {
+            announce(
+              `Feedback on powering devices modal cancelled and closed.`,
+              'assertive',
+            );
+            setShowModal(false);
+          }}
+          target={containerRef && containerRef.current}
+        >
+          <Form
+            method="post"
+            validate="submit"
+            kind="survey"
+            onChange={(nextValue, { touched }) => {
+              setRating(touched);
+            }}
+            onSubmit={value => {
+              console.log('submit', value);
+              setIsSuccessful(false);
+              setRating(undefined);
+              setShowFeedback(false);
+            }}
+          >
+            <Box gap="medium">
+              <ModalBody>
+                <FormField
+                  // remove border when using StarRating & ThumbRating
+                  contentProps={{
+                    border: false,
+                  }}
+                  label="How easy was it for you to power off your selected devices?"
+                  htmlFor="power-rating"
+                  name="power-rating-example"
+                >
+                  <StarRating id="power-rating" name="power-rating-example" />
+                </FormField>
+                {rating && (
+                  <Box gap="medium">
+                    <FormField
                       name="power-textArea-example"
-                      id="power-text-area-example"
-                      placeholder="i.e. ideas, inspirations, or concerns"
-                    />
-                  </FormField>
+                      htmlFor="power-text-area-example"
+                      label="How satisfied were you with this process?"
+                    >
+                      <TextArea
+                        name="power-textArea-example"
+                        id="power-text-area-example"
+                        placeholder="i.e. ideas, inspirations, or concerns"
+                      />
+                    </FormField>
+                  </Box>
+                )}
+              </ModalBody>
+              <ModalFooter justify="end">
+                <Box direction="row" gap="small">
+                  <Button
+                    label="Cancel"
+                    onClick={() => {
+                      announce(
+                        `Feedback on powering devices modal cancelled and closed.`,
+                        'assertive',
+                      );
+                      setShowFeedback(false);
+                      setIsSuccessful(false);
+                    }}
+                  />
+                  <Button label="Submit Feedback" primary type="submit" />
                 </Box>
-              )}
-            </Form>
-          </Box>
-        </FeedbackLayout>
+              </ModalFooter>
+            </Box>
+          </Form>
+        </ModalDialog>
       )}
-    </Card>
+    </Box>
   );
 };
