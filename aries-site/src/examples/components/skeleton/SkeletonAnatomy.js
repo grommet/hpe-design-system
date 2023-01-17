549b58afa9de6f1de6c3d01bf4e746b1e1a87270
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2023-01-17 13:39:34 -0700

    update page with new figma chaanges

--- aries-site/src/examples/components/skeleton/SkeletonAnatomy.js
+++ aries-site/src/examples/components/skeleton/SkeletonAnatomy.js
@@ -1,87 +1,10 @@
 import React from 'react';
-import {
-  Box,
-  Button,
-  Card,
-  CardBody,
-  Diagram,
-  Grid,
-  Heading,
-  Paragraph,
-  Stack,
-} from 'grommet';
-import { Annotation } from '../../../layouts';
+import { Box, Button, Card, CardBody, Paragraph } from 'grommet';
 
-const connection = (fromTarget, toTarget) => ({
-  color: 'border',
-  anchor: 'horizontal',
-  thickness: 'hair',
-  type: 'direct',
-  fromTarget,
-  toTarget,
-});
-
-const connections = [
-  connection('1', 'icon'),
-  connection('2a', 'title'),
-  connection('2b', 'text-body'),
-  connection('3', 'button'),
-  connection('4', 'container'),
-];
-
-export const SkeletonAnatomy = () => (
-  <Stack interactiveChild="first">
-    <Grid
-      columns={['xxsmall', 'medium', 'xxsmall']}
-      gap={{ column: 'medium' }}
-      rows={['90px', '54px', '80px', '60px']}
-      areas={[
-        ['annotation-1', 'card-example', 'empty-1'],
-        ['annotation-2a', 'card-example', 'annotation-4'],
-        ['annotation-2b', 'card-example', 'empty-2'],
-        ['annotation-3', 'card-example', 'empty-3'],
-      ]}
-    >
-      <Annotation
-        id="1"
-        target="1"
-        gridArea="annotation-1"
-        margin={{ top: 'medium' }}
-      />
-      <Annotation
-        id="2a"
-        target="2a"
-        gridArea="annotation-2a"
-        margin={{ top: 'small' }}
-      />
-      <Annotation
-        id="2b"
-        target="2b"
-        gridArea="annotation-2b"
-        margin={{ top: 'medium' }}
-      />
-      <Annotation
-        id={3}
-        target="3"
-        gridArea="annotation-3"
-        margin={{ top: 'small' }}
-      />
-      <CardExample gridArea="card-example" />
-      <Annotation
-        id={4}
-        target="4"
-        gridArea="annotation-4"
-        margin={{ top: 'medium' }}
-      />
-    </Grid>
-    <Diagram connections={connections} />
-  </Stack>
-);
-
-const CardExample = ({ ...rest }) => (
-  <Card id="container" flex={false} width="medium" {...rest}>
+export const SkeletonAnatomy = ({ ...rest }) => (
+  <Card id="container" flex={false} width="large" {...rest}>
     <CardBody align="start" gap="medium">
-      <Box id="icon" margin={{ bottom: 'medium' }}>
+      <Box align="center" gap="small" direction="row">
         <Box
           round="small"
           width="xxsmall"
@@ -89,18 +12,29 @@ const CardExample = ({ ...rest }) => (
           background="background-back"
           flex={false}
         />
-      </Box>
-      <Box flex={false} skeleton id="title">
-        <Heading size="small" margin="none" level={3} />
+        <Box
+          round="large"
+          width="xxsmall"
+          height="xxsmall"
+          background="background-back"
+          flex={false}
+        />
+        <Box skeleton flex={false}>
+          <Button label="View Guides" secondary />
+        </Box>
+        <Box
+          round="xsmall"
+          width="small"
+          height="24px" // match pixel size with button or text
+          background="background-back"
+          flex={false}
+        />
       </Box>
       <Box skeleton width="medium" id="text-body">
         <Paragraph>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </Paragraph>
-      </Box>
-      <Box skeleton id="button" flex={false}>
-        <Button label="View Guides" secondary />
       </Box>
     </CardBody>
   </Card>
