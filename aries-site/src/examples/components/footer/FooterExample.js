3235038b6ef61c29896eb40363b64aa4d8de2b29
Matt Glissmann <mdglissmann@gmail.com>
2023-01-24 13:44:35 -0700

    remove size prop references from typography elements on footer page

--- aries-site/src/examples/components/footer/FooterExample.js
+++ aries-site/src/examples/components/footer/FooterExample.js
@@ -24,9 +24,7 @@ export const FooterExample = () => {
         align={!['xsmall', 'small'].includes(size) ? 'center' : undefined}
         gap="xsmall"
       >
-        <Text size="small">
-          &copy; {year} Hewlett Packard Enterprise Development LP
-        </Text>
+        <Text>&copy; {year} Hewlett Packard Enterprise Development LP</Text>
       </Box>
       <Box
         direction="row"
