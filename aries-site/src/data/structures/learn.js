c699b8b7619f179fe513350b9692622a52f2d885
Taylor Seamans <taylor.seamans@yahoo.com>
2023-02-22 15:27:33 -0800

    Add preview

--- aries-site/src/data/structures/learn.js
+++ aries-site/src/data/structures/learn.js
@@ -1,7 +1,22 @@
 /* eslint-disable max-len */
-import { DataHowTo } from '../../examples/cardPreviews';
+import { BoxPreview, DataHowTo } from '../../examples/cardPreviews';
 
 export const learn = [
+  {
+    name: 'The Box Model Part One',
+    render: 'The Box Model: Part One',
+    description:
+      'Build a hero section to learn about fundamental Box properties.',
+    preview: {
+      component: () => <BoxPreview />,
+      background: 'background-front',
+    },
+    seoDescription:
+      'Build a hero section to learn about fundamental Box properties.',
+    sections: [],
+    relatedContent: ['Box', 'Button', 'Typography'],
+    tags: ['Box', 'layout', 'fundamentals'],
+  },
   {
     name: 'How to add search and filter to DataTable with Data',
     description:
@@ -14,17 +29,6 @@ export const learn = [
       'This how-to guide shows you how to use the Data component to easily add searching and filtering to a DataTable.',
     sections: [],
     relatedContent: [],
-    tags: [],
-  },
-  {
-    name: 'The Box Model Part One',
-    render: 'The Box Model: Part One',
-    description:
-      'Build a hero section to learn about fundamental Box properties.',
-    seoDescription:
-      'Build a hero section to learn about fundamental Box properties.',
-    sections: [],
-    relatedContent: ['Box', 'Button', 'Typography'],
     tags: [],
   },
   {
