5f9b3fa41e5b06df19a69527a00c0a18a0784e86
Matt Glissmann <mdglissmann@gmail.com>
2023-04-03 09:30:49 -0600

    revert icon imports aries-core

--- aries-core/src/js/components/core/Nav/Nav.js
+++ aries-core/src/js/components/core/Nav/Nav.js
@@ -2,9 +2,7 @@
 import React, { useState, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Box, Button, Collapsible, Text, ResponsiveContext } from 'grommet';
-import { Close } from 'grommet-icons/icons/Close';
-import { Hpe } from 'grommet-icons/icons/Hpe';
-import { Menu } from 'grommet-icons/icons/Menu';
+import { Close, Hpe, Menu } from 'grommet-icons';
 
 const PAD_SIZES = ['xxsmall', 'xsmall', 'small', 'medium', 'large', 'xlarge'];
 
