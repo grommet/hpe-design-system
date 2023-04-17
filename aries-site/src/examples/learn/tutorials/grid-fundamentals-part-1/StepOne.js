fb319272667a00a62290a954fbbddfca11e498ac
Matt Glissmann <mdglissmann@gmail.com>
2023-04-17 17:20:01 -0600

    incorporate last round of feedback and some alt tags

--- aries-site/src/examples/learn/tutorials/grid-fundamentals-part-1/StepOne.js
+++ aries-site/src/examples/learn/tutorials/grid-fundamentals-part-1/StepOne.js
@@ -8,25 +8,25 @@ const ProductsPage = () => {
     <>
       <Heading level={1}>Products</Heading>
       <Grid columns={{ count: 3, size: 'auto' }}>
-        <Box background="blue" border pad="medium" align="center">
+        <Box background="blue!" border pad="medium" align="center">
           <Text size="large">Product 1</Text>
         </Box>
-        <Box background="blue" border pad="medium" align="center">
+        <Box background="blue!" border pad="medium" align="center">
           <Text size="large">Product 2</Text>
         </Box>
-        <Box background="blue" border pad="medium" align="center">
+        <Box background="blue!" border pad="medium" align="center">
           <Text size="large">Product 3</Text>
         </Box>
-        <Box background="blue" border pad="medium" align="center">
+        <Box background="blue!" border pad="medium" align="center">
           <Text size="large">Product 4</Text>
         </Box>
-        <Box background="blue" border pad="medium" align="center">
+        <Box background="blue!" border pad="medium" align="center">
           <Text size="large">Product 5</Text>
         </Box>
-        <Box background="blue" border pad="medium" align="center">
+        <Box background="blue!" border pad="medium" align="center">
           <Text size="large">Product 6</Text>
         </Box>
-        <Box background="blue" border pad="medium" align="center">
+        <Box background="blue!" border pad="medium" align="center">
           <Text size="large">Product 7</Text>
         </Box>
       </Grid>
