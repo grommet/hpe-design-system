34f0012506f4e8b2676d03bc9f01bec2e09437a7
Matt Glissmann <mdglissmann@gmail.com>
2023-05-04 16:01:13 -0600

    clean up anatomy a bit

--- aries-site/src/examples/templates/inline-notifications/InlineNotificationAnatomy.js
+++ aries-site/src/examples/templates/inline-notifications/InlineNotificationAnatomy.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Anchor, Box, Button, Diagram, Grid, Stack, Text } from 'grommet';
-import { FormClose } from 'grommet-icons/icons/FormClose';
-import { StatusWarningSmall } from 'grommet-icons/icons/StatusWarningSmall';
+import { Anchor, Box, Button, Diagram, Grid, Stack, Paragraph } from 'grommet';
+import { FormClose, StatusWarningSmall } from 'grommet-icons';
+import { TextEmphasis } from 'aries-core';
 import { Annotation } from '../../../layouts';
 import { connection } from '../../../utils';
 
 const connections = [
-  connection('1', 'icon', 'vertical'),
+  connection('1', 'icon', 'horizontal'),
   connection('2', 'title', 'vertical'),
-  connection('3', 'message', 'horizontal'),
+  connection('3', 'message', 'vertical'),
   connection('4', 'anchor', 'vertical'),
   connection('5', 'close-button', 'vertical'),
 ];
@@ -19,6 +19,7 @@ const NotificationContent = ({ ...rest }) => (
     alignSelf="start"
     round="xsmall"
     background="validation-warning"
+    fill
     {...rest}
   >
     <Box
@@ -26,84 +27,86 @@ const NotificationContent = ({ ...rest }) => (
       gap="xsmall"
       pad={{ horizontal: 'small', vertical: 'xsmall' }}
     >
-      <StatusWarningSmall id="icon" color="status-warning" />
+      <StatusWarningSmall
+        gridArea="icon"
+        id="icon"
+        color="status-warning"
+        height="medium"
+      />
       <Box fill border={{ style: 'dashed' }}>
-        <Box alignSelf="start" id="title">
-          <Text>Title</Text>
-        </Box>
+        <TextEmphasis id="title" alignSelf="start">
+          Title
+        </TextEmphasis>
         <Box direction="row" gap="xsmall">
-          <Box id="message">
-            <Text>Message</Text>
-          </Box>
+          <Paragraph id="message" margin="none">
+            Message
+          </Paragraph>
           <Anchor id="anchor">Anchor</Anchor>
         </Box>
       </Box>
-      <Box>
-        <Button
-          id="close-button"
-          hoverIndicator
-          plain
-          icon={<FormClose color="text-strong" />}
-        />
-      </Box>
+      <Button
+        id="close-button"
+        alignSelf="start"
+        hoverIndicator
+        plain
+        icon={<FormClose color="text-strong" />}
+      />
     </Box>
   </Box>
 );
 
 export const InlineNotificationAnatomy = () => {
-  let columns = ['34px', '44px', '24px', '94px', 'auto'];
-  let rows = ['30px', 'auto'];
-
-  let areas = [
-    ['empty', 'annotation-1', 'annotation-2', 'annotation-4', 'annotation-5'],
+  const columns = ['xxsmall', 'auto', 'auto', 'auto', 'flex', 'auto'];
+  const rows = ['xxsmall', 'auto', 'auto', 'xxsmall'];
+  const areas = [
     [
-      'annotation-3',
+      'empty-1-1',
+      'empty-2-1',
+      'annotation-2',
+      'empty-4-1',
+      'empty-5-1',
+      'annotation-5',
+    ],
+    [
+      'annotation-1',
+      'notification',
       'notification',
       'notification',
       'notification',
       'notification',
     ],
+    [
+      'empty-1-3',
+      'notification',
+      'notification',
+      'notification',
+      'notification',
+      'notification',
+    ],
+    [
+      'empty-1-4',
+      'empty-2-4',
+      'annotation-3',
+      'annotation-4',
+      'empty-5',
+      'empty-5',
+    ],
   ];
 
   return (
     <Stack interactiveChild="first">
-      <Grid columns={columns} rows={rows} areas={areas} gap={{ row: 'large' }}>
-        <Box gridArea="empty" />
-        <Box>
-          <Annotation
-            alignSelf="center"
-            id="1"
-            target="1"
-            gridArea="annotation-1"
-          />
-        </Box>
-        <Box>
-          <Annotation
-            alignSelf="center"
-            id="2"
-            target="2"
-            gridArea="annotation-2"
-          />
-        </Box>
-        <Box>
-          <Annotation
-            alignSelf="center"
-            id="4"
-            target="4"
-            gridArea="annotation-4"
-          />
-        </Box>
-        <Box pad={{ right: 'xsmall' }}>
-          <Annotation
-            alignSelf="end"
-            id="5"
-            target="5"
-            gridArea="annotation-5"
-          />
-        </Box>
-        <Box pad={{ bottom: 'xsmall' }} alignSelf="end">
-          <Annotation id="3" target="3" gridArea="annotation-3" />
-        </Box>
+      <Grid
+        columns={columns}
+        rows={rows}
+        areas={areas}
+        align="center"
+        justify="center"
+      >
+        <Annotation id="1" target="1" gridArea="annotation-1" />
+        <Annotation id="2" target="2" gridArea="annotation-2" />
+        <Annotation id="3" target="3" gridArea="annotation-3" />
+        <Annotation id="4" target="4" gridArea="annotation-4" />
+        <Annotation id="5" target="5" gridArea="annotation-5" />
         <NotificationContent gridArea="notification" />
       </Grid>
       <Diagram connections={connections} />
