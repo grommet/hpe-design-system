e996cf3c4dbef99a69da43b3c52ad080ea21df00
Jeffrey Chen <j.chen14641@gmail.com>
2023-05-18 12:09:25 -0700

    Export Data Container in Modal (#3356)
    * first version of export modal
    
    * incorporate feedback for caller behavior
    
    * incorporate more feedback and fix action menu
    
    * update yarn.lock
    
    * update yarn.lock
    
    * Update aries-site/src/examples/components/pageheader/PageHeaderIntroExample.js
    
    ---------
    
    Co-authored-by: Jeffrey Chen <jeffreychen@Jeffreys-MacBook-Pro.local>
    Co-authored-by: Matthew Glissmann <mdglissmann@gmail.com>

--- aries-site/src/examples/components/pageheader/PageHeaderIntroExample.js
+++ aries-site/src/examples/components/pageheader/PageHeaderIntroExample.js
@@ -1,17 +1,41 @@
-import React, { useContext } from 'react';
-import { Button, PageHeader, ResponsiveContext } from 'grommet';
+import React, { useState, useContext } from 'react';
+import {
+  AnnounceContext,
+  Box,
+  Button,
+  ResponsiveContext,
+  PageHeader,
+} from 'grommet';
 import { ActionMenu } from 'aries-core';
+
+import { ExportDataContainer } from '../../templates/export-data';
+
 import { ReverseAnchor } from '../../templates';
 
-const actions = [
-  {
-    label: 'Export devices',
-    secondary: true,
-  },
-];
+const label = 'Export report';
 
 export const PageHeaderIntroExample = () => {
   const breakpoint = useContext(ResponsiveContext);
+  const [showDataExport, setShowDataExport] = useState(false);
+
+  const announce = useContext(AnnounceContext);
+  const onClose = () => {
+    setShowDataExport(false);
+  };
+
+  const ExportContainer = (
+    <ExportDataContainer title={label} onClose={onClose} announce={announce} />
+  );
+
+  const actions = [
+    {
+      label,
+      secondary: true,
+      onClick: () => {
+        setShowDataExport(true);
+      },
+    },
+  ];
 
   return (
     <PageHeader
@@ -20,9 +44,21 @@ export const PageHeaderIntroExample = () => {
       parent={<ReverseAnchor label="Dashboard" />}
       actions={
         breakpoint !== 'xsmall' ? (
-          <Button label="Export devices" secondary />
+          <Box>
+            <Button
+              label={label}
+              secondary
+              onClick={() => {
+                setShowDataExport(true);
+              }}
+            />
+            {showDataExport ? ExportContainer : null}
+          </Box>
         ) : (
-          <ActionMenu items={actions} />
+          <Box>
+            <ActionMenu items={actions} />
+            {showDataExport ? ExportContainer : null}
+          </Box>
         )
       }
     />
