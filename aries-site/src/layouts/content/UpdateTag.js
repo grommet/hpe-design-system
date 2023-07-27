b4446b872cd17735d2d94d06ce793cbe0c4141b8
beahackman <hackmanbeatrice@gmail.com>
2023-07-27 15:26:50 -0700

    added visitation tracking, search tags, and nav tags

--- aries-site/src/layouts/content/UpdateTag.js
+++ aries-site/src/layouts/content/UpdateTag.js
@@ -1,32 +1,71 @@
 import { Anchor, Text, Notification } from 'grommet';
 import { CircleInformation } from 'grommet-icons';
+import pageVisitTracker from '../../utils/pageVisitTracker';
+import { createBrowserHistory } from 'history';
 
 export const UpdateTag = ({name}) => {
-    const history = JSON.parse(window.localStorage.getItem("update-history"));
-    let newUpdate;
-    if(name in history){
-      newUpdate = history[name].update;
-    }else{
-      newUpdate = false;
+    const viewHistory = JSON.parse(window.localStorage.getItem("update-history"));
+    const tokenName = `${name?.toLowerCase().replace(/\s+/g,'-')  }-last-visited`;
+    const dateTime = new Date().getTime();
+    let newUpdate, actionLink;
+
+    //In general, I decided to keep the logic for tracking visitation in the updateTag
+    //I did this because I ran into a lot of bugs with having it in Layout.js (trying to track data for parent pages and needing to communicate with content card)
+    //so narrowing it down to this component (that will always be shown when there's an update) worked the best in this case
+    if(viewHistory){
+        if(name in viewHistory){
+            let history = createBrowserHistory();
+
+            if(history.location.hash.length === 0 && history.location.search.length === 0){
+            //if we are on a guidance w/ no hash or query in the URL
+            //this is the case when people enter the page for the first time that session (assuming they don't refresh the page)
+                newUpdate = pageVisitTracker(name); //check if they've seen it before
+                actionLink = viewHistory[name].action; //grab the section link
+
+                //since it is their first time in this session viewing the page (assuming they don't refresh the page), now store the information
+                window.localStorage.setItem(tokenName, dateTime); //holds the most recent viewing time
+                viewHistory[name].update = newUpdate; //sets the "update state" for the component in overarching update-history
+                window.localStorage.setItem("update-history", JSON.stringify(viewHistory));
+                //^^this allows for easy access across other components that need to access it in strange cases (like the refresh in the internal navigation)
+            }else{ 
+            //the case where they have come to the page, but it refreshes it with a hash added to the link via the sideNav bar
+            //it keeps the pre-established update state in the general "update-history" from when they first came on the page
+            //this is the part that keeps the inline notif and sidebar navigation as they internally navigate the page
+                newUpdate = viewHistory[name].update;
+                //^^grab it from the previously stated update state
+                //^^trying to use the pageVisitTracker function in this case would grab the most recent visit, say it's been seen, and report that the update shouldnt be shown (and it should be shown)
+                actionLink = viewHistory[name].action;
+            }
+        }else{
+            newUpdate = false;
+        }
     }
-    let link = history[name].action;
+
     return(
         <>
-        {newUpdate && 
+        {newUpdate &&  (viewHistory[name].type === "Update") &&
             <Notification width='large'
-            title={new Date(history[name].date).toDateString()} 
+            title={"Updated " + new Date(viewHistory[name].date).toDateString()} 
             message={
                 <>
-                <Text>{history[name].description}</Text>
-                {history[name].action && 
-                    <Anchor label='See section here' href={link}/> 
-                    //WARNING!! depending on what the devs tell us, this is assuming they give us a link to the section
-                    //this will re-route them to the actual HPE site and not the local host so it's a little funky right now on a localHost
-                    //another way we could do an action button: they give us the name of the section changed, and i write a little function
-                    //to turn it into proper href formatting and I can take them there from that
-                    //however, this would rely on the devs perfect spelling/spacing of the title for it to work properly
-                    //and i think accuracy is more achievable from a link
+                <Text>{viewHistory[name].description + "    "}
+                {viewHistory[name].action.length > 1 && 
+                    <Anchor label='Link' href={actionLink}/> 
                 }
+                </Text>
+                </>
+            } 
+            margin={{ bottom: 'medium' }} icon={<CircleInformation/>}
+            />
+        }
+        {newUpdate &&  (viewHistory[name].type === "New") &&
+            <Notification width='large'
+            title={"Added on " + new Date(viewHistory[name].date).toDateString()} 
+            message={
+                <>
+                <Text>
+                This item is new. Let the Design Systems team know if you have any feedback. 
+                </Text>
                 </>
             } 
             margin={{ bottom: 'medium' }} icon={<CircleInformation/>}
--- aries-site/src/utils/pageVisitTracker.js
+++ aries-site/src/utils/pageVisitTracker.js
@@ -1,38 +1,26 @@
-import contributorCommitMessages from '../layouts/content/ContributerCommitMessages';
-//NOT FUNCTIONAL WITH THE PR METHOD YET, THIS IS WHAT I'M WORKING ON NOW
-// I haven't connected it just yet, but I would implement this in the ContentCard.js and Layout.js for the updateTag
-const pageVisitTracker = function(title, currentFileName, topic){
-  const dateTime = new Date().getTime();
+//this is the function i call to report whether or not the update should be shown given the most recent commit and last-visit records
+const pageVisitTracker = function(title){
   const tokenName = `${title?.toLowerCase().replace(/\s+/g,'-')  }-last-visited`; // the name associated with the values in localStorage
   let newUpdate;
-  const updateLog = contributorCommitMessages(currentFileName, topic);
-  console.log(updateLog);
-
-  // add edge cases for the main catalog pages
-  // if the info already exists and it's not a main page we are on, then go ahead and check the next
-  if(window.localStorage.getItem(tokenName)){
-    // if the time they last visited the page (minus 5 seconds to cover that weird refresh) is older than the most recent commit
-    if((window.localStorage.getItem(tokenName)) < new Date(updateLog[0].date).getTime()){
-      newUpdate = true; // then show the update
-    }else{ // in the case where they have the token, but they have already seen the update
-      newUpdate = false;
-    }
-    if(window.localStorage.getItem(tokenName) > new Date(now.getTime() - 5*1000)){ // if the most recent token is older than 5 seconds ago then set it, otherwise dont. this covers cases of the page refreshing a few times
-      window.localStorage.setItem(tokenName, dateTime);
-    }
-  }else if(updateLog.length === 0){
-    console.log("here!");
-    newUpdate=false;
-  }
-  else{ // in the case that they do not have the token
-    if(new Date(updateLog[0].date).getTime() > new Date(new Date().setDate(dateTime.getDate() - 30))){ // if it is been less than 30 days since the commit
+  let history = JSON.parse(window.localStorage.getItem("update-history"));
+  if(title in history){ //there has been a reported update within the last 30 days
+    if(window.localStorage.getItem(tokenName)){ //there has been a guaranteed update and we've seen this page before
+      if(window.localStorage.getItem(tokenName) > new Date(history[title].date).getTime()){ //when we've seen this page, has it been after the update?
+        //console.log("A");
+        newUpdate = false; //this means that the page has a reported update within 30 days, it has been seen before, but they have already seen this update
+      }else{
+        //console.log("B");
+        newUpdate = true; //this means that the page has a reported update within 30 days, it has been seen before, and when it has been seen, it was before the update
+      }
+    }else{ //it's within 30 days but we have never seen the page before
+      //console.log("C");
       newUpdate = true;
-    }else{ // if the most recent commit isn't new anymore
-      newUpdate = false;
     }
-    window.localStorage.setItem(tokenName, dateTime);// set token
+  }else{
+    //console.log("D");
+    newUpdate = false; //definitely no update, nothing reported in the PRs
   }
-  return newUpdate; 
+  return newUpdate;
 };
 
 export default pageVisitTracker;
