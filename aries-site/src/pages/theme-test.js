ab5c62d42c01fb08cac78ab00127e599ede0a5c5
Taylor Seamans <taylor.seamans@yahoo.com>
2023-09-20 11:06:10 -0700

    Add mock detail page

--- aries-site/src/pages/theme-test.js
+++ aries-site/src/pages/theme-test.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import {
+  Anchor,
   Box,
   Button,
   Heading,
+  NameValueList,
+  NameValuePair,
   Page,
   PageContent,
   PageHeader,
@@ -15,7 +18,8 @@ import { hpe } from 'grommet-theme-hpe';
 import { ButtonGroup } from 'aries-core';
 import { Meta } from '../components';
 import { aries } from '../themes/aries';
-import { FilterServers } from '../examples';
+import { FilterServers, ReverseAnchor } from '../examples';
+import servers from '../data/mockData/servers.json';
 // import { getPageDetails } from '../utils';
 
 const title = 'ThemeTest';
@@ -31,7 +35,8 @@ const Container = ({ ...rest }) => (
 );
 
 const MockPage = () => (
-  <Page background="background-front">
+  <Page background="background-front" pad={{ bottom: 'xlarge' }}>
+    <hpe-global-header kind="glcp" />
     <PageContent gap="medium">
       <PageHeader
         title="Devices"
@@ -44,16 +49,71 @@ const MockPage = () => (
           </ButtonGroup>
         }
       />
-      {/* <Box gap="xsmall">
-        <Heading level={2} margin="none">
-          Manage devices
-        </Heading>
-        <Paragraph margin="none">Here is some supplemental info.</Paragraph>
-      </Box> */}
       <FilterServers />
     </PageContent>
   </Page>
 );
+
+const MockDetailPage = () => {
+  const server = servers.servers.items[0];
+
+  return (
+    <Page background="background-front" pad={{ bottom: 'xlarge' }}>
+      <hpe-global-header kind="glcp" />
+      <PageContent gap="medium">
+        <PageHeader
+          parent={<ReverseAnchor label="Servers" />}
+          title={server.displayName}
+          subtitle="Manage details for this server."
+          actions={
+            <ButtonGroup>
+              <Button secondary label="Edit details" />
+            </ButtonGroup>
+          }
+        />
+        <Box align="start" gap="medium" pad={{ bottom: 'medium' }}>
+          <Box gap="xsmall">
+            <Heading level={2} margin="none">
+              Hardware
+            </Heading>
+            <Paragraph margin="none">
+              Here is a paragraph if it were to be here with a{' '}
+              <Anchor>link to something</Anchor>.
+            </Paragraph>
+          </Box>
+          <NameValueList nameProps={{ width: ['auto', 'max-content'] }}>
+            <NameValuePair name="Serial number">
+              {server.hardware.serialNumber}
+            </NameValuePair>
+            <NameValuePair name="Product ID">
+              {server.hardware.productId}
+            </NameValuePair>
+            <NameValuePair name="Power state">
+              {server.hardware.powerState}
+            </NameValuePair>
+            <NameValuePair name="Indicator LED">
+              {server.hardware.indicatorLed}
+            </NameValuePair>
+          </NameValueList>
+        </Box>
+        <Box gap="medium">
+          <Heading level={2} margin="none">
+            Host
+          </Heading>
+          <NameValueList nameProps={{ width: ['auto', 'max-content'] }}>
+            <NameValuePair name="Host name">
+              {server.host.hostname}
+            </NameValuePair>
+            <NameValuePair name="OS name">{server.host.osName}</NameValuePair>
+            <NameValuePair name="OS version">
+              {server.host.osVersion}
+            </NameValuePair>
+          </NameValueList>
+        </Box>
+      </PageContent>
+    </Page>
+  );
+};
 
 const ButtonChanges = () => (
   <Box direction="row" align="start" gap="small">
@@ -72,6 +132,17 @@ const TabsChanges = () => (
   </Tabs>
 );
 
+const currentTheme = {
+  ...hpe,
+  button: {
+    toolbar: {
+      border: {
+        width: undefined,
+      },
+    },
+  },
+};
+
 const ThemeTest = () => (
   <>
     <Meta
@@ -79,13 +150,19 @@ const ThemeTest = () => (
       canonicalUrl="https://design-system.hpe.design/theme-test"
     />
     <Box gap="medium">
-      <ThemeContext.Extend value={hpe}>
+      <ThemeContext.Extend value={currentTheme}>
         <MockPage />
       </ThemeContext.Extend>
       <ThemeContext.Extend value={aries}>
         <MockPage />
       </ThemeContext.Extend>
-      <ThemeContext.Extend value={hpe}>
+      <ThemeContext.Extend value={currentTheme}>
+        <MockDetailPage />
+      </ThemeContext.Extend>
+      <ThemeContext.Extend value={aries}>
+        <MockDetailPage />
+      </ThemeContext.Extend>
+      <ThemeContext.Extend value={currentTheme}>
         <Page background="background-front" pad={{ vertical: 'large' }}>
           <PageContent gap="large">
             <Heading margin="none">Before and after</Heading>
