ae750a1173160c380d7efda74ef40b8b1bdb7094
Matt Glissmann <mdglissmann@gmail.com>
2023-12-02 14:40:10 -0700

    initial design system manager app commit

---
+++ design-system-manager/src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from 'next'
+import { Footer, Grommet, Header, Main } from 'grommet'
+import { hpe } from '../themes/hpe'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | HPE Design System Manager',
+    default: 'HPE Design System Manager'
+  },
+  description: 'Manage design system assets and supporting resources.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Grommet theme={hpe} themeMode="auto" full background="background-back">
+          <Header background="background-front" pad="small">My header</Header>
+          <Main>{children}</Main>
+          <Footer background="background-contrast" pad="small">My footer</Footer>
+        </Grommet>
+      </body>
+    </html>
+  )
+}
---
+++ design-system-manager/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import {Page, PageContent, PageHeader} from 'grommet'
+
+export default function Home() {
+  return (
+    <Page>
+      <PageContent>
+        <PageHeader title="Home" />
+      </PageContent>
+    </Page>
+  )
+}
