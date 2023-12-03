b8e094d6a839eaeca98c4de3410dba67d6052b11
Matt Glissmann <mdglissmann@gmail.com>
2023-12-03 16:22:23 -0700

    modify ReveserAnchor to forward ref from routed link

--- aries-core/src/js/components/controls/ReverseAnchor.js
+++ aries-core/src/js/components/controls/ReverseAnchor.js
@@ -1,18 +1,22 @@
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Anchor, Text } from 'grommet';
+import { Anchor } from 'grommet';
 import { FormPrevious } from 'grommet-icons';
 
-export const ReverseAnchor = ({ href, label }) => {
-  return (
-    <Anchor
-      as={Text}
-      href={href}
-      label={label}
-      icon={<FormPrevious />}
-      gap="hair"
-    />
-  );
-};
+export const ReverseAnchor = React.forwardRef(
+  ({ href, label, ...rest }, ref) => {
+    return (
+      <Anchor
+        ref={ref}
+        href={href}
+        label={label}
+        icon={<FormPrevious />}
+        gap="hair"
+        {...rest}
+      />
+    );
+  },
+);
 
 ReverseAnchor.propTypes = {
   href: PropTypes.string.isRequired,
