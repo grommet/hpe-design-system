a6f3e9a12c62c33b0fbb339187b2f04a02bacef1
Matt Glissmann <mdglissmann@gmail.com>
2023-12-05 17:43:02 -0700

    make page layout kind narrow

--- design-system-manager/src/app/components/[id]/page.tsx
+++ design-system-manager/src/app/components/[id]/page.tsx
@@ -16,7 +16,26 @@ const Component = async ({
 }) => {
   const component: ComponentType = await getComponent(params.id);
 
-  return (<Detail component={component} />);
+  return (
+  <>
+    <Detail component={component} />
+    <PageHeader title="Resources" level={2} actions={<Button label="Edit" />} />
+    <Heading level={3}>Design</Heading>
+    <Heading level={3}>Code</Heading>
+    <Heading level={2}>Design</Heading>
+    <Heading level={3}>Anatomy</Heading>
+    <Heading level={3}>Interactive states</Heading>
+    <Heading level={3}>Color</Heading>
+    <Heading level={3}>Behaviors</Heading>
+    <Heading level={2}>Usage</Heading>
+    <Heading level={3}>Modifiers and configurations</Heading>
+    <Heading level={3}>Use case examples</Heading>
+    <Heading level={2}>References</Heading>
+    <Heading level={3}>Documentation</Heading>
+    <Heading level={3}>Research</Heading>
+    <Heading level={3}>Design criteria</Heading>
+    <Heading level={3}>Design rationale</Heading>
+  </>);
 };
 
 export default Component;
