229ddc5ef305320228ff89ffa84e60a6d54f4802
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2023-12-12 10:12:12 -0700

    use empty state component

--- aries-site/src/examples/cardPreviews/emptystate.js
+++ aries-site/src/examples/cardPreviews/emptystate.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Box, Button, Heading, Paragraph } from 'grommet';
+import { EmptyState } from 'aries-core';
+import { Button } from 'grommet';
 import { CircleInformation } from 'grommet-icons';
 
-export const EmptyStatePreview = () => (
-  <Box gap="small" align="center" flex={false}>
-    <CircleInformation />
-    <Box align="center" gap="xsmall">
-      <Heading margin="none" size='small' level={4}>
-        No items exist
-      </Heading>
-      <Paragraph margin="none" textAlign="center">
-        Error message will be displayed here.
-      </Paragraph>
-    </Box>
-    <Button label="Action item" primary />
-  </Box>
-);
+export const EmptyStatePreview = () => {
+  return (
+    <EmptyState
+      title="Empty state title"
+      description="Empty state message will be displayed here."
+      icon={<CircleInformation />}
+      actions={<Button tabIndex={-1} label="action item" primary />}
+      level={3}
+    />
+  );
+};
