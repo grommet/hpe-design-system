c7654fd6d809eab5a38644d4106eaea99cd6dffb
Taylor Seamans <taylor.seamans@yahoo.com>
2024-02-21 15:12:51 -0800

    Preliminary pipeline to aries theme

---
+++ aries-site/src/pages/demo.js
@@ -0,0 +1,150 @@
+import React, { useContext } from 'react';
+import {
+  Box,
+  Button,
+  Grid,
+  Heading,
+  PageContent,
+  Paragraph,
+  PageHeader,
+  ResponsiveContext,
+  ThemeContext,
+  Card,
+  CardBody,
+  Text,
+  Meter,
+} from 'grommet';
+import { AppsRounded, Configure, PlayFill, ShareRounded } from 'grommet-icons';
+import { Meta } from '../components';
+import { aries } from '../themes/aries';
+import { Card as TemplateCard } from '../examples';
+
+const title = 'Design Tokens Demo';
+
+const BriefCard = ({ description, title, icon, ...rest }) => (
+  <Card onClick={() => {}} {...rest}>
+    <CardBody direction="row" align="center" justify="between" gap="medium">
+      <Box gap="xsmall">
+        <Heading level={3} margin="none">
+          {title}
+        </Heading>
+        <Text margin="none" size="small" weight={500}>
+          {description}
+        </Text>
+      </Box>
+      <Box background={{ dark: true }}>{icon}</Box>
+    </CardBody>
+  </Card>
+);
+
+const MeterCard = () => (
+  <TemplateCard title="Issues" level={3}>
+    <Box gap="medium" pad={{ top: 'medium' }}>
+      <Box round="large" overflow="hidden">
+        <Meter
+          type="bar"
+          values={[
+            { value: 30, color: 'status-critical' },
+            { value: 28, color: 'status-warning' },
+            { value: 42, color: 'blue' },
+          ]}
+        />
+      </Box>
+      <Box gap="xsmall">
+        <Box direction="row" gap="xsmall" align="center">
+          <Box pad="xsmall" round background="status-critical" />
+          <Text>High severity</Text>
+        </Box>
+        <Box direction="row" gap="xsmall" align="center">
+          <Box pad="xsmall" round background="status-warning" />
+          <Text>Medium severity</Text>
+        </Box>
+        <Box direction="row" gap="xsmall" align="center">
+          <Box pad="xsmall" round background="blue" />
+          <Text>Low severity</Text>
+        </Box>
+      </Box>
+    </Box>
+  </TemplateCard>
+);
+
+const Demo = () => {
+  const size = useContext(ResponsiveContext);
+
+  return (
+    <>
+      <Meta title={title} />
+      <ThemeContext.Extend value={aries}>
+        <PageContent>
+          <PageHeader title="Console home" />
+          <Grid columns={['flex', 'medium']} gap="medium">
+            <Box gap="medium">
+              <Grid columns={['flex', 'flex']} gap="medium">
+                <BriefCard
+                  title="Block Storage"
+                  description=" Last used yesterday, at 8:36pm"
+                  icon={<ShareRounded size="xxlarge" color="green" />}
+                />
+
+                <BriefCard
+                  title="Private Cloud Enterprise"
+                  description="Last used on July 17, 2023, at 8:36pm"
+                  icon={<ShareRounded size="xxlarge" color="green" />}
+                />
+              </Grid>
+              <Card>
+                <CardBody gap="medium">
+                  <Box gap="xsmall">
+                    <Heading level={3} margin="none">
+                      Capacity
+                    </Heading>
+                    <Paragraph size="small" margin="none">
+                      Volume
+                    </Paragraph>
+                  </Box>
+                  <Text size="32px" weight={500}>
+                    32%{' '}
+                    <Text size="medium" weight={500}>
+                      used <Text weight={400}>(240 of 750 TiB)</Text>
+                    </Text>
+                  </Text>
+                </CardBody>
+              </Card>
+              <Grid columns={['flex', 'flex']} gap="medium">
+                <MeterCard />
+              </Grid>
+            </Box>
+            <Box gap="medium">
+              <BriefCard
+                title="Find Services"
+                description="Discover and lunch services from out catalog."
+                icon={<AppsRounded size="xxlarge" color="green" />}
+              />
+              <BriefCard
+                title="Manage Workspace"
+                description="Setup this workspace, users, access, and more."
+                icon={<Configure size="xxlarge" color="green" />}
+              />
+              <Card background="background-contrast">
+                <CardBody align="start" pad="large" gap="medium">
+                  <Heading level={3} margin="none">
+                    Learn about the new HPE GreenLake Console experience
+                  </Heading>
+                  <Button
+                    kind="toolbar"
+                    label="Watch now"
+                    icon={<PlayFill color="green" />}
+                    reverse
+                  />
+                </CardBody>
+              </Card>
+              <Box>Map goes here</Box>
+            </Box>
+          </Grid>
+        </PageContent>
+      </ThemeContext.Extend>
+    </>
+  );
+};
+
+export default Demo;
---
+++ design-tokens/src/build.js
@@ -0,0 +1,118 @@
+import { mkdirSync, readFileSync, writeFileSync } from 'fs';
+
+const resolveTokens = (tokens, primitives) => {
+  const result = tokens;
+
+  const descend = (node, path = []) => {
+    Object.keys(node).forEach(key => {
+      const value = node[key];
+      const keyPath = [...path, key];
+      if (typeof value === 'object') return descend(value, keyPath);
+      if (key === '$value' && /^{.*}$/.test(value)) {
+        let parts = value.slice(1, -1).split('.');
+        let obj = primitives;
+        while (obj && parts.length) obj = obj[parts.shift()];
+        // referencing something already in semantic set
+        if (!obj) {
+          parts = value.slice(1, -1).split('.');
+          obj = tokens;
+          while (obj && parts.length) obj = obj[parts.shift()];
+        }
+        // reassign value
+        let original = result;
+        keyPath.pop();
+        while (original && keyPath.length) original = original[keyPath.shift()];
+        if (typeof obj?.$value === 'number')
+          original.$value = `${obj?.$value}px`;
+        else original.$value = obj?.$value;
+      }
+    });
+  };
+
+  descend(tokens);
+
+  return result;
+};
+
+// primitives
+const rawPrimitives = readFileSync('./tokens/primitives.base.json');
+const primitives = JSON.parse(rawPrimitives);
+
+// dark colors
+const rawDark = readFileSync('./tokens/color.dark.json');
+const dark = JSON.parse(rawDark);
+const resolvedDark = resolveTokens(dark, primitives);
+
+// light colors
+const rawLight = readFileSync('./tokens/color.light.json');
+const light = JSON.parse(rawLight);
+const resolvedLight = resolveTokens(light, primitives);
+
+// desktop dimensions
+const rawDesktop = readFileSync('./tokens/dimension.desktop.json');
+const desktop = JSON.parse(rawDesktop);
+const resolvedDesktop = resolveTokens(desktop, primitives);
+
+// mobile dimensions
+const rawMobile = readFileSync('./tokens/dimension.desktop.json');
+const mobile = JSON.parse(rawMobile);
+const resolvedMobile = resolveTokens(mobile, primitives);
+
+mkdirSync('./dist');
+
+// do we only want to export resolved ones?
+// is there any value to having the references (for example, figma uses it)
+writeFileSync(
+  './dist/color.dark.js',
+  `export default ${JSON.stringify(resolvedDark, null, 2)}`,
+);
+// //
+// writeFileSync(
+//   './dist/color.dark-resolved.js',
+//   `export default ${JSON.stringify(resolved, null, 2)}`,
+// );
+
+writeFileSync(
+  './dist/color.light.js',
+  `export default ${JSON.stringify(resolvedLight, null, 2)}`,
+);
+
+writeFileSync(
+  './dist/dimension.desktop.js',
+  `export default ${JSON.stringify(resolvedDesktop, null, 2)}`,
+);
+
+writeFileSync(
+  './dist/dimension.mobile.js',
+  `export default ${JSON.stringify(resolvedMobile, null, 2)}`,
+);
+
+writeFileSync(
+  './dist/index.js',
+  `export { default as dark } from './color.dark.js';
+export { default as light } from './color.light.js';
+export { default as desktop } from './dimension.desktop.js';
+export { default as mobile } from './dimension.mobile.js';`,
+);
+
+// const [light, dark] = splitTheme(resolved);
+
+// const stringified = stringifyTokens(light);
+
+// writeFileSync(
+//   './structured-tokens.ts',
+//   `export default ${JSON.stringify(stringified, null, 2)}`,
+// );
+
+// const stringifiedDark = stringifyTokens(dark);
+
+// writeFileSync(
+//   './structured-tokens-dark.ts',
+//   `export default ${JSON.stringify(stringifiedDark, null, 2)}`,
+// );
+
+// writeFileSync('./dist/tokens.json', JSON.stringify(flat, null, 2));
+
+// const [cssVars, cssDarkVars] = generateCssVars(flat);
+// writeFileSync('./dist/tokens.css', cssVars);
+// writeFileSync('./dist/tokens-dark.css', cssDarkVars);
