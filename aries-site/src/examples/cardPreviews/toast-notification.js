c057d72c6509be071130af562ec0378dfd536a14
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2024-02-28 18:04:15 -0700

    fixed indentation

--- aries-site/src/examples/cardPreviews/toast-notification.js
+++ aries-site/src/examples/cardPreviews/toast-notification.js
@@ -17,14 +17,15 @@ export const ToastPreview = ({ card, message, title }) => {
         background={{ color: 'background-front' }}
       >
         <Box direction="row" gap="xsmall">
-          <StatusGoodSmall color="status-ok" height="medium" 
-        id="status-indicator" />
+          <StatusGoodSmall
+            color="status-ok"
+            height="medium"
+            id="status-indicator"
+          />
           <Text weight="500" id="title">
             {title}
           </Text>
-          <Text id="message">
-            {message}
-          </Text>
+          <Text id="message">{message}</Text>
         </Box>
         <Close
           id="close-button"
