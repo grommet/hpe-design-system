d20e7f2255c3531ee70a4a9fd2ed3cc3d310b690
Brittany Archibeque <brittanyarchibeque@Brittanys-MacBook-Pro-2.local>
2024-02-28 18:04:15 -0700

    fixed indentation

--- aries-site/src/examples/components/notifications/StateVsStatusDiagram.js
+++ aries-site/src/examples/components/notifications/StateVsStatusDiagram.js
@@ -2,5 +2,5 @@ import React from 'react';
 import { ToastPreview } from '../../cardPreviews';
 
 export const StateVsStatusDiagram = () => (
-        <ToastPreview title="Invitation sent to user" onClose />       
+  <ToastPreview title="Invitation sent to user" onClose />
 );
