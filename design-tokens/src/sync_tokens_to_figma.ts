184c9012b1ba73c7b310a53e8a237478235e168f
Taylor Seamans <taylor.seamans@yahoo.com>
2024-03-18 14:10:34 -0700

    Update build step, use style dictionary, adjust theme

--- design-tokens/src/sync_tokens_to_figma.ts
+++ design-tokens/src/sync_tokens_to_figma.ts
@@ -1,55 +1,81 @@
-import 'dotenv/config'
-import * as fs from 'fs'
+import 'dotenv/config';
+import * as fs from 'fs';
 
-import FigmaApi from './figma_api.js'
+import FigmaApi from './figma_api.js';
 
-import { green } from './utils.js'
-import { generatePostVariablesPayload, readJsonFiles } from './token_import.js'
+import { green } from './utils.js';
+import { generatePostVariablesPayload, readJsonFiles } from './token_import.js';
 
 async function main() {
   if (!process.env.PERSONAL_ACCESS_TOKEN || !process.env.FILE_KEY) {
-    throw new Error('PERSONAL_ACCESS_TOKEN and FILE_KEY environemnt variables are required')
+    throw new Error(
+      'PERSONAL_ACCESS_TOKEN and FILE_KEY environemnt variables are required',
+    );
   }
-  const fileKey = process.env.FILE_KEY
+  const fileKey = process.env.FILE_KEY;
 
-  const TOKENS_DIR = 'tokens'
-  const tokensFiles = fs.readdirSync(TOKENS_DIR).map((file: string) => `${TOKENS_DIR}/${file}`)
+  const TOKENS_DIR = 'tokens';
+  const tokensFiles = fs
+    .readdirSync(TOKENS_DIR)
+    .map((file: string) =>
+      // Figma doesn't support elevation/shadow tokens yet, so don't try to sync
+      !file.includes('elevation') ? `${TOKENS_DIR}/${file}` : '',
+    )
+    .filter(file => file);
 
-  const tokensByFile = readJsonFiles(tokensFiles)
+  const tokensByFile = readJsonFiles(tokensFiles);
 
-  console.log('Read tokens files:', Object.keys(tokensByFile))
+  console.log('Read tokens files:', Object.keys(tokensByFile));
 
-  const api = new FigmaApi(process.env.PERSONAL_ACCESS_TOKEN)
-  const localVariables = await api.getLocalVariables(fileKey)
+  const api = new FigmaApi(process.env.PERSONAL_ACCESS_TOKEN);
+  const localVariables = await api.getLocalVariables(fileKey);
 
-  const postVariablesPayload = generatePostVariablesPayload(tokensByFile, localVariables)
+  const postVariablesPayload = generatePostVariablesPayload(
+    tokensByFile,
+    localVariables,
+  );
 
-  if (Object.values(postVariablesPayload).every((value) => value.length === 0)) {
-    console.log(green('✅ Tokens are already up to date with the Figma file'))
-    return
+  if (Object.values(postVariablesPayload).every(value => value.length === 0)) {
+    console.log(green('✅ Tokens are already up to date with the Figma file'));
+    return;
   }
 
-  const apiResp = await api.postVariables(fileKey, postVariablesPayload)
+  const apiResp = await api.postVariables(fileKey, postVariablesPayload);
 
-  console.log('POST variables API response:', apiResp)
+  console.log('POST variables API response:', apiResp);
 
-  if (postVariablesPayload.variableCollections && postVariablesPayload.variableCollections.length) {
-    console.log('Updated variable collections', postVariablesPayload.variableCollections)
+  if (
+    postVariablesPayload.variableCollections &&
+    postVariablesPayload.variableCollections.length
+  ) {
+    console.log(
+      'Updated variable collections',
+      postVariablesPayload.variableCollections,
+    );
   }
 
-  if (postVariablesPayload.variableModes && postVariablesPayload.variableModes.length) {
-    console.log('Updated variable modes', postVariablesPayload.variableModes)
+  if (
+    postVariablesPayload.variableModes &&
+    postVariablesPayload.variableModes.length
+  ) {
+    console.log('Updated variable modes', postVariablesPayload.variableModes);
   }
 
   if (postVariablesPayload.variables && postVariablesPayload.variables.length) {
-    console.log('Updated variables', postVariablesPayload.variables)
+    console.log('Updated variables', postVariablesPayload.variables);
   }
 
-  if (postVariablesPayload.variableModeValues && postVariablesPayload.variableModeValues.length) {
-    console.log('Updated variable mode values', postVariablesPayload.variableModeValues)
+  if (
+    postVariablesPayload.variableModeValues &&
+    postVariablesPayload.variableModeValues.length
+  ) {
+    console.log(
+      'Updated variable mode values',
+      postVariablesPayload.variableModeValues,
+    );
   }
 
-  console.log(green('✅ Figma file has been updated with the new tokens'))
+  console.log(green('✅ Figma file has been updated with the new tokens'));
 }
 
-main()
+main();
--- sandbox/grommet-app/src/theme.js
+++ sandbox/grommet-app/src/theme.js
@@ -3,99 +3,100 @@ import { deepMerge } from 'grommet/utils';
 import {
   dark,
   light,
-  desktop,
-  mobile,
+  large,
+  small,
   warmLight,
   warmDark,
+  elevationLight,
 } from 'design-tokens';
 
 const dimensions = {
   borderSize: {
-    xsmall: desktop.border.xsmall.$value,
-    small: desktop.border.small.$value,
-    medium: desktop.border.medium.$value,
-    large: desktop.border.large.$value,
-    xlarge: desktop.border.xlarge.$value,
+    xsmall: large.border.xsmall,
+    small: large.border.small,
+    medium: large.border.medium,
+    large: large.border.large,
+    xlarge: large.border.xlarge,
   },
   edgeSize: {
-    none: desktop.spacing.none.$value,
-    hair: desktop.spacing.hair.$value,
-    xxsmall: desktop.spacing.xxsmall.$value,
-    xsmall: desktop.spacing.xsmall.$value,
-    small: desktop.spacing.small.$value,
-    medium: desktop.spacing.medium.$value,
-    large: desktop.spacing.large.$value,
-    xlarge: desktop.spacing.xlarge.$value,
+    none: large.spacing.none,
+    hair: large.spacing.hair,
+    xxsmall: large.spacing.xxsmall,
+    xsmall: large.spacing.xsmall,
+    small: large.spacing.small,
+    medium: large.spacing.medium,
+    large: large.spacing.large,
+    xlarge: large.spacing.xlarge,
     responsiveBreakpoint: 'small',
   },
   size: {
-    xxsmall: desktop.content.xxsmall.$value,
-    xsmall: desktop.content.xsmall.$value,
-    small: desktop.content.small.$value,
-    medium: desktop.content.medium.$value,
-    large: desktop.content.large.$value,
-    xlarge: desktop.content.xlarge.$value,
-    xxlarge: desktop.content.xxlarge.$value,
+    xxsmall: large.content.xxsmall,
+    xsmall: large.content.xsmall,
+    small: large.content.small,
+    medium: large.content.medium,
+    large: large.content.large,
+    xlarge: large.content.xlarge,
+    xxlarge: large.content.xxlarge,
     full: '100%',
   },
   breakpoints: {
     xsmall: {
       borderSize: {
-        xsmall: mobile.border.xsmall.$value,
-        small: mobile.border.small.$value,
-        medium: mobile.border.medium.$value,
-        large: mobile.border.large.$value,
-        xlarge: mobile.border.xlarge.$value,
+        xsmall: small.border.xsmall,
+        small: small.border.small,
+        medium: small.border.medium,
+        large: small.border.large,
+        xlarge: small.border.xlarge,
       },
       edgeSize: {
-        none: mobile.spacing.none.$value,
-        hair: mobile.spacing.hair.$value,
-        xxsmall: mobile.spacing.xxsmall.$value,
-        xsmall: mobile.spacing.xsmall.$value,
-        small: mobile.spacing.small.$value,
-        medium: mobile.spacing.medium.$value,
-        large: mobile.spacing.large.$value,
-        xlarge: mobile.spacing.xlarge.$value,
+        none: small.spacing.none,
+        hair: small.spacing.hair,
+        xxsmall: small.spacing.xxsmall,
+        xsmall: small.spacing.xsmall,
+        small: small.spacing.small,
+        medium: small.spacing.medium,
+        large: small.spacing.large,
+        xlarge: small.spacing.xlarge,
         responsiveBreakpoint: 'small',
       },
       size: {
-        xxsmall: mobile.content.xxsmall.$value,
-        xsmall: mobile.content.xsmall.$value,
-        small: mobile.content.small.$value,
-        medium: mobile.content.medium.$value,
-        large: mobile.content.large.$value,
-        xlarge: mobile.content.xlarge.$value,
-        xxlarge: mobile.content.xxlarge.$value,
+        xxsmall: small.content.xxsmall,
+        xsmall: small.content.xsmall,
+        small: small.content.small,
+        medium: small.content.medium,
+        large: small.content.large,
+        xlarge: small.content.xlarge,
+        xxlarge: small.content.xxlarge,
         full: '100%',
       },
     },
     small: {
       borderSize: {
-        xsmall: mobile.border.xsmall.$value,
-        small: mobile.border.small.$value,
-        medium: mobile.border.medium.$value,
-        large: mobile.border.large.$value,
-        xlarge: mobile.border.xlarge.$value,
+        xsmall: small.border.xsmall,
+        small: small.border.small,
+        medium: small.border.medium,
+        large: small.border.large,
+        xlarge: small.border.xlarge,
       },
       edgeSize: {
-        none: mobile.spacing.none.$value,
-        hair: mobile.spacing.hair.$value,
-        xxsmall: mobile.spacing.xxsmall.$value,
-        xsmall: mobile.spacing.xsmall.$value,
-        small: mobile.spacing.small.$value,
-        medium: mobile.spacing.medium.$value,
-        large: mobile.spacing.large.$value,
-        xlarge: mobile.spacing.xlarge.$value,
+        none: small.spacing.none,
+        hair: small.spacing.hair,
+        xxsmall: small.spacing.xxsmall,
+        xsmall: small.spacing.xsmall,
+        small: small.spacing.small,
+        medium: small.spacing.medium,
+        large: small.spacing.large,
+        xlarge: small.spacing.xlarge,
         responsiveBreakpoint: 'small',
       },
       size: {
-        xxsmall: mobile.content.xxsmall.$value,
-        xsmall: mobile.content.xsmall.$value,
-        small: mobile.content.small.$value,
-        medium: mobile.content.medium.$value,
-        large: mobile.content.large.$value,
-        xlarge: mobile.content.xlarge.$value,
-        xxlarge: mobile.content.xxlarge.$value,
+        xxsmall: small.content.xxsmall,
+        xsmall: small.content.xsmall,
+        small: small.content.small,
+        medium: small.content.medium,
+        large: small.content.large,
+        xlarge: small.content.xlarge,
+        xxlarge: small.content.xxlarge,
         full: '100%',
       },
     },
@@ -103,207 +104,207 @@ const dimensions = {
 };
 
 const buildTheme = tokens => {
-  const [light, dark] = tokens;
+  const [light, dark, elevationLight] = tokens;
   return deepMerge(hpe, {
     global: {
       ...dimensions,
       colors: {
         brand: {
-          dark: dark.color.brand.$value,
-          light: light.color.brand.$value,
+          dark: dark.color.brand,
+          light: light.color.brand,
         },
         primary: {
-          dark: dark.color.primary.$value,
-          light: light.color.primary.$value,
+          dark: dark.color.primary,
+          light: light.color.primary,
         },
         background: {
-          dark: dark.color.background.default.$value,
-          light: light.color.background.default.$value,
+          dark: dark.color.background.default,
+          light: light.color.background.default,
         },
         'background-back': {
-          dark: dark.color.background.back.$value,
-          light: light.color.background.back.$value,
+          dark: dark.color.background.back,
+          light: light.color.background.back,
         },
         'background-front': {
-          dark: dark.color.background.front.$value,
-          light: light.color.background.front.$value,
+          dark: dark.color.background.front,
+          light: light.color.background.front,
         },
         'background-contrast': {
-          dark: dark.color.background.contrast.$value, // 6%
-          light: light.color.background.contrast.$value,
+          dark: dark.color.background.contrast, // 6%
+          light: light.color.background.contrast,
         },
         'background-status-critical': {
-          dark: dark.color.background.status.critical.$value, // 6%
-          light: light.color.background.status.critical.$value,
+          dark: dark.color.background.status.critical, // 6%
+          light: light.color.background.status.critical,
         },
         'background-status-warning': {
-          dark: dark.color.background.status.warning.$value, // 6%
-          light: light.color.background.status.warning.$value,
+          dark: dark.color.background.status.warning, // 6%
+          light: light.color.background.status.warning,
         },
         'background-status-ok': {
-          dark: dark.color.background.status.ok.$value, // 6%
-          light: light.color.background.status.ok.$value,
+          dark: dark.color.background.status.ok, // 6%
+          light: light.color.background.status.ok,
         },
         'background-status-info': {
-          dark: dark.color.background.status.info.$value, // 6%
-          light: light.color.background.status.info.$value,
+          dark: dark.color.background.status.info, // 6%
+          light: light.color.background.status.info,
         },
         'active-background': {
-          dark: dark.color.background.active.$value,
-          light: light.color.background.active.$value,
+          dark: dark.color.background.active,
+          light: light.color.background.active,
         },
         text: {
-          dark: dark.color.text.default.$value,
-          light: light.color.text.default.$value,
+          dark: dark.color.text.default,
+          light: light.color.text.default,
         },
         'text-strong': {
-          dark: dark.color.text.strong.$value,
-          light: light.color.text.strong.$value,
+          dark: dark.color.text.strong,
+          light: light.color.text.strong,
         },
         'text-weak': {
-          dark: dark.color.text.weak.$value,
-          light: light.color.text.weak.$value,
+          dark: dark.color.text.weak,
+          light: light.color.text.weak,
         },
         'text-xweak': {
-          dark: dark.color.text.xweak.$value,
-          light: light.color.text.xweak.$value,
+          dark: dark.color.text.xweak,
+          light: light.color.text.xweak,
         },
         border: {
-          dark: dark.color.border.default.$value,
-          light: light.color.border.default.$value,
+          dark: dark.color.border.default,
+          light: light.color.border.default,
         },
         'border-strong': {
-          dark: dark.color.border.strong.$value,
-          light: light.color.border.strong.$value,
+          dark: dark.color.border.strong,
+          light: light.color.border.strong,
         },
         'border-weak': {
-          dark: dark.color.border.weak.$value,
-          light: light.color.border.weak.$value,
+          dark: dark.color.border.weak,
+          light: light.color.border.weak,
         },
         'selected-text': {
-          dark: dark.color.foreground.selected.$value,
-          light: light.color.foreground.selected.$value,
+          dark: dark.color.foreground.selected,
+          light: light.color.foreground.selected,
         },
         'status-critical': {
-          dark: dark.color.foreground.status.critical.$value,
-          light: light.color.foreground.status.critical.$value,
+          dark: dark.color.foreground.status.critical,
+          light: light.color.foreground.status.critical,
         },
         'status-warning': {
-          dark: dark.color.foreground.status.warning.$value,
-          light: light.color.foreground.status.warning.$value,
+          dark: dark.color.foreground.status.warning,
+          light: light.color.foreground.status.warning,
         },
         'status-ok': {
-          dark: dark.color.foreground.status.ok.$value,
-          light: light.color.foreground.status.ok.$value,
+          dark: dark.color.foreground.status.ok,
+          light: light.color.foreground.status.ok,
         },
         'status-unknown': {
-          dark: dark.color.foreground.status.unknown.$value,
-          light: light.color.foreground.status.unknown.$value,
+          dark: dark.color.foreground.status.unknown,
+          light: light.color.foreground.status.unknown,
         },
         'validation-critical': {
-          light: light.color.background.validation.critical.$value,
-          dark: dark.color.background.validation.critical.$value,
+          light: light.color.background.validation.critical,
+          dark: dark.color.background.validation.critical,
         },
         'validation-ok': {
-          light: light.color.background.validation.ok.$value,
-          dark: dark.color.background.validation.ok.$value,
+          light: light.color.background.validation.ok,
+          dark: dark.color.background.validation.ok,
         },
         'validation-warning': {
-          light: light.color.background.validation.warning.$value,
-          dark: dark.color.background.validation.critical.$value,
+          light: light.color.background.validation.warning,
+          dark: dark.color.background.validation.critical,
         },
         'chart-qualitative-10': {
-          light: light.color.chart.qualitative[10].$value,
-          dark: dark.color.chart.qualitative[10].$value,
+          light: light.color.chart.qualitative[10],
+          dark: dark.color.chart.qualitative[10],
         },
         'chart-qualitative-10-weak': {
-          light: light.color.chart.qualitative['10-secondary'].$value,
-          dark: dark.color.chart.qualitative['10-secondary'].$value,
+          light: light.color.chart.qualitative['10-secondary'],
+          dark: dark.color.chart.qualitative['10-secondary'],
         },
         'chart-qualitative-20': {
-          light: light.color.chart.qualitative[20].$value,
-          dark: dark.color.chart.qualitative[20].$value,
+          light: light.color.chart.qualitative[20],
+          dark: dark.color.chart.qualitative[20],
         },
         'chart-qualitative-20-weak': {
-          light: light.color.chart.qualitative['20-secondary'].$value,
-          dark: dark.color.chart.qualitative['20-secondary'].$value,
+          light: light.color.chart.qualitative['20-secondary'],
+          dark: dark.color.chart.qualitative['20-secondary'],
         },
         'chart-qualitative-30': {
-          light: light.color.chart.qualitative[30].$value,
-          dark: dark.color.chart.qualitative[30].$value,
+          light: light.color.chart.qualitative[30],
+          dark: dark.color.chart.qualitative[30],
         },
         'chart-qualitative-30-weak': {
-          light: light.color.chart.qualitative['30-secondary'].$value,
-          dark: dark.color.chart.qualitative['30-secondary'].$value,
+          light: light.color.chart.qualitative['30-secondary'],
+          dark: dark.color.chart.qualitative['30-secondary'],
         },
         'chart-qualitative-40': {
-          light: light.color.chart.qualitative[40].$value,
-          dark: dark.color.chart.qualitative[40].$value,
+          light: light.color.chart.qualitative[40],
+          dark: dark.color.chart.qualitative[40],
         },
         'chart-qualitative-40-weak': {
-          light: light.color.chart.qualitative['40-secondary'].$value,
-          dark: dark.color.chart.qualitative['40-secondary'].$value,
+          light: light.color.chart.qualitative['40-secondary'],
+          dark: dark.color.chart.qualitative['40-secondary'],
         },
         'chart-qualitative-50': {
-          light: light.color.chart.qualitative[50].$value,
-          dark: dark.color.chart.qualitative[50].$value,
+          light: light.color.chart.qualitative[50],
+          dark: dark.color.chart.qualitative[50],
         },
         'chart-qualitative-50-weak': {
-          light: light.color.chart.qualitative['50-secondary'].$value,
-          dark: dark.color.chart.qualitative['50-secondary'].$value,
+          light: light.color.chart.qualitative['50-secondary'],
+          dark: dark.color.chart.qualitative['50-secondary'],
         },
         'chart-qualitative-60': {
-          light: light.color.chart.qualitative[60].$value,
-          dark: dark.color.chart.qualitative[60].$value,
+          light: light.color.chart.qualitative[60],
+          dark: dark.color.chart.qualitative[60],
         },
         'chart-qualitative-60-weak': {
-          light: light.color.chart.qualitative['60-secondary'].$value,
-          dark: dark.color.chart.qualitative['60-secondary'].$value,
+          light: light.color.chart.qualitative['60-secondary'],
+          dark: dark.color.chart.qualitative['60-secondary'],
         },
         'chart-qualitative-70': {
-          light: light.color.chart.qualitative[70].$value,
-          dark: dark.color.chart.qualitative[70].$value,
+          light: light.color.chart.qualitative[70],
+          dark: dark.color.chart.qualitative[70],
         },
         'chart-qualitative-70-weak': {
-          light: light.color.chart.qualitative['70-secondary'].$value,
-          dark: dark.color.chart.qualitative['70-secondary'].$value,
+          light: light.color.chart.qualitative['70-secondary'],
+          dark: dark.color.chart.qualitative['70-secondary'],
         },
         'background-brand-default': {
-          light: light.color.background.brand.default.$value,
-          dark: dark.color.background.brand.default.$value,
+          light: light.color.background.brand.default,
+          dark: dark.color.background.brand.default,
         },
         'background-brand-weak': {
-          light: light.color.background.brand.weak.$value,
-          dark: dark.color.background.brand.weak.$value,
+          light: light.color.background.brand.weak,
+          dark: dark.color.background.brand.weak,
         },
         'foreground-brand-default': {
-          light: light.color.foreground.brand.default.$value,
-          dark: dark.color.foreground.brand.default.$value,
+          light: light.color.foreground.brand.default,
+          dark: dark.color.foreground.brand.default,
         },
         'foreground-status-critical': {
-          light: light.color.foreground.status.critical.$value,
-          dark: dark.color.foreground.status.critical.$value,
+          light: light.color.foreground.status.critical,
+          dark: dark.color.foreground.status.critical,
         },
         'foreground-status-warning': {
-          light: light.color.foreground.status.warning.$value,
-          dark: dark.color.foreground.status.warning.$value,
+          light: light.color.foreground.status.warning,
+          dark: dark.color.foreground.status.warning,
         },
         'foreground-status-ok': {
-          light: light.color.foreground.status.ok.$value,
-          dark: dark.color.foreground.status.ok.$value,
+          light: light.color.foreground.status.ok,
+          dark: dark.color.foreground.status.ok,
         },
         'foreground-status-info': {
-          light: light.color.foreground.status.info.$value,
-          dark: dark.color.foreground.status.info.$value,
+          light: light.color.foreground.status.info,
+          dark: dark.color.foreground.status.info,
         },
 
         'foreground-onBrand': {
-          light: light.color.foreground.onBrand.$value,
-          dark: dark.color.foreground.onBrand.$value,
+          light: light.color.foreground.onBrand,
+          dark: dark.color.foreground.onBrand,
         },
         'button-secondary-border-default': {
-          light: light.color.button.secondary.border.default.$value,
-          dark: dark.color.button.secondary.border.default.$value,
+          light: light.color.button.secondary.border.default,
+          dark: dark.color.button.secondary.border.default,
         },
         // TO DO do we want these as tokens?
         'background-layer-overlay': '#00000080',
@@ -323,6 +324,13 @@ const buildTheme = tokens => {
         placeholder: 'text-weak',
         'text-primary-button': '#FFFFFF',
         'background-cta-alternate': '#F2F2F2',
+      },
+      elevation: {
+        light: {
+          small: elevationLight.elevation.small,
+          medium: elevationLight.elevation.medium,
+          large: elevationLight.elevation.large,
+        },
       },
     },
     button: {
@@ -402,5 +410,5 @@ const buildTheme = tokens => {
   });
 };
 
-export const current = buildTheme([light, dark]);
-export const warm = buildTheme([warmLight, warmDark]);
+export const current = buildTheme([light, dark, elevationLight]);
+export const warm = buildTheme([warmLight, warmDark, elevationLight]);
