e6e3ced3b7b64501b159d91ba338666505355405
Taylor Seamans <taylor.seamans@yahoo.com>
2024-05-10 11:43:34 -0700

    Latest changes

--- aries-core/src/js/components/core/Selector/SelectorHeader.js
+++ aries-core/src/js/components/core/Selector/SelectorHeader.js
@@ -1,23 +1,40 @@
 import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 import { Box, CheckBox, RadioButton, Text } from 'grommet';
 import { SelectorGroupContext } from './SelectorGroup';
 
-export const SelectorHeader = ({ icon, description, title, selected }) => {
+const SelectorIndicator = ({ selected }) => {
   const { multiple } = useContext(SelectorGroupContext);
-  return (
-    <Box align="start" direction="row" pad="small">
-      <Box direction="row" gap="small" flex wrap>
-        {icon}
-        <Box>
-          <Text weight={500}>{title}</Text>
-          <Text>{description}</Text>
-        </Box>
-      </Box>
-      {multiple ? (
-        <CheckBox tabIndex={-1} checked={selected} />
-      ) : (
-        <RadioButton tabIndex={-1} checked={selected} />
-      )}
-    </Box>
+
+  return multiple ? (
+    <CheckBox tabIndex={-1} checked={selected} readOnly />
+  ) : (
+    <RadioButton tabIndex={-1} checked={selected} readOnly />
   );
 };
+
+const SelectorHeader = ({ icon, description, title, selected }) => (
+  <Box align="start" direction="row" pad="small">
+    <Box direction="row" gap="small" flex wrap>
+      {icon}
+      <Box>
+        {title && <Text weight={500}>{title}</Text>}
+        {description && <Text>{description}</Text>}
+      </Box>
+    </Box>
+    <SelectorIndicator selected={selected} />
+  </Box>
+);
+
+SelectorIndicator.propTypes = {
+  selected: PropTypes.bool,
+};
+
+SelectorHeader.propTypes = {
+  icon: PropTypes.element,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  selected: PropTypes.bool,
+};
+
+export { SelectorHeader };
