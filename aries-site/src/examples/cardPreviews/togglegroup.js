91ae83be88824e402581d00dd9f699551cd72a2f
Taylor Seamans <taylor.seamans@yahoo.com>
2024-05-24 11:35:51 -0700

    Draft ToggleGroup docs

---
+++ aries-site/src/examples/cardPreviews/togglegroup.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Box, ToggleGroup } from 'grommet';
+import { Table, List, MapLocation } from 'grommet-icons';
+
+export const ToggleGroupPreview = () => (
+  <Box align="center" justify="center">
+    <ToggleGroup
+      options={[
+        {
+          icon: <Table />,
+          value: 'table',
+        },
+        {
+          icon: <List />,
+          value: 'list',
+        },
+        {
+          icon: <MapLocation />,
+          value: 'map',
+        },
+      ]}
+    />
+  </Box>
+);
