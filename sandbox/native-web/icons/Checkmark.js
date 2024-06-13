3499f106fcea12dfc161632ef35d7a4451934403
Taylor Seamans <taylor.seamans@yahoo.com>
2024-06-13 14:46:52 -0700

    Add CheckBox tokens

---
+++ sandbox/native-web/icons/Checkmark.js
@@ -0,0 +1,14 @@
+/* eslint-disable max-len */
+export const Checkmark = ({ color = '#000', size = 'medium' }) => `<svg
+viewBox="0 0 24 24"
+a11yTitle="User"
+class="icon 
+${color} icon-${size} pad-none"
+>
+<path
+        fill="none"
+        stroke="#000"
+        strokeWidth="2"
+        d="m2 14 7 6L22 4"
+      />
+</svg>`;
