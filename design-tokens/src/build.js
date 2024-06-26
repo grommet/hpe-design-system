35f4096e0b548398e74e5a32b9edb8cfe6b72088
Taylor Seamans <taylor.seamans@yahoo.com>
2024-06-26 15:02:35 -0700

    Add esm-grommet build script and align CSS files with latest tokens

--- design-tokens/src/build.js
+++ design-tokens/src/build.js
@@ -1,6 +1,7 @@
 import { mkdirSync, readFileSync, writeFileSync, readdirSync } from 'fs';
 
 // for now, convert from number (which Figma wants) to dimension with units
+// TO DO clean up this function
 const numberToDimension = (primitives, component, unit = 'px') => {
   const result = primitives;
 
@@ -104,7 +105,17 @@ const rawComponents = readFileSync(`./${TOKENS_DIR}/component.default.json`);
 const components = JSON.parse(rawComponents);
 const dimensionComponents = numberToDimension(components, true);
 
-// typography
+// currently used by style-dictionary. Figma Variables doesn't want units,
+// but code needs
+writeFileSync(
+  './dist/primitives.base.json',
+  `${JSON.stringify(dimensionPrimitives, null, 2)}`,
+);
+writeFileSync(
+  './dist/component.default.json',
+  `${JSON.stringify(dimensionComponents, null, 2)}`,
+);
+
 const typographyFiles = readdirSync(TOKENS_DIR)
   .map(file =>
     file.includes('typography') ? `${TOKENS_DIR}/${file}` : undefined,
@@ -121,13 +132,116 @@ typographyFiles.forEach(file => {
   );
 });
 
-// currently used by style-dictionary. Figma Variables doesn't want units,
-// but code needs
+// ---------------- Build step for Grommet components ----------------- //
+const tokens = readdirSync(TOKENS_DIR).map(file => `${TOKENS_DIR}/${file}`);
+let allTokens = {};
+tokens.forEach(file => {
+  const raw = readFileSync(file);
+  const parsed = JSON.parse(raw);
+  allTokens = { ...allTokens, ...parsed };
+});
+
+const access = (path, object) => {
+  return path.split('.').reduce((o, i) => o[i], object);
+};
+
+const isReference = value => /{.*}/.test(value);
+
+const getValue = valueArg => {
+  let value = valueArg;
+  if (
+    typeof value === 'string' &&
+    (value.split('.')[0].includes('base') ||
+      value.split('.')[0].includes('static'))
+  ) {
+    value = value.slice(1, -1);
+    const a = access(value, allTokens).$value;
+    value = getValue(a);
+  } else if (
+    // keep as grommet-like reference. For example,
+    // '{spacing.small}' --> 'small'
+    // '{color.text.strong} --> 'text-strong'
+    // 'color.background.validation.critical' --> 'validation-critical'
+    // 'color.background.back' --> 'background-back'
+    // NOTE: We may not be able to fully automate this process,
+    // so we may need to maintain a map of the exceptions
+    // (e.g., validation-critical)
+    // OR in grommet-theme-hpe, we can add the new namespace
+    // and in a future major version deprecate the old value
+    typeof value === 'string' &&
+    // TO DO can we automate this list?
+    (value.split('.')[0].includes('spacing') ||
+      value.split('.')[0].includes('radius') ||
+      value.split('.')[0].includes('content') ||
+      value.split('.')[0].includes('border') ||
+      value.split('.')[0].includes('color'))
+  ) {
+    value = value.slice(1, -1);
+    value = value.split('.');
+    value.shift();
+    value = value.join('-');
+  }
+  // when referencing another component token, resolve
+  // down to the nearest semantic or primitive token
+  else if (
+    isReference(value) &&
+    !value.includes('base') &&
+    !value.includes('static')
+  ) {
+    value = value.slice(1, -1);
+    const a = access(value, allTokens).$value;
+    value = getValue(a);
+  }
+
+  return value;
+};
+
+const jsonToNestedValue = token => {
+  // is non-object value
+  if (!token || typeof token !== 'object') return token;
+  // is design token
+  if ('$value' in token) {
+    let value = token.$value;
+    // if reference
+    if (/{.*}/.test(value)) {
+      value = getValue(value);
+    }
+    return value;
+  }
+  // is obj
+  const nextObj = {};
+  Object.entries(token).forEach(([prop, value]) => {
+    nextObj[prop] = jsonToNestedValue(value);
+  });
+
+  return nextObj;
+};
+
+// TO DO make dynamic
+const exclude = [
+  'static',
+  'base',
+  'color',
+  'TBD',
+  'spacing',
+  'radius',
+  'border',
+  'content',
+];
+
+allTokens = numberToDimension(allTokens, true);
+let res = jsonToNestedValue(allTokens);
+
+exclude.forEach(category => delete res[category]);
+
+// TO DO make prefix dynamic
+res = { hpe: { ...res } };
+mkdirSync('./dist/esm/');
 writeFileSync(
-  './dist/primitives.base.json',
-  `${JSON.stringify(dimensionPrimitives, null, 2)}`,
+  './dist/esm/components.default.js',
+  `export default ${JSON.stringify(res, null, 2)}`,
 );
 writeFileSync(
-  './dist/component.default.json',
-  `${JSON.stringify(dimensionComponents, null, 2)}`,
+  './dist/esm/index.js',
+  "export { default as components } from './components.default';\n",
 );
--- sandbox/tailwind-app/src/App.jsx
+++ sandbox/tailwind-app/src/App.jsx
@@ -1,5 +1,30 @@
+/* eslint-disable react/prop-types */
 import './App.css';
+import React from 'react';
+import * as Switch from '@radix-ui/react-switch';
+import * as Select from '@radix-ui/react-select';
 import { Button } from './components/Button';
+import {
+  // CheckIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+} from '@radix-ui/react-icons';
+
+const Text = ({ size, children }) => (
+  <span key={size} className={`text-${size} text-text-default`}>
+    {children}
+  </span>
+);
+
+const SelectItem = React.forwardRef(({ children, ...props }, forwardedRef) => {
+  return (
+    <Select.Item className="SelectItem" {...props} ref={forwardedRef}>
+      <Select.ItemText>{children}</Select.ItemText>
+    </Select.Item>
+  );
+});
+
+SelectItem.displayName = 'SelectItem';
 
 function App() {
   return (
@@ -28,7 +53,7 @@ function App() {
         </div>
       </div>
       <div className="flex flex-col bg-background-front p-medium rounded-small w-full gap-small">
-        <h2 className="text-text-strong">Subsection A</h2>
+        <h2 className="text-text-strong">Tailwind classes on native HTML</h2>
         <p className="text-text-default">
           Banjo pour-over kinfolk mukbang taiyaki semiotics. Craft beer neutral
           milk hotel tonx chicharrones small batch taxidermy hexagon church-key
@@ -46,10 +71,41 @@ function App() {
           '4xl',
           '5xl',
         ].map(size => (
-          <span key={size} className={`text-${size} text-text-default`}>
-            {size} text
-          </span>
+          <Text key={size} size={size}>
+            {size}
+          </Text>
         ))}
+      </div>
+      <div className="flex flex-col items-start bg-background-front p-medium rounded-small w-full gap-small">
+        <h2 className="text-text-strong">RadixUI with CSS variables</h2>
+        <Switch.Root className="SwitchRoot" id="airplane-mode">
+          <Switch.Thumb className="SwitchThumb" />
+        </Switch.Root>
+        <Select.Root>
+          <Select.Trigger className="SelectTrigger" aria-label="Food">
+            <Select.Value placeholder="Select a fruit" />
+            <Select.Icon className="SelectIcon">
+              <ChevronDownIcon />
+            </Select.Icon>
+          </Select.Trigger>
+          <Select.Portal>
+            <Select.Content className="SelectContent">
+              <Select.ScrollUpButton className="SelectScrollButton">
+                <ChevronUpIcon />
+              </Select.ScrollUpButton>
+              <Select.Viewport className="SelectViewport">
+                <SelectItem value="apple">Apple</SelectItem>
+                <SelectItem value="banana">Banana</SelectItem>
+                <SelectItem value="blueberry">Blueberry</SelectItem>
+                <SelectItem value="grapes">Grapes</SelectItem>
+                <SelectItem value="pineapple">Pineapple</SelectItem>
+              </Select.Viewport>
+              <Select.ScrollDownButton className="SelectScrollButton">
+                <ChevronDownIcon />
+              </Select.ScrollDownButton>
+            </Select.Content>
+          </Select.Portal>
+        </Select.Root>
       </div>
     </div>
   );
