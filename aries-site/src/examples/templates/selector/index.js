a4e7bf894b5e70382a9e569bedad2ce043141c75
britt6612 <brittany.archibeque@hpe.com>
2024-07-11 12:53:06 -0600

    add visials for types

--- aries-site/src/examples/templates/selector/index.js
+++ aries-site/src/examples/templates/selector/index.js
@@ -2,4 +2,6 @@ export * from './TEMP';
 export * from './SupportSelector';
 export * from './QuickFilter';
 export * from './SelectorAnatomy';
+export * from './SelectorMutli';
 export * from './SelectorSimple';
+export * from './SelectorSingle';
