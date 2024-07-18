4d5a895b7af5e732de862083e6bcb1bc52b7fd93
britt6612 <brittany.archibeque@hpe.com>
2024-07-18 09:44:31 -0600

    add files to test theme on tokens

--- aries-site/src/examples/templates/filtering/FilteringTable/FilterServers.js
+++ aries-site/src/examples/templates/filtering/FilteringTable/FilterServers.js
@@ -1,7 +1,6 @@
 import { useContext, useState } from 'react';
 import {
   Box,
-  Button,
   Data,
   DataFilters,
   DataSearch,
@@ -9,7 +8,9 @@ import {
   DataTable,
   DataTableColumns,
   Grid,
+  Menu,
   ResponsiveContext,
+  Pagination,
   Text,
   Toolbar,
 } from 'grommet';
@@ -91,15 +92,20 @@ export const FilterServers = () => (
     height={{ min: 'medium' }}
   >
     <Data data={serverhealth} properties={properties}>
-      <Toolbar gap="medium">
-        <Toolbar>
-          <DataSearch responsive />
-          <DataFilters layer />
-        </Toolbar>
+      <Toolbar>
+        <DataSearch responsive />
         <DataTableColumns drop tip="Configure columns" options={options} />
+        <DataFilters layer />
         {/* Flex Box added for spacing between Button */}
         <Box flex />
-        <Button secondary label="Add server" />
+        <Menu
+          label="Actions"
+          kind="toolbar"
+          items={[
+            [{ label: 'New server' }, { label: 'Edit server groups' }],
+            [{ label: 'Delete server' }],
+          ]}
+        />
       </Toolbar>
       <DataSummary />
       <ServerResults />
@@ -112,7 +118,7 @@ const ServerResults = () => {
   const [selected, setSelected] = useState([]);
 
   return (
-    <Box flex overflow="auto">
+    <Box gap="small">
       <DataTable
         aria-describedby="servers-heading"
         columns={[
@@ -126,12 +132,14 @@ const ServerResults = () => {
           },
           ...columns,
         ]}
-        pin
         primaryKey="id"
         sortable
         onSelect={nextSelected => setSelected(nextSelected)}
         select={selected}
       />
+      <Box pad={{ vertical: 'small' }} border={{ side: 'top' }}>
+        <Pagination alignSelf="end" />
+      </Box>
     </Box>
   );
 };
