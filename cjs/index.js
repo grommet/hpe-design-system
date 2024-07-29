36cf61ddbc9337ed5393df36f31cce05203c09ce
Taylor Seamans <taylor.seamans@yahoo.com>
2024-07-29 16:35:39 -0700

    Remove commonJS files

--- cjs/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "base": "require(./base)",
-  "dark": "require(./color.dark)",
-  "light": "require(./color.light)",
-  "components": "require(./components.default)",
-  "large": "require(./dimension.large)",
-  "small": "require(./dimension.small)"
-}
