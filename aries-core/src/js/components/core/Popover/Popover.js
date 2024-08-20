6ebce958c4bc7021ee80ba8d391447c1dd804e71
Brittany Archibeque <archibeq@hpe-k12m5pw4jt.home>
2024-08-20 10:30:34 -0600

    add changes to popover component

--- aries-core/src/js/components/core/Popover/Popover.js
+++ aries-core/src/js/components/core/Popover/Popover.js
@@ -1,27 +1,10 @@
-import { Box, Button, Drop, Heading } from 'grommet';
+import { Box, Button, Drop } from 'grommet';
+import PropTypes from 'prop-types';
 import { Close } from 'grommet-icons';
-
-const align = { top: 'bottom', left: 'left' };
-
-const PopoverHeading = ({ heading, onClose }) => (
-  <Box align='center' justify="between" direction="row">
-    <Heading size="small" level={2} margin="none">
-      {heading}
-    </Heading>
-    <Button
-      icon={<Close size="small" />}
-      onClick={onClose}
-      a11yTitle={`You are in a popover containing... To close this popover,
-      press Enter.`}
-      autoFocus
-    />
-  </Box>
-);
 
 export const Popover = ({
   heading,
   children,
-  body,
   footer,
   targetRef,
   onClose,
@@ -30,18 +13,39 @@ export const Popover = ({
   return (
     <Drop
       round="xxsmall"
+      role="dialog"
       elevation="medium"
       stretch={false}
-      align={align}
       target={targetRef?.current}
       onClose={onClose}
       {...rest}
     >
-      <Box flex gap="small" pad="small">
-        <PopoverHeading onClose={onClose} heading={heading} />
-        {children}
-        {footer}
+      <Box direction='row'>
+        <Box flex gap="small" pad="small">
+          {heading}
+          {children}
+          {footer}
+        </Box>
+        <Box pad="small">
+          <Button
+            icon={<Close size="small" />}
+            onClick={onClose}
+            a11yTitle={`You are in a popover containing information,
+            to close this popover, press Enter.`}
+            autoFocus
+          />
+        </Box>
       </Box>
     </Drop>
   );
+};
+
+Popover.propTypes = {
+  children: PropTypes.node,
+  heading: PropTypes.node,
+  footer: PropTypes.node,
+  targetRef: PropTypes.shape({
+    current: PropTypes.instanceOf(Element),
+  }),
+  onClose: PropTypes.func,
 };
