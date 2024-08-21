02fdb95412a1ee8cd4904442b8f72a5eb35dce43
taysea <taylor.seamans@yahoo.com>
2024-08-21 13:00:40 -0700

    Remove accidental file add

--- esm/index.js
+++
@@ -1,9 +0,0 @@
-export { default as components } from './components.default';
-export { default as base } from './base';
-export { default as global } from './global';
-export { default as dark } from './color.dark';
-export { default as light } from './color.light';
-export { default as large } from './dimension.large';
-export { default as small } from './dimension.small';
-export { default as elevationdark } from './elevation.dark';
-export { default as elevationlight } from './elevation.light';
