fbca8e033c1b3d02e7ce9e6e8c05f645d8bb0361
britt6612 <brittany.archibeque@hpe.com>
2024-08-28 13:20:30 -0600

    add new anatomy and guidance

--- aries-site/src/examples/templates/popover/index.js
+++ aries-site/src/examples/templates/popover/index.js
@@ -1,3 +1,6 @@
 export * from './PopoverAnatomy';
+export * from './PopoverDoExample';
+export * from './PopoverDontNestExample';
+export * from './PopoverDontPlacementExample';
 export * from './PopoverInlineExample';
 export * from './PopoverSimpleExample';
