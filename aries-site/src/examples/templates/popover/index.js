e00750e4ab3140e678c9f40463bd012a9108c8e6
britt6612 <brittany.archibeque@hpe.com>
2024-08-29 08:42:04 -0600

    update files

--- aries-site/src/examples/templates/popover/index.js
+++ aries-site/src/examples/templates/popover/index.js
@@ -1,6 +1,3 @@
 export * from './PopoverAnatomy';
-export * from './PopoverDoExample';
-export * from './PopoverDontNestExample';
-export * from './PopoverDontPlacementExample';
 export * from './PopoverInlineExample';
 export * from './PopoverSimpleExample';
