d9973a49e129e30539b305f27aaf4379da03ef3d
taysea <taylor.seamans@yahoo.com>
2024-09-06 13:45:02 -0700

    Remove unsupported formats and update package.json

--- esm-unresolved/index.js
+++
@@ -1,9 +0,0 @@
-export { default as large } from './dimension.large';
-export { default as small } from './dimension.small';
-export { default as components } from './component.default';
-export { default as base } from './primitives.base';
-export { default as dark } from './color.dark';
-export { default as light } from './color.light';
-export { default as elevationdark } from './elevation.dark';
-export { default as elevationlight } from './elevation.light';
-export { default as default } from './global.default';
