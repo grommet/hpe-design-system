1be90f97b30aa29d24fe5d3dc97c382517dc5e87
britt6612 <brittany.archibeque@hpe.com>
2024-09-17 13:22:59 -0600

    make changes to align with figma file

---
+++ aries-site/src/examples/components/tag/TagMetaData.js
@@ -0,0 +1,65 @@
+import React, { useContext } from 'react';
+import { Box, Heading, Grid, ResponsiveContext, Tag } from 'grommet';
+import { User } from 'grommet-icons';
+import { Card } from '../../templates';
+
+const products = [
+  {
+    tags: ['USA', 'Colorado', 'HR', 'Full-time'],
+  },
+  {
+    tags: [
+      { name: 'Country', value: 'USA' },
+      { name: 'Location', value: 'Colorado' },
+      { name: 'Department', value: 'HR' },
+      { name: 'Type', value: 'Full-time' },
+    ],
+  },
+];
+const columns = {
+  xsmall: ['auto'],
+  small: ['auto'],
+  medium: ['auto'],
+  large: ['auto', 'auto'],
+  xlarge: ['auto', 'auto'],
+};
+
+export const TagMetaData = () => {
+  const breakpoint = useContext(ResponsiveContext);
+
+  return (
+    <Box gap="medium">
+      <Heading level={2} margin="none">
+        My Products
+      </Heading>
+      <Grid columns={columns[breakpoint]} gap="medium">
+        {products.map((product, index) => (
+          <Card
+            key={index}
+            icon={<User />}
+            title="Program Advisor"
+            subtitle="June 25, 2024"
+            description={`Manages the end to end
+              recruitment process including consulting with HR
+              and business partners.`}
+            actions={
+              <Box direction="row" gap="xsmall" wrap>
+                {product.tags?.map((tag, j) => (
+                  <Tag
+                    name={tag.name ? tag.name : undefined}
+                    value={tag.value ? tag.value : tag}
+                    key={j}
+                    margin={{ bottom: 'xsmall' }}
+                  />
+                ))}
+              </Box>
+            }
+            alignActions="end"
+            onClick={() => {}}
+            level={3}
+          />
+        ))}
+      </Grid>
+    </Box>
+  );
+};
