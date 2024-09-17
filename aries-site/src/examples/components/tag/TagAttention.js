869a0c8b0c2ba3bd42c69711962658f7d3333656
britt6612 <brittany.archibeque@hpe.com>
2024-09-17 13:22:59 -0600

    make changes to align with figma file

---
+++ aries-site/src/examples/components/tag/TagAttention.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Box, Tag } from 'grommet';
+import { Card } from '../../templates';
+
+export const TagAttention = () => {
+  return (
+    <Box gap="medium">
+      <Card
+        icon={<Tag value="new" margin={{ bottom: 'xsmall' }} />}
+        title="Compute Ops Management"
+        subtitle="Compute"
+        description={`Securely manage your compute infrastructure
+             wherever it is lives.`}
+        alignActions="end"
+        onClick={() => {}}
+        level={3}
+      />
+    </Box>
+  );
+};
