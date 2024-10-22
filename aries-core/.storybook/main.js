e1aa1d4e0daf0cdd1cdd365f311450a00fa973dd
Jessica Filben <54560994+jcfilben@users.noreply.github.com>
2024-10-22 17:36:29 -0600

    Setup storybook (#4278)
    * setup storybook
    
    * remove extra files
    
    * cleanup preview file
    
    * remove chromatic
    
    * yarn.lock
    
    * Update .gitignore
    
    Co-authored-by: Taylor Seamans <taylor.seamans@yahoo.com>
    
    ---------
    
    Co-authored-by: Taylor Seamans <taylor.seamans@yahoo.com>

---
+++ aries-core/.storybook/main.js
@@ -0,0 +1,27 @@
+import { join, dirname } from 'path';
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
+
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    getAbsolutePath('@storybook/addon-webpack5-compiler-swc'),
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@storybook/addon-a11y'),
+  ],
+  framework: {
+    name: getAbsolutePath('@storybook/react-webpack5'),
+    options: {},
+  },
+};
+export default config;
