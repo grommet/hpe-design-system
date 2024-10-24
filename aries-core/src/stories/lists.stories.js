9ccbc228d2667e1a525d6cdf5403eafb7d9f3538
Jessica Filben <54560994+jcfilben@users.noreply.github.com>
2024-10-24 15:49:30 -0600

    Add more storybook stories

---
+++ aries-core/src/stories/lists.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+/* eslint-disable max-len */
+import { ListIconIdentifierExample } from 'aries-site/src/examples/templates/list-views/ListIconIdentifierExample';
+import { ListImageIdentifierExample } from 'aries-site/src/examples/templates/list-views/ListImageIdentifierExample';
+import { ListNameDescriptionOptionExample } from 'aries-site/src/examples/templates/list-views/ListNameDescriptionOptionExample';
+import { ListNameExample } from 'aries-site/src/examples/templates/list-views/ListNameExample';
+import { ListNameOptionActionExample } from 'aries-site/src/examples/templates/list-views/ListNameOptionActionExample';
+import { ListOrderExample } from 'aries-site/src/examples/templates/list-views/ListOrderExample';
+import { ListScreenExample } from 'aries-site/src/examples/templates/list-views/ListScreenExample';
+
+export const IconIdentifier = () => <ListIconIdentifierExample />;
+export const ImageIdentifier = () => <ListImageIdentifierExample />;
+export const NameDescriptionOption = () => <ListNameDescriptionOptionExample />;
+export const Name = () => <ListNameExample />;
+export const NameOptionAction = () => <ListNameOptionActionExample />;
+export const Order = () => <ListOrderExample />;
+export const Screen = () => <ListScreenExample />;
+
+export default {
+  title: 'Lists',
+};
