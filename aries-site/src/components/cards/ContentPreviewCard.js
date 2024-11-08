65a069f9cc059b5afe339d9a821db3a004015623
taysea <taylor.seamans@yahoo.com>
2024-11-08 14:52:08 -0800

    Fix card style on home page

--- aries-site/src/components/cards/ContentPreviewCard.js
+++ aries-site/src/components/cards/ContentPreviewCard.js
@@ -1,14 +1,33 @@
 import React, { forwardRef } from 'react';
+import { ThemeContext } from 'grommet';
 import { LinkCard } from './LinkCard';
 
 export const ContentPreviewCard = forwardRef(({ ...rest }, ref) => (
-  <LinkCard
-    align="start"
-    fill="horizontal"
-    background="background-front"
-    pad="large"
-    round="small"
-    ref={ref}
-    {...rest}
-  />
+  <ThemeContext.Extend
+    value={{
+      card: {
+        container: {
+          elevation: 'none',
+          border: {
+            color: 'border-weak',
+          },
+        },
+        hover: {
+          container: {
+            elevation: 'medium',
+          },
+        },
+      },
+    }}
+  >
+    <LinkCard
+      align="start"
+      fill="horizontal"
+      background="background-front"
+      pad="large"
+      round="small"
+      ref={ref}
+      {...rest}
+    />
+  </ThemeContext.Extend>
 ));
