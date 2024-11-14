ae94bd0d14e7c6cfa2cec4a94847740c5f169314
taysea <taylor.seamans@yahoo.com>
2024-11-14 12:48:29 -0800

    Remove unused transform

--- design-tokens/src/transforms/javascriptW3c.ts
+++ design-tokens/src/transforms/javascriptW3c.ts
@@ -2,7 +2,6 @@ export const javascriptW3c: string[] = [
   'attribute/cti',
   'name/dot',
   'size/rem',
-  // 'color/hex',
   'color/css', // support alpha
   'cubicBezier/css', // TO DO revisit if we want to apply this or not
   'shadow/css/shorthand',
