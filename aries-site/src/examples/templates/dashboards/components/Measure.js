cc1f05f54f28dcfd000591400275520fb249551b
Mike Kingdom <michael.kingdom@hpe.com>
2024-11-15 17:13:37 -0700

    Cleanup usage of dl/dt/dd
    Signed-off-by: Mike Kingdom <michael.kingdom@hpe.com>

--- aries-site/src/examples/templates/dashboards/components/Measure.js
+++ aries-site/src/examples/templates/dashboards/components/Measure.js
@@ -3,45 +3,7 @@ import PropTypes from 'prop-types';
 import { Box, Button, NameValuePair, Text } from 'grommet';
 import { TextEmphasis } from 'aries-core';
 
-export const Measure = ({ name, value: valueProp, onClick, ...rest }) => {
-  const { icon: iconProp, label } = name;
-  const icon = iconProp ? cloneElement(iconProp, { size: 'small' }) : null;
-
-  const value = valueProp?.value || valueProp;
-  const valueSize = valueProp?.size || 'xxlarge';
-
-  let contents = (
-    <NameValuePair
-      name={
-        <Box
-          direction="row"
-          align="center"
-          gap="small"
-          // margin is needed to keep consistent with the spacing
-          // delivered by the theme when name is typeof 'string'
-          // https://github.com/grommet/grommet/blob/db5be926eb7c2f791534f02dd55b0f9997e959db/src/js/themes/base.js#L1072
-          margin={{ bottom: 'xxsmall' }}
-        >
-          {icon}
-          <Text size={name.label?.size || 'small'}>
-            {label?.label || label}
-          </Text>
-        </Box>
-      }
-    >
-      <TextEmphasis size={valueSize}>{value}</TextEmphasis>
-    </NameValuePair>
-  );
-
-  if (onClick) {
-    contents = <Button onClick={onClick}>{contents}</Button>;
-  }
-
-  return <Box {...rest}>{contents}</Box>;
-};
-
-Measure.propTypes = {
-  name: PropTypes.shape({
+const NameType = PropTypes.shape({
     label: PropTypes.oneOfType([
       PropTypes.string.isRequired,
       PropTypes.shape({
@@ -50,14 +12,75 @@ Measure.propTypes = {
       }),
     ]).isRequired,
     icon: PropTypes.node,
-  }),
-  value: PropTypes.oneOfType([
+  });
+const ValueType = PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.number,
     PropTypes.shape({
       value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
       size: PropTypes.string,
     }),
-  ]),
+  ]);
+
+const Name = ({ name }) => {
+  const { icon: iconProp, label } = name;
+  const icon = iconProp ? cloneElement(iconProp, { size: 'small' }) : null;
+
+  return (
+    <Box
+      direction="row"
+      align="center"
+      gap="small"
+      // margin is needed to keep consistent with the spacing
+      // delivered by the theme when name is typeof 'string'
+      // https://github.com/grommet/grommet/blob/db5be926eb7c2f791534f02dd55b0f9997e959db/src/js/themes/base.js#L1072
+      margin={{ bottom: 'xxsmall' }}
+    >
+      {icon}
+      <Text size={label?.size || 'small'}>
+        {label?.label || label}
+      </Text>
+    </Box>
+  );
+};
+
+Name.propTypes = {
+  name: NameType,
+};
+
+const Value = ({ value: valueProp }) => {
+  const value = valueProp?.value || valueProp;
+  const valueSize = valueProp?.size || 'xxlarge';
+  
+  return <TextEmphasis size={valueSize}>{value}</TextEmphasis>;
+};
+
+Value.propTypes = {
+  value: ValueType,
+};
+
+export const Measure = ({ name, value, onClick }) => {
+  // If this measure is clickable, render a Button with the Name and Value
+  // rather a NameValuePair since <button> or even another <div> to attach
+  // the onClick which contains a <dt><dd> isn't correct markup.
+  return onClick ? (
+      <Button onClick={onClick}>
+        <>
+          <Name name={name} />
+          <Value value={value} />
+        </>
+      </Button>
+    ) : (
+      <NameValuePair
+        name={<Name name={name} />}
+      >
+        <Value value={value}/>
+      </NameValuePair>
+    );
+};
+
+Measure.propTypes = {
+  name: NameType,
+  value: ValueType,
   onClick: PropTypes.func,
 };
