fe3fb139837f32f073309c18eaeb83adb327ed86
Brittany <42451602+britt6612@users.noreply.github.com>
2024-11-19 13:13:49 -0700

    fix banner landmark violations (#4429)
    * fix banner landmark violations
    
    * add aria label

--- aries-site/src/examples/templates/inline-notifications/PageBannerExample.js
+++ aries-site/src/examples/templates/inline-notifications/PageBannerExample.js
@@ -3,6 +3,7 @@ import {
   Anchor,
   Box,
   Grid,
+  Main,
   Notification,
   Page,
   PageContent,
@@ -32,66 +33,73 @@ export const PageBannerExample = () => {
           title="Machine Learning Ops"
           parent={<ReverseAnchor label="Private Cloud Home" />}
         />
-        <Box gap="medium">
-          <Notification
-            flex={false}
-            status="critical"
-            message={`Support for CentOS v7.5 ended on 04/04/021.
+        <Main>
+          <Box gap="medium">
+            <Notification
+              flex={false}
+              status="critical"
+              message={`Support for CentOS v7.5 ended on 04/04/021.
             One or more applications require updates to a later version.`}
-            // onClick is used for Design System site demonstration only.
-            // in production, onClick should be replaced with an href that
-            // changes the route and sets the active tab.
-            actions={[{ label: 'View details', onClick: () => setActive(2) }]}
-          />
-          <Tabs justify="start" activeIndex={active}>
-            <Tab title="Projects" onClick={() => setActive(0)}>
-              <Box pad={{ vertical: 'medium' }}>
-                <Grid columns="medium" rows="small" gap="medium">
-                  {[0, 1, 2, 3, 4].map(index => (
-                    <Card title={`Project${index}`} key={index} level={2} />
-                  ))}
-                </Grid>
-              </Box>
-            </Tab>
-            <Tab title="Applications" onClick={() => setActive(1)}>
-              <Box pad={{ vertical: 'medium' }}>
-                <Grid columns="medium" rows="small" gap="medium">
-                  {[0, 1, 2, 3, 4].map(index => (
-                    <Card title={`Application${index}`} key={index} level={2} />
-                  ))}
-                </Grid>
-              </Box>
-            </Tab>
-            <Tab title="Sites" onClick={() => setActive(2)}>
-              <Box pad={{ vertical: 'medium' }} gap="medium">
-                <Notification
-                  status="critical"
-                  message={
-                    <Paragraph margin="none">
-                      <TextEmphasis>DemoSite2</TextEmphasis>,{' '}
-                      <TextEmphasis>BLR2-setup1</TextEmphasis>, and{' '}
-                      {/* eslint-disable-next-line max-len */}
-                      <TextEmphasis>DemoSite4</TextEmphasis> require updates to{' '}
-                      <Anchor
-                        href="#"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        CentOS v8.1
-                      </Anchor>
-                      . <Anchor href="#">Apply updates</Anchor> to CentOS.
-                    </Paragraph>
-                  }
-                />
-                <Grid columns="medium" gap="medium">
-                  {sites.map((site, index) => (
-                    <Card title={site} key={index} level={2} />
-                  ))}
-                </Grid>
-              </Box>
-            </Tab>
-          </Tabs>
-        </Box>
+              // onClick is used for Design System site demonstration only.
+              // in production, onClick should be replaced with an href that
+              // changes the route and sets the active tab.
+              actions={[{ label: 'View details', onClick: () => setActive(2) }]}
+            />
+            <Tabs justify="start" activeIndex={active}>
+              <Tab title="Projects" onClick={() => setActive(0)}>
+                <Box pad={{ vertical: 'medium' }}>
+                  <Grid columns="medium" rows="small" gap="medium">
+                    {[0, 1, 2, 3, 4].map(index => (
+                      <Card title={`Project${index}`} key={index} level={2} />
+                    ))}
+                  </Grid>
+                </Box>
+              </Tab>
+              <Tab title="Applications" onClick={() => setActive(1)}>
+                <Box pad={{ vertical: 'medium' }}>
+                  <Grid columns="medium" rows="small" gap="medium">
+                    {[0, 1, 2, 3, 4].map(index => (
+                      <Card
+                        title={`Application${index}`}
+                        key={index}
+                        level={2}
+                      />
+                    ))}
+                  </Grid>
+                </Box>
+              </Tab>
+              <Tab title="Sites" onClick={() => setActive(2)}>
+                <Box pad={{ vertical: 'medium' }} gap="medium">
+                  <Notification
+                    status="critical"
+                    message={
+                      <Paragraph margin="none">
+                        <TextEmphasis>DemoSite2</TextEmphasis>,{' '}
+                        <TextEmphasis>BLR2-setup1</TextEmphasis>, and{' '}
+                        {/* eslint-disable-next-line max-len */}
+                        <TextEmphasis>DemoSite4</TextEmphasis> require updates
+                        to{' '}
+                        <Anchor
+                          href="#"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          CentOS v8.1
+                        </Anchor>
+                        . <Anchor href="#">Apply updates</Anchor> to CentOS.
+                      </Paragraph>
+                    }
+                  />
+                  <Grid columns="medium" gap="medium">
+                    {sites.map((site, index) => (
+                      <Card title={site} key={index} level={2} />
+                    ))}
+                  </Grid>
+                </Box>
+              </Tab>
+            </Tabs>
+          </Box>
+        </Main>
       </PageContent>
     </Page>
   );
