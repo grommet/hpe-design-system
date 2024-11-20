6f36dc4c6051a1992dcf564fcb4f8c55f35f9b42
taysea <taylor.seamans@yahoo.com>
2024-11-19 17:26:36 -0800

    Upgrade to style-dictionary v4

--- design-tokens/src/formats/commonJsGrommetRefs.ts
+++ design-tokens/src/formats/commonJsGrommetRefs.ts
@@ -1,31 +1,30 @@
-import StyleDictionary from 'style-dictionary';
-import { FormatterArguments } from 'style-dictionary/types/Format.js';
+import { fileHeader, minifyDictionary } from 'style-dictionary/utils';
+import { FormatFn, FormatFnArguments } from 'style-dictionary/types';
 import { getGrommetValue } from './utils/getGrommetValue.js';
 import { jsonToNestedValue } from './utils/jsonToNestedValue.js';
 import { access } from '../utils.js';
 
-const { fileHeader } = StyleDictionary.formatHelpers;
-
-export const commonJsGrommetRefs = ({
+export const commonJsGrommetRefs: FormatFn = async ({
   dictionary,
   file,
   platform = {},
-}: FormatterArguments) => {
+}: FormatFnArguments) => {
   const { prefix } = platform;
   let tokens = dictionary.tokens;
 
   dictionary.allTokens.forEach((token: any) => {
     const value = getGrommetValue(token, dictionary);
     const originalToken = access(token.path.join('.'), tokens);
-    originalToken.value = value;
+    originalToken.$value = value;
   });
 
   tokens = prefix ? { [prefix]: tokens } : tokens;
-  const output = `${fileHeader({ file })}module.exports = ${JSON.stringify(
-    jsonToNestedValue(tokens),
+  const output = `${await fileHeader({
+    file,
+  })}module.exports = ${JSON.stringify(
+    jsonToNestedValue(minifyDictionary(tokens, true)), // build in minify
     null,
     2,
   )}\n`;
-
   return output;
 };
--- design-tokens/src/formats/esmGrommetRefs.ts
+++ design-tokens/src/formats/esmGrommetRefs.ts
@@ -1,29 +1,27 @@
-import StyleDictionary from 'style-dictionary';
-import { FormatterArguments } from 'style-dictionary/types/Format.js';
+import { fileHeader, minifyDictionary } from 'style-dictionary/utils';
+import { FormatFn, FormatFnArguments } from 'style-dictionary/types';
 import { getGrommetValue } from './utils/getGrommetValue.js';
 import { jsonToNestedValue } from './utils/jsonToNestedValue.js';
 import { access } from '../utils.js';
-const { fileHeader } = StyleDictionary.formatHelpers;
 
-export const esmGrommetRefs = ({
+export const esmGrommetRefs: FormatFn = async ({
   dictionary,
   file,
   platform = {},
-}: FormatterArguments) => {
+}: FormatFnArguments) => {
   const { prefix } = platform;
   let tokens = dictionary.tokens;
   dictionary.allTokens.forEach((token: any) => {
     const value = getGrommetValue(token, dictionary);
     const originalToken = access(token.path.join('.'), tokens);
-    originalToken.value = value;
+    originalToken.$value = value;
   });
 
   tokens = prefix ? { [prefix]: tokens } : tokens;
-  const output = `${fileHeader({ file })}export default ${JSON.stringify(
-    jsonToNestedValue(tokens),
+  const output = `${await fileHeader({ file })}export default ${JSON.stringify(
+    jsonToNestedValue(minifyDictionary(tokens, true)), // build in minify
     null,
     2,
   )}\n`;
-
   return output;
 };
