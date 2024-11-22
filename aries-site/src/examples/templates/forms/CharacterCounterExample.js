ec3cf0fcdac47dbe80a1adb959222e6aa5e1e607
britt6612 <brittany.archibeque@hpe.com>
2024-11-21 21:09:34 -0700

    add announce to textarea for character limit

--- aries-site/src/examples/templates/forms/CharacterCounterExample.js
+++ aries-site/src/examples/templates/forms/CharacterCounterExample.js
@@ -1,32 +1,50 @@
-import React from 'react';
-import { Form, FormField, TextArea } from 'grommet';
+import React, { useContext, useState } from 'react';
+import { AnnounceContext, Form, FormField, TextArea } from 'grommet';
 
 export const CharacterCounterExample = () => {
-  const maxLength = 40;
-  const threshold = 0.5;
+  const announce = useContext(AnnounceContext);
+  const maxCharacterCount = 40;
+  const threshold = maxCharacterCount * 0.5;
+  const [value, setValue] = useState('');
 
+  const handleFocus = () => {
+    announce('The textarea has a maxium character count of 40.');
+  };
+
+  const handleChange = event => {
+    const newValue = event.target.value;
+    setValue(newValue);
+
+    // Announce when the user exceeds the threshold
+    if (newValue.length >= threshold && newValue.length < maxCharacterCount) {
+      announce(
+        `You have reached ${Math.round(
+          (newValue.length / maxCharacterCount) * 100,
+        )}% of the character limit.`,
+      );
+    } else if (newValue.length >= maxCharacterCount) {
+      announce(
+        `You have exceeded the maximum character
+         limit of ${maxCharacterCount}.`,
+      );
+    }
+  };
   return (
     <Form validate="change">
       <FormField
         contentProps={{ width: 'medium' }}
         label="Issue description"
-        validate={value =>
-          value.length / maxLength > threshold
-            ? {
-                status: maxLength - value.length >= 0 ? 'info' : 'error',
-                message:
-                  maxLength - value.length >= 0
-                    ? `${maxLength - value.length} characters left`
-                    : `${value.length - maxLength} character${
-                        value.length - maxLength > 1 ? 's' : ''
-                      } over limit`,
-              }
-            : undefined
-        }
+        validate={{ max: 40, threshold: 0.5 }}
         name="issue-description"
         htmlFor="issue-description"
       >
-        <TextArea id="issue-description" name="issue-description" />
+        <TextArea
+          value={value}
+          onChange={handleChange}
+          id="issue-description"
+          name="issue-description"
+          onFocus={handleFocus}
+        />
       </FormField>
     </Form>
   );
