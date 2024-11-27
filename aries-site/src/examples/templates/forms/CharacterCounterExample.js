78412cd59c65740f0384cd857aa08bac9c493705
Jessica Rosenquist <54560994+jcfilben@users.noreply.github.com>
2024-11-27 11:04:41 -0700

    added aria-label to character counter examples (#4484)

--- aries-site/src/examples/templates/forms/CharacterCounterExample.js
+++ aries-site/src/examples/templates/forms/CharacterCounterExample.js
@@ -26,7 +26,11 @@ export const CharacterCounterExample = () => {
         name="issue-description"
         htmlFor="issue-description"
       >
-        <TextArea id="issue-description" name="issue-description" />
+        <TextArea
+          aria-label="Text area with 40 character limit"
+          id="issue-description"
+          name="issue-description"
+        />
       </FormField>
     </Form>
   );
