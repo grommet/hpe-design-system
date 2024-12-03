e5c64e6f4cf8a7daaad626c5c8128cacca1b2f1b
Matt Glissmann <mdglissmann@gmail.com>
2024-12-03 16:31:43 -0700

    refactor scale build up and size results to desired step

--- design-tokens-manager/src/routes/Scaler/Results.jsx
+++ design-tokens-manager/src/routes/Scaler/Results.jsx
@@ -6,15 +6,27 @@ const roundToNearest = (value, nearest) => {
   return Math.round(value / nearest) * nearest;
 };
 
-const createScale = (base, factor, steps, nearest) => {
-  const stepsAbove = Math.ceil(steps / 2);
-  const stepsBelow = Math.floor(steps / 2);
+const createScale = (base, factor, steps, gridUnit) => {
   const values = [];
-  for (let i = 0; i < stepsAbove; i++) {
-    values.push(roundToNearest(base * Math.pow(factor, i), nearest || base));
+  // above base
+  for (let i = 0; i < steps; i++) {
+    values.push(roundToNearest(base * Math.pow(factor, i), gridUnit || base));
   }
-  for (let i = 1; i < stepsBelow; i++) {
-    values.push(Math.round(base / Math.pow(factor, i)));
+  // below base
+  for (let i = 0; i < steps; i++) {
+    const value = base / Math.pow(factor, i);
+    let nearest = gridUnit || base;
+
+    // round to nearest even number
+    if (value >= 4 && value < gridUnit) {
+      nearest = 2;
+    }
+    // round to nearest integer
+    else if (value < 4) {
+      nearest = 1;
+    }
+
+    values.push(roundToNearest(value, nearest));
   }
 
   const result = values
@@ -26,7 +38,13 @@ const createScale = (base, factor, steps, nearest) => {
     }, [])
     .sort((a, b) => a - b);
 
-  return result;
+  const stepsBelow = Math.ceil(steps / 2);
+  const stepsAbove = Math.floor(steps / 2);
+  const baseIndex = result.indexOf(base);
+  const below = result.slice(baseIndex + 1 - stepsBelow, baseIndex + 1);
+  const above = result.slice(baseIndex + 1, baseIndex + 1 + stepsAbove);
+
+  return [...below, ...above];
 };
 
 const defaultCopyTip = 'Copy scale to clipboard';
@@ -55,13 +73,65 @@ const CopyButton = ({ scale, ...rest }) => {
   );
 };
 
+const tshirtSizes = [
+  'xxxsmall',
+  'xxsmall',
+  'xsmall',
+  'small',
+  'medium',
+  'large',
+  'xlarge',
+  'xxlarge',
+  'xxxlarge',
+];
+
 export const Results = ({ base, factor, steps, nearest, setOpen, ...rest }) => {
   const [scale, setScale] = useState([]);
+  const [stops, setStops] = useState([]);
+  const [tshirtScale, setTshirtScale] = useState([]);
 
+  // Create scale
   useEffect(() => {
     const nextScale = createScale(base, factor, steps, nearest);
     setScale(nextScale);
   }, [base, factor, steps, nearest]);
+
+  // Create stops
+  useEffect(() => {
+    const nextStops = [base];
+    const baseIndex = scale.indexOf(base);
+
+    for (let i = baseIndex; i < scale.length; i++) {
+      if (scale[i] / nextStops[nextStops.length - 1] > factor) {
+        nextStops.push(scale[i]);
+      }
+    }
+    for (let i = baseIndex - 1; i >= 0; i--) {
+      if (nextStops[0] / scale[i] > factor) {
+        nextStops.unshift(scale[i]);
+      }
+    }
+    setStops(nextStops);
+  }, [base, factor, steps, scale]);
+
+  // Create t-shirt sizes
+  useEffect(() => {
+    const baseIndex = scale.indexOf(base);
+    const mediumIndex = tshirtSizes.indexOf('medium');
+
+    const tshirt = tshirtSizes.reduce((acc, size, index) => {
+      const value = scale[baseIndex - mediumIndex + index];
+      if (value) {
+        acc[size] = value;
+      }
+      return acc;
+    }, {});
+
+    const nextTshirtScale = Object.entries(tshirt).map(([size, value]) => {
+      return { size, value };
+    });
+    setTshirtScale(nextTshirtScale);
+  }, [base, scale]);
 
   return (
     <Box
@@ -71,29 +141,61 @@ export const Results = ({ base, factor, steps, nearest, setOpen, ...rest }) => {
       height={{ min: 'medium' }}
       {...rest}
     >
-      <Box
-        direction="row"
-        align="end"
-        alignSelf="center"
-        cssGap
-        gap="medium"
-        wrap
-      >
-        {scale &&
-          scale.map((value, index) => {
-            return (
-              <Box key={value} align="center" gap="xsmall">
-                <Box
-                  background="brand"
-                  width={`${value}px`}
-                  height={`${value}px`}
-                />
-                <Text key={index} size="small">
-                  {value}px
-                </Text>
-              </Box>
-            );
-          })}
+      <Box>
+        <Box
+          direction="row"
+          align="end"
+          alignSelf="center"
+          cssGap
+          gap="medium"
+          wrap
+        >
+          {scale &&
+            scale.map((value, index) => {
+              return (
+                <Box key={value} align="center" gap="xsmall">
+                  <Box
+                    background={
+                      value === base
+                        ? 'purple'
+                        : stops.includes(value)
+                        ? 'purple!'
+                        : 'brand'
+                    }
+                    width={`${value}px`}
+                    height={`${value}px`}
+                  />
+                  <Text key={index} size="small">
+                    {value}px
+                  </Text>
+                </Box>
+              );
+            })}
+        </Box>
+        <Box
+          direction="row"
+          align="end"
+          alignSelf="center"
+          cssGap
+          gap="medium"
+          wrap
+        >
+          {stops &&
+            stops.map((value, index) => {
+              return (
+                <Box key={value} align="center" gap="xsmall">
+                  <Box
+                    background={stops.includes(value) ? 'purple!' : 'brand'}
+                    width={`${value}px`}
+                    height={`${value}px`}
+                  />
+                  <Text key={index} size="small">
+                    {value}px
+                  </Text>
+                </Box>
+              );
+            })}
+        </Box>
       </Box>
       <Box gap="xsmall" flex={false}>
         <Button
