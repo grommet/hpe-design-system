a8e247ebc7cb3945d77a381560370363ca6019e0
Matt Glissmann <mdglissmann@gmail.com>
2024-12-13 14:49:46 -0700

    Removed unused import

--- sandbox/grommet-app/src/themes/theme.jsx
+++ sandbox/grommet-app/src/themes/theme.jsx
@@ -1,5 +1,5 @@
 import { css } from 'styled-components';
-import { deepFreeze, deepMerge } from 'grommet/utils';
+import { deepFreeze } from 'grommet/utils';
 import {
   dark as localDark,
   light as localLight,
