89901e2733922e11413fa24295cbb09b9cd04635
Matthew Glissmann <mdglissmann@gmail.com>
2024-12-17 14:23:24 -0700

    Sticker-sheet - Adds tabs to break up content sections (#4610)
    * Resolved console error regarding defaultProps deprecation
    
    * Moved Compare, ModeContext, and StyleInProgress to local components
    
    * Moved Type components
    
    * Moved Controls components
    
    * Added tabs for sticker-sheet content
    
    * Keep track of active tab index
    
    * Moved Inputs components
    
    * Moved Visualizations components
    
    * Moved Layout components

---
+++ sandbox/grommet-app/src/pages/sticker-sheet/content/Type/Anchors.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import { Anchor, Box } from 'grommet';
+import ContentPane from '../../../../components/ContentPane';
+import { Compare } from '../../components';
+
+export const Anchors = ({ textSizes }) => {
+  return (
+    <ContentPane>
+      <Box gap="small">
+        {textSizes.map(size => (
+          <Compare key={size}>
+            <Anchor size={size} key={size}>
+              Anchor {size}
+            </Anchor>
+          </Compare>
+        ))}
+      </Box>
+    </ContentPane>
+  );
+};
+
+Anchors.propTypes = {
+  textSizes: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
---
+++ sandbox/grommet-app/src/pages/sticker-sheet/content/Type/Headings.jsx
@@ -0,0 +1,33 @@
+import { Box, Heading } from 'grommet';
+import ContentPane from '../../../../components/ContentPane';
+import { Compare } from '../../components';
+
+const HEADING_LEVELS = [1, 2, 3, 4, 5, 6];
+
+export const Headings = () => {
+  return (
+    <ContentPane>
+      <Box gap="medium">
+        {HEADING_LEVELS.map(level => (
+          <Box gap="small" key={level}>
+            {/* Heading sizes are not relevant to product teams because our guidance
+                  discourages use of them. */}
+            {/* {['small', 'medium', 'large', 'xlarge'].map(size => ( */}
+            <Compare guidingChild="last">
+              <Heading
+                // size={size}
+                key={`${level}`}
+                level={level}
+                margin="none"
+                style={{ color: 'inherit' }}
+              >
+                Heading {level}
+              </Heading>
+            </Compare>
+            {/* ))} */}
+          </Box>
+        ))}
+      </Box>
+    </ContentPane>
+  );
+};
