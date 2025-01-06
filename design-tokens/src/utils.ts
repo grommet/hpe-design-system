e8f8178f3bfe0b33d779e1669946a0cca28fc3bc
taysea <taylor.seamans@yahoo.com>
2025-01-06 15:32:21 -0800

    Fix error message to be more generic

--- design-tokens/src/utils.ts
+++ design-tokens/src/utils.ts
@@ -105,7 +105,7 @@ export const verifyReferences = (
           invalidVariables.includes(modeValue.id)
         ) {
           console.error(
-            `🛑 Invalid color collection reference for value of: ${variable.name}`,
+            `🛑 Invalid collection reference for value of: ${variable.name}`,
           );
         }
       });
