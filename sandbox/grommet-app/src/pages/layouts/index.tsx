0fd48adf471cc5b57a86dd604c020a66489bfe26
Taylor Seamans <taylor.seamans@yahoo.com>
2025-01-22 13:20:48 -0800

    Add Form example to grommet-app (#4729)
    * Add Form example
    
    * Update CheckboxGroup validation

--- sandbox/grommet-app/src/pages/layouts/index.tsx
+++ sandbox/grommet-app/src/pages/layouts/index.tsx
@@ -1,19 +1,42 @@
-import React from "react";
+import React from 'react';
 import { Link, Route } from 'react-router-dom';
-import { List, Page, PageContent, PageHeader } from "grommet";
+import { List, Page, PageContent, PageHeader } from 'grommet';
 import { RoutedAnchor } from '../../components';
-import { Collection, EmptyState } from './kinds';
+import { Collection, EmptyState, Form } from './kinds';
 
-const layouts = ['Collection', 'Dashboard', 'Detail', 'Empty state', 'Form', 'Home', 'List', 'Login', 'Main', 'NotFound', 'Settings', 'Splash', 'Wizard'];
+const layouts = [
+  'Collection',
+  'Dashboard',
+  'Detail',
+  'Empty state',
+  'Form',
+  'Home',
+  'List',
+  'Login',
+  'Main',
+  'NotFound',
+  'Settings',
+  'Splash',
+  'Wizard',
+];
 
 const Layouts = () => {
   return (
-    <Page pad={{ bottom: "xlarge" }}>
+    <Page pad={{ bottom: 'xlarge' }}>
       <PageContent>
         <PageHeader title="Layouts" />
-        <List data={layouts} defaultItemProps={{ pad: { vertical: 'xxsmall' } }}>
-          {(item) => (
-            <RoutedAnchor key={item} as={Link} to={`/layouts/${item.toLowerCase().replace(' ', '-')}`} label={item} alignSelf="start" />
+        <List
+          data={layouts}
+          defaultItemProps={{ pad: { vertical: 'xxsmall' } }}
+        >
+          {item => (
+            <RoutedAnchor
+              key={item}
+              as={Link}
+              to={`/layouts/${item.toLowerCase().replace(' ', '-')}`}
+              label={item}
+              alignSelf="start"
+            />
           )}
         </List>
       </PageContent>
@@ -22,9 +45,22 @@ const Layouts = () => {
 };
 
 const routes = [
-  <Route key="collection" path="/layouts/collection" element={<Collection />} />,
-  <Route key="dashboard" path="/layouts/dashboard" element={<div>Dashboard</div>} />,
-  <Route key="empty-state" path="/layouts/empty-state" element={<EmptyState />} />,
-]
+  <Route
+    key="collection"
+    path="/layouts/collection"
+    element={<Collection />}
+  />,
+  <Route
+    key="dashboard"
+    path="/layouts/dashboard"
+    element={<div>Dashboard</div>}
+  />,
+  <Route
+    key="empty-state"
+    path="/layouts/empty-state"
+    element={<EmptyState />}
+  />,
+  <Route key="form" path="/layouts/form" element={<Form />} />,
+];
 
 export { Layouts, routes };
--- sandbox/grommet-app/src/pages/layouts/kinds/index.ts
+++ sandbox/grommet-app/src/pages/layouts/kinds/index.ts
@@ -1,2 +1,3 @@
 export * from './Collection';
 export * from './EmptyState';
+export * from './Form';
