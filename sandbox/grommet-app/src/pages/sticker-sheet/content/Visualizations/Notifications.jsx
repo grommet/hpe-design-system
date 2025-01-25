f1035fc3eda5cc770dee91f5c169723120f9d4f4
Taylor Seamans <taylor.seamans@yahoo.com>
2025-01-24 16:26:55 -0800

    Theme token cleanups (#4732)
    * Theme token cleanups
    
    * Additional v1 refinements
    
    * Apply suggestions from code review
    
    * Add global focus tokens
    
    * Pull latest tokens
    
    * Cleanups
    
    * Add checked and disabled switch
    
    * Pull latest from Figma
    
    * Update yarn.lock
    
    * Add adjacent calendar day
    
    * Add radiobutton theme fixes
    
    * Styling cleanups
    
    * Refine styles
    
    * Remove temp for background gradient
    
    * Update yarn.lock
    
    * Fix scope

--- sandbox/grommet-app/src/pages/sticker-sheet/content/Visualizations/Notifications.jsx
+++ sandbox/grommet-app/src/pages/sticker-sheet/content/Visualizations/Notifications.jsx
@@ -4,45 +4,50 @@ import ContentPane from '../../../../components/ContentPane';
 
 export const Notifications = () => {
   return (
-    <ContentPane>
+    <ContentPane overflow="auto">
       <Box gap="medium">
         <Compare>
           <Notification
             status="critical"
-            message="this is a message"
+            message="This is a critical notification. It should be used to alert the user of a critical issue."
             title="this is a title"
+            width="large"
           />
         </Compare>
         <Compare>
           <Notification
             status="warning"
-            message="this is a message"
+            message="This is a warning notification. It should be used to indicate caution to the user."
             title="this is a title"
             onClose={() => {}}
+            width="large"
           />
         </Compare>
         <Compare>
           <Notification
             status="normal"
-            message="this is a message"
+            message="This is a success notification. It should be used to inform the user of a successful action."
             title="this is a title"
             onClose={() => {}}
+            width="large"
           />
         </Compare>
         <Compare>
           <Notification
             status="info"
-            message="this is a message"
+            message="This is an information notification. It should be used for teriary information."
             title="this is a title"
             onClose={() => {}}
+            width="large"
           />
         </Compare>
         <Compare>
           <Notification
             status="unknown"
-            message="this is a message"
+            message="This is an unknown notification. It should be used when that status can't be inferred."
             title="this is a title"
             onClose={() => {}}
+            width="large"
           />
         </Compare>
       </Box>
