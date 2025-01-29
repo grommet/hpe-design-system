bcb33090a0f3d2c812d845126b5c2456b681d153
Matt Glissmann <mdglissmann@gmail.com>
2025-01-28 17:14:18 -0700

    extended Metric component functionality

--- sandbox/grommet-app/src/pages/layouts/kinds/RecordDetailDSCC/SystemSummary.tsx
+++ sandbox/grommet-app/src/pages/layouts/kinds/RecordDetailDSCC/SystemSummary.tsx
@@ -1,20 +1,39 @@
 import React from "react";
-import { Box } from "grommet";
-import { DashboardCard, Metric } from "../../../../components";
+import { Box, Text } from "grommet";
+import { DashboardCard, Metric, metricSizes } from "../../../../components";
+import { StatusGoodSmall } from "grommet-icons";
 
 
 export const SystemSummary = ({ ...rest }) => {
   return (
     <DashboardCard
       title="System"
+      subtitle={undefined}
       level={3}
       hideCta
+      icon={undefined}
+      external={undefined}
+      footer={undefined}
+      inline={undefined}
       {...rest}
     >
-      <Metric
-        label="Call home"
-        value="Enabled"
-      />
+      <Box direction="row" gap="medium">
+        <Metric
+          label="Call home"
+          value={
+            <Box direction="row" align="center" gap="xsmall">
+              <StatusGoodSmall color="status-ok" />
+              <Text size={metricSizes.medium.value} color="text-strong" weight={500}>On</Text>
+            </Box>
+          }
+          reverse
+        />
+        <Metric
+          label="Source"
+          value="System name"
+          reverse
+        />
+      </Box>
     </DashboardCard>
   );
 };
