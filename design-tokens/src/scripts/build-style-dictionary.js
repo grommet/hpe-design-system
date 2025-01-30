1bd0dff109f2b4e8adf8e070575b9ab188d94ff7
taysea <taylor.seamans@yahoo.com>
2025-01-30 13:59:53 -0800

    Fix ESM build format

--- design-tokens/src/scripts/build-style-dictionary.js
+++ design-tokens/src/scripts/build-style-dictionary.js
@@ -117,7 +117,7 @@ try {
         files: [
           {
             destination: 'global.js',
-            format: 'esmGrommetRefs',
+            format: 'javascript/esm',
             filter: token =>
               token.filePath === `${TOKENS_DIR}/semantic/global.default.json`,
           },
@@ -414,7 +414,7 @@ try {
             filter: token =>
               token.filePath.includes(`${TOKENS_DIR}/component/`) &&
               !token.path.includes(FIGMA_PREFIX),
-            format: 'esmGrommetRefs',
+            format: 'javascript/esm',
           },
         ],
       },
