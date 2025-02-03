1c5aca55711fd0ec9d5ab152cd32c0b230f51a6a
taysea <taylor.seamans@yahoo.com>
2025-02-03 15:40:56 -0800

    Add grommet commonjs export

--- design-tokens/src/formats/index.ts
+++ design-tokens/src/formats/index.ts
@@ -1,4 +1,5 @@
 export { commonJs } from './commonJs.js';
+export { commonJsGrommetRefs } from './commonJsGrommetRefs.js';
 export { cssColorModes } from './cssColorModes.js';
 export { cssBreakpoints } from './cssBreakpoints.js';
 export { esmGrommetRefs } from './esmGrommetRefs.js';
