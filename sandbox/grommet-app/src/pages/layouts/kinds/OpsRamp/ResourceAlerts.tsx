62d69132678a1f85c0f0533e5d6cd985893dacff
britt6612 <brittany.archibeque@hpe.com>
2025-02-20 14:49:31 -0700

    fix metric

--- sandbox/grommet-app/src/pages/layouts/kinds/OpsRamp/ResourceAlerts.tsx
+++ sandbox/grommet-app/src/pages/layouts/kinds/OpsRamp/ResourceAlerts.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Box, Text } from 'grommet';
-import { Metric } from '../../../../components';
+import { Metric, metricSizes } from '../../../../components';
+import {
+  StatusGoodSmall,
+  StatusCriticalSmall,
+  StatusWarningSmall,
+  StatusInfo,
+  StatusPlaceholderSmall,
+} from 'grommet-icons';
 
-const AlertMetric = ({ label, value }) => (
+const AlertMetric = ({ label, size, value }) => (
   <Metric
     align="center"
     label={label}
-    unit={null}
     value={value}
+    unit={null}
     options={{}}
     reverse
-    size="small"
+    size={size}
   />
 );
 
@@ -25,7 +32,7 @@ const TicketNotesSummary = () => (
   </Box>
 );
 
-export const ResourceAlerts = () => (
+export const ResourceAlerts = ({ size }) => (
   <>
     <Box pad={{ vertical: 'small' }}>
       <Text>Resource Alerts</Text>
@@ -36,12 +43,102 @@ export const ResourceAlerts = () => (
       direction="row"
       gap="medium"
     >
-      <AlertMetric label="All" value={12} />
-      <AlertMetric label="Critical" value={4} />
-      <AlertMetric label="Warning" value={2} />
-      <AlertMetric label="OK" value={0} />
-      <AlertMetric label="Info" value={4} />
-      <AlertMetric label="Observed" value={2} />
+      <AlertMetric
+        size={size}
+        label="All"
+        value={
+          <Box direction="row" align="center" gap="xsmall">
+            <StatusGoodSmall color="status-ok" />
+            <Text
+              size={metricSizes[size].value}
+              color="text-strong"
+              weight={500}
+            >
+              12
+            </Text>
+          </Box>
+        }
+      />
+      <AlertMetric
+        size={size}
+        label="Critical"
+        value={
+          <Box direction="row" align="center" gap="xsmall">
+            <StatusCriticalSmall color="status-critical" />
+            <Text
+              size={metricSizes[size].value}
+              color="text-strong"
+              weight={500}
+            >
+              4
+            </Text>
+          </Box>
+        }
+      />
+      <AlertMetric
+        size={size}
+        label="Warning"
+        value={
+          <Box direction="row" align="center" gap="xsmall">
+            <StatusWarningSmall color="status-warning" />
+            <Text
+              size={metricSizes[size].value}
+              color="text-strong"
+              weight={500}
+            >
+              2
+            </Text>
+          </Box>
+        }
+      />
+      <AlertMetric
+        size={size}
+        label="OK"
+        value={
+          <Box direction="row" align="center" gap="xsmall">
+            <StatusGoodSmall color="status-ok" />
+            <Text
+              size={metricSizes[size].value}
+              color="text-strong"
+              weight={500}
+            >
+              0
+            </Text>
+          </Box>
+        }
+      />
+      <AlertMetric
+        size={size}
+        label="Info"
+        value={
+          <Box direction="row" align="center" gap="xsmall">
+            <StatusInfo />
+            <Text
+              size={metricSizes[size].value}
+              color="text-strong"
+              weight={500}
+            >
+              4
+            </Text>
+          </Box>
+        }
+      />
+      <AlertMetric
+        size={size}
+        label="Observed"
+        value={
+          <Box direction="row" align="center" gap="xsmall">
+            <StatusPlaceholderSmall />
+            <Text
+              size={metricSizes[size].value}
+              color="text-strong"
+              weight={500}
+            >
+              2
+            </Text>
+          </Box>
+        }
+      />
     </Box>
     <TicketNotesSummary />
   </>
