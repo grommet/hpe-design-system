c3a1d8fd30514668168f4b2574e944f515cd5542
taysea <taylor.seamans@yahoo.com>
2025-02-25 10:35:13 -0800

    Ensure card previews are representative of current styles

--- aries-site/src/examples/cardPreviews/textinput.js
+++ aries-site/src/examples/cardPreviews/textinput.js
@@ -2,19 +2,15 @@ import React from 'react';
 import { Form, FormField, TextInput } from 'grommet';
 
 export const TextInputPreview = () => (
-    <Form>
-      <FormField htmlFor="focus-id" name="focus">
-        <TextInput
-          aria-label="preview"
-          id="focus-id"
-          name="focus"
-          style={{
-            boxShadow: '0 0 2px 2px #00E8CF',
-            borderRadius: '4px',
-          }}
-          placeholder="Enter a username"
-          tabIndex={-1}
-        />
-      </FormField>
-    </Form>
-  );
+  <Form>
+    <FormField htmlFor="focus-id" name="focus">
+      <TextInput
+        aria-label="preview"
+        id="focus-id"
+        name="focus"
+        placeholder="Enter a username"
+        tabIndex={-1}
+      />
+    </FormField>
+  </Form>
+);
