80587eee735d950bc871e1c3bf263009ecfff757
britt6612 <brittany.archibeque@hpe.com>
2025-02-28 12:50:44 -0700

    fix smaller break points

--- sandbox/grommet-app/src/pages/layouts/kinds/HPEMarketing/HPEAI.tsx
+++ sandbox/grommet-app/src/pages/layouts/kinds/HPEMarketing/HPEAI.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
-import { Box, Button, Heading, Page, PageContent, Text } from 'grommet';
+import React, { useContext } from 'react';
+import {
+  Box,
+  Button,
+  Grid,
+  Heading,
+  Page,
+  PageContent,
+  Text,
+  ResponsiveContext,
+} from 'grommet';
 import { LinkNext } from 'grommet-icons';
 
-// when I apply background image the text color is black we need white
-const textColor = '#FFFFFF';
-
 export const HPEAI = () => {
+  const breakpoint = useContext(ResponsiveContext);
+
+  const columns = ['xsmall', 'small'].includes(breakpoint)
+    ? ['auto']
+    : ['1/2', '1/2'];
+  const rows = ['auto'];
+  const gap = {
+    medium: 'medium',
+    large: 'large',
+    xlarge: 'large',
+  };
+
+  const areasDefault = [['learn', 'hpeLogo']];
+
+  const areasSmall = [['learn', 'unassigned']];
+
+  const areas = ['xsmall', 'small'].includes(breakpoint)
+    ? areasSmall
+    : areasDefault;
+
+  let padTop: string = 'none';
+  if (['xsmall', 'small'].includes(breakpoint)) {
+    padTop = 'none';
+  } else if (breakpoint === 'medium') {
+    padTop = 'small';
+  } else if (breakpoint === 'large' || breakpoint === 'xlarge') {
+    padTop = 'large';
+  }
   return (
     <Page pad={{ bottom: 'xlarge' }}>
       <PageContent
@@ -13,38 +47,59 @@ export const HPEAI = () => {
         // cloest we have is 96. As for the top and bottom they have 40px
         // we have 48px
         pad={{ vertical: 'large', horizontal: 'xlarge' }}
-        background={{
-          fill: 'horizontal',
-          image: 'url(/public/marketing-background.png)',
-        }}
+        background={
+          ['xsmall', 'small'].includes(breakpoint)
+            ? { color: 'background', dark: true }
+            : {
+                fill: 'horizontal',
+                image: 'url(/public/marketing-background.png)',
+                dark: true,
+              }
+        }
+        fill="horizontal"
       >
-        <Box gap="medium" width="large">
-          <Box pad={{ top: 'large' }}>
+        <Grid areas={areas} columns={columns} rows={rows} gap={gap[breakpoint]}>
+          <Box
+            align={
+              ['xsmall', 'small'].includes(breakpoint) ? 'center' : 'start'
+            }
+            gap="medium"
+            gridArea="learn"
+          >
             {/* // this is off a little for the padding on top compared to page. */}
-            <Text weight="bold" color={textColor} size="small">
-              Artifical Intelligence (AL) solutions
-            </Text>
+            <Box pad={{ top: padTop }}>
+              <Text weight="bold" size="small">
+                Artificial Intelligence (AI) solutions
+              </Text>
+            </Box>
+            {/* // this heading is not large enough can we use size or we steering away? */}
+            <Heading margin="none" level="1">
+              Learn how HPE and NVIDIA unlock AL
+            </Heading>
+            <Box gap="large">
+              <Text
+                size={
+                  ['xsmall', 'small'].includes(breakpoint) ? 'small' : 'xlarge'
+                }
+              >
+                AI is everywhere, disrupting every industry and creating
+                limitless opportunities. HPE and NVIDIA are collaborating to
+                deliver co-developed solutions to help you accelerate the
+                adoption of generative AI.
+              </Text>
+              {/* // alignement between text and icon is off */}
+              <Button
+                label="HPE ships first NVIDIA Grace Blackwell system"
+                kind="cta-primary"
+                alignSelf={
+                  ['xsmall', 'small'].includes(breakpoint) ? 'none' : 'start'
+                }
+                icon={<LinkNext size="small" />}
+              />
+            </Box>
+            <Box gridArea="hpeLogo" />
           </Box>
-          {/* // this heading is not large enough can we use size or we steering away? */}
-          <Heading color={textColor} margin="none" level="1">
-            Learn how HPE and NVIDIA unlock AL
-          </Heading>
-          <Box gap="large">
-            <Text size="xlarge" color={textColor}>
-              AI is everywhere, disrupting every industry and creating limitless
-              opportunities. HPE and NVIDIA are collaborating to deliver
-              co-developed solutions to help you accelerate the adoption of
-              generative AI.
-            </Text>
-            {/* // alignement between text and icon is off */}
-            <Button
-              label="HPE ships first NVIDIA Grace Blackwell system"
-              kind="cta-primary"
-              alignSelf="start"
-              icon={<LinkNext size="small" />}
-            />
-          </Box>
-        </Box>
+        </Grid>
       </PageContent>
     </Page>
   );
