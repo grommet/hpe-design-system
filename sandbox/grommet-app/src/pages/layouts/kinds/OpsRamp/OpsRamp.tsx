0663b5f03bde4fd1cced2bcdb60089dd240f9d5a
britt6612 <brittany.archibeque@hpe.com>
2025-03-03 22:44:11 -0700

    address more feedback

--- sandbox/grommet-app/src/pages/layouts/kinds/OpsRamp/OpsRamp.tsx
+++ sandbox/grommet-app/src/pages/layouts/kinds/OpsRamp/OpsRamp.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
   Box,
+  Grid,
   Page,
   PageContent,
   PageHeader,
@@ -9,15 +10,45 @@ import {
   ResponsiveContext,
 } from 'grommet';
 import { RoutedAnchor, NavSidebar } from '../../../../components';
+import ContentPane from '../../../../components/ContentPane';
 import { Previous } from 'grommet-icons';
 import { NavItems } from './navItems';
 import { ServersTable } from './ServersTable';
+import { ResourceDetails } from './ResourceDetails';
 
 const defaultSelected = NavItems['Container Orchestration'][0];
 
 export const OpsRamp: React.FC = () => {
   const breakpoint = React.useContext(ResponsiveContext);
   const isSidebarCollapsed = ['xsmall', 'small'].includes(breakpoint);
+
+  console.log(breakpoint);
+
+  const [showResultDetails, setShowResultDetails] = useState(false);
+
+  const gridColumns = ['xsmall', 'small'].includes(breakpoint)
+    ? ['flex', 'auto']
+    : ['auto', 'auto'];
+
+  const columnsGap = {
+    xsmall: 'xsmall',
+    small: 'xsmall',
+    medium: 'small',
+    large: 'medium',
+    xlarge: 'medium',
+  };
+
+  const gap = {
+    column: columnsGap[breakpoint],
+  };
+
+  const areasDefault = [['datetable', 'resourcedetails']];
+
+  const areasSmall = [['datatable', 'unassigned']];
+
+  const areas = ['xsmall', 'small'].includes(breakpoint)
+    ? areasSmall
+    : areasDefault;
 
   return (
     <Box direction="row" fill>
@@ -43,7 +74,36 @@ export const OpsRamp: React.FC = () => {
               />
             }
           />
-          <ServersTable />
+          <Grid
+            columns={showResultDetails ? gridColumns : 'full'}
+            areas={showResultDetails ? areas : undefined}
+            gap={{ column: gap.column }}
+          >
+            <ContentPane
+              gridArea=""
+              heading=""
+              level={2}
+              actions={undefined}
+              skeleton={undefined}
+              overflow="auto"
+            >
+              <ServersTable
+                showResultDetails={showResultDetails}
+                setShowResultDetails={setShowResultDetails}
+              />
+            </ContentPane>
+            {showResultDetails && (
+              <ResourceDetails
+                layer={['xsmall', 'small', 'medium'].includes(breakpoint)}
+                onClose={() => setShowResultDetails(false)}
+                animation={
+                  ['xsmall', 'small', 'medium'].includes(breakpoint)
+                    ? false
+                    : ['slideLeft', 'fadeIn']
+                }
+              />
+            )}
+          </Grid>
         </PageContent>
       </Page>
     </Box>
--- sandbox/grommet-app/src/pages/layouts/kinds/OpsRamp/ResourceDetails.tsx
+++ sandbox/grommet-app/src/pages/layouts/kinds/OpsRamp/ResourceDetails.tsx
@@ -4,9 +4,9 @@ import {
   Button,
   Menu,
   Text,
-  ResponsiveContext,
   Layer,
   Heading,
+  ResponsiveContext,
 } from 'grommet';
 import { LinkNext, StatusGoodSmall } from 'grommet-icons';
 import ContentPane from '../../../../components/ContentPane';
@@ -91,10 +91,8 @@ export const ResourceDetails: React.FC<ResourceDetailsProps> = ({
   const metricSize = ['xsmall', 'small', 'medium'].includes(breakpoint)
     ? 'small'
     : 'medium';
-
   const content = (
     <ContentPane
-      gap="small"
       heading={
         <Box align="center" direction="row" gap="medium">
           <Button onClick={onClose} icon={<LinkNext />} />
@@ -107,19 +105,20 @@ export const ResourceDetails: React.FC<ResourceDetailsProps> = ({
       actions={<ResourceActions onClose={onClose} />}
       skeleton={undefined}
       animation={animation}
+      height="fit-content"
       {...rest}
     >
       <ResourceSections size={metricSize} />
     </ContentPane>
   );
 
-  return layer ? (
-    <Layer full modal={false}>
-      {content}
-    </Layer>
-  ) : (
-    content
-  );
-};
+  if (layer) {
+    return (
+      <Layer full modal={false} animation={animation}>
+        {content}
+      </Layer>
+    );
+  }
 
-export default ResourceDetails;
+  return content;
+};
