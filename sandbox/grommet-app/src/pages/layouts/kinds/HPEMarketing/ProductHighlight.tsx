8a7ce18fd81f302d15161bdcc055ca9d079c8c38
britt6612 <brittany.archibeque@hpe.com>
2025-03-05 12:05:43 -0700

    add comment

--- sandbox/grommet-app/src/pages/layouts/kinds/HPEMarketing/ProductHighlight.tsx
+++ sandbox/grommet-app/src/pages/layouts/kinds/HPEMarketing/ProductHighlight.tsx
@@ -4,6 +4,7 @@ import { Box, Image, Text } from 'grommet';
 export const ProductHightlight = ({ icon, desc, title }) => {
   return (
     <Box align="center" gap="medium">
+      {/* icon here are much smaller then design intends */}
       {icon}
       <Text textAlign="center" size="large">
         {title}
