db9ec627ae225b50eb725b0b05d6bc2db1b4d541
Jessica Rosenquist <54560994+jcfilben@users.noreply.github.com>
2025-03-07 14:37:10 -0700

    Update accessibility docs now that the site has the new theme applied (#4841)
    * update accessibility docs now that the site has the new theme applied
    
    * fix button doc

--- aries-site/src/data/structures/components.js
+++ aries-site/src/data/structures/components.js
@@ -31,7 +31,7 @@ import { TagPreview } from '../../examples/cardPreviews/tag';
 export const components = [
   {
     name: 'Anchor',
-    accessibility: 'Passed WCAG 2.2 AA',
+    accessibility: 'Passed WCAG 2.2 AAA',
     category: 'Controls',
     description:
       'Hyperlinks used with text-based navigation, such as inline text.',
@@ -845,7 +845,7 @@ export const components = [
   },
   {
     name: 'Menu',
-    accessibility: 'Failed WCAG 2.2 AA',
+    accessibility: 'Passed WCAG 2.2 AA',
     category: 'Controls',
     description:
       'Menu is a component that contains a list of actions. When a user clicks an item in the menu, the menu closes and the action is executed.',
