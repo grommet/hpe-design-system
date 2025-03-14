3396194a3a757034c0cff1acca354a8003ffdc7c
Lucas Speyer <lucas.speyer@gmail.com>
2025-03-14 18:11:27 -0300

    Update WCAGRuleDetail.js

--- aries-site/src/layouts/content/WCAGRuleDetail.js
+++ aries-site/src/layouts/content/WCAGRuleDetail.js
@@ -64,7 +64,7 @@ const WCAGAccessibilityCardView = ({
             ruleDescription
           ) : (
             <Text>
-              {ruleDescription}
+              {' '}{ruleDescription}
               <Anchor
                 label="Read more"
                 aria-label={`Read more about ${ruleName}`}
