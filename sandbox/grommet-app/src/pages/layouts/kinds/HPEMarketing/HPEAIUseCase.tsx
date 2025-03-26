8a65803a6625f1cbe3ffc503ade5ccf0edb23c29
Brittany <42451602+britt6612@users.noreply.github.com>
2025-03-26 13:20:42 -0600

    Update sandbox/grommet-app/src/pages/layouts/kinds/HPEMarketing/HPEAIUseCase.tsx
    Co-authored-by: Taylor Seamans <taylor.seamans@yahoo.com>

--- sandbox/grommet-app/src/pages/layouts/kinds/HPEMarketing/HPEAIUseCase.tsx
+++ sandbox/grommet-app/src/pages/layouts/kinds/HPEMarketing/HPEAIUseCase.tsx
@@ -48,7 +48,7 @@ export const HPEAIUseCase = () => {
             // button sizes are off in design they have 42px height and we have 36px
             <Button
               label="View the use case brief"
-              aria-label="View the usecase brief on Virtual Assistants"
+              aria-label="View the use case brief on Virtual Assistants"
               reverse
               primary
               icon={<LinkNext />}
