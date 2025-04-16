5c7aa2ed562ec270e89d658f3c911698617c316f
julauxen <158175533+julauxen@users.noreply.github.com>
2025-04-16 11:30:11 +0100

    Added accessibility information to NameValueList

--- aries-site/src/data/structures/components.js
+++ aries-site/src/data/structures/components.js
@@ -1215,6 +1215,7 @@ export const components = [
   },
   {
     name: 'NameValueList',
+    accessibility: 'Passed WCAG 2.2 AAA',
     category: 'Visualizations',
     description: 'A NameValueList displays a group of NameValuePairs.',
     seoDescription: 'A NameValueList displays a group of NameValuePairs.',
