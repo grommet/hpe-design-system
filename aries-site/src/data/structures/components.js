f2963d4980570e226d18edf1856f84092f020997
britt6612 <brittany.archibeque@hpe.com>
2025-05-02 12:17:38 -0600

    add datatable accessibility evaluation

--- aries-site/src/data/structures/components.js
+++ aries-site/src/data/structures/components.js
@@ -1486,6 +1486,7 @@ export const components = [
   },
   {
     name: 'DataTable',
+    accessibility: 'Failed WCAG 2.2 AA',
     category: 'Visualizations',
     description: 'DataTable presents data in a column and row format.',
     preview: {
