0098e163f0d8e4a0867ad00c44c592015720bdd5
taysea <taylor.seamans@yahoo.com>
2025-05-06 16:28:20 -0700

    POC for updated tabs styles

--- aries-site/src/themes/aries.js
+++ aries-site/src/themes/aries.js
@@ -1,6 +1,8 @@
 import { hpe, hpePop } from 'grommet-theme-hpe';
 import { deepMerge } from 'grommet/utils';
 import { Info } from 'grommet-icons';
+// eslint-disable-next-line import/no-unresolved
+import { dimension, components } from 'hpe-design-tokens/grommet';
 
 export const aries = deepMerge(hpe, {
   defaultMode: 'dark',
@@ -44,6 +46,63 @@ export const aries = deepMerge(hpe, {
       weight: 500,
       alignSelf: 'end',
     },
+  },
+  tab: {
+    color: 'text',
+    active: {
+      background: undefined,
+      color: 'text-primary',
+      weight: 600,
+    },
+    hover: { background: 'transparent', color: 'text-strong' },
+    border: {
+      side: 'all',
+      color: 'transparent',
+      size: components.hpe.element.medium.borderWidth,
+      active: { color: undefined },
+      disabled: { color: undefined },
+      hover: { color: undefined },
+    },
+    disabled: { background: 'background-disabled', color: 'text-disabled' },
+    pad: {
+      bottom: components.hpe.element?.medium.paddingY,
+      top: components.hpe.element?.medium.paddingY,
+      horizontal: components.hpe.element?.medium?.paddingX?.narrow,
+    },
+    margin: { vertical: 'none', horizontal: 'none' },
+    extend: 'font-weight: 500;',
+  },
+  tabs: {
+    gap: 'medium',
+    header: {
+      border: undefined,
+      // padding-bottom ensures the marker is not cut off by subsequent
+      // page elements.
+      extend: ({ theme }) => `
+      padding-bottom: ${dimension.hpe.borderWidth.medium};
+      & button {
+        border-radius: ${dimension.hpe.radius.xxsmall};
+      }
+      & button[aria-selected="true"] {
+          position: relative;
+          &::before {
+            display: block;
+            position: absolute;
+            content: '';
+            height: ${dimension.hpe.borderWidth.medium};
+            border-radius: 9999px;
+            bottom: -${dimension.hpe.borderWidth.medium};
+            left: 0;
+            right: 0;
+            background: ${
+              theme.global.colors['border-selected'][
+                theme.dark ? 'dark' : 'light'
+              ]
+            };
+          }
+          }`,
+    },
+    step: { xsmall: 1, xlarge: 3 },
   },
 });
 
