2b63f64bef772a55681c568138e0fcf983c7914d
britt6612 <brittany.archibeque@hpe.com>
2025-05-15 13:25:20 -0600

    update DataView

--- aries-site/src/data/structures/components.js
+++ aries-site/src/data/structures/components.js
@@ -1926,7 +1926,7 @@ export const components = [
   },
   {
     name: 'DataView',
-    accessibility: 'Failed WCAG 2.2 AA',
+    accessibility: 'Passed WCAG 2.2 AA',
     available: true,
     category: 'Data',
     description:
