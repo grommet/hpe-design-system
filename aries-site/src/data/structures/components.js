1dee9507edd2975e08db76cfa3c40b86ad1898af
Kenny Tran <86075038+KennyAtHPE@users.noreply.github.com>
2025-05-20 13:04:49 -0700

    Tip WCAG eval

--- aries-site/src/data/structures/components.js
+++ aries-site/src/data/structures/components.js
@@ -493,6 +493,7 @@ export const components = [
   },
   {
     name: 'Tip',
+    accessibility: 'Passed WCAG 2.2 AAA',
     category: 'Controls',
     description:
       'A Tip is a small layer that opens when the user moves the mouse pointer over an element. The intention is to offer helpful information in the immediate context.',
