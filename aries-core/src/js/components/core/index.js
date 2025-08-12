55924836dafdc1df035e999688418fe4fb25c6b4
Matthew Glissmann <mdglissmann@gmail.com>
2025-08-12 14:10:04 -0600

    Start of NavigationMenu

--- aries-core/src/js/components/core/index.js
+++ aries-core/src/js/components/core/index.js
@@ -1,5 +1,6 @@
 export * from './Identifier';
 export * from './Nav';
+export * from './NavigationMenu/index.ts';
 export * from './Popover';
 export * from './RangeSelector';
 export * from './Tile';
---
+++ aries-core/src/stories/navigation.stories.tsx
@@ -0,0 +1,15 @@
+import { NavigationMenu } from '../js/components';
+
+export const NavigationMenuExample = () => {
+  const items = [
+    { id: 'home', label: 'Home' },
+    { id: 'about', label: 'About' },
+    { id: 'contact', label: 'Contact' },
+  ];
+
+  return <NavigationMenu items={items} />;
+};
+
+export default {
+  title: 'Navigation',
+};
