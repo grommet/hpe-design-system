1b714c6463e2a9bae909a8d4e4713e0f365f76e8
Matthew Glissmann <mdglissmann@gmail.com>
2025-08-13 13:50:26 -0600

    progress

--- aries-core/src/js/components/core/NavigationMenu/NavigationMenu.tsx
+++ aries-core/src/js/components/core/NavigationMenu/NavigationMenu.tsx
@@ -1,19 +1,100 @@
-interface NavigationMenuProps {
-  items: Array<{
-    id: string;
-    label: string;
-  }>;
+import { useState } from 'react';
+import { type BoxProps, Button, Collapsible, List, Nav, Sidebar } from 'grommet';
+import {
+	Down,
+	Sidebar as SidebarIcon,
+	Up,
+} from 'grommet-icons';
+import { NavItem, NavItemType } from './NavItem';
+import { NavContainer } from './NavContainer';
+
+interface NavigationMenuProps extends BoxProps {
+  items: NavItemType[];
 }
 
-export const NavigationMenu = ({items}: NavigationMenuProps) => {
-  return (
-    <nav>
-      <ul>
-        {items.map(item => (
-          <li key={item.id}>{item.label}</li>
-        ))}
-      </ul>
-    </nav>
-  );
+export const NavigationMenu = ({ items, ...rest }: NavigationMenuProps) => {
+	const [open, setOpen] = useState(false);
+	const [expanded, setExpanded] = useState<string[]>([]);
+
+	return (
+		<NavContainer open={open} setOpen={setOpen} {...rest}>
+			{open && (
+				<Nav a11yTitle="Main menu" gap="xsmall">
+					<List
+						role="menubar"
+						data={items}
+						defaultItemProps={{
+							pad: { vertical: 'xsmall' },
+						}}
+					>
+						{(item) => {
+							let result = null;
+							if (item.children) {
+								result = (
+									<NavItem
+										id={item.label}
+                    aria-haspopup="true"
+										aria-expanded={expanded.includes(item.label)}
+										label={item.label}
+										icon={item.icon}
+										actions={
+											expanded.includes(item.label) ? (
+												<Up aria-hidden="true" />
+											) : (
+												<Down aria-hidden="true" />
+											)
+										}
+										onClick={() => {
+											setExpanded((prev) => {
+												let result: string[];
+												if (prev.includes(item.label)) {
+													result = prev.filter((i) => i !== item.label);
+												} else {
+													result = [...prev, item.label];
+												}
+												return result;
+											});
+										}}
+									>
+										<Collapsible open={expanded.includes(item.label)}>
+											<List
+												role="menu"
+												aria-labelledby={item.label}
+												data={item.children}
+												defaultItemProps={{
+													pad: { vertical: 'xxsmall' },
+													margin: { left: 'small' },
+												}}
+											>
+												{(child) => (
+													<NavItem
+														label={child.label}
+														href={child.url}
+														icon={child.icon}
+														active={location.pathname === child.url}
+													/>
+												)}
+											</List>
+										</Collapsible>
+									</NavItem>
+								);
+							} else {
+								result = (
+									<NavItem
+										label={item.label}
+										href={item.url}
+										icon={item.icon}
+										active={location.pathname.includes(item.url || '')}
+									/>
+								);
+							}
+							return result;
+						}}
+					</List>
+				</Nav>
+			)}
+		</NavContainer>
+	);
 };
+
 
--- aries-core/src/stories/navigation.stories.tsx
+++ aries-core/src/stories/navigation.stories.tsx
@@ -1,13 +1,77 @@
-import { NavigationMenu } from '../js/components';
+import { Grid, Main, Page, PageContent, PageHeader } from 'grommet';
+import { AppsRounded as Components } from 'grommet-icons';
+import { NavigationMenu, type NavItemType } from '../js/components';
+
+const gridAreas = [
+  ['nav', 'header', 'aside'],
+  ['nav', 'main', 'aside'],
+];
+
+const navItems: NavItemType[] = [
+	{
+		label: 'Components',
+		url: '/components',
+		icon: <Components aria-hidden="true" />,
+	},
+	{
+		label: 'Adoption',
+		children: [
+			{ label: 'Teams', url: '/adoption' },
+			{ label: 'Adoption Levels', url: '/adoption-levels' },
+		],
+	},
+  {
+    label: 'Products',
+    children: [
+      { label: 'Storage', children: [
+        { label: 'Block Storage', url: '/block-storage' },
+        { label: 'Object Storage', url: '/object-storage' },
+        { label: 'File Storage', url: '/file-storage' },
+      ] },
+      { label: 'Compute', children: [
+        { label: 'Virtual Machines', url: '/virtual-machines' },
+        { label: 'Containers', url: '/containers' },
+      ] },
+      { label: 'Networking', children: [
+        { label: 'VPC', url: '/vpc' },
+        { label: 'Load Balancers', url: '/load-balancers' },
+      ] },
+      { label: 'Security', children: [
+        { label: 'IAM', url: '/iam' },
+        { label: 'KMS', url: '/kms' },
+      ] },
+      { label: 'AI', children: [
+        { label: 'Machine Learning', url: '/machine-learning' },
+        { label: 'Natural Language Processing', url: '/natural-language-processing' },
+      ] },
+    ]
+  }
+];
 
 export const NavigationMenuExample = () => {
-  const items = [
-    { id: 'home', label: 'Home' },
-    { id: 'about', label: 'About' },
-    { id: 'contact', label: 'Contact' },
-  ];
-
-  return <NavigationMenu items={items} />;
+  return (
+    <Grid
+      areas={gridAreas}
+      columns={['auto', 'flex', 'auto']}
+      rows={['auto', 'flex']}
+      border
+      // height={{ min: '100vh' }}
+      height="100%"
+    >
+      <NavigationMenu
+        gridArea="nav"
+        items={navItems}
+        background="background-front"
+      />
+      <Main gridArea="main">
+        <Page background="background-back" fill>
+          <PageContent>
+            <PageHeader title="Navigation" />
+          </PageContent>
+        </Page>
+      </Main>
+    </Grid>
+  );
 };
 
 export default {
