6dea2a409668699b344193b383ceb6311a71aea0
Matthew Glissmann <mdglissmann@gmail.com>
2025-08-21 13:24:55 -0600

    added use session storage hook

---
+++ shared/hooks/src/__tests__/setup.ts
@@ -0,0 +1,18 @@
+const mockSessionStorage = (() => {
+    let store = {};
+
+    return {
+        getItem: (key) => store[key] || null,
+        setItem: (key, value) => {
+            store[key] = value.toString();
+        },
+        removeItem: (key) => {
+            delete store[key];
+        },
+        clear: () => {
+            store = {};
+        }
+    };
+})();
+
+global.sessionStorage = mockSessionStorage;
---
+++ shared/hooks/src/useSessionStorage.ts
@@ -0,0 +1,53 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+export const useSessionStorage = <T>(key: string, initialValue: T) => {
+  // Check if we are in a browser environment vs. server-side rendering where window is not defined
+  const isBrowser = typeof window !== 'undefined';
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    if (!isBrowser) {
+      return initialValue;
+    }
+    try {
+      const item = sessionStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: T | ((val: T) => T)) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      sessionStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    const onStorageChange = (event: StorageEvent) => {
+      if (event.key === key) {
+        if (
+          event.newValue === undefined ||
+          event.newValue === null ||
+          event.newValue === ''
+        ) {
+          setStoredValue(initialValue);
+        } else {
+          setStoredValue(JSON.parse(event.newValue || 'null'));
+        }
+      }
+    };
+    window.addEventListener('storage', onStorageChange);
+    return () => {
+      window.removeEventListener('storage', onStorageChange);
+    };
+  }, []);
+
+  return [storedValue, setValue] as const;
+};
