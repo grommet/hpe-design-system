89727f7306adaf31c84ed64a16101bf7a296c528
Matthew Glissmann <mdglissmann@gmail.com>
2025-08-21 16:51:25 -0600

    clean up tests

--- shared/hooks/src/__tests__/setup.ts
+++ shared/hooks/src/__tests__/setup.ts
@@ -1,18 +1,25 @@
 const mockSessionStorage = (() => {
-    let store = {};
+  let store: Record<string, string> = {};
 
-    return {
-        getItem: (key) => store[key] || null,
-        setItem: (key, value) => {
-            store[key] = value.toString();
-        },
-        removeItem: (key) => {
-            delete store[key];
-        },
-        clear: () => {
-            store = {};
-        }
-    };
+  return {
+    getItem: (key: string) => store[key] || null,
+    setItem: (key: string, value: string) => {
+      store[key] = value.toString();
+    },
+    removeItem: (key: string) => {
+      delete store[key];
+    },
+    clear: () => {
+      store = {};
+    },
+    get length() {
+      return Object.keys(store).length;
+    },
+    key: (index: number): string | null => {
+      const keys = Object.keys(store);
+      return keys[index] || null;
+    },
+  } as Storage;
 })();
 
 global.sessionStorage = mockSessionStorage;
