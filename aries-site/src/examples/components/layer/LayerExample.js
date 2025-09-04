2d84cc4b316f21b0e128bab08c5bd3fc875fbebf
Jessica Rosenquist <54560994+jcfilben@users.noreply.github.com>
2025-09-04 10:41:13 -0600

    Add announcements to Layer example

--- aries-site/src/examples/components/layer/LayerExample.js
+++ aries-site/src/examples/components/layer/LayerExample.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Button, Box, Layer } from 'grommet';
+import React, { useContext } from 'react';
+import { AnnounceContext, Button, Box, Layer } from 'grommet';
 import { LayerHeader } from 'aries-core';
 import { LayerForm } from './ConfigurationForm';
 import {
@@ -9,33 +9,50 @@ import {
   useConfirmation,
 } from './components';
 
-export const LayerExample = () => (
-  <ConfirmationProvider>
-    <ConfirmationContext.Consumer>
-      {({ showLayer, showConfirmation, setShowLayer }) => (
-        <>
-          <Box align="center">
-            <Button
-              label="Show me the layer"
-              onClick={() => setShowLayer(true)}
-              primary
-            />
-          </Box>
-          {showLayer ? <AddApplication /> : null}
-          {showConfirmation ? <DoubleConfirmation title="application" /> : null}
-        </>
-      )}
-    </ConfirmationContext.Consumer>
-  </ConfirmationProvider>
-);
-
-const AddApplication = ({ ...rest }) => {
-  const { onClose } = useConfirmation();
+export const LayerExample = () => {
+  const announce = useContext(AnnounceContext);
 
   return (
-    <Layer position="right" full="vertical" onEsc={onClose} {...rest}>
+    <ConfirmationProvider>
+      <ConfirmationContext.Consumer>
+        {({ showLayer, showConfirmation, setShowLayer }) => {
+          return (
+            <>
+              <Box align="center">
+                <Button
+                  label="Show me the layer"
+                  onClick={() => {
+                    announce('Add application modal opened.', 'polite');
+                    setShowLayer(true);
+                  }}
+                  primary
+                />
+              </Box>
+              {showLayer ? <AddApplication /> : null}
+              {showConfirmation ? (
+                <DoubleConfirmation title="application" />
+              ) : null}
+            </>
+          );
+        }}
+      </ConfirmationContext.Consumer>
+    </ConfirmationProvider>
+  );
+};
+
+const AddApplication = ({ ...rest }) => {
+  const announce = useContext(AnnounceContext);
+  const { onClose } = useConfirmation();
+
+  const closeLayer = () => {
+    announce('Add application modal closed.', 'polite');
+    onClose();
+  };
+
+  return (
+    <Layer position="right" full="vertical" onEsc={closeLayer} {...rest}>
       <Box pad="medium" gap="medium" overflow="auto">
-        <LayerHeader title="Add application" onClose={onClose} />
+        <LayerHeader title="Add application" onClose={closeLayer} />
         <Box flex={false}>
           <LayerForm />
         </Box>
@@ -46,7 +63,7 @@ const AddApplication = ({ ...rest }) => {
             type="submit"
             form="application-form"
           />
-          <Button label="Cancel" onClick={onClose} />
+          <Button label="Cancel" onClick={closeLayer} />
         </Box>
       </Box>
     </Layer>
