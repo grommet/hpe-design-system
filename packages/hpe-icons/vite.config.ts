2f5e449443fb0852ebb9a2067488a6db68554d28
Matthew Glissmann <mdglissmann@gmail.com>
2025-09-08 12:58:03 -0600

    project setup

--- packages/hpe-icons/vite.config.ts
+++ packages/hpe-icons/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: ['src/index.ts'],
+      name: 'hpe-icons',
+      fileName: 'hpe-icons',
+      formats: ['es', 'cjs'],
+    },
+    outDir: 'dist',
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
