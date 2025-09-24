47235750e2f278ce7ebda72a28f5938cf0c13980
Matthew Glissmann <mdglissmann@gmail.com>
2025-09-24 10:38:33 -0600

    create icons/grommet directory

---
+++ packages/icons/core/src/main.ts
@@ -0,0 +1,9 @@
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>HPE Icons Core</h1>
+    <p>SVG icons for use with the HPE Design System</p>
+    <div>
+      <button type="button">Click me</button>
+    </div>
+  </div>
+`;
