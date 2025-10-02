49ffc260ec04dd6fad6f7a1b5716d68f91405e22
Matthew Glissmann <mdglissmann@gmail.com>
2025-10-02 15:28:52 -0600

    Added story for testing theme and props

--- aries-core/src/stories/icons/icons.stories.tsx
+++ aries-core/src/stories/icons/icons.stories.tsx
@@ -2,20 +2,24 @@ import { Box } from 'grommet';
 import { Add, Alert, Apps, Archive } from 'grommet-icons';
 import { Accessibility } from '@hpe-design/icons-grommet';
 
-export const IconsGrommet = () => (
-  <Box pad="large" gap="large">
-    <Box direction="row" gap="medium" align="center">
-      <Add color="brand" />
-      <Alert color="icon-warning" />
-      <Apps color="icon-strong" />
+export const IconsGrommet = () => {
+  return (
+    <Box pad="large" gap="large">
+      <Box direction="row" gap="medium" align="center">
+        <Add color="brand" />
+        <Alert color="icon-warning" />
+        <Apps color="icon-strong" />
       <Archive />
     </Box>
     <Box direction="row" gap="medium" align="center">
-      <Accessibility color="brand" size="small" />
+      <Accessibility color="brand" size="small" height="medium"/>
       <Accessibility size="xlarge" a11yTitle="Accessibility Icon" color="icon-warning" />
+      <Accessibility size="xlarge" a11yTitle="Accessibility Icon" color="icon-strong" />
+      <Accessibility size="xlarge" a11yTitle="Accessibility Icon" color="icon-default" />
     </Box>
   </Box>
 );
+};
 
 export default {
   title: 'HPE Icons',
