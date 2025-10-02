7a27e2da18e3a02ed57db160c3963b43464f89fc
Matthew Glissmann <mdglissmann@gmail.com>
2025-10-02 17:14:04 -0600

    js to jsx

---
+++ packages/icons/grommet-icons-test/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: {
+        'hpe-icons': 'src/js/index.js',
+      },
+      formats: ['es', 'cjs'],
+    },
+    outDir: 'dist',
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
+});
