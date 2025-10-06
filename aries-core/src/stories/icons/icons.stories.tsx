5643dcff31e5f3c171f69092c6282ad3c1382356
Matthew Glissmann <mdglissmann@gmail.com>
2025-10-06 16:49:21 -0600

    added icons storybook story

--- aries-core/src/stories/icons/icons.stories.tsx
+++ aries-core/src/stories/icons/icons.stories.tsx
@@ -1,31 +1,53 @@
 import { Box } from 'grommet';
-import { Add, Alert, Apps, Archive } from 'grommet-icons';
-import { Accessibility } from '@hpe-design/icons-grommet';
-import { AIGen } from '@hpe-design/grommet-icons-test';
+import { Add, Alert, Apps, Archive, StatusUnknown } from 'grommet-icons';
+import {
+  AIGenFill,
+  StatusCritical,
+  StatusGood,
+  StatusInfo,
+  StatusWarning,
+} from '@hpe-design/icons-grommet';
 
 export const IconsGrommet = () => {
   return (
     <Box pad="large" gap="large">
       <Box direction="row" gap="medium" align="center">
-        <Add color="brand" />
-        <Alert color="icon-warning" />
-        <Apps color="icon-strong" />
-      <Archive />
+        <AIGenFill size="xsmall" />
+        <AIGenFill size="small" />
+        <AIGenFill size="medium" />
+        <AIGenFill size="large" />
+        <AIGenFill size="xlarge" />
+        <AIGenFill size="xxlarge" />
+      </Box>
+      <Box direction="row" gap="medium" align="center">
+        <StatusGood
+          size="xxlarge"
+          a11yTitle="AI Generation Icon"
+          color="icon-ok"
+        />
+        <StatusWarning
+          size="xxlarge"
+          a11yTitle="AI Generation Icon"
+          color="icon-warning"
+        />
+        <StatusCritical
+          size="xxlarge"
+          a11yTitle="AI Generation Icon"
+          color="icon-critical"
+        />
+        <StatusInfo
+          size="xxlarge"
+          a11yTitle="AI Generation Icon"
+          color="icon-info"
+        />
+        <StatusUnknown
+          size="xxlarge"
+          a11yTitle="AI Generation Icon"
+          color="icon-unknown"
+        />
+      </Box>
     </Box>
-    <Box direction="row" gap="medium" align="center">
-      <Accessibility color="brand" size="small" height="medium"/>
-      <Accessibility size="xlarge" a11yTitle="Accessibility Icon" color="icon-warning" />
-      <Accessibility size="xlarge" a11yTitle="Accessibility Icon" color="icon-strong" />
-      <Accessibility size="xlarge" a11yTitle="Accessibility Icon" color="icon-default" />
-    </Box>
-    <Box direction="row" gap="medium" align="center">
-      <AIGen color="brand" size="small" height="medium"/>
-      <AIGen size="xlarge" a11yTitle="AI Generation Icon" color="icon-warning" />
-      <AIGen size="xlarge" a11yTitle="AI Generation Icon" color="icon-strong" />
-      <AIGen size="xlarge" a11yTitle="AI Generation Icon" color="icon-default" />
-    </Box>
-  </Box>
-);
+  );
 };
 
 export default {
