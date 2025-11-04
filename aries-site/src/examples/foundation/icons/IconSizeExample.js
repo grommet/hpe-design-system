d7b0eeb4f7d47fe5ab182f096fb99a41c140438b
SeamusLeonardHPE <135040188+SeamusLeonardHPE@users.noreply.github.com>
2025-11-04 13:31:36 +0000

    Increase contract in icon size examples
    Co-authored-by: Matthew Glissmann <mdglissmann@gmail.com>

--- aries-site/src/examples/foundation/icons/IconSizeExample.js
+++ aries-site/src/examples/foundation/icons/IconSizeExample.js
@@ -4,11 +4,11 @@ import { Alarm } from '@hpe-design/icons-grommet';
 
 export const IconSizeExample = () => (
   <Box direction="row-responsive" gap="medium">
-    <Alarm size="xxlarge" />
-    <Alarm size="xlarge" />
-    <Alarm size="large" />
-    <Alarm size="medium" />
-    <Alarm size="small" />
-    <Alarm size="xsmall" />
+    <Alarm size="xxlarge" color="icon-strong" />
+    <Alarm size="xlarge" color="icon-strong" />
+    <Alarm size="large" color="icon-strong" />
+    <Alarm size="medium" color="icon-strong" />
+    <Alarm size="small" color="icon-strong" />
+    <Alarm size="xsmall" color="icon-strong" />
   </Box>
 );
