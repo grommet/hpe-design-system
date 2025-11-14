7d19a4c0f4c09e20496da91f82b68dac1857671b
Lucas Speyer <lucas.speyer@gmail.com>
2025-11-14 15:48:57 -0300

    removing unused code

--- aries-site/src/components/home/index.js
+++ aries-site/src/components/home/index.js
@@ -2,7 +2,6 @@ export * from './Community';
 export * from './CreativeToolkit';
 export * from './Featured';
 export * from './Hero';
-export * from './Highlights';
 export * from './Quote';
 export * from './Video';
 export * from './WhatIs';
