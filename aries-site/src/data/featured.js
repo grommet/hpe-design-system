39547eca818108c78f771122869b669487eb0ce0
Lucas Speyer <lucas.speyer@gmail.com>
2025-11-18 07:27:50 -0300

    Update aries-site/src/data/featured.js
    Co-authored-by: Matthew Glissmann <mdglissmann@gmail.com>

--- aries-site/src/data/featured.js
+++ aries-site/src/data/featured.js
@@ -3,7 +3,7 @@ import { Theme, Code } from '@hpe-design/icons-grommet';
 export const featured = [
   {
     name: 'Design',
-    description: 'Access our Figma UI kits and brand principles.',
+    description: 'Access Figma UI kits and brand principles.',
     icon: <Theme size="xlarge" color="text-strong" aria-hidden="true" />,
   },
   {
