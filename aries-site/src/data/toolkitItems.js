ab9e84dd7c62a4a8ab118c5d28a85dc89117f1b4
Lucas Speyer <lucas.speyer@gmail.com>
2025-11-19 18:09:34 -0300

    Landing page updates (#5522)
    * Better paint
    
    added hero background image
    
    * fixing hero header background
    
    * changing sun/moon icons for new Mode icon
    
    * System Assets, light images
    
    * Community section
    
    * toolkit
    
    * fixing comments
    
    * trigger fresh netlify build
    
    * refactoring for responsiveness
    
    * lint fix
    
    * small adjustment
    
    * small adjustments
    
    * minor adjustment
    
    * Update aries-site/src/components/content/ThemeModeToggle.js
    
    Co-authored-by: Matthew Glissmann <mdglissmann@gmail.com>
    
    * address changes to code per discussion with lucus
    
    * Changes after review
    
    * Removing unused code
    
    * removing unused code
    
    * new images, spacing fixes, font fixes
    
    * Update aries-site/src/components/home/CreativeToolkit.js
    
    Co-authored-by: Matthew Glissmann <mdglissmann@gmail.com>
    
    * Hero refactor, text, dark bg imag updated (a11y)
    
    * section header level and sentence case text
    
    * small heading size fix
    
    * Update aries-site/src/data/featured.js
    
    Co-authored-by: Matthew Glissmann <mdglissmann@gmail.com>
    
    * Update aries-site/src/data/featured.js
    
    Co-authored-by: Matthew Glissmann <mdglissmann@gmail.com>
    
    * Fixes for Brittany's comments
    
    * Update [yarn.lock](http://_vscodecontentref_/5) from master
    
    * changed xsmall to small due to missing token
    
    ---------
    
    Co-authored-by: Matthew Glissmann <mdglissmann@gmail.com>
    Co-authored-by: britt6612 <brittany.archibeque@hpe.com>
    Co-authored-by: Brittany <42451602+britt6612@users.noreply.github.com>

---
+++ aries-site/src/data/toolkitItems.js
@@ -0,0 +1,26 @@
+export const toolkitItems = [
+  {
+    name: 'Foundation',
+    href: '/foundation',
+    image: {
+      src:  '/creativetoolkitimages/foundation.svg',
+      alt: 'Foundation',
+    },
+},
+{
+  name: 'Components',
+  href: '/components',
+  image: {
+    src: '/creativetoolkitimages/components.svg',
+    alt: 'Components',
+  },
+},
+{
+  name: 'Templates',
+  href: '/templates',
+  image: {
+    src: '/creativetoolkitimages/templates.svg',
+    alt: 'Templates',
+  },
+},
+];
