aefbf6e62c03f97f0ef801e68152e92f8f88649d
Matthew Glissmann <mdglissmann@gmail.com>
2025-11-19 15:09:41 -0700

    completed spacing scale figure

--- aries-site/src/examples/foundation/spacing/SpacingScale.js
+++ aries-site/src/examples/foundation/spacing/SpacingScale.js
@@ -1,47 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Box, Text } from 'grommet';
 import { useDesignTokens } from '../../../components/content/designTokenUtils';
 
+const Space = ({ token }) => {
+  return (
+    <Box key={token.id} align="center" gap="3xsmall" flex={false}>
+      <Box
+        width={token.value}
+        pad={{ vertical: '3xsmall' }}
+        border={{
+          color: 'decorative-purple',
+          side: 'vertical',
+          size: 'small',
+        }}
+        // don't allow box and borders to shrink on small screens
+        responsive={false}
+      >
+        <Box
+          border={{
+            color: 'decorative-purple',
+            side: 'bottom',
+            style: 'dotted',
+            size: 'small',
+          }}
+          // don't allow box and borders to shrink on small screens
+          responsive={false}
+        />
+      </Box>
+      <Text size="small">{token.token.split('hpe.spacing.')[1]}</Text>
+    </Box>
+  );
+};
+
+Space.propTypes = {
+  token: PropTypes.object.isRequired,
+};
+
 export const SpacingScale = () => {
   const { data: spacingTokens } = useDesignTokens('semantic.spacing');
-  console.log(spacingTokens);
 
   const spacingTokenList = Object.keys(spacingTokens).map(tokenKey => {
     const token = spacingTokens[tokenKey];
 
     return (
-      <Box
-        key={token.id}
-        // direction="row"
-        align="center"
-        gap="3xsmall"
-        margin={{ bottom: 'xsmall' }}
-      >
-        <Box
-          width={token.value}
-          pad={{ vertical: 'xxsmall' }}
-          border={{
-            color: 'decorative-purple',
-            side: 'vertical',
-            size: 'small',
-          }}
-        >
-          <Box
-            border={{
-              color: 'decorative-purple',
-              side: 'bottom',
-              style: 'dashed',
-              size: 'small',
-            }}
-          />
-        </Box>
-        <Text size="small">{token.token.split('hpe.spacing.')[1]}</Text>
-      </Box>
+      <Space key={token.id} token={token} />
     );
   });
 
   return (
-    <Box direction="row" gap="small">
+    <Box direction="row" gap={{ column: 'small', row: 'medium' }} wrap>
       {spacingTokenList}
     </Box>
   );
