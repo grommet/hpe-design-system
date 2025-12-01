0b78172c05f855d4cd8ad6a75575a866bc9dfe50
britt6612 <brittany.archibeque@hpe.com>
2025-12-01 11:06:02 -0700

    revert un needed changes

---
+++ aries-core/src/stories/popover.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+/* eslint-disable max-len */
+import { PopoverInlineExample } from 'aries-site/src/examples/templates/popover/PopoverInlineExample';
+import { PopoverSimpleExample } from 'aries-site/src/examples/templates/popover/PopoverSimpleExample';
+
+export const PopoverInline = () => <PopoverInlineExample />;
+export const PopoverSimple = () => <PopoverSimpleExample />;
+
+export default {
+  title: 'Popover',
+};
