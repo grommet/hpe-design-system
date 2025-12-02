d0c61936d12ed9a5317d002289b270d36177cdca
Brittany <42451602+britt6612@users.noreply.github.com>
2025-12-02 09:40:33 -0700

    Update Genie.tsx

--- aries-core/src/stories/navigation/content/Genie.tsx
+++ aries-core/src/stories/navigation/content/Genie.tsx
@@ -1,6 +1,6 @@
 import { Box, Heading } from 'grommet';
 
-export const Genie = ({ ...rest }) => {
+export const Genie = ({...rest}) => {
   return (
     <Box pad="medium" {...rest}>
       <Heading level={3}>My genie content</Heading>
