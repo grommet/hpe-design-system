a534cedfae0d53d2c1d23b244d2e123af76876e3
britt6612 <brittany.archibeque@hpe.com>
2025-12-04 09:24:35 -0700

    add comment

--- aries-site/src/themes/aries.js
+++ aries-site/src/themes/aries.js
@@ -1,5 +1,6 @@
 import { hpe, hpePop } from 'grommet-theme-hpe';
 import { deepMerge } from 'grommet/utils';
+// changing this to @hpe icons is failing TODO
 import { Info } from 'grommet-icons';
 
 export const aries = deepMerge(hpe, {
