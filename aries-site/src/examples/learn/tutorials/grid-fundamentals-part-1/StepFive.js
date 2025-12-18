cbc8dc26134b4a0c9166c46d0bc8214a9972b7a0
sulaymon333 <tjsulaymon@gmail.com>
2025-12-18 12:15:10 +0000

    remove background=transparent on Grommet for stepFive.js

--- aries-site/src/examples/learn/tutorials/grid-fundamentals-part-1/StepFive.js
+++ aries-site/src/examples/learn/tutorials/grid-fundamentals-part-1/StepFive.js
@@ -27,9 +27,13 @@ const ProductsPage = () => {
 
 export const App = () => {
   return (
-    <Grommet theme={hpe} background={{ color: 'transparent' }}>
-      <Box pad="small" round="small" background={{ color: 'background-back' }} 
-      border={{ color: 'border-weak' }}>
+    <Grommet theme={hpe}>
+      <Box
+        pad="small"
+        round="small"
+        background={{ color: 'background-back' }}
+        border={{ color: 'border-weak' }}
+      >
         <ProductsPage />
       </Box>
     </Grommet>
