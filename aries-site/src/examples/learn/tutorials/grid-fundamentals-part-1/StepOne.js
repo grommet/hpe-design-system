661b31c2e9e9db20c0b15956bc62ae8073198abd
SeamusLeonardHPE <seamus.leonard@hpe.com>
2025-12-19 13:45:20 +0000

    Replace background value with correct semantic one.

--- aries-site/src/examples/learn/tutorials/grid-fundamentals-part-1/StepOne.js
+++ aries-site/src/examples/learn/tutorials/grid-fundamentals-part-1/StepOne.js
@@ -12,7 +12,7 @@ const ProductsPage = () => {
       <Grid columns={{ count: 3, size: 'auto' }}>
 
         <Box
-          background={{ color: 'background-info' }}
+          background={{color: "decorative-blue", opacity: 'weak'}}
           border
           pad="medium"
           align="center"
@@ -20,7 +20,7 @@ const ProductsPage = () => {
           <Text size="large">Product 1</Text>
         </Box>
         <Box
-          background={{ color: 'background-info' }}
+          background={{color: "decorative-blue", opacity: 'weak'}}
           border
           pad="medium"
           align="center"
@@ -28,7 +28,7 @@ const ProductsPage = () => {
           <Text size="large">Product 2</Text>
         </Box>
         <Box
-          background={{ color: 'background-info' }}
+          background={{color: "decorative-blue", opacity: 'weak'}}
           border
           pad="medium"
           align="center"
@@ -36,7 +36,7 @@ const ProductsPage = () => {
           <Text size="large">Product 3</Text>
         </Box>
         <Box
-          background={{ color: 'background-info' }}
+          background={{color: "decorative-blue", opacity: 'weak'}}
           border
           pad="medium"
           align="center"
@@ -44,7 +44,7 @@ const ProductsPage = () => {
           <Text size="large">Product 4</Text>
         </Box>
         <Box
-          background={{ color: 'background-info' }}
+          background={{color: "decorative-blue", opacity: 'weak'}}
           border
           pad="medium"
           align="center"
@@ -52,7 +52,7 @@ const ProductsPage = () => {
           <Text size="large">Product 5</Text>
         </Box>
         <Box
-          background={{ color: 'background-info' }}
+          background={{color: "decorative-blue", opacity: 'weak'}}
           border
           pad="medium"
           align="center"
