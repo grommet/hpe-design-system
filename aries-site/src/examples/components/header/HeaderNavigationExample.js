fb5c7df4acbd2f00cb2beaecd1a553ed1213e433
sulaymon333 <tjsulaymon@gmail.com>
2026-01-09 20:42:35 +0000

    fix AppIdentity import in header comp.

--- aries-site/src/examples/components/header/HeaderNavigationExample.js
+++ aries-site/src/examples/components/header/HeaderNavigationExample.js
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
 import { Button, Header, Menu, Nav, ResponsiveContext } from 'grommet';
-// eslint-disable-next-line max-len
-import { AppIdentity } from '@shared/aries-core/src/js/components/helpers/AppIdentity';
+import { AppIdentity } from '@shared/aries-core';
 
 const items = [
   { label: 'Label 1' },
