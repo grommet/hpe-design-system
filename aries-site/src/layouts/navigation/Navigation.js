4fddf9931a9b85c2dd54f36475c7811f0e1a27fc
Matthew Glissmann <mdglissmann@gmail.com>
2026-01-20 17:45:12 -0700

    Added onSelect routing

--- aries-site/src/layouts/navigation/Navigation.js
+++ aries-site/src/layouts/navigation/Navigation.js
@@ -1,14 +1,26 @@
+import { useState } from 'react';
+import { useAnalytics } from 'grommet';
+import { useRouter } from 'next/router';
 import { NavigationMenu } from '@shared/aries-core';
 import { navItems } from './navItems.ts';
 
 export const Navigation = ({ ...rest }) => {
+  const [activeItem, setActiveItem] = useState('Home');
+  const router = useRouter();
+  const sendAnalytics = useAnalytics();
+
+  const handleSelect = ({ item, event }) => {
+    event.preventDefault();
+    router.push(item.url);
+    sendAnalytics({ type: 'navItemClick', href: item.url });
+    setActiveItem(item.label);
+  };
+
   return (
     <NavigationMenu
+      activeItem={activeItem}
       items={navItems}
-      onSelect={({ item, event }) => {
-        event.preventDefault();
-        console.log(`Navigate to ${item.url}`);
-      }}
+      onSelect={handleSelect}
       {...rest}
     />
   );
