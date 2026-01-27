123759a980ccc79bac5f3d4a4dbbad55153a0a38
britt6612 <brittany.archibeque@hpe.com>
2026-01-27 12:14:03 -0700

    make components in cards non focusable

--- aries-site/src/examples/cardPreviews/togglegroup.js
+++ aries-site/src/examples/cardPreviews/togglegroup.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Box, ToggleGroup } from 'grommet';
 import { Table, List, Map } from '@hpe-design/icons-grommet';
 
-export const ToggleGroupPreview = () => (
-  <Box align="center" justify="center">
-    <ToggleGroup
-      tabIndex={-1}
-      options={[
-        {
-          icon: <Table />,
-          value: 'table',
-        },
-        {
-          icon: <List />,
-          value: 'list',
-        },
-        {
-          icon: <Map />,
-          value: 'map',
-        },
-      ]}
-    />
-  </Box>
-);
+export const ToggleGroupPreview = () => {
+  const ref = useRef();
+
+  useEffect(() => {
+    if (ref.current) {
+      const buttons = ref.current.querySelectorAll('button');
+      buttons.forEach(button => {
+        button.setAttribute('tabindex', '-1');
+      });
+    }
+  }, []);
+
+  return (
+    <Box align="center" justify="center" ref={ref}>
+      <ToggleGroup
+        options={[
+          {
+            icon: <Table />,
+            value: 'table',
+          },
+          {
+            icon: <List />,
+            value: 'list',
+          },
+          {
+            icon: <Map />,
+            value: 'map',
+          },
+        ]}
+      />
+    </Box>
+  );
+};
