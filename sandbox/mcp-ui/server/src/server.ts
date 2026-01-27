4b8b80f60de7fef170fb05b1b9bf2aec98f11c4f
Matthew Glissmann <mdglissmann@gmail.com>
2026-01-27 14:26:39 -0700

    Added basic server and project set up

---
+++ sandbox/mcp-ui/server/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
