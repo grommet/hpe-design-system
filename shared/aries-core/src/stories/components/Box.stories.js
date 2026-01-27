fe62fa698c8775389de5bcf9555d9304853b1c3b
Jessica Rosenquist <54560994+jcfilben@users.noreply.github.com>
2026-01-27 09:52:44 -0700

    move more items to common args

--- shared/aries-core/src/stories/components/Box.stories.js
+++ shared/aries-core/src/stories/components/Box.stories.js
@@ -1,74 +1,23 @@
 import React from 'react';
 import { Box, Text } from 'grommet';
-import {
-  backgroundColors,
-  tShirtSizes,
-  spacingSizes,
-  containerSizes,
-  radiusSizes,
-} from '../utils/commonArgs';
+import { boxArgs } from '../utils/commonArgs';
 
 const meta = {
   title: 'Components/Box',
   component: Box,
-  argTypes: {
-    background: {
-      control: { type: 'select' },
-      options: backgroundColors,
-    },
-    border: {
-      control: { type: 'boolean' },
-    },
-    direction: {
-      control: { type: 'select' },
-      options: ['row', 'column', 'row-responsive'],
-    },
-    elevation: {
-      control: { type: 'select' },
-      options: tShirtSizes,
-    },
-    fill: {
-      control: { type: 'select' },
-      options: [true, false, 'horizontal', 'vertical'],
-    },
-    gap: {
-      control: { type: 'select' },
-      options: spacingSizes,
-    },
-    height: {
-      control: { type: 'select' },
-      options: containerSizes,
-    },
-    pad: {
-      control: { type: 'select' },
-      options: spacingSizes,
-    },
-    round: {
-      control: { type: 'select' },
-      options: radiusSizes,
-    },
-    skeleton: {
-      control: { type: 'boolean' },
-    },
-    width: {
-      control: { type: 'select' },
-      options: containerSizes,
-    },
-  },
+  argTypes: boxArgs,
 };
 
 export default meta;
 
 export const Default = {
-  render: args => {
-    return (
-      <Box {...args}>
-        <Text>First item</Text>
-        <Text>Second item</Text>
-        <Text>Third item</Text>
-      </Box>
-    );
-  },
+  render: args => (
+    <Box {...args}>
+      <Text>First item</Text>
+      <Text>Second item</Text>
+      <Text>Third item</Text>
+    </Box>
+  ),
   args: {
     direction: 'column',
     pad: 'medium',
