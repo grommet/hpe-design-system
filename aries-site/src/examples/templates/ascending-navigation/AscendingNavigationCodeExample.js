37de7d3f56c83da0f535bb160f7fda0f20dfff6d
SeamusLeonardHPE <seamus.leonard@hpe.com>
2026-02-05 11:29:51 +0000

    Fixed build error

--- aries-site/src/examples/templates/ascending-navigation/AscendingNavigationCodeExample.js
+++ aries-site/src/examples/templates/ascending-navigation/AscendingNavigationCodeExample.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
-import { PageHeader } from 'grommet';
+import React from 'react';
+import { Anchor, Button, PageHeader } from 'grommet';
 import { Left } from '@hpe-design/icons-grommet';
 
 export const AscendingNavigationCodeExample = () => {
