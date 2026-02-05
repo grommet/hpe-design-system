e7d599401a35118d9a7552a8e378e915e99f935b
sulaymon333 <tjsulaymon@gmail.com>
2026-02-05 10:54:01 +0000

    remove space from icons only menu Grid -> Box

--- aries-site/src/examples/components/menu/MenuIconExample.js
+++ aries-site/src/examples/components/menu/MenuIconExample.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Grid, Menu } from 'grommet';
+import { Box, Menu } from 'grommet';
 import { More, Settings } from '@hpe-design/icons-grommet';
 
 export const MenuIconExample = () => {
   const items = [{ label: 'Action' }, { label: 'Action' }, { label: 'Action' }];
 
   return (
-    <Grid columns="3xsmall" align="start" justify="start">
+    <Box direction="row" align="start" gap="small">
       <Menu icon={<More />} hoverIndicator items={items} />
       <Menu icon={<Settings />} hoverIndicator items={items} />
-    </Grid>
+    </Box>
   );
 };
