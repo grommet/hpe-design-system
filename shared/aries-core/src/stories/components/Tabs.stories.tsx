66623a3e90f5c9c7b9afd4bf9a7ad83367237116
Jessica Rosenquist <54560994+jcfilben@users.noreply.github.com>
2026-02-06 14:38:39 -0700

    Added tabs story

---
+++ shared/aries-core/src/stories/components/Tabs.stories.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import type { StoryObj } from '@storybook/react';
+import { Tabs, Tab, Text, Box, TabsExtendedProps } from 'grommet';
+import { alignArg, marginArg } from '../utils/commonArgs';
+
+const meta = {
+  title: 'Components/Tabs',
+  component: Tabs,
+  argTypes: {
+    activeIndex: {
+      control: { type: 'number', min: 0, max: 2 },
+    },
+    alignControls: alignArg,
+    margin: marginArg,
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const BasicTabs: Story = {
+  render: (args: TabsExtendedProps) => (
+    <Tabs {...args}>
+      <Tab title="General">
+        <Box pad={{ vertical: 'medium' }}>
+          <Text>
+            Tab content should be flush with the left edge of the Tab. A
+            "medium" vertical padding should be applied to create space between
+            the Tab and its content.
+          </Text>
+        </Box>
+      </Tab>
+      <Tab title="Account">
+        <Box pad={{ vertical: 'medium' }}>
+          <Text>Account Information</Text>
+        </Box>
+      </Tab>
+      <Tab title="Billing">
+        <Box pad={{ vertical: 'medium' }}>
+          <Text>Billing Information</Text>
+        </Box>
+      </Tab>
+    </Tabs>
+  ),
+  // ts-ignore is needed because TabsExtended props requires 'children' prop
+  // @ts-ignore
+  args: {
+    alignControls: 'start',
+  },
+  name: 'Tabs',
+};
