876894e76c679d30a135f02f618a84f47629ef80
Jessica Rosenquist <54560994+jcfilben@users.noreply.github.com>
2026-02-09 15:19:59 -0700

    typescript changes

--- shared/aries-core/src/stories/components/Tabs.stories.tsx
+++ shared/aries-core/src/stories/components/Tabs.stories.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import type { StoryObj } from '@storybook/react';
-import { Tabs, Tab, Text, Box, TabsExtendedProps } from 'grommet';
+import type { Meta, StoryObj } from '@storybook/react';
+import { Tabs, Tab, Text, Box } from 'grommet';
 import { alignArg, marginArg } from '../utils/commonArgs';
 
 const meta = {
   title: 'Components/Tabs',
   component: Tabs,
   argTypes: {
-    alignControls: alignArg,
     activeIndex: {
       control: { type: 'number', min: 0, max: 2 },
     },
+    alignControls: alignArg,
     margin: marginArg,
   },
-};
+} satisfies Meta<typeof Tabs>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const BasicTabs: Story = {
-  render: (args: TabsExtendedProps) => (
+  render: args => (
     <Tabs {...args}>
       <Tab title="General">
         <Box pad={{ vertical: 'medium' }}>
@@ -45,8 +45,8 @@ export const BasicTabs: Story = {
   // ts-ignore is needed because TabsExtended props requires 'children' prop
   // @ts-ignore
   args: {
-    alignControls: 'start',
     activeIndex: undefined,
+    alignControls: 'start',
     margin: undefined,
   },
   name: 'Tabs',
