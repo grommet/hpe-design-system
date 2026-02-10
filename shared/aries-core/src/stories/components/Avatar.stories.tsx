076c452006d5d0a89bf24416bf5d959bc2d6ab95
britt6612 <brittany.archibeque@hpe.com>
2026-02-09 17:05:13 -0700

    edit the ts

--- shared/aries-core/src/stories/components/Avatar.stories.tsx
+++ shared/aries-core/src/stories/components/Avatar.stories.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import type { StoryObj } from '@storybook/react';
-import { Avatar, AvatarProps } from 'grommet';
+import type { Meta, StoryObj } from '@storybook/react';
+import { Avatar } from 'grommet';
 import { a11yTitleArg, labelArg } from '../utils/commonArgs';
 
 const meta = {
@@ -14,18 +13,17 @@ const meta = {
     },
     src: labelArg,
   },
-};
+} satisfies Meta<typeof Avatar>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  render: (args: AvatarProps) => {
-    return <Avatar {...args} />;
-  },
+export const Default = {
+  name: 'Avatar',
+  render: args => <Avatar {...args} />,
   args: {
     a11yTitle: 'Image of Jessica',
+    size: 'medium',
     src: 'https://1.gravatar.com/avatar/5134a384e69b1f9e2ff2f792145b240d714b79c4a0d49ae02874eb5e96802fd8?size=256&d=initials',
   },
-  name: 'Avatar',
-};
+} satisfies Story;
