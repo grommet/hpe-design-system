92c2cc9a06311f6d309f25505d5c643af15f70e6
Brittany <42451602+britt6612@users.noreply.github.com>
2026-02-12 12:18:09 -0700

    Apply suggestion from @britt6612

--- apps/docs/src/examples/templates/ascending-navigation/components/SectionCard.js
+++ apps/docs/src/examples/templates/ascending-navigation/components/SectionCard.js
@@ -5,7 +5,7 @@ import { LinkNext } from '@hpe-design/icons-grommet';
 export const SectionCard = ({ section, onClick }) => (
   <Card default onClick={() => onClick(section.id)} pad="small">
     <Box direction="row" fill justify="between">
-      <Heading level="2" size="medium" margin="none">
+      <Heading level="2" margin="none">
         {section.label}
       </Heading>
       <LinkNext height="xlarge" />
