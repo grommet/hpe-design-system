eb5e68c99da9d15b73a7b33c179e82c5a1905b1a
britt6612 <brittany.archibeque@hpe.com>
2026-02-13 12:29:59 -0700

    add Datasort story

---
+++ shared/aries-core/src/stories/components/DataSort.stories.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { Data, DataSort, DataTable } from 'grommet';
+
+const meta = {
+  title: 'Components/DataSort',
+  component: DataSort,
+  argTypes: {
+    drop: {
+      control: { type: 'boolean' },
+      description: 'Whether to show the controls via a DropButton.',
+    },
+  },
+} satisfies Meta<typeof DataSort>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default = {
+  name: 'DataSort',
+  render: args => (
+    <Data
+      data={[
+        { name: 'Scott' },
+        { name: 'Matt' },
+        { name: 'Jessica' },
+        { name: 'Mike' },
+        { name: 'Julia' },
+        { name: 'Seamus' },
+      ]}
+    >
+      <DataSort {...args} />
+      <DataTable />
+    </Data>
+  ),
+  args: {
+    drop: true,
+  },
+} satisfies Story;
