ea8e57a95794ad21d10c304194621ceba3f0038d
britt6612 <brittany.archibeque@hpe.com>
2026-02-17 14:09:38 -0700

    remove messages from datasummary props

--- shared/aries-core/src/stories/components/DataSummary.stories.tsx
+++ shared/aries-core/src/stories/components/DataSummary.stories.tsx
@@ -5,13 +5,7 @@ import { Data, DataSummary } from 'grommet';
 const meta = {
   title: 'Components/DataSummary',
   component: DataSummary,
-  argTypes: {
-    messages: {
-      control: { type: 'object' },
-      description:
-        'Customize the messages shown. This can also be driven via Data "messages".',
-    },
-  },
+  argTypes: {},
 } satisfies Meta<typeof DataSummary>;
 
 export default meta;
@@ -20,19 +14,35 @@ type Story = StoryObj<typeof meta>;
 export const Default = {
   name: 'DataSummary',
   render: args => (
-    <Data data={[{ name: 'Scott' }, { name: 'Zelda' }]}>
+    <Data
+      data={[
+        {
+          id: 1,
+          name: 'Scott',
+          age: 28,
+          department: 'Engineering',
+          salary: 75000,
+        },
+        { id: 2, name: 'Zelda', age: 32, department: 'Design', salary: 80000 },
+        {
+          id: 3,
+          name: 'Alex',
+          age: 25,
+          department: 'Engineering',
+          salary: 70000,
+        },
+        {
+          id: 4,
+          name: 'Morgan',
+          age: 29,
+          department: 'Marketing',
+          salary: 65000,
+        },
+        { id: 5, name: 'Jordan', age: 35, department: 'Design', salary: 85000 },
+      ]}
+    >
       <DataSummary {...args} />
     </Data>
   ),
-  args: {
-    messages: {
-      filtered: '{filteredTotal} results of {total} {items}',
-      filteredSingle: '{filteredTotal} result of {total} {items}',
-      items: 'items',
-      itemsSingle: 'item',
-      selected: '{selected} selected',
-      total: '{total} {items}',
-      totalSingle: '{total} {items}',
-    },
-  },
+  args: {},
 } satisfies Story;
