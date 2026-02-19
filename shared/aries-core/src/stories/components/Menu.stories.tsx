593b15af2b28dc87928e9b5d1a7a60090c40187b
Sean Hoang Hill <seanhillweb@gmail.com>
2026-02-19 09:39:37 -0600

    [DS Storybook] Menu Story (#5757)
    * fix for import path lint error
    
    * removed old commonArgs
    
    * removed align, dropProps, justify, margin, and open argTypes
    
    * removed icon argType

---
+++ shared/aries-core/src/stories/components/Menu.stories.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { Menu } from 'grommet';
+import {
+  a11yTitleArg,
+  disabledArg,
+  labelArg,
+  tShirtSizes,
+} from '../utils/commonArgs';
+
+const meta = {
+  title: 'Components/Menu',
+  component: Menu,
+  argTypes: {
+    a11yTitle: a11yTitleArg,
+    disabled: disabledArg,
+
+    items: {
+      control: { type: 'object' },
+    },
+    label: labelArg,
+    size: {
+      control: { type: 'select' },
+      options: tShirtSizes,
+    },
+  },
+} satisfies Meta<typeof Menu>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default = {
+  name: 'Menu',
+  // Story specific args, sets default args for argTypes
+  args: {
+    a11yTitle: undefined,
+    disabled: false,
+    items: [
+      { label: 'Profile', onClick: () => {} },
+      { label: 'Settings', onClick: () => {} },
+      { label: 'FAQ', onClick: () => {} },
+    ],
+    label: 'Menu',
+    size: 'medium',
+  },
+  render: (args: any) => {
+    return <Menu {...args} width="large" />;
+  },
+} satisfies Story;
