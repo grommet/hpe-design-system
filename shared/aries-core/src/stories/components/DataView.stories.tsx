e32a0ba8e19f3cc3dee5dcc29803813060a32444
Brittany <42451602+britt6612@users.noreply.github.com>
2026-02-19 07:32:56 -0700

    DS Storybook - Add DataView story (#5816)
    * add dataview story
    
    * fix error

---
+++ shared/aries-core/src/stories/components/DataView.stories.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { Data, DataView, DataTable, Toolbar } from 'grommet';
+
+const meta = {
+  title: 'Components/DataView',
+  component: DataView,
+  argTypes: {
+    // DataView has no props to control
+  },
+} satisfies Meta<typeof DataView>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default = {
+  name: 'DataView',
+  render: args => (
+    <Data
+      data={[
+        { id: 1, name: 'Person 1', age: 12 },
+        { id: 2, name: 'Person 2', age: 91 },
+      ]}
+      views={[
+        { name: 'oldest', sort: { property: 'age', direction: 'desc' } },
+        { name: 'youngest', sort: { property: 'age', direction: 'asc' } },
+      ]}
+    >
+      <Toolbar>
+        <DataView {...args} />
+      </Toolbar>
+      <DataTable
+        columns={[
+          {
+            property: 'name',
+            header: 'Name',
+          },
+          {
+            property: 'age',
+            header: 'Age',
+          },
+        ]}
+      />
+    </Data>
+  ),
+  args: {},
+} satisfies Story;
