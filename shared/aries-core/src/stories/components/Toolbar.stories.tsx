0c916c5894f66e0ca74e480e28dc66a14e447bc5
britt6612 <brittany.archibeque@hpe.com>
2026-02-19 15:15:08 -0700

    address feedback

--- shared/aries-core/src/stories/components/Toolbar.stories.tsx
+++ shared/aries-core/src/stories/components/Toolbar.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { Data, DataSearch, DataSort, DataSummary, Toolbar } from 'grommet';
+import { Data, DataSearch, DataSort, DataTable, Toolbar } from 'grommet';
 
 // Mock data for the toolbar example
 const mockApplications = [
@@ -39,6 +39,7 @@ const meta = {
   parameters: {
     layout: 'fullscreen',
   },
+  argTypes: {},
 } satisfies Meta<typeof Toolbar>;
 
 export default meta;
@@ -47,11 +48,45 @@ type Story = StoryObj<typeof meta>;
 export const WithDataControls: Story = {
   name: 'Toolbar',
   render: args => {
+    const columns = [
+      {
+        property: 'title',
+        header: 'Title',
+        primary: true,
+      },
+      {
+        property: 'publisher',
+        header: 'Publisher',
+      },
+      {
+        property: 'categories',
+        header: 'Categories',
+        render: (datum: any) => datum.categories.join(', '),
+      },
+      {
+        property: 'rating',
+        header: 'Rating',
+        render: (datum: any) => datum.rating.toString(),
+      },
+      {
+        property: 'pricing',
+        header: 'Pricing',
+      },
+      {
+        property: 'delivery',
+        header: 'Delivery',
+      },
+    ];
+
     return (
-      <Toolbar {...args}>
-        <DataSearch />
-        <DataSort drop />
-      </Toolbar>
+      <Data data={mockApplications}>
+        <Toolbar {...args}>
+          <DataSearch />
+          <DataSort drop />
+        </Toolbar>
+        <DataTable columns={columns} />
+      </Data>
     );
   },
+  args: {},
 };
