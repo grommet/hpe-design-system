445af97cefa27c5bb1bc286426b5f0fb6331e331
sulaymon333 <tjsulaymon@gmail.com>
2026-02-23 17:01:50 +0000

    update layer anatomy

--- apps/docs/src/examples/components/layer/LayerAnatomy.js
+++ apps/docs/src/examples/components/layer/LayerAnatomy.js
@@ -1,192 +1,181 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import {
   Box,
+  Button,
   Card,
   CardBody,
   Diagram,
+  Footer,
   Grid,
   Stack,
-  ResponsiveContext,
 } from 'grommet';
 import { LayerHeader } from '@shared/aries-core';
 import { Annotation } from '../../../layouts';
-import { connection } from '../../../utils';
+
+const color = 'border';
+const thickness = 'hair';
+const type = 'direct';
 
 const connections = [
-  connection('1a', 'layer-title'),
-  connection('1b', 'layer-subtitle'),
-  connection('2', 'layer-body'),
-  connection('3', 'layer-footer'),
-  connection('pad', 'layer-container', 'vertical'),
-  connection('elevation', 'layer-container', 'vertical'),
-  connection('gap', 'layer-gap', 'horizontal', 'rectilinear'),
-  connection('gap', 'first-gap', 'horizontal', 'rectilinear'),
-  connection('round', 'bottom-pad'),
+  {
+    anchor: 'horizontal',
+    type,
+    color,
+    thickness,
+    fromTarget: '1',
+    toTarget: 'layer-header',
+  },
+  {
+    anchor: 'horizontal',
+    type,
+    color,
+    thickness,
+    fromTarget: '1a',
+    toTarget: 'layer-title',
+  },
+  {
+    anchor: 'horizontal',
+    type,
+    color,
+    thickness,
+    fromTarget: '1b',
+    toTarget: 'layer-subtitle',
+  },
+  {
+    anchor: 'horizontal',
+    type,
+    color,
+    thickness,
+    fromTarget: '1c',
+    toTarget: 'layer-close',
+  },
+  {
+    anchor: 'horizontal',
+    type,
+    color,
+    thickness,
+    fromTarget: '2',
+    toTarget: 'layer-body',
+  },
+  {
+    anchor: 'horizontal',
+    type,
+    color,
+    thickness,
+    fromTarget: '3',
+    toTarget: 'layer-footer',
+  },
+  {
+    anchor: 'vertical',
+    type,
+    color,
+    thickness,
+    fromTarget: '3a',
+    toTarget: 'layer-confirm',
+  },
+  {
+    anchor: 'vertical',
+    type,
+    color,
+    thickness,
+    fromTarget: '3b',
+    toTarget: 'layer-cancel',
+  },
 ];
 
+const AnatomyGrid = ({ ...rest }) => (
+  <Grid
+    columns={['5xsmall', 'medium', '3xsmall']}
+    rows={[
+      '5xsmall',
+      'flex',
+      'flex',
+      'flex',
+      'flex',
+      'flex',
+      'flex',
+      '5xsmall',
+    ]}
+    areas={[
+      ['annotation-1a', 'layer-area', 'annotation-1c'],
+      ['annotation-1b', 'layer-area', 'annotation-1'],
+      ['empty-0', 'layer-area', 'empty-1'],
+      ['empty-2', 'layer-area', 'annotation-2'],
+      ['empty-3', 'layer-area', 'empty-4'],
+      ['empty-5', 'layer-area', 'annotation-3'],
+      ['empty-6', 'layer-area', 'empty-7'],
+      ['empty-8', 'annotation-3b', 'annotation-3a'],
+    ]}
+    align="center"
+    justify="center"
+    columnGap="large"
+    {...rest}
+  />
+);
+
 export const LayerAnatomy = () => {
-  const breakpoint = useContext(ResponsiveContext);
-  const mobile = !['large', 'xlarge'].includes(breakpoint);
-  let columns = ['36px', 'medium', 'auto'];
-  let rows = [
-    '48px',
-    '24px',
-    '37px',
-    '25px',
-    '24px',
-    'xsmall',
-    '24px',
-    '74px',
-    '24px',
-    '48px',
+  const annotations = [
+    { id: '1', gridArea: 'annotation-1', target: '1' },
+    { id: '1a', gridArea: 'annotation-1a', target: '1a' },
+    { id: '1b', gridArea: 'annotation-1b', target: '1b' },
+    { id: '1c', gridArea: 'annotation-1c', target: '1c' },
+    { id: '2', gridArea: 'annotation-2', target: '2' },
+    { id: '3', gridArea: 'annotation-3', target: '3' },
+    { id: '3a', gridArea: 'annotation-3a', target: '3a' },
+    { id: '3b', gridArea: 'annotation-3b', target: '3b' },
   ];
 
-  let areas = [
-    ['empty-0', 'annotation-pad', 'empty'],
-    ['empty-0', 'layer', 'empty'],
-    ['annotation-1a', 'layer', 'empty'],
-    ['annotation-1b', 'layer', 'empty-3'],
-    ['gap-1', 'layer', 'empty-3'],
-    ['annotation-2', 'layer', 'annotation-gap'],
-    ['gap-2', 'layer', 'empty-4'],
-    ['annotation-3', 'layer', 'empty-4'],
-    ['empty-2', 'layer', 'annotation-round'],
-    ['empty-2', 'layer', 'empty-5'],
-    ['empty-2', 'annotation-elevation', 'empty-5'],
-  ];
-
-  if (['xsmall', 'small'].includes(breakpoint)) {
-    columns = ['36px', 'flex'];
-    rows = [
-      '48px',
-      '24px',
-      '37px',
-      '25px',
-      '24px',
-      '3xsmall',
-      '3xsmall',
-      '24px',
-      '74px',
-      '24px',
-      '48px',
-    ];
-    areas = [
-      ['empty-0', 'annotation-pad'],
-      ['empty-0', 'layer'],
-      ['annotation-1a', 'layer'],
-      ['annotation-1b', 'layer'],
-      ['gap-1', 'layer'],
-      ['annotation-2', 'layer'],
-      ['annotation-gap', 'layer'],
-      ['gap-2', 'layer'],
-      ['annotation-3', 'layer'],
-      ['annotation-round', 'layer'],
-      ['empty-2', 'layer'],
-      ['empty-2', 'annotation-elevation'],
-    ];
-  }
   return (
-    <Stack interactiveChild="first">
-      <Grid
-        columns={columns}
-        rows={rows}
-        areas={areas}
-        gap={{ column: 'xlarge' }}
-      >
-        <Annotation
-          alignSelf="center"
-          id="1a"
-          target="1a"
-          gridArea="annotation-1a"
-        />
-        <Annotation
-          alignSelf="center"
-          id="1b"
-          target="1b"
-          gridArea="annotation-1b"
-        />
-        <Annotation
-          alignSelf="center"
-          id="2"
-          target="2"
-          gridArea="annotation-2"
-        />
-        <Annotation
-          alignSelf="center"
-          id="3"
-          target="3"
-          gridArea="annotation-3"
-        />
-        <Box align="center" gridArea="annotation-pad" width="100%">
+    <Stack margin={{ bottom: 'medium' }} interactiveChild="last">
+      <AnatomyGrid>
+        {annotations.map(({ id, gridArea, target, ...rest }) => (
           <Annotation
-            id="pad"
-            kind="style"
-            target={mobile ? '4' : 'pad="medium"'}
+            key={id}
+            id={id}
+            target={target}
+            gridArea={gridArea}
+            {...rest}
           />
-        </Box>
-        <Annotation
-          kind="style"
-          alignSelf="center"
-          id="gap"
-          target={mobile ? '5' : 'gap="medium"'}
-          gridArea="annotation-gap"
-        />
-        <Box align="center" gridArea="annotation-elevation" width="100%">
-          <Annotation
-            id="elevation"
-            kind="style"
-            target={mobile ? '7' : 'elevation="large"'}
-          />
-        </Box>
-        <Annotation
-          alignSelf="center"
-          id="round"
-          kind="style"
-          target={mobile ? '6' : 'round="small"'}
-          gridArea="annotation-round"
-        />
-        <LayerContent gridArea="layer" />
-      </Grid>
+        ))}
+
+        <Card
+          gridArea="layer-area"
+          background="background-front"
+          round="small"
+          elevation="large"
+        >
+          <CardBody pad="medium" gap="medium">
+            <LayerHeader
+              id="layer-header"
+              closeId="layer-close"
+              title="Create pipeline"
+              subtitle="A form for entering the details needed to create a new 
+              pipeline."
+              onClose={() => {}}
+              border={{ style: 'dashed' }}
+            />
+            <Box
+              id="layer-body"
+              border={{ style: 'dashed' }}
+              align="center"
+              justify="center"
+              pad="3xsmall"
+            >
+              Body
+            </Box>
+            <Footer
+              id="layer-footer"
+              border={{ style: 'dashed' }}
+              justify="end"
+              gap="small"
+            >
+              <Button id="layer-cancel" label="Cancel" />
+              <Button id="layer-confirm" label="Create pipeline" primary />
+            </Footer>
+          </CardBody>
+        </Card>
+      </AnatomyGrid>
       <Diagram connections={connections} />
     </Stack>
   );
 };
-
-const LayerContent = ({ ...rest }) => (
-  <Card
-    id="layer-container"
-    alignSelf="start"
-    round="medium"
-    elevation="large"
-    {...rest}
-  >
-    <CardBody pad={{ top: 'medium', horizontal: 'medium' }}>
-      <LayerHeader
-        border={{ style: 'dashed' }}
-        subtitle="An optional, concise subtitle for added context."
-      />
-      <Box pad="xsmall" id="first-gap" />
-      <Box
-        id="layer-body"
-        border={{ style: 'dashed' }}
-        height="xsmall"
-        align="center"
-        justify="center"
-      >
-        Body
-      </Box>
-      <Box pad="xsmall" id="layer-gap" />
-      <Box
-        id="layer-footer"
-        border={{ style: 'dashed' }}
-        align="center"
-        justify="center"
-        flex={false}
-        pad="medium"
-      >
-        Footer
-      </Box>
-      <Box pad="xsmall" id="bottom-pad" />
-    </CardBody>
-  </Card>
-);
