d1fd8343a9c2776777cb7ad9acd2700a3bc1ee15
britt6612 <brittany.archibeque@hpe.com>
2026-03-06 11:00:27 -0700

    fix accessibility issue on empty state story

--- apps/docs/src/examples/templates/empty-state/CardEmptyState.js
+++ apps/docs/src/examples/templates/empty-state/CardEmptyState.js
@@ -1,12 +1,5 @@
 import React from 'react';
-import {
-  Box,
-  // Main,
-  Grid,
-  Page,
-  PageContent,
-  PageHeader,
-} from 'grommet';
+import { Box, Main, Grid, Page, PageContent, PageHeader } from 'grommet';
 import { Card } from '../Card';
 import { ErrorManagementEmptyState } from './ErrorManagementEmptyState';
 import { CostByMonth, CostByYear } from '../dashboards';
@@ -16,19 +9,17 @@ export const CardEmptyState = () => {
     <Page>
       <PageContent gap="medium">
         <PageHeader aria-label="dashboard view" title="Dashboard" />
-        {/* Main is commented out for this example, but should be used in a
-        real application. */}
-        {/* <Main> */}
-        <Grid columns="medium" gap="medium">
-          <CostByMonth period="Last Year" />
-          <CostByYear period="Lifetime" />
-          <Card align="center" title="Cost by service" level={2}>
-            <Box pad={{ vertical: 'medium' }}>
-              <ErrorManagementEmptyState level={3} />
-            </Box>
-          </Card>
-        </Grid>
-        {/* </Main> */}
+        <Main>
+          <Grid columns="medium" gap="medium">
+            <CostByMonth period="Last Year" />
+            <CostByYear period="Lifetime" />
+            <Card align="center" title="Cost by service" level={2}>
+              <Box pad={{ vertical: 'medium' }}>
+                <ErrorManagementEmptyState level={3} />
+              </Box>
+            </Card>
+          </Grid>
+        </Main>
       </PageContent>
     </Page>
   );
