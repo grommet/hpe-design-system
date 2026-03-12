e3244061de849a7bb81428e4f658568230b22267
sulaymon333 <tjsulaymon@gmail.com>
2026-03-12 17:57:23 +0000

    fix ref anchor for db conf. preview card

--- apps/docs/src/examples/cardPreviews/double-confirmation.js
+++ apps/docs/src/examples/cardPreviews/double-confirmation.js
@@ -22,25 +22,21 @@ export const DoubleConfirmationPreview = () => {
   }, []);
 
   return (
-    <div ref={ref}>
-      <Card elevation="none" width="small">
-        <CardBody pad={{ horizontal: 'small', vertical: 'small' }}>
-          <Box gap="3xsmall">
-            <Heading level={2} margin="none" size="xsmall">
-              Discard Changes?
-            </Heading>
-            <Paragraph margin="none">
-              Your changes will not be applied.
-            </Paragraph>
-          </Box>
-        </CardBody>
-        <CardFooter pad={{ top: 'none', bottom: 'small', horizontal: 'small' }}>
-          <Box fill direction="row" gap="xsmall" justify="end">
-            <Button label="Cancel" />
-            <Button label="Discard" primary />
-          </Box>
-        </CardFooter>
-      </Card>
-    </div>
+    <Card ref={ref} elevation="none" width="small">
+      <CardBody pad={{ horizontal: 'small', vertical: 'small' }}>
+        <Box gap="3xsmall">
+          <Heading level={2} margin="none" size="xsmall">
+            Discard Changes?
+          </Heading>
+          <Paragraph margin="none">Your changes will not be applied.</Paragraph>
+        </Box>
+      </CardBody>
+      <CardFooter pad={{ top: 'none', bottom: 'small', horizontal: 'small' }}>
+        <Box fill direction="row" gap="xsmall" justify="end">
+          <Button label="Cancel" />
+          <Button label="Discard" primary />
+        </Box>
+      </CardFooter>
+    </Card>
   );
 };
