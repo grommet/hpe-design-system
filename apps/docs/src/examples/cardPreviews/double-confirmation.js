7ab031be4fe303b80b7bc189eeffdbfd69887075
sulaymon333 <tjsulaymon@gmail.com>
2026-03-12 17:19:45 +0000

    update db conf. cards to use Grommet Card components

--- apps/docs/src/examples/cardPreviews/double-confirmation.js
+++ apps/docs/src/examples/cardPreviews/double-confirmation.js
@@ -3,7 +3,7 @@ import {
   Button,
   Card,
   CardBody,
-  Footer,
+  CardFooter,
   Heading,
   Paragraph,
 } from 'grommet';
@@ -23,24 +23,24 @@ export const DoubleConfirmationPreview = () => {
 
   return (
     <div ref={ref}>
-      <Box background="background-back" pad="medium">
-        <Card alignSelf="center" round="medium" elevation="none" width="small">
-          <CardBody pad="medium" gap="none">
-            <Box gap="3xsmall">
-              <Heading level={2} margin="none" size="xsmall">
-                Discard Changes?
-              </Heading>
-              <Paragraph margin="none">
-                Your changes will not be applied.
-              </Paragraph>
-            </Box>
-            <Footer direction="row" gap="xsmall" justify="end">
-              <Button label="Cancel" />
-              <Button label="Discard" primary />
-            </Footer>
-          </CardBody>
-        </Card>
-      </Box>
+      <Card elevation="none" width="small">
+        <CardBody pad={{ horizontal: 'small', vertical: 'small' }}>
+          <Box gap="3xsmall">
+            <Heading level={2} margin="none" size="xsmall">
+              Discard Changes?
+            </Heading>
+            <Paragraph margin="none">
+              Your changes will not be applied.
+            </Paragraph>
+          </Box>
+        </CardBody>
+        <CardFooter pad={{ top: 'none', bottom: 'small', horizontal: 'small' }}>
+          <Box fill direction="row" gap="xsmall" justify="end">
+            <Button label="Cancel" />
+            <Button label="Discard" primary />
+          </Box>
+        </CardFooter>
+      </Card>
     </div>
   );
 };
--- apps/docs/src/examples/templates/double-confirmation/DoubleConfirmation.js
+++ apps/docs/src/examples/templates/double-confirmation/DoubleConfirmation.js
@@ -1,6 +1,5 @@
-import React from 'react';
 import PropTypes from 'prop-types';
-import { Box, Button, Layer } from 'grommet';
+import { Box, Button, Card, CardBody, CardFooter, Layer } from 'grommet';
 import { LayerHeader } from '@shared/aries-core';
 import { useConfirmation } from '@shared/hooks';
 
@@ -9,16 +8,22 @@ export const DoubleConfirmation = ({ title, ...rest }) => {
 
   return (
     <Layer {...rest}>
-      <Box pad="medium" gap="medium">
-        <LayerHeader
-          title={`Discard ${title}?`}
-          subtitle="Your changes will not be applied."
-        />
-        <Box direction="row" gap="xsmall" justify="end">
-          <Button label="Cancel" onClick={cancelConfirmation} />
-          <Button label="Discard" primary onClick={acceptConfirmation} />
-        </Box>
-      </Box>
+      <Card>
+        <CardBody pad="medium" gap="medium">
+          <LayerHeader
+            title={`Discard ${title}?`}
+            subtitle="Your changes will not be applied."
+          />
+        </CardBody>
+        <CardFooter
+          pad={{ top: 'none', bottom: 'medium', horizontal: 'medium' }}
+        >
+          <Box fill direction="row" gap="xsmall" justify="end">
+            <Button label="Cancel" onClick={cancelConfirmation} />
+            <Button label="Discard" primary onClick={acceptConfirmation} />
+          </Box>
+        </CardFooter>
+      </Card>
     </Layer>
   );
 };
