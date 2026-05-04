fd4d95fac1d501b02951f2458400e0fb99e38db0
britt6612 <brittany.archibeque@hpe.com>
2026-05-04 13:52:58 -0600

    update tag

--- packages/code-connect/src/Tag.figma.jsx
+++ packages/code-connect/src/Tag.figma.jsx
@@ -1,40 +1,55 @@
 import figma from '@figma/code-connect';
 import { Tag } from 'grommet';
 
+const FIGMA_URL =
+  'https://www.figma.com/design/HDckqS2MWhINfC8EIQPMV1/HPE-Design-System-Components-V2?node-id=3039-2385';
+
+const sharedProps = {
+  value: figma.string('Value'),
+  size: figma.enum('Size', {
+    xSmall: 'xsmall',
+    small: 'small',
+    medium: 'medium',
+    large: 'large',
+    xLarge: 'xlarge',
+  }),
+  onClick: figma.enum('is Clickable', {
+    true: () => {},
+    false: undefined,
+  }),
+  onRemove: figma.boolean('is Removable', {
+    true: () => {},
+    false: undefined,
+  }),
+};
+
 /**
- * Figma Code Connect mapping for the HPE Design System Tag
+ * Tag with name label visible
  */
-figma.connect(
-  Tag,
-  'https://www.figma.com/design/HDckqS2MWhINfC8EIQPMV1/HPE-Design-System-Components-V2?node-id=3039-2385',
-  {
-    props: {
-      name: figma.string('Name'),
-      value: figma.string('Value'),
-      size: figma.enum('Size', {
-        xSmall: 'xsmall',
-        small: 'small',
-        medium: 'medium',
-        large: 'large',
-        xLarge: 'xlarge',
-      }),
-      onClick: figma.enum('is Clickable', {
-        true: () => {},
-        false: undefined,
-      }),
-      onRemove: figma.enum('is Removable', {
-        true: () => {},
-        false: undefined,
-      }),
-    },
-    example: ({ name, value, size, onClick, onRemove }) => (
-      <Tag
-        name={name}
-        value={value}
-        size={size}
-        onClick={onClick}
-        onRemove={onRemove}
-      />
-    ),
+figma.connect(Tag, FIGMA_URL, {
+  variant: { 'show Name': 'True' },
+  props: {
+    ...sharedProps,
+    name: figma.string('Name'),
   },
-);
+  example: ({ name, value, size, onClick, onRemove }) => (
+    <Tag
+      name={name}
+      value={value}
+      size={size}
+      onClick={onClick}
+      onRemove={onRemove}
+    />
+  ),
+});
+
+/**
+ * Tag without name label (value only)
+ */
+figma.connect(Tag, FIGMA_URL, {
+  variant: { 'show Name': 'False' },
+  props: sharedProps,
+  example: ({ value, size, onClick, onRemove }) => (
+    <Tag value={value} size={size} onClick={onClick} onRemove={onRemove} />
+  ),
+});
