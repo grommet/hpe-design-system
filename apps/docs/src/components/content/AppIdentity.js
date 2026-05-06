29cf1aa6bf69ed1939c3baf457f0c3f405f9ce50
sulaymon333 <tjsulaymon@gmail.com>
2026-05-06 22:18:06 +0100

    fix compopnents/content AppIdentity conflict

--- apps/docs/src/components/content/AppIdentity.js
+++ apps/docs/src/components/content/AppIdentity.js
@@ -3,50 +3,6 @@ import PropTypes from 'prop-types';
 import { Box, Button, Image, Text } from 'grommet';
 import { ThemeContext } from 'styled-components';
 
-
-export const AppIdentity = forwardRef(
-  ({
-    logo = true,
-    logoOnly = false,
-    href = '/',
-    title,
-    ...rest
-  }, ref) => {
-    const textSize = 'medium';
-    const theme = useContext(ThemeContext);
-    const linkLabel = title ? `Go to ${title} home` : 'Go to home';
-
-    return (
-      <Button
-        href={href}
-        ref={ref}
-        a11yTitle={linkLabel}
-        {...rest}
-      >
-        <Box
-          direction="row"
-          align="center"
-          gap="small"
-          // pad maintains accessible hit target
-          // non-responsive maintains same dimensions for mobile
-          pad={{ vertical: 'xsmall' }}
-          responsive={false}
-        >
-          {logo && (<Box width="4xsmall">
-            <Image
-              alt="HPE logo"
-              fit="contain"
-              src={theme?.dark
-                ? '/HPE_logo_full-clr_rev_rgb.svg'
-                : '/HPE_logo_full-clr_pos_rgb.svg'}
-            />
-          </Box>)}
-          {!logoOnly && (
-import React, { forwardRef, useContext } from 'react';
-import PropTypes from 'prop-types';
-import { Box, Button, Image, Text } from 'grommet';
-import { ThemeContext } from 'styled-components';
-
 const HpeLogo = () => {
   const theme = useContext(ThemeContext);
   return (
@@ -61,21 +17,19 @@ const HpeLogo = () => {
     </Box>
   );
 };
-
 export const AppIdentity = forwardRef(
   (
-    { 
-      logo = true, 
+    {
+      logo = true,
       logoOnly = false,
-      title = 'HPE Design System', 
-      ...rest 
+      title = 'HPE Design System',
+      ...rest
     },
     ref,
   ) => {
     const textSize = 'medium';
     const href = '/';
     const linkLabel = title ? `Go to ${title} home` : 'Go to home';
-
     return (
       <Button href={href} ref={ref} a11yTitle={linkLabel} {...rest}>
         <Box
@@ -90,7 +44,7 @@ export const AppIdentity = forwardRef(
           {logo && <HpeLogo />}
           {!logoOnly && (
             <Box direction="row" gap="3xsmall" wrap>
-              {!logo && 
+              {!logo &&
                 <Text weight="bold" size={textSize} color="text-strong">
                   HPE
                 </Text>
@@ -105,24 +59,8 @@ export const AppIdentity = forwardRef(
     );
   },
 );
-
 AppIdentity.propTypes = {
   logo: PropTypes.bool,
   logoOnly: PropTypes.bool,
-  title: PropTypes.string,
-};
-            </Box>
-          )}
-        </Box>
-      </Button>
-    );
-  },
-);
-
-
-AppIdentity.propTypes = {
-  logo: PropTypes.bool,
-  logoOnly: PropTypes.bool,
-  href: PropTypes.string,
   title: PropTypes.string,
 };
