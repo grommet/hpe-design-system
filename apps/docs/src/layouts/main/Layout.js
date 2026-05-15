91469e44fd1b62c0c6736405fe13c4c3b6c7ce63
sulaymon333 <tjsulaymon@gmail.com>
2026-05-15 13:20:55 +0100

    restore merge issue with DOCS-NEXT

--- apps/docs/src/layouts/main/Layout.js
+++ apps/docs/src/layouts/main/Layout.js
@@ -22,6 +22,7 @@ import { siteContents } from '../../data/search/contentForSearch';
 import { ViewContext } from '../../pages/_app';
 import { UserFeedback } from './UserFeedback';
 import { Navigation } from '../navigation';
+import { NavProvider } from '../navigation/NavContext';
 
 const gridAreas = [
   ['nav', 'header', 'context-pane'],
@@ -103,55 +104,57 @@ export const Layout = ({
         {backgroundImage && (
           <PageBackground backgroundImage={backgroundImage} />
         )}
-        <Grid
-          areas={gridAreas}
-          columns={gridColumns}
-          rows={gridRows}
-        >
-          {/* Only render Header for non-home pages.
+        <NavProvider>
+          <Grid
+            areas={gridAreas}
+            columns={gridColumns}
+            rows={gridRows}
+          >
+            {/* Only render Header for non-home pages.
               Homepage header is rendered in index.js
               to have the same background as the hero. */}
-          {title && title.toLowerCase() !== 'home' && (
-            <AppHeader gridArea="header" />
-          )}
-          <Navigation
-            gridArea="nav"
-            as="aside"
-            a11yTitle="Site navigation"
-            background="background-front"
-            height="100vh"
-            overflow="auto"
-            style={{
-              position: 'sticky',
-              top: 0,
-              overscrollBehavior: 'contain',
-            }}
-          />
-          <Main overflow="visible" gridArea="main">
-            <Page>
-              {layout !== 'plain' ? (
-                <DocsLayout
-                  title={title}
-                  topic={topic}
-                  render={render}
-                  headings={headings}
-                  relatedContent={relatedContent}
-                  showInPageNav={showInPageNav}
-                  pageUpdateReady={pageUpdateReady}
-                  contentHistory={contentHistory}
-                >
-                  {children}
-                </DocsLayout>
-              ) : (
-                <>
-                  <SkipLinkTarget id="main" label="Main content" />
-                  {children}
-                </>
-              )}
-            </Page>
-          </Main>
-          <UserFeedback />
-        </Grid>
+            {title && title.toLowerCase() !== 'home' && (
+              <AppHeader gridArea="header" />
+            )}
+            <Navigation
+              gridArea="nav"
+              as="aside"
+              a11yTitle="Site navigation"
+              background="background-front"
+              height="100vh"
+              overflow="auto"
+              style={{
+                position: 'sticky',
+                top: 0,
+                overscrollBehavior: 'contain',
+              }}
+            />
+            <Main overflow="visible" gridArea="main">
+              <Page>
+                {layout !== 'plain' ? (
+                  <DocsLayout
+                    title={title}
+                    topic={topic}
+                    render={render}
+                    headings={headings}
+                    relatedContent={relatedContent}
+                    showInPageNav={showInPageNav}
+                    pageUpdateReady={pageUpdateReady}
+                    contentHistory={contentHistory}
+                  >
+                    {children}
+                  </DocsLayout>
+                ) : (
+                  <>
+                    <SkipLinkTarget id="main" label="Main content" />
+                    {children}
+                  </>
+                )}
+              </Page>
+            </Main>
+            <UserFeedback />
+          </Grid>
+        </NavProvider>
       </Stack>
     </>
   );
