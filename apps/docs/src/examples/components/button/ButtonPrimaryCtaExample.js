0e6ad388f7f680389c803f48413f8c1d9eb07198
julauxen <julia.lauxen@hpe.com>
2026-05-18 13:53:31 +0100

    cta example

--- apps/docs/src/examples/components/button/ButtonPrimaryCtaExample.js
+++ apps/docs/src/examples/components/button/ButtonPrimaryCtaExample.js
@@ -1,48 +1,32 @@
 import React from 'react';
-import {
-  Box,
-  Button,
-  Card,
-  CardBody,
-  CardHeader,
-  Text,
-} from 'grommet';
+import { Box, Button, PageHeader } from 'grommet';
 import { ButtonGroup } from '@shared/aries-core';
 
 export const ButtonPrimaryCtaExample = () => (
-  <Box align="center" fill justify="center" pad="medium">
-    <Card width="large">
-      <CardHeader pad="medium">
-        <Box gap="xsmall">
-          <Text size="large" weight="bold">
-            GreenLake Monitoring
-          </Text>
-          <Text color="status-critical" size="small">
-            Status: Inactive
-          </Text>
-        </Box>
-      </CardHeader>
-      <CardBody gap="medium" pad="medium">
-        <Text>
-          Enable health and performance monitoring
-          for your GreenLake platform resources.
-        </Text>
-        <ButtonGroup>
-          <Button
-            label="Activate service"
-            onClick={() => {}}
-            primary
-          />
-          <Button
-            label="View details"
-            onClick={() => {}}
-          />
-          <Button
-            label="Schedule"
-            onClick={() => {}}
-          />
-        </ButtonGroup>
-      </CardBody>
-    </Card>
+  <Box fill pad="medium">
+    <Box width="large">
+      <PageHeader
+        title="GreenLake Monitoring"
+        subtitle="Enable health and performance monitoring
+          for your GreenLake platform resources."
+        actions={
+          <ButtonGroup>
+            <Button
+              label="Activate service"
+              onClick={() => {}}
+              primary
+            />
+            <Button
+              label="View details"
+              onClick={() => {}}
+            />
+            <Button
+              label="Schedule"
+              onClick={() => {}}
+            />
+          </ButtonGroup>
+        }
+      />
+    </Box>
   </Box>
 );
