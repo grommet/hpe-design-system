2a9716fb99eb81f7e0b7547562a010798529ce0c
Matthew Glissmann <mdglissmann@gmail.com>
2026-05-22 15:30:53 -0600

    removed unneeded pad

--- apps/docs/src/examples/components/button/ButtonActionLabelsExample.js
+++ apps/docs/src/examples/components/button/ButtonActionLabelsExample.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Box, Button } from 'grommet';
 
 export const ButtonActionLabelsExample = () => (
-  <Box direction="row" gap="xsmall" pad="medium" wrap>
+  <Box direction="row" gap="xsmall" wrap>
     <Button label="Apply filters" onClick={() => {}} primary />
     <Button label="Reset" onClick={() => {}} secondary />
     <Button label="Cancel" onClick={() => {}} />
