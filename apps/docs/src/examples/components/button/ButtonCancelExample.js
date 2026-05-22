8e4b662e0cbcd2f60cf73f01e3e3ff0ff1974bb6
Matthew Glissmann <mdglissmann@gmail.com>
2026-05-22 15:42:44 -0600

    cancel example button alignment

--- apps/docs/src/examples/components/button/ButtonCancelExample.js
+++ apps/docs/src/examples/components/button/ButtonCancelExample.js
@@ -62,6 +62,7 @@ export const ButtonCancelExample = () => {
             label="Edit device"
             onClick={handleEdit}
             secondary
+            alignSelf="start"
           />
         </Box>
       </Box>
