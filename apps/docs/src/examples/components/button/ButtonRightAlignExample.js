9be30eb6ca22b16e87c83897878f736395587b6d
sulaymon333 <tjsulaymon@gmail.com>
2026-06-11 15:12:43 +0100

    dos and donts cleanup, remove no-op onClick and alignment cleanup

--- apps/docs/src/examples/components/button/ButtonRightAlignExample.js
+++ apps/docs/src/examples/components/button/ButtonRightAlignExample.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import { Box, Button, Header } from 'grommet';
+import { Box, Button } from 'grommet';
 import { ButtonGroup, TextEmphasis } from '@shared/aries-core';
 
 export const ButtonRightAlignExample = () => (
   <Box width="xlarge" gap="medium" pad="xsmall">
-    <Header
-      direction="column"
-      align="start"
-      gap="3xsmall"
-      pad={{ horizontal: '5xsmall' }}
-    >
-      <TextEmphasis alignSelf="end" size="large">
-        Right-aligned buttons
-      </TextEmphasis>
-    </Header>
+    <TextEmphasis alignSelf="end" size="large">
+      Right-aligned buttons
+    </TextEmphasis>
     <ButtonGroup justify="end">
-      <Button label="Default" onClick={() => {}} />
-      <Button secondary label="Secondary" onClick={() => {}} />
-      <Button primary label="Primary" onClick={() => {}} />
+      <Button label="Default" />
+      <Button secondary label="Secondary" />
+      <Button primary label="Primary" />
     </ButtonGroup>
   </Box>
 );
