de33049595279f4cbf7c9b7322c75f8e68f1f728
sulaymon333 <tjsulaymon@gmail.com>
2026-06-11 21:04:08 +0100

    fix formatting

--- apps/docs/src/examples/components/button/ButtonAnatomy.js
+++ apps/docs/src/examples/components/button/ButtonAnatomy.js
@@ -8,131 +8,127 @@ const thickness = 'hair';
 const type = 'direct';
 
 const connections = [
-    {
-        anchor: 'horizontal',
-        type,
-        color,
-        thickness,
-        fromTarget: '1',
-        toTarget: 'button-container',
-    },
-    {
-        anchor: 'vertical',
-        type,
-        color,
-        thickness,
-        fromTarget: '1a',
-        toTarget: 'button-label',
-    },
-    {
-        anchor: 'vertical',
-        type,
-        color,
-        thickness,
-        fromTarget: '1b',
-        toTarget: 'button-icon',
-    },
-    {
-        anchor: 'vertical',
-        type,
-        color,
-        thickness,
-        fromTarget: '1c',
-        toTarget: 'button-badge',
-    },
+  {
+    anchor: 'horizontal',
+    type,
+    color,
+    thickness,
+    fromTarget: '1',
+    toTarget: 'button-container',
+  },
+  {
+    anchor: 'vertical',
+    type,
+    color,
+    thickness,
+    fromTarget: '1a',
+    toTarget: 'button-label',
+  },
+  {
+    anchor: 'vertical',
+    type,
+    color,
+    thickness,
+    fromTarget: '1b',
+    toTarget: 'button-icon',
+  },
+  {
+    anchor: 'vertical',
+    type,
+    color,
+    thickness,
+    fromTarget: '1c',
+    toTarget: 'button-badge',
+  },
 ];
 
 const AnatomyGrid = ({ ...rest }) => (
-    <Grid
-        columns={['max-content', 'max-content', '5xsmall']}
-        rows={['auto', 'auto', 'auto']}
-        areas={[
-            ['annotation-1b', 'annotation-1a', 'annotation-1c'],
-            ['button-area', 'button-area', 'empty-0'],
-            ['button-area', 'button-area', 'annotation-1'],
-        ]}
-        align="center"
-        justify="center"
-        gap={{ column: 'medium', row: 'small' }}
-        {...rest}
-    />
+  <Grid
+    columns={['max-content', 'max-content', '5xsmall']}
+    rows={['auto', 'auto', 'auto']}
+    areas={[
+      ['annotation-1b', 'annotation-1a', 'annotation-1c'],
+      ['button-area', 'button-area', 'empty-0'],
+      ['button-area', 'button-area', 'annotation-1'],
+    ]}
+    align="center"
+    justify="center"
+    gap={{ column: 'medium', row: 'small' }}
+    {...rest}
+  />
 );
 
 export const ButtonAnatomy = () => {
-    const theme = useContext(ThemeContext);
-    const annotations = [
-        {
-            id: '1',
-            gridArea: 'annotation-1',
-            target: '1',
-            style: { justifySelf: 'start' },
-        },
-        { id: '1a', gridArea: 'annotation-1a', target: '1a' },
-        {
-            id: '1b',
-            gridArea: 'annotation-1b',
-            target: '1b',
-            style: { justifySelf: 'start' },
-        },
-        {
-            id: '1c',
-            gridArea: 'annotation-1c',
-            target: '1c',
-            style: { justifySelf: 'start' },
-        },
-    ];
+  const theme = useContext(ThemeContext);
+  const annotations = [
+    {
+      id: '1',
+      gridArea: 'annotation-1',
+      target: '1',
+      style: { justifySelf: 'start' },
+    },
+    { id: '1a', gridArea: 'annotation-1a', target: '1a' },
+    {
+      id: '1b',
+      gridArea: 'annotation-1b',
+      target: '1b',
+      style: { justifySelf: 'start' },
+    },
+    {
+      id: '1c',
+      gridArea: 'annotation-1c',
+      target: '1c',
+      style: { justifySelf: 'start' },
+    },
+  ];
 
-    return (
-        <Stack margin={{ bottom: 'medium' }} interactiveChild="last">
-            <AnatomyGrid>
-                {annotations.map(({ id, gridArea, target, ...rest }) => (
-                    <Annotation
-                        key={id}
-                        id={id}
-                        target={target}
-                        gridArea={gridArea}
-                        {...rest}
-                    />
-                ))}
+  return (
+    <Stack margin={{ bottom: 'medium' }} interactiveChild="last">
+      <AnatomyGrid>
+        {annotations.map(({ id, gridArea, target, ...rest }) => (
+          <Annotation
+            key={id}
+            id={id}
+            target={target}
+            gridArea={gridArea}
+            {...rest}
+          />
+        ))}
 
-                <Box gridArea="button-area" align="start">
-                    <Button
-                        id="button-container"
-                        secondary
-                        label={<Box id="button-label">Notifications</Box>}
-                        icon={<Notification id="button-icon" />}
-                        tabIndex={-1}
-                        // we need to build the badge manually so we can attach 
-                        // `id="button-badge"` for the anatomy diagram target. 
-                        // So, we cannot rely on the out-of-the-box badge here  
-                        // which already has theme-driven colors. 
-                        // Hence, we apply an explicit theme workaround.
-                        badge={
-                            <Box
-                                id="button-badge"
-                                background={{
-                                    color: 'background-neutral-xstrong',
-                                    dark: theme.dark,
-                                }}
-                                round
-                                align="center"
-                                justify="center"
-                                width={{ min: '18px' }}
-                                height="18px"
-                            >
-                                <Text
-                                    color="text-onStrong"
-                                    size="xsmall"
-                                    weight="normal"
-                                >
-                                    2
-                                </Text>
-                            </Box>
-                        }
-                    />
-                </Box>
-            </AnatomyGrid>
-            <Diagram connections={connections} />
-        </Stack>
-    );
+        <Box gridArea="button-area" align="start">
+          <Button
+            id="button-container"
+            secondary
+            label={<Box id="button-label">Notifications</Box>}
+            icon={<Notification id="button-icon" />}
+            tabIndex={-1}
+            // we need to build the badge manually so we can attach
+            // `id="button-badge"` for the anatomy diagram target.
+            // So, we cannot rely on the out-of-the-box badge here
+            // which already has theme-driven colors.
+            // Hence, we apply an explicit theme workaround.
+            badge={
+              <Box
+                id="button-badge"
+                background={{
+                  color: 'background-neutral-xstrong',
+                  dark: theme.dark,
+                }}
+                round
+                align="center"
+                justify="center"
+                width={{ min: '18px' }}
+                height="18px"
+              >
+                <Text color="text-onStrong" size="xsmall" weight="normal">
+                  2
+                </Text>
+              </Box>
+            }
+          />
+        </Box>
+      </AnatomyGrid>
+      <Diagram connections={connections} />
+    </Stack>
+  );
 };
