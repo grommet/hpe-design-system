a3cf47a8cffcf37798214496194d7788ceeebcc8
sulaymon333 <tjsulaymon@gmail.com>
2026-06-11 21:10:16 +0100

    fix formatting

--- apps/docs/src/examples/components/checkbox/CheckBoxAnatomy.js
+++ apps/docs/src/examples/components/checkbox/CheckBoxAnatomy.js
@@ -8,107 +8,101 @@ const thickness = 'hair';
 const type = 'direct';
 
 const connections = [
-    {
-        anchor: 'vertical',
-        type,
-        color,
-        thickness,
-        fromTarget: 'checkbox-annotation-1',
-        toTarget: 'checkbox-control',
-    },
-    {
-        anchor: 'vertical',
-        type,
-        color,
-        thickness,
-        fromTarget: 'checkbox-annotation-1a',
-        toTarget: 'checkmark-indicator',
-    },
-    {
-        anchor: 'horizontal',
-        type,
-        color,
-        thickness,
-        fromTarget: 'checkbox-annotation-2',
-        toTarget: 'checkbox-label',
-    },
+  {
+    anchor: 'vertical',
+    type,
+    color,
+    thickness,
+    fromTarget: 'checkbox-annotation-1',
+    toTarget: 'checkbox-control',
+  },
+  {
+    anchor: 'vertical',
+    type,
+    color,
+    thickness,
+    fromTarget: 'checkbox-annotation-1a',
+    toTarget: 'checkmark-indicator',
+  },
+  {
+    anchor: 'horizontal',
+    type,
+    color,
+    thickness,
+    fromTarget: 'checkbox-annotation-2',
+    toTarget: 'checkbox-label',
+  },
 ];
 
 const AnatomyGrid = ({ ...rest }) => (
-    <Grid
-        columns={['max-content', 'max-content', '4xsmall']}
-        rows={['5xsmall', '5xsmall', '5xsmall']}
-        areas={[
-            ['empty-0', 'annotation-1', 'empty-1'],
-            ['empty-2', 'checkbox-area', 'annotation-2'],
-            ['empty-3', 'annotation-1a', 'empty-4'],
-        ]}
-        align="center"
-        justify="center"
-        {...rest}
-    />
+  <Grid
+    columns={['max-content', 'max-content', '4xsmall']}
+    rows={['5xsmall', '5xsmall', '5xsmall']}
+    areas={[
+      ['empty-0', 'annotation-1', 'empty-1'],
+      ['empty-2', 'checkbox-area', 'annotation-2'],
+      ['empty-3', 'annotation-1a', 'empty-4'],
+    ]}
+    align="center"
+    justify="center"
+    {...rest}
+  />
 );
 
 export const CheckBoxAnatomy = () => {
-    const theme = useContext(ThemeContext);
-    const checkboxTheme = theme.checkBox;
+  const theme = useContext(ThemeContext);
+  const checkboxTheme = theme.checkBox;
 
-    const annotations = [
-        {
-            id: 'checkbox-annotation-1',
-            gridArea: 'annotation-1',
-            target: '1',
-        },
-        {
-            id: 'checkbox-annotation-1a',
-            gridArea: 'annotation-1a',
-            target: '1a',
-        },
-        {
-            id: 'checkbox-annotation-2',
-            gridArea: 'annotation-2',
-            target: '2',
-        },
-    ];
+  const annotations = [
+    {
+      id: 'checkbox-annotation-1',
+      gridArea: 'annotation-1',
+      target: '1',
+    },
+    {
+      id: 'checkbox-annotation-1a',
+      gridArea: 'annotation-1a',
+      target: '1a',
+    },
+    {
+      id: 'checkbox-annotation-2',
+      gridArea: 'annotation-2',
+      target: '2',
+    },
+  ];
 
-    return (
-        <Stack
-            margin={{ bottom: 'medium' }}
-            interactiveChild="last"
-        >
-            <AnatomyGrid>
-                {annotations.map(({ id, gridArea, target, ...rest }) => (
-                    <Annotation
-                        key={id}
-                        id={id}
-                        target={target}
-                        gridArea={gridArea}
-                        {...rest}
-                    />
-                ))}
+  return (
+    <Stack margin={{ bottom: 'medium' }} interactiveChild="last">
+      <AnatomyGrid>
+        {annotations.map(({ id, gridArea, target, ...rest }) => (
+          <Annotation
+            key={id}
+            id={id}
+            target={target}
+            gridArea={gridArea}
+            {...rest}
+          />
+        ))}
 
-                <Box gridArea="checkbox-area" align="start">
-                    <CheckBox
-                        label={
-                            <Box
-                                id="checkbox-label"
-                                align={checkboxTheme.label.align}
-                            >
-                                Choice
-                            </Box>
-                        }
-                    >
-                        {() => (
-                            <CheckBoxControlMock
-                                checkBoxTheme={checkboxTheme}
-                                controlId="checkbox-control"
-                                indicatorId="checkmark-indicator"
-                            />
-                        )}
-                    </CheckBox>
-                </Box>
-            </AnatomyGrid>
-            <Diagram connections={connections} />
-        </Stack>
-    );
+        <Box gridArea="checkbox-area" align="start">
+          <CheckBox
+            label={
+              <Box id="checkbox-label" align={checkboxTheme.label.align}>
+                Choice
+              </Box>
+            }
+          >
+            {() => (
+              <CheckBoxControlMock
+                checkBoxTheme={checkboxTheme}
+                controlId="checkbox-control"
+                indicatorId="checkmark-indicator"
+              />
+            )}
+          </CheckBox>
+        </Box>
+      </AnatomyGrid>
+      <Diagram connections={connections} />
+    </Stack>
+  );
 };
--- apps/docs/src/examples/components/checkbox/CheckBoxControlMock.js
+++ apps/docs/src/examples/components/checkbox/CheckBoxControlMock.js
@@ -4,71 +4,67 @@ import styled from 'styled-components';
 import { Box } from 'grommet';
 
 const CheckBoxControl = styled(Box)`
-    ${({ theme }) =>
-        theme.checkBox.check.extend({
-            theme,
-            checked: true,
-            indeterminate: false,
-            disabled: false,
-        })}
-    width: ${({ theme }) => theme.checkBox.size};
-    height: ${({ theme }) => theme.checkBox.size};
+  ${({ theme }) =>
+    theme.checkBox.check.extend({
+      theme,
+      checked: true,
+      indeterminate: false,
+      disabled: false,
+    })}
+  width: ${({ theme }) => theme.checkBox.size};
+  height: ${({ theme }) => theme.checkBox.size};
 `;
 
 const CheckmarkIcon = styled.svg`
-    width: ${({ theme }) => theme.checkBox.icon.size};
-    height: ${({ theme }) => theme.checkBox.icon.size};
-    & path {
-        fill: none;
-        stroke-width: ${({ theme }) => theme.checkBox.check.thickness};
-    }
-    ${({ theme }) => theme.checkBox.icon.extend};
+  width: ${({ theme }) => theme.checkBox.icon.size};
+  height: ${({ theme }) => theme.checkBox.icon.size};
+  & path {
+    fill: none;
+    stroke-width: ${({ theme }) => theme.checkBox.check.thickness};
+  }
+  ${({ theme }) => theme.checkBox.icon.extend};
 `;
 
 export const CheckBoxControlMock = ({
-    checkBoxTheme,
-    controlId,
-    indicatorId,
-    ...rest
+  checkBoxTheme,
+  controlId,
+  indicatorId,
+  ...rest
 }) => {
-    const iconSize = checkBoxTheme.icon.size || checkBoxTheme.size;
+  const iconSize = checkBoxTheme.icon.size || checkBoxTheme.size;
 
-    return (
-        <CheckBoxControl
-            id={controlId}
-            align="center"
-            justify="center"
-            round={checkBoxTheme.check.radius}
-            {...rest}
+  return (
+    <CheckBoxControl
+      id={controlId}
+      align="center"
+      justify="center"
+      round={checkBoxTheme.check.radius}
+      {...rest}
+    >
+      <Box id={indicatorId} width={iconSize} height={iconSize}>
+        <CheckmarkIcon
+          aria-hidden="true"
+          viewBox="0 0 24 24"
+          preserveAspectRatio="xMidYMid meet"
         >
-            <Box
-                id={indicatorId}
-                width={iconSize}
-                height={iconSize}
-            >
-                <CheckmarkIcon
-                    aria-hidden="true"
-                    viewBox="0 0 24 24"
-                    preserveAspectRatio="xMidYMid meet"
-                >
-                    <path d="M6,11.3 L10.3,16 L18,6.2" />
-                </CheckmarkIcon>
-            </Box>
-        </CheckBoxControl>
-    );
+          <path d="M6,11.3 L10.3,16 L18,6.2" />
+        </CheckmarkIcon>
+      </Box>
+    </CheckBoxControl>
+  );
 };
 
 CheckBoxControlMock.propTypes = {
-    checkBoxTheme: PropTypes.shape({
-        size: PropTypes.string.isRequired,
-        icon: PropTypes.shape({
-            size: PropTypes.string,
-        }).isRequired,
-        check: PropTypes.shape({
-            radius: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
-            thickness: PropTypes.string.isRequired,
-        }).isRequired,
+  checkBoxTheme: PropTypes.shape({
+    size: PropTypes.string.isRequired,
+    icon: PropTypes.shape({
+      size: PropTypes.string,
     }).isRequired,
-    controlId: PropTypes.string.isRequired,
-    indicatorId: PropTypes.string.isRequired,
+    check: PropTypes.shape({
+      radius: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+      thickness: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+  controlId: PropTypes.string.isRequired,
+  indicatorId: PropTypes.string.isRequired,
 };
