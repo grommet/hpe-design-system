5ea03c3928034ecebffe3ae1410c135298dc953d
michael-kingdom_hpeprod <michael.kingdom@hpe.com>
2026-06-16 10:58:02 -0600

    Fix incorrect example links

--- apps/docs/src/components/content/HighlightPhrase.js
+++ apps/docs/src/components/content/HighlightPhrase.js
@@ -58,7 +58,9 @@ const formatMatchedPhrase = (matchObj, highlightProps) => {
       nextIndex = match.index + match[0].length;
       nextPhrase.push(
         <HighlightedText key={index} fade={fade} {...rest}>
+          &nbsp;
           {match.input.slice(match.index, match.index + match[0].length)}
+          &nbsp;
         </HighlightedText>,
       );
       if (index + 1 === matchObj.length) {
