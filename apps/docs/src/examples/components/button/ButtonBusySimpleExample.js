acae9f1cf8acb1d2d8c5c4608079895d3a349eca
sulaymon333 <tjsulaymon@gmail.com>
2026-06-22 14:20:32 +0100

    update form gap based on parent layout

--- apps/docs/src/examples/components/button/ButtonBusySimpleExample.js
+++ apps/docs/src/examples/components/button/ButtonBusySimpleExample.js
@@ -29,44 +29,52 @@ export const ButtonBusySimpleExample = () => {
   return (
     <ContentPane width="medium">
       <Form onSubmit={handleSubmit}>
-        <FormField
-          htmlFor="fullName"
-          label="Full name"
-          name="fullName"
-          required
-        >
-          <TextInput
-            id="fullName"
-            name="fullName"
-            placeholder="Your full name"
-          />
-        </FormField>
-        <FormField htmlFor="email" label="Email address" name="email" required>
-          <TextInput
-            id="email"
-            name="email"
-            placeholder="you@example.com"
-            type="email"
-          />
-        </FormField>
-        <FormField htmlFor="jobTitle" label="Job title" name="jobTitle">
-          <TextInput
-            id="jobTitle"
-            name="jobTitle"
-            placeholder="e.g. Platform engineer"
-          />
-        </FormField>
-        <Box
-          align={!['xsmall', 'small'].includes(size) ? 'start' : undefined}
-          margin={{ top: 'medium', bottom: 'xsmall' }}
-        >
-          <Button
-            busy={busy}
-            label="Save profile"
-            primary
-            success={success}
-            type="submit"
-          />
+        <Box gap="small">
+          <Box>
+            <FormField
+              htmlFor="fullName"
+              label="Full name"
+              name="fullName"
+              required
+            >
+              <TextInput
+                id="fullName"
+                name="fullName"
+                placeholder="Your full name"
+              />
+            </FormField>
+            <FormField
+              htmlFor="email"
+              label="Email address"
+              name="email"
+              required
+            >
+              <TextInput
+                id="email"
+                name="email"
+                placeholder="you@example.com"
+                type="email"
+              />
+            </FormField>
+            <FormField htmlFor="jobTitle" label="Job title" name="jobTitle">
+              <TextInput
+                id="jobTitle"
+                name="jobTitle"
+                placeholder="e.g. Platform engineer"
+              />
+            </FormField>
+          </Box>
+          <Box
+            align={!['xsmall', 'small'].includes(size) ? 'start' : undefined}
+          >
+            <Button
+              busy={busy}
+              label="Save profile"
+              primary
+              success={success}
+              type="submit"
+            />
+          </Box>
         </Box>
       </Form>
     </ContentPane>
