b7e7dd98f964a0a806305941fee6fc2fce4968e4
Matthew Glissmann <mdglissmann@gmail.com>
2026-07-15 16:24:15 -0600

    added fallback mock data

--- apps/docs/src/examples/templates/dashboards/content/chartCards/utils/window.js
+++ apps/docs/src/examples/templates/dashboards/content/chartCards/utils/window.js
@@ -1,12 +1,17 @@
 // eslint-disable-next-line max-len
 const mockAccountData = require('../../../../../../data/mockData/accounts.json');
 
+const account = mockAccountData.accounts.find(
+  a => a.id === '407f1f7g6ty86cd56te3903y' || {
+      id: '407f1f7g6ty86cd56te3903y',
+      name: 'Sirius Cybernetics Corp.',
+      createdDate: '2015-02-08T13:09:54.584Z',
+    },
+);
+
 // Date examples should use to represent "today's date". Since mock data
 // is currently static, chose a fixed date which aligns with the data.
 export const DEMO_DATE = new Date('2022-02-22');
-const accountCreatedDate =
-  mockAccountData.accounts.find(account => account.createdDate)?.createdDate ||
-  DEMO_DATE.toISOString();
 const current = DEMO_DATE;
 const endDate = current;
 const beginDate = new Date(new Date().setDate(current.getDate() - 365));
@@ -16,6 +21,7 @@ export const defaultWindow = { begin: beginDate, end: endDate };
 export const REPORT_WINDOW_MAP = {
   'Last 30 Days': 30,
   'Last Year': 365,
-  Lifetime: 365 * (current.getFullYear() - 
-  new Date(accountCreatedDate).getFullYear() + 1),
+  Lifetime:
+    365 *
+    (current.getFullYear() - new Date(account.createdDate).getFullYear() + 1),
 };
