3f2a2eb5f6abaf2962eb27dc024ababd8885f09e
SeamusLeonardHPE <135040188+SeamusLeonardHPE@users.noreply.github.com>
2026-08-17 23:27:11 +0100

    Updated icon details (#6390)
    Used StausInfo icon as all the examples on page are Status related
    Modified copy from "normal" to "ok" to match icon semantic naming convention

--- apps/docs/src/examples/cardPreviews/status-indicator.js
+++ apps/docs/src/examples/cardPreviews/status-indicator.js
@@ -8,7 +8,7 @@ import {
   StatusWarning,
   StatusCritical,
   StatusUnknown,
-  Info,
+  StatusInfo,
 } from '@hpe-design/icons-grommet';
 
 import { Box } from 'grommet';
@@ -28,7 +28,7 @@ export const StatusBox = ({ toast }) => (
   <Box direction="row" gap="xlarge" pad={{ bottom: 'medium' }}>
     <Box align="center">
       <StatusGood color="icon-ok" />
-      Normal
+      Ok
     </Box>
     <Box align="center">
       <StatusWarning color="icon-warning" />
@@ -45,7 +45,7 @@ export const StatusBox = ({ toast }) => (
       Unknown
     </Box>
     <Box align="center">
-      <Info color="icon-info" />
+      <StatusInfo color="icon-info" />
       Info
     </Box>
   </Box>
